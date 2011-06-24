e12b6f74de84a43fed314bbb328cae5f3d2b52e3
Tom Smyth <thomas.smyth@gatech.edu>
2011-06-24 15:18:23 -0400

    Loading indicator for place update

--- public/javascripts/place_lookups.js
+++ public/javascripts/place_lookups.js
@@ -1,7 +1,12 @@
-function lookup_places(query, div_id) {
+function lookup_places() {
+  var div_id = 'place_lookup_results';
+  
+  // show loading indicator
+  $(div_id).innerHTML = '<div class="loader">Loading suggestions...</div>';
+
   // send updater request
   new Ajax.Updater(div_id, '/place_lookups/suggest', {
-    parameters: { query: query }, 
+    parameters: { query: $('place_lookup_query').value }, 
     method: "get"
   });
 }
