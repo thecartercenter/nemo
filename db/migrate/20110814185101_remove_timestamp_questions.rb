f4def406d890b7bd9cce1927219a4c67c1b60848
Tom Smyth <thomas.smyth@gatech.edu>
2011-08-14 15:37:28 -0400

    cleaned up remove timestamp migration again

--- db/migrate/20110814185101_remove_timestamp_questions.rb
+++ db/migrate/20110814185101_remove_timestamp_questions.rb
@@ -2,7 +2,7 @@ class RemoveTimestampQuestions < ActiveRecord::Migration
   def self.up
     if @qt = QuestionType.find_by_name("start_timestamp")
       @qs = Question.find_all_by_question_type_id(@qt.id)
-      @qs.each{|q| q.destroy}
+      @qs.each{|q| q.answers.each{|a| a.destroy}; q.questionings.each{|qing| qing.destroy}; q.destroy}
     end
   end
 
