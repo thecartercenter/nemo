51a59fbd5f7c02c86cd349ab31edf71c117f1877
Tom Smyth <thomas.smyth@gatech.edu>
2011-08-16 15:49:46 -0400

    fixed migration

--- db/migrate/20110816185426_rename_numeric_to_integer.rb
+++ db/migrate/20110816185426_rename_numeric_to_integer.rb
@@ -1,6 +1,7 @@
 class RenameNumericToInteger < ActiveRecord::Migration
   def self.up
     execute("update question_types set name = 'integer', long_name = 'Integer' where name = 'numeric'")
+    QuestionType.create(:name => "decimal", :long_name => "Decimal", :odk_name => "decimal", :odk_tag => "input")
   end
 
   def self.down
