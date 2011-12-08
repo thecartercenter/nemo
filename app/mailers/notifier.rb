4659db24ddb89738848376077decb7d046c9e111
Thomas Smyth <tom@tomsmyth.ca>
2011-12-08 12:05:03 -0500

    Added license notices

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,3 +1,19 @@
+# Elmo - Secure, robust, and versatile data collection.
+# Copyright 2011 The Carter Center
+#
+# Elmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Elmo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Elmo.  If not, see <http://www.gnu.org/licenses/>.
+# 
 class Notifier < ActionMailer::Base  
   default(:from => configatron.site_email)
   
--- app/models/smser.rb
+++ app/models/smser.rb
@@ -1,3 +1,19 @@
+# Elmo - Secure, robust, and versatile data collection.
+# Copyright 2011 The Carter Center
+#
+# Elmo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Elmo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Elmo.  If not, see <http://www.gnu.org/licenses/>.
+# 
 class Smser
   def self.deliver(recips, msg)
     adapter = Kernel.const_get(configatron.outgoing_sms_adapter)
