f9970e5ea6a347ac68d3a6274cf83bac8db2d683
Thomas Smyth <tom@tomsmyth.ca>
2012-02-03 15:29:34 -0500

    added standard date/time format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CommandCenter::Application.initialize!
+
+# Standard date-time format
+Time::DATE_FORMATS[:std_datetime] = "%Y-%m-%d %l:%M%p"
