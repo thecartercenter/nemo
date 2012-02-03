635fe6961ab6d1e84a0f605daf0e061f5c5f45e0
Thomas Smyth <tom@tomsmyth.ca>
2012-02-03 12:42:52 -0500

    fixed bug in search query to str migration

--- db/migrate/20120110163625_rename_search_query_to_str.rb
+++ db/migrate/20120110163625_rename_search_query_to_str.rb
@@ -1,7 +1,7 @@
 class RenameSearchQueryToStr < ActiveRecord::Migration
   def self.up
-    change_column :searches, :query, :text
-    rename_column :searches, :query, :str
+    change_column :search_searches, :query, :text
+    rename_column :search_searches, :query, :str
   end
 
   def self.down
