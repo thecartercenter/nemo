269606c287dc2c4ac79b66f125250c0fdd1839a5
Thomas Smyth <tom@tomsmyth.ca>
2012-09-27 09:12:45 -0500

    removed validation check from old user migration

--- db/migrate/20111103141618_change_user_to_one_name_field.rb
+++ db/migrate/20111103141618_change_user_to_one_name_field.rb
@@ -1,7 +1,7 @@
 class ChangeUserToOneNameField < ActiveRecord::Migration
   def self.up
     add_column :users, :name, :string
-    User.all.each{|u| u.name = "#{u.first_name} #{u.last_name}"; u.save}
+    User.all.each{|u| u.name = "#{u.first_name} #{u.last_name}"; u.save(:validate => false)}
     remove_column :users, :first_name
     remove_column :users, :last_name
   end
