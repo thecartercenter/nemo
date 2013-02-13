1bb4fb3c732c1ba02269c452f8a8281fc44629ab
Thomas Smyth <tom@tomsmyth.ca>
2013-02-13 18:20:11 -0500

    renamed default locale to use 3 letter lang code

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,9 @@ module ELMO
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+
+    # ELMO uses 3 letter language codes since they exist for more obscure languages
+    config.i18n.default_locale = :eng
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
