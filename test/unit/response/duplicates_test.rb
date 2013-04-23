2998f4b27f9975009645974ffd7eda1de6198b8b
Tim Hui <timboh56@gmail.com>
2013-04-22 23:05:03 -0700

    minor changes to duplicate testing

--- test/unit/response/duplicates_test.rb
+++ test/unit/response/duplicates_test.rb
@@ -7,33 +7,36 @@ class Response::DuplicatesTest < ActiveSupport::TestCase
   end
     
   test "duplicates testing" do
-    
+
     # create a form
-    create_form(:name => "form1")
+    form1 = create_form(:name => "form1")
     
     # create option set for question
     create_opt_set(%w(Yes No Maybe))
     
     # create question with selectable answer
-    create_question(:code => "ee", :type => "select_one")
+    puts("created form " + @forms[:form1].id.to_s )
+    create_question(:code => "ee", :type => "select_one", :option_set => @option_sets[:Yes_No_Maybe], :forms => [form1] )
     
     # create question with integer answer
     # create_question(:code => "int ", :type => "integer")
-    
+
     # create a response using question
-    first_response = create_response(:form => @forms[:form1], :answers => { :ee => "Yes" })
+    first_response = create_response(:form => form1, :answers => { :ee => "Yes" })
     
     # create a duplicate response
-    duplicate_response = create_response(:form => @forms[:form1], :answers => { :ee => "Yes" })
+    duplicate_response = create_response(:form => form1, :answers => { :ee => "Yes" })
     
     # create a different response
-    different_response = create_response(:form => @forms[:form1], :answers => { :ee => "Maybe" })
+    different_response = create_response(:form => form1, :answers => { :ee => "Maybe" })
     
     # assert the two duplicate response signatures are equal
     assert_equal(first_response.signature,duplicate_response.signature)
     
     # assert the different response signature is not equal to the first response signature
-    assert_not_equal(different_response.signature,first_response.signature)
+    
+    puts(" ID 1 " + different_response.id.to_s + " ID 2 " + duplicate_response.id.to_s)
+    assert_not_equal(Response.find_by_id(different_response.id).signature,Response.find_by_id(duplicate_response.id).signature)
     
   end
 
