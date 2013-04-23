608f98a37831d51cb48ba5744e1ac49872a4b717
Tim Hui <timboh56@gmail.com>
2013-04-22 23:28:21 -0700

    Added conditional to duplicates checking

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -87,11 +87,13 @@ class ResponsesController < ApplicationController
       # try to save
       begin
         @response.update_attributes!(params[:response])
-        puts("params " + params[:response].to_s)
         
-        # if no possible duplicates are found, update response object with 0 for duplicate column
-        # otherwise, update response object with 1 for duplicat ecolumn
-        @response.find_duplicates.empty? ? @response.update_attributes!("duplicate" => 0) : @response.update_attributes!("duplicate" => 1)
+        if params[:response][:duplicate] != "0"
+          
+          # if no possible duplicates are found, update response object with 0 for duplicate column
+          # otherwise, update response object with 1 for duplicat ecolumn
+          @response.find_duplicates.empty? ? @response.update_attributes!("duplicate" => 0) : @response.update_attributes!("duplicate" => 1)
+        end
         
         flash[:success] = "Response #{action}d successfully."
         redirect_to(:action => :index)
