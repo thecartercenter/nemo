38caf8a202cda77ed2e7949ce10a11f3d258b6fb
Thomas Smyth <tom@tomsmyth.ca>
2013-06-13 18:33:56 -0400

    set locale on login based on pref_lang

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,6 +15,10 @@ class UserSessionsController < ApplicationController
     # if the save is successful, the user is logged in automatically
     if @user_session.save
       
+      # set the locale based on the user's pref_lang (if it's supported)
+      pref_lang = @user_session.user.pref_lang.to_sym
+      I18n.locale = configatron.locales.include?(pref_lang) ? pref_lang : I18n.default_locale
+      
       # do post login housekeeping
       return unless post_login_housekeeping
 
