c567bb795c45fd66277a81fe574df4c8cdd5a229
Thomas Smyth <tom@tomsmyth.ca>
2013-08-11 13:09:15 -0400

    added delegation to translatable concern

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -3,22 +3,46 @@ module Translatable
   extend ActiveSupport::Concern
   
   included do
+    
   end
   
   module ClassMethods
-    def translates(*fields)
+    
+    def translates(*args)
+      # shave off the optional options hash at the end
+      class_variable_set('@@translate_options', args[-1].is_a?(Hash) ? args.delete_at(-1) : {})
+    
       # save the list of translated fields
-      @@translated_fields = fields
+      class_variable_set('@@translated_fields', args)
       
-      # set up the _tranlsations fields to serialize
-      fields.each do |f|
-        serialize "#{f}_translations", JSON
+      # set up the _tranlsations fields to serialize, unless we're delegating
+      unless translation_delegated?
+        translated_fields.each do |f|
+          serialize "#{f}_translations", JSON
+        end
       end
     end
-    
-    # accessor
+
+    def translation_delegated?
+      !translate_options[:delegate_to].nil?
+    end
+
+    # special accessors that we have to use for class vars in concerns
     def translated_fields
-      @@translated_fields
+      class_variable_defined?('@@translated_fields') ? class_variable_get('@@translated_fields') : nil
+    end
+
+    def translate_options
+      class_variable_defined?('@@translate_options') ? class_variable_get('@@translate_options') : nil
+    end
+  end
+
+  # gets the method target, which is self unless delegated
+  def translate_method_target
+    if self.class.translate_options[:delegate_to]
+      send(self.class.translate_options[:delegate_to])
+    else
+      self
     end
   end
   
@@ -40,49 +64,12 @@ module Translatable
     # check if this is a translation method and get the pieces
     field, locale, is_setter, options = parse_method(args[0], args[1], args[2])
     
+    # if the method looks like a translation method
     if field
-
-      # if we're setting the value
-      if is_setter
-        # init the empty hash if it's nil
-        send("#{field}_translations=", {}) if send("#{field}_translations").nil?
-
-        # set the value in the appropriate translation hash
-        # we use the merge method because otherwise the _changed? method doesn't work right
-        send("#{field}_translations=", send("#{field}_translations").merge(locale => args[1]))
-        
-        # if the locale is the default locale, also cache the value in the _ attribute
-        send("_#{field}=", args[1]) if locale.to_sym == I18n.default_locale
-        
-      # otherwise just return what we have
+      if translate_method_target.nil?
+        raise ArgumentError.new('translation delegate does not exist')
       else
-        if send("#{field}_translations").nil?
-          str = nil
-        else
-          # try the specified locale
-          str = send("#{field}_translations")[locale]
-        
-          # if the translation is blank and strict mode is off
-          if str.blank? && !options[:strict]
-            # try the current locale
-            str = send("#{field}_translations")[I18n.locale.to_s]
-            
-            if str.blank?
-              # try the default locale
-              str = send("#{field}_translations")[I18n.default_locale.to_s]
-          
-              # if str is still blank, search the translations for /any/ non-blank string
-              if str.blank?
-                if (non_blank_pair = send("#{field}_translations").find{|locale, value| !value.blank?})
-                  str = non_blank_pair[1]
-                end
-              end
-            end
-          end
-        end
-        
-        # return whatever we have at this point, could be nil
-        return str
+        translate_method_target.process_translation(field, locale, is_setter, options, args)
       end
     else
       super
@@ -90,14 +77,62 @@ module Translatable
   end
   
   def respond_to?(symbol, *)
-    parse_method(symbol) || super
+    !self.class.translated_fields.nil? && parse_method(symbol) || super
   end
   
   def respond_to_missing?(symbol, include_private)
-    parse_method(symbol) || super
+    !self.class.translated_fields.nil? && parse_method(symbol) || super
+  end
+  
+  def process_translation(field, locale, is_setter, options, args)
+    
+    # if we're setting the value
+    if is_setter
+      # init the empty hash if it's nil
+      send("#{field}_translations=", {}) if send("#{field}_translations").nil?
+  
+      # set the value in the appropriate translation hash
+      # we use the merge method because otherwise the _changed? method doesn't work right
+      send("#{field}_translations=", send("#{field}_translations").merge(locale => args[1]))
+      
+      # if the locale is the default locale, also cache the value in the _ attribute
+      send("_#{field}=", args[1]) if locale.to_sym == I18n.default_locale
+      
+    # otherwise just return what we have
+    else
+      if send("#{field}_translations").nil?
+        str = nil
+      else
+        # try the specified locale
+        str = send("#{field}_translations")[locale]
+      
+        # if the translation is blank and strict mode is off
+        if str.blank? && !options[:strict]
+          # try the current locale
+          str = send("#{field}_translations")[I18n.locale.to_s]
+          
+          if str.blank?
+            # try the default locale
+            str = send("#{field}_translations")[I18n.default_locale.to_s]
+        
+            # if str is still blank, search the translations for /any/ non-blank string
+            if str.blank?
+              if (non_blank_pair = send("#{field}_translations").find{|locale, value| !value.blank?})
+                str = non_blank_pair[1]
+              end
+            end
+          end
+        end
+      end
+      
+      # return whatever we have at this point, could be nil
+      return str
+    end
   end
   
   def parse_method(symbol, arg1 = nil, arg2 = nil)
+    return nil if self.class.translated_fields.nil?
+    
     fields = self.class.translated_fields.join("|")
     if symbol.to_s.match(/^(#{fields})(_([a-z]{2}))?(_before_type_cast)?(=?)$/) 
     
@@ -138,6 +173,15 @@ module Translatable
   end  
   
   def available_locales(options = {})
+    # delegate if appropriate
+    if self.class.translation_delegated?
+      if translate_method_target.nil?
+        raise ArgumentError.new('translation delegate does not exist')
+      else
+        return translate_method_target.available_locales 
+      end
+    end
+    
     # get union of all locales of all translated fields, and convert to symbol
     locales = self.class.translated_fields.inject([]) do |union, field|
       trans = send("#{field}_translations")
