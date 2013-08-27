79ae931c579a80b68e1fb7b1d45164458ba9ffae
Thomas Smyth <tom@tomsmyth.ca>
2013-08-27 10:34:46 -0400

    fixed failing test due to new fk constraints

--- test/unit/condition_test.rb
+++ test/unit/condition_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ConditionTest < ActiveSupport::TestCase
   setup do
-    clear_objects(Question, Questioning, Form, OptionSet, Option, Condition)
+    clear_objects(Questioning, Question, Form, OptionSet, Option, Condition)
   end
   
   test "create" do
@@ -85,7 +85,7 @@ class ConditionTest < ActiveSupport::TestCase
   private
 
     def build_condition(params = {})
-    	clear_objects(Form, Question, Questioning, OptionSet)
+      clear_objects(Questioning, Question, Form, Optioning, Option, OptionSet)
       f = FactoryGirl.create(:form, :question_types => params.delete(:question_types) || %w(integer integer integer))
       q = f.questionings.last
       
