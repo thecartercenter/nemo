78428788257f8dcc702073e5bfe19638ff1c4123
Thomas Smyth <tom@tomsmyth.ca>
2013-10-09 14:08:51 -0400

    moved whole row link code into view model, and added misclick prevention (closes #634)

--- app/assets/javascripts/views/index_table.js
+++ app/assets/javascripts/views/index_table.js
@@ -6,9 +6,17 @@
   // constructor
   ns.IndexTable = klass = function(params) {
     // flash the modified obj if given
-    console.log(params.class_name + '_' + params.modified_obj_id)
     if (params.modified_obj_id)
       $('#' + params.class_name + '_' + params.modified_obj_id).effect("highlight", {}, 1000);
+
+    // hook up whole row link unless told not to
+    if (!params.no_whole_row_link)
+      $('table.index_table tbody').on('click', 'tr', function(e) {
+        // go to the tr's href if the click was on a non-active element
+        // but don't do it with td.actions_col or td.actions_col > div to avoid misclick
+        if ($(e.target).is('div:not(.actions_col > div), td:not(.actions_col)'))
+          window.location.href = $(e.currentTarget).data('href'); 
+      });
   }
 
 })(ELMO.Views);
