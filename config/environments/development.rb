9d8ce2fba88d1c9ec1ffd959458c9c079c20ac2e
Thomas Smyth <tom@tomsmyth.ca>
2013-10-09 12:43:16 -0400

    fixed comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ ELMO::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
-  # Don't care if the mailer can't send
+  # care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
