d1b14a72544369a681e2f67a3245e34cb7131864
Thomas Smyth <tom@tomsmyth.ca>
2013-10-11 08:14:05 -0400

    changed report default question labels to title

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131009183730) do
+ActiveRecord::Schema.define(:version => 20131011121258) do
 
   create_table "answers", :force => true do |t|
     t.integer  "response_id"
@@ -258,7 +258,7 @@ ActiveRecord::Schema.define(:version => 20131009183730) do
     t.string   "display_type",     :default => "table"
     t.string   "bar_style",        :default => "side_by_side"
     t.boolean  "unreviewed",       :default => false
-    t.string   "question_labels",  :default => "code"
+    t.string   "question_labels",  :default => "title"
     t.string   "percent_type",     :default => "none"
     t.boolean  "unique_rows"
     t.string   "aggregation_name"
