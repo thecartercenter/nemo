badf9dc889854e4e2eb8a321b0090ef123532237
Thomas Smyth <tom@tomsmyth.ca>
2013-10-14 09:37:31 -0400

    refactored question summary test

--- test/unit/report/question_summary_test.rb
+++ test/unit/report/question_summary_test.rb
@@ -4,24 +4,17 @@ require 'unit/report/report_test_helper'
 class Report::QuestionSummaryTest < ActiveSupport::TestCase
 
   test "summary should contain question type" do
-    build_form_and_responses
-    build_and_run_report
+    prepare_form_and_report('integer', [0])
     assert_equal('integer', @report.summaries[0].qtype.name)
   end
 
   test "integer summary should be correct" do
-    build_form_and_responses
-    build_and_run_report
+    prepare_form_and_report('integer', [10, 7, 6, 1, 1])
     assert_equal({:mean => 5.0, :median => 6.0, :max => 10.0, :min => 1.0}, @report.summaries[0].items)
   end
 
   test "integer summary should not include nil or blank values" do
-    @form = FactoryGirl.create(:form, :question_types => %w(integer))
-    FactoryGirl.create(:response, :form => @form, :_answers => [5])
-    FactoryGirl.create(:response, :form => @form, :_answers => [nil])
-    FactoryGirl.create(:response, :form => @form, :_answers => [''])
-    FactoryGirl.create(:response, :form => @form, :_answers => [2])
-    build_and_run_report
+    prepare_form_and_report('integer', [5, nil, '', 2])
     assert_equal({:mean => 3.5, :median => 3.5, :max => 5.0, :min => 2.0}, @report.summaries[0].items)
   end
 
@@ -30,16 +23,9 @@ class Report::QuestionSummaryTest < ActiveSupport::TestCase
   end
 
   private
-    def build_form_and_responses
-      @form = FactoryGirl.create(:form, :question_types => %w(integer integer))
-      FactoryGirl.create(:response, :form => @form, :_answers => [10, 0])
-      FactoryGirl.create(:response, :form => @form, :_answers => [7,  0])
-      FactoryGirl.create(:response, :form => @form, :_answers => [6,  0])
-      FactoryGirl.create(:response, :form => @form, :_answers => [1,  0])
-      FactoryGirl.create(:response, :form => @form, :_answers => [1,  0])
-    end
-
-    def build_and_run_report
+    def prepare_form_and_report(qtype, answers)
+      @form = FactoryGirl.create(:form, :question_types => [qtype])
+      answers.each{|a| FactoryGirl.create(:response, :form => @form, :_answers => [a])}
       @report = FactoryGirl.create(:standard_form_report, :form => @form)
       @report.run
     end
