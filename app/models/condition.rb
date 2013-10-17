9c538fd4a86f23fb7fcb0a8c523c72e673bbfe71
Thomas Smyth <tom@tomsmyth.ca>
2013-10-17 15:46:17 -0400

    fixed bug in condition replication options

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -29,7 +29,7 @@ class Condition < ActiveRecord::Base
     {:name => :ninc, :types => %w(select_multiple), :code => "!="}
   ]     
 
-  replicable :after_copy_attribs => :copy_ref_qing_and_option, :parent_assoc => :questionings, :dont_copy => [:ref_qing_id]
+  replicable :after_copy_attribs => :copy_ref_qing_and_option, :parent_assoc => :questioning, :dont_copy => [:ref_qing_id]
   
   # all questionings that can be referred to by this condition
   def refable_qings
