7c7c54f3f96a1476ca893f5181bc297689924428
Thomas Smyth <tom@tomsmyth.ca>
2013-12-10 18:21:08 -0500

    disabled bullet gem for performance reasons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,11 +40,11 @@ ELMO::Application.configure do
   config.cache_store = :dalli_store
   
   # bullet gem for query optimization
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-  end
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.console = true
+  #   Bullet.rails_logger = true
+  # end
 end
 
