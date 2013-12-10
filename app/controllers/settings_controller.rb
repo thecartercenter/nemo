0ca97594ecb700595dd5e8dd7573bf7023d6e522
Winston Tsang <rubyconsumer@rubyconsumer.com>
2013-12-10 10:49:02 -0500

    White Space.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,36 +1,36 @@
 class SettingsController < ApplicationController
   # no authorization up here because we do it manually because Setting is atypical
-  
+
   def index
     # setting is already loaded by application controller
 
     # do authorization check
     authorize!(:update, @setting)
-    
+
     prepare_and_render_form
   end
-  
+
   def update
     begin
       # setting is already loaded by application controller
 
       # do auth check so cancan doesn't complain
       authorize!(:update, @setting)
-      
+
       @setting.update_attributes!(params[:setting])
-      
+
       set_success_and_redirect(@setting)
     rescue ActiveRecord::RecordInvalid
       prepare_and_render_form
     end
   end
-  
+
   private
     # prepares objects and renders the form template (which in this case is really the index template)
     def prepare_and_render_form
       # load options for sms adapter dropdown
       @adapter_options = Sms::Adapters::Factory::VALID_ADAPTERS
-      
+
       # render the template
       render(:index)
     end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,7 +8,7 @@ class Assignment < ActiveRecord::Base
   default_scope(includes(:mission))
   scope(:sorted_recent_first, order("created_at DESC"))
   scope(:active, where(:active => true))
-  
+
   # checks if there are any duplicates in the given set of assignments
   def self.duplicates?(assignments)
     # uniq! returns nil if there are no duplicates
