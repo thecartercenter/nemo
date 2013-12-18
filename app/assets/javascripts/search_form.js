91e0b4e0d5c1f0f084a38a06df193a994327f670
Thomas Smyth <tom@tomsmyth.ca>
2013-12-18 09:40:11 -0500

    added search help modal (closes #773)

--- app/assets/javascripts/search_form.js
+++ app/assets/javascripts/search_form.js
@@ -10,6 +10,19 @@
     $("form.search_form input#clear_button").click(function() {
       window.location.href = window.location.href.split('?')[0];
     });
+
+    // setup the search dialog
+    $('div.search_footer a').on('click', function(e){
+      $("div.search_help").dialog({
+        dialogClass: "no-close search_help_modal",
+        buttons: [{text: I18n.t('common.ok'), click: function() { $(this).dialog('close'); }}],
+        modal: true,
+        autoOpen: true,
+        width: 800,
+        height: 600
+      });
+      e.preventDefault();
+    });
   }
 
 })(ELMO);
