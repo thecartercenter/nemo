5bc4a034c04263a0eaa5b2314db5db56706e0d0c
Winston Tsang <rubyconsumer@rubyconsumer.com>
2013-12-19 21:37:45 -0500

    added roles to test setup. (#244)

--- test/unit/mission_locked_test.rb
+++ test/unit/mission_locked_test.rb
@@ -5,9 +5,26 @@ class MissionLockedTest < ActiveSupport::TestCase
 
 
   setup do
-    @mission = FactoryGirl.create(:mission, :name => 'Locked mission', :locked => true)
+    @mission = FactoryGirl.create(:mission, :name => 'Locked mission')#, :locked => true)
+
     @admin = FactoryGirl.create(:user, :admin => true)
+
+    @staffer = FactoryGirl.create(:user)
+    @staffer.assignments.create(:mission => @mission, :active => true, :role => "staffer")
+
+    @observer = FactoryGirl.create(:user)
+    @observer.assignments.create(:mission => @mission, :active => true, :role => "observer")
+
+    @coordinator = FactoryGirl.create(:user)
+    @coordinator.assignments.create(:mission => @mission, :active => true, :role => "coordinator")
+
+    @mission.locked = true
+    @mission.save
+
     @admin.change_mission!(@mission)
+    @staffer.change_mission!(@mission)
+    @observer.change_mission!(@mission)
+    @coordinator.change_mission!(@mission)
   end
 
   test "user cannot create a new option set for a locked mission" do
