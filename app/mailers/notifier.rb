8bf0e0ab78631332b180ba6783e3bf0bc0665d43
Thomas Smyth <tom@tomsmyth.ca>
2013-12-30 16:05:56 -0500

    fixed links in notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,13 +2,13 @@ class Notifier < ActionMailer::Base
   default(:from => configatron.site_email)
 
   def password_reset_instructions(user)
-    @reset_url = edit_password_reset_url(user.perishable_token, :protocol => configatron.mailer_url_protocol)
+    @reset_url = edit_password_reset_url(user.perishable_token, :protocol => configatron.mailer_url_protocol, :admin_mode => nil)
     mail(:to => user.email, :subject => t("notifier.password_reset_instructions"))
   end
 
   def intro(user)
     @user = user
-    @reset_url = edit_password_reset_url(user.perishable_token, :protocol => configatron.mailer_url_protocol)
+    @reset_url = edit_password_reset_url(user.perishable_token, :protocol => configatron.mailer_url_protocol, :admin_mode => nil)
     mail(:to => user.email, :subject => t("notifier.welcome", :site => configatron.site_name))
   end
 end
