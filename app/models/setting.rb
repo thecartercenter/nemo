7ddd55de02faeac806ca957e37c567fbf57d0474
Thomas Smyth <tom@tomsmyth.ca>
2014-01-02 15:25:48 -0500

    changed code generation to use Random class

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -61,8 +61,8 @@ class Setting < ActiveRecord::Base
     setting
   end
 
-  def generate_override_code!(size_of_code = 6)
-    self.override_code = ('a'..'z').to_a.shuffle[0,size_of_code].join
+  def generate_override_code!(size = 6)
+    self.override_code = Random.alphanum_no_zero(size)
     self.save!
   end
 
--- db/migrate/20140102200507_populate_override_codes.rb
+++ db/migrate/20140102200507_populate_override_codes.rb
@@ -4,7 +4,7 @@ end
 class PopulateOverrideCodes < ActiveRecord::Migration
   def up
     Setting.where('mission_id IS NOT NULL').each do |s|
-      s.override_code = ('a'..'z').to_a.shuffle[0,6].join
+      s.override_code = Random.alphanum_no_zero(6)
       s.save(:validate => false)
     end
   end
