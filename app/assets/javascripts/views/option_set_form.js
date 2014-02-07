d87f04a54d516582a6ef527831e6013c77ea77e9
Thomas Smyth <tom@tomsmyth.ca>
2014-02-07 13:34:49 -0500

    converted option set form and rendering fixed bug

--- app/assets/javascripts/views/option_set_form.js
+++ app/assets/javascripts/views/option_set_form.js
@@ -127,6 +127,9 @@
       // or if the global removable permission is false
       if (self.params.can_remove_options && optioning['removable?']) links.append(self.params.remove_link);
 
+      // add a spacer if empty, else it won't render right
+      if (links.is(':empty'))
+        links.append('&nbsp;')
 
       links.appendTo(inner);
     }
