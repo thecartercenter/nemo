da355e9f561f8c883f1d6623515fc063e03727af
Thomas Smyth <tom@tomsmyth.ca>
2014-02-11 12:06:55 -0500

    added migration to remove old refs to form_type attrib that were causing errors (closes #943)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140211144733) do
+ActiveRecord::Schema.define(:version => 20140211165731) do
 
   create_table "answers", :force => true do |t|
     t.integer  "response_id"
