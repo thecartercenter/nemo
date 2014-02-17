fb888b1fa40686e3e1f6fe2769a480207bfae303
Thomas Smyth <tom@tomsmyth.ca>
2014-02-17 14:00:51 -0500

    converted fields_for to elmo_fields_for

--- app/helpers/elmo_form_helper.rb
+++ app/helpers/elmo_form_helper.rb
@@ -4,7 +4,13 @@ module ElmoFormHelper
   # renders a form using the ElmoFormBuilder
   def elmo_form_for(obj, *args, &block)
     options = args.extract_options!
-    form_for(obj, *(args << options.merge(:builder => ElmoFormBuilder, :html => {:class => "#{obj.class.model_name.singular}_form"})), &block)
+    base_errors(obj) + form_for(obj, *(args << options.merge(:builder => ElmoFormBuilder, :html => {:class => "#{obj.class.model_name.singular}_form"})), &block)
+  end
+
+  # renders a set of form fields using the ElmoFormBuilder
+  def elmo_fields_for(obj, *args, &block)
+    options = args.extract_options!
+    base_errors(obj) + fields_for(obj, *(args << options.merge(:builder => ElmoFormBuilder)), &block)
   end
 
   # gets the mode a form should be displayed in: one of new, edit, or show
@@ -15,5 +21,9 @@ module ElmoFormHelper
   # renders the standard 'required' symbol, which is an asterisk
   def reqd_sym
     content_tag(:div, '*', :class => 'reqd_sym')
+  end
+
+  def base_errors(obj)
+    obj.errors[:base].join(', ')
   end
 end
