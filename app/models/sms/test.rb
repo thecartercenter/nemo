59ccfbffb2bc6ac0e9fe6b71ca510eb19937d13c
Winston Tsang <rubyconsumer@rubyconsumer.com>
2014-03-24 01:18:00 -0400

    add minimal implementation for ActiveModel::Errors (BUG1053)

--- app/models/sms/test.rb
+++ app/models/sms/test.rb
@@ -4,13 +4,22 @@ class Sms::Test
   extend ActiveModel::Naming
   extend ActiveModel::Translation
 
-  attr_accessor :from, :body
+  attr_accessor :from, :body, :name
+  attr_reader   :errors
 
   def initialize(attribs = {})
+    @errors = ActiveModel::Errors.new(self)
     attribs.each{|k,v| instance_variable_set("@#{k}", v)}
   end
 
   def persisted?
     false
   end
+
+# The following method are needed to be minimally implemented for ActiveModel::Errors
+
+  def read_attribute_for_validation(attr)
+    send(attr)
+  end
+
 end
