215dfa56fd913eb7d72ddf9ccb7ac0d820de2711
Tom Smyth <tom@sassafras.coop>
2014-04-22 16:13:45 -0400

    refactored routes and added basic routes spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,34 @@ ELMO::Application.routes.draw do
 
   # redirects for ODK
   # shortened (/m)
-  match "/m/:mission_compact_name/formList" => 'forms#index', :format => :xml
-  match "/m/:mission_compact_name/forms/:id" => 'forms#show', :format => :xml, :as => :form_with_mission
-  match "/m/:mission_compact_name/submission" => 'responses#create', :format => :xml
+  match '/m/:mission_compact_name/formList' => 'forms#index', :format => :xml
+  match '/m/:mission_compact_name/forms/:id' => 'forms#show', :format => :xml, :as => :form_with_mission
+  match '/m/:mission_compact_name/submission' => 'responses#create', :format => :xml
   # full (/missions)
-  match "/missions/:mission_compact_name/formList" => 'forms#index', :format => :xml
-  match "/missions/:mission_compact_name/forms/:id" => 'forms#show', :format => :xml
-  match "/missions/:mission_compact_name/submission" => 'responses#create', :format => :xml
+  match '/missions/:mission_compact_name/formList' => 'forms#index', :format => :xml
+  match '/missions/:mission_compact_name/forms/:id' => 'forms#show', :format => :xml
+  match '/missions/:mission_compact_name/submission' => 'responses#create', :format => :xml
 
   # the routes in this scope /require/ admin mode
-  scope "(:locale)(/:admin_mode)", :locale => /[a-z]{2}/, :admin_mode => /admin/ do
+  scope '(:locale)(/:admin_mode)', :locale => /[a-z]{2}/, :admin_mode => /admin/ do
     resources :missions
   end
 
   # the routes in this scope are not valid in admin mode
-  scope "(:locale)", :locale => /[a-z]{2}/ do
-    resources(:broadcasts){collection{post 'new_with_users'}}
-    resources :password_resets
-    resources :responses
-    resources :sms, :only => [:index, :create]
-    resources :sms_tests
-    resource :user_session do
-      get 'logged_out', :on => :collection
+  scope '(:locale)', :locale => /[a-z]{2}/ do
+    resources(:broadcasts) do
+      collection do
+        post 'new_with_users', :path => 'new-with-users'
+      end
+    end
+    resources(:password_resets)
+    resources(:responses)
+    resources(:sms, :only => [:index, :create])
+    resources(:sms_tests)
+    resource(:user_session) do
+      collection do
+        get 'logged_out', :path => 'logged-out'
+      end
     end
 
     namespace :report  do
@@ -37,24 +43,26 @@ ELMO::Application.routes.draw do
     end
 
     # special dashboard routes
-    match '/info_window' => 'welcome#info_window', :as => :dashboard_info_window
-    match '/report_update/:id' => 'welcome#report_update'
+    match '/info-window' => 'welcome#info_window', :as => :dashboard_info_window
+    match '/report-update/:id' => 'welcome#report_update'
 
     # login/logout shortcut
-    match "/logged_out" => "user_sessions#logged_out", :as => :logged_out
-    match "/logout" => "user_sessions#destroy", :as => :logout
-    match "/login" => "user_sessions#new", :as => :login
+    match '/logged-out' => 'user_sessions#logged_out', :as => :logged_out
+    match '/logout' => 'user_sessions#destroy', :as => :logout
+    match '/login' => 'user_sessions#new', :as => :login
   end
 
   # the routes in this scope are admin mode optional
-  scope "(:locale)(/:admin_mode)", :locale => /[a-z]{2}/ do
+  scope '(:locale)(/:admin_mode)', :locale => /[a-z]{2}/ do
 
     # the rest of these routes can have admin mode or not
     resources :forms do
       member do
-        post :add_questions, :remove_questions
-        put  :clone, :publish
-        get  :choose_questions
+        post 'add_questions', :path => 'add-questions'
+        post 'remove_questions', :path => 'remove-questions'
+        put 'clone'
+        put 'publish'
+        get 'choose_questions', :path => 'choose-questions'
       end
     end
     resources :markers
@@ -63,39 +71,32 @@ ELMO::Application.routes.draw do
     resources :settings
     resources :users do
       member do
-        get 'login_instructions'
-        get 'exit_admin_mode'
+        get 'login_instructions', :path => 'login-instructions'
+        get 'exit_admin_mode', :path => 'exit-admin-mode'
       end
       post 'export', :on => :collection
     end
     resources :user_batches
     resources :groups
 
-    # looks nicer
     resources :option_sets, :path => 'option-sets' do
       put 'clone', :on => :member
     end
 
-    # for legacy support
-    resources :option_sets do
-      put 'clone', :on => :member
-    end
-
-
     # import routes for standardizeable objects
     %w(forms questions option_sets).each do |k|
-      post("/#{k}/import_standard" => "#{k}#import_standard", :as => "import_standard_#{k}")
+      post("/#{k.gsub('_', '-')}/import-standard" => "#{k}#import_standard", :as => "import_standard_#{k}")
     end
 
     # special route for option suggestions
     match '/options/suggest' => 'options#suggest', :as => :suggest_options
 
-    root :to => "welcome#index"
+    root :to => 'welcome#index'
   end
 
   # need this so that '/' will work
-  match '/' => "welcome#index"
+  match '/' => 'welcome#index'
 
   # proxies for ajax
-  match "proxies/:action", :controller => "proxies"
+  match 'proxies/:action', :controller => 'proxies'
 end
---
+++ spec/routing/basic_routes_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'router' do
+  it 'routes root' do
+    { :get => '/' }.should route_to(:controller => 'welcome', :action => 'index')
+  end
+
+  it 'routes root with locale' do
+    { :get => '/en' }.should route_to(:controller => 'welcome', :action => 'index', :locale => 'en')
+  end
+
+  it 'routes root with locale and trailing slash' do
+    { :get => '/en/' }.should route_to(:controller => 'welcome', :action => 'index', :locale => 'en')
+  end
+
+  it 'routes login with locale' do
+    { :get => '/en/login' }.should route_to(:controller => 'user_sessions', :action => 'new', :locale => 'en')
+  end
+
+  it 'routes login without locale' do
+    { :get => '/login' }.should route_to(:controller => 'user_sessions', :action => 'new')
+  end
+
+  it 'routes logout without locale' do
+    { :get => '/en/logout' }.should route_to(:controller => 'user_sessions', :action => 'destroy', :locale => 'en')
+  end
+
+  it 'routes proxy requests without locale' do
+    { :get => '/proxies/geocoder' }.should route_to(:controller => 'proxies', :action => 'geocoder')
+  end
+end
