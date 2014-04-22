b65c9808560bff6f427f20765335c7aec7594cdf
Tom Smyth <tom@sassafras.coop>
2014-04-22 16:57:32 -0400

    added mission route spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 ELMO::Application.routes.draw do
 
-  # redirects for ODK
-  # shortened (/m)
-  match '/m/:mission_compact_name/formList' => 'forms#index', :format => :xml
-  match '/m/:mission_compact_name/forms/:id' => 'forms#show', :format => :xml, :as => :form_with_mission
-  match '/m/:mission_compact_name/submission' => 'responses#create', :format => :xml
-  # full (/missions)
-  match '/missions/:mission_compact_name/formList' => 'forms#index', :format => :xml
-  match '/missions/:mission_compact_name/forms/:id' => 'forms#show', :format => :xml
-  match '/missions/:mission_compact_name/submission' => 'responses#create', :format => :xml
+  MISSION_NAME_CONSTRAINT = { :id => /[a-z][a-z0-9]*/ }
 
-  # the routes in this scope /require/ admin mode
-  scope '(:locale)(/:admin_mode)', :locale => /[a-z]{2}/, :admin_mode => /admin/ do
-    resources :missions
-  end
-
-  # the routes in this scope are not valid in admin mode
-  scope '(:locale)', :locale => /[a-z]{2}/ do
+  def mission_routes
     resources(:broadcasts) do
       collection do
         post 'new_with_users', :path => 'new-with-users'
@@ -46,53 +32,81 @@ ELMO::Application.routes.draw do
     match '/info-window' => 'welcome#info_window', :as => :dashboard_info_window
     match '/report-update/:id' => 'welcome#report_update'
 
+    # for /en/m/mission123
+    root :to => 'welcome#index'
+  end
+
+  # redirects for ODK
+  # shortened (/m)
+  match '/m/:mission_compact_name/formList' => 'forms#index', :format => :xml
+  match '/m/:mission_compact_name/forms/:id' => 'forms#show', :format => :xml, :as => :form_with_mission
+  match '/m/:mission_compact_name/submission' => 'responses#create', :format => :xml
+  # full (/missions)
+  match '/missions/:mission_compact_name/formList' => 'forms#index', :format => :xml
+  match '/missions/:mission_compact_name/forms/:id' => 'forms#show', :format => :xml
+  match '/missions/:mission_compact_name/submission' => 'responses#create', :format => :xml
+
+  # the routes in this scope /require/ admin mode
+  scope '(:locale)(/:admin_mode)', :locale => /[a-z]{2}/, :admin_mode => /admin/ do
+    resources :missions
+  end
+
+  # Mission-only routes
+  scope '(:locale)/:mode/:mission_id', :locale => /[a-z]{2}/, :mode => /m/, :mission_id => /[a-z][a-z0-9]*/ do
+    mission_routes
+  end
+
+  scope '(:locale)', :locale => /[a-z]{2}/ do
     # login/logout shortcut
     match '/logged-out' => 'user_sessions#logged_out', :as => :logged_out
     match '/logout' => 'user_sessions#destroy', :as => :logout
     match '/login' => 'user_sessions#new', :as => :login
+
+    # /en/, /en
+    match '/' => 'welcome#index'
   end
 
   # the routes in this scope are admin mode optional
-  scope '(:locale)(/:admin_mode)', :locale => /[a-z]{2}/ do
+  # scope '(:locale)(/:admin_mode)', :locale => /[a-z]{2}/ do
 
-    # the rest of these routes can have admin mode or not
-    resources :forms do
-      member do
-        post 'add_questions', :path => 'add-questions'
-        post 'remove_questions', :path => 'remove-questions'
-        put 'clone'
-        put 'publish'
-        get 'choose_questions', :path => 'choose-questions'
-      end
-    end
-    resources :markers
-    resources :questions
-    resources :questionings
-    resources :settings
-    resources :users do
-      member do
-        get 'login_instructions', :path => 'login-instructions'
-        get 'exit_admin_mode', :path => 'exit-admin-mode'
-      end
-      post 'export', :on => :collection
-    end
-    resources :user_batches
-    resources :groups
+  #   # the rest of these routes can have admin mode or not
+  #   resources :forms do
+  #     member do
+  #       post 'add_questions', :path => 'add-questions'
+  #       post 'remove_questions', :path => 'remove-questions'
+  #       put 'clone'
+  #       put 'publish'
+  #       get 'choose_questions', :path => 'choose-questions'
+  #     end
+  #   end
+  #   resources :markers
+  #   resources :questions
+  #   resources :questionings
+  #   resources :settings
+  #   resources :users do
+  #     member do
+  #       get 'login_instructions', :path => 'login-instructions'
+  #       get 'exit_admin_mode', :path => 'exit-admin-mode'
+  #     end
+  #     post 'export', :on => :collection
+  #   end
+  #   resources :user_batches
+  #   resources :groups
 
-    resources :option_sets, :path => 'option-sets' do
-      put 'clone', :on => :member
-    end
+  #   resources :option_sets, :path => 'option-sets' do
+  #     put 'clone', :on => :member
+  #   end
 
-    # import routes for standardizeable objects
-    %w(forms questions option_sets).each do |k|
-      post("/#{k.gsub('_', '-')}/import-standard" => "#{k}#import_standard", :as => "import_standard_#{k}")
-    end
+  #   # import routes for standardizeable objects
+  #   %w(forms questions option_sets).each do |k|
+  #     post("/#{k.gsub('_', '-')}/import-standard" => "#{k}#import_standard", :as => "import_standard_#{k}")
+  #   end
 
-    # special route for option suggestions
-    match '/options/suggest' => 'options#suggest', :as => :suggest_options
+  #   # special route for option suggestions
+  #   match '/options/suggest' => 'options#suggest', :as => :suggest_options
 
-    root :to => 'welcome#index'
-  end
+  #   root :to => 'welcome#index'
+  # end
 
   # need this so that '/' will work
   match '/' => 'welcome#index'
@@ -100,3 +114,4 @@ ELMO::Application.routes.draw do
   # proxies for ajax
   match 'proxies/:action', :controller => 'proxies'
 end
+
---
+++ spec/routing/mission_routes_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'router' do
+  it 'routes broadcasts with locale and prefix' do
+    { :get => '/en/m/mission123/broadcasts' }.should route_to(
+      :controller => 'broadcasts', :action => 'index', :locale => 'en', :mode => 'm', :mission_id => 'mission123')
+  end
+
+  it 'rejects invalid mission name' do
+    { :get => '/en/m/mission!123/broadcasts' }.should_not be_routable
+  end
+
+  it 'rejects admin prefix' do
+    { :get => '/en/admin/mission123/broadcasts' }.should_not be_routable
+  end
+
+  it 'routes without explicit locale' do
+    { :get => '/m/mission123/broadcasts' }.should route_to(
+      :controller => 'broadcasts', :action => 'index', :mode => 'm', :mission_id => 'mission123')
+  end
+
+  it 'routes with report namespace' do
+    { :get => '/en/m/mission123/report/reports' }.should route_to(
+      :controller => 'report/reports', :action => 'index', :locale => 'en', :mode => 'm', :mission_id => 'mission123')
+  end
+
+  it 'routes special info-window route' do
+    { :get => '/en/m/mission123/info-window' }.should route_to(
+      :controller => 'welcome', :action => 'info_window', :locale => 'en', :mode => 'm', :mission_id => 'mission123')
+  end
+
+  it 'routes mission root' do
+    # Note this will also route a mistaken URL like /en/m/broadcasts, but that's the expected behavior
+    { :get => '/en/m/mission123' }.should route_to(
+      :controller => 'welcome', :action => 'index', :locale => 'en', :mode => 'm', :mission_id => 'mission123')
+  end
+
+  it 'rejects if missing mission and prefix' do
+    { :get => '/en/broadcasts' }.should_not be_routable
+  end
+
+  it 'rejects if missing locale, mission and prefix' do
+    { :get => '/broadcasts' }.should_not be_routable
+  end
+end
