33d63f56461393695834d85ceb9adb22436eb1fe
rubygeek <nola@rubygeek.com>
2014-04-28 07:20:56 -0500

    code for getting one question with answers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,5 +1,19 @@
 class API::V1::AnswersController < API::V1::BaseController
   
+  def one
+    responses = Form.find(params[:form_id]).responses
+    data = []
+    responses.each do |resp|
+      resp.answers.where(questionable_id: params[:question_id]).each do |answer|
+        data << {question_id: answer.question.id, 
+                 question: answer.question._name,
+                 answer_id: answer.id,  
+                 answer: answer.value}
+      end
+    end
+    render json: data.to_json
+  end
+
   def all
     responses = Form.find(params[:form_id]).responses
     data = []
@@ -10,5 +24,5 @@ class API::V1::AnswersController < API::V1::BaseController
     end
     render json: data.to_json
   end
-
+  
 end
