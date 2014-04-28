2c690f6d0a68acd660b8e9811347f88dbb18f425
rubygeek <nola@rubygeek.com>
2014-04-28 17:00:05 -0500

    refactor into a model poro class

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,28 +1,13 @@
 class API::V1::AnswersController < API::V1::BaseController
   
   def one
-    responses = Form.find(params[:form_id]).responses
-    data = []
-    responses.each do |resp|
-      resp.answers.where(questionable_id: params[:question_id]).each do |answer|
-        data << {question_id: answer.question.id, 
-                 question: answer.question._name,
-                 answer_id: answer.id,  
-                 answer: answer.value}
-      end
-    end
+    data = API::V1::Answers.for_one(params)
     render json: data.to_json
   end
 
   def all
-    responses = Form.find(params[:form_id]).responses
-    data = []
-    responses.each do |resp|
-      resp.answers.each do |answer|
-        data << {question: answer.question._name, answer: answer.value}
-      end
-    end
+    data = API::V1::Answers.for_all(params)
     render json: data.to_json
   end
-  
+
 end
