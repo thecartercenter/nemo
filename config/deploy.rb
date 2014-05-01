d2bb52993f4ce2a2ddce0b445af8e307733a5670
Jill Dimond <jill@sassafrastech.com>
2014-05-01 16:30:01 -0400

    added new staging deployment settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@
 
 require 'bundler/capistrano'
 
-set :stages, %w(master staging demo)
+set :stages, %w(master staging demo staging-api)
 set :default_stage, "staging"
 require "capistrano/ext/multistage"
 
