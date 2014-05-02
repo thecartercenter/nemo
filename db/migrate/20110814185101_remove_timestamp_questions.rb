8615783be2a1d0d3e817a0965c6c552c7c7dbed5
Jill Dimond <jill@sassafrastech.com>
2014-05-02 14:17:30 -0400

    fixed error in migration

--- db/migrate/20110814185101_remove_timestamp_questions.rb
+++ db/migrate/20110814185101_remove_timestamp_questions.rb
@@ -3,8 +3,7 @@ class RemoveTimestampQuestions < ActiveRecord::Migration
     #if @qt = QuestionType.find_by_name("start_timestamp")
     #  @qs = Question.find_all_by_question_type_id(@qt.id)
     #  @qs.each{|q| q.answers.each{|a| a.destroy}; q.questionings.each{|qing| qing.destroy}; Question.find(q.id).destroy}
-
-    end
+    # end
   end
 
   def self.down
