df23a55673ffeebaf48c8559e8be96c129860957
Tom Smyth <tom@sassafras.coop>
2014-05-05 10:06:49 -0400

    fixed old migrations to work when run on empty db

--- db/migrate/20120626184028_remove_question_code_spaces.rb
+++ db/migrate/20120626184028_remove_question_code_spaces.rb
@@ -1,6 +1,7 @@
 class RemoveQuestionCodeSpaces < ActiveRecord::Migration
   def up
-    Question.all.each{|q| q.code = q.code.gsub(" ", ""); q.save(:validate => false)}
+    # Now obsolete
+    # Question.all.each{|q| q.code = q.code.gsub(" ", ""); q.save(:validate => false)}
   end
 
   def down
