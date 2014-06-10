34ba5ef0440e3eff9f5f8260697a383483c921ec
rubygeek <nola@rubygeek.com>
2014-06-10 11:50:30 -0500

    fixed a boo boo, had the labels switched. doh.

--- app/models/access_level.rb
+++ app/models/access_level.rb
@@ -4,7 +4,7 @@ class AccessLevel
   PROTECTED = 3
 
   def self.option_list(params = {})
-    levels = {I18n.t('api_levels.level_2')  => PUBLIC, I18n.t('api_levels.level_1') => PRIVATE}
+    levels = {I18n.t('api_levels.level_2')  => PRIVATE, I18n.t('api_levels.level_1') => PUBLIC}
     # Right now only forms have option to have protected as access level
     levels.merge!({I18n.t('api_levels.level_3') => PROTECTED}) if params[:protected].present?
     levels
