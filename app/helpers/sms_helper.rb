478dd2a256825b4836721ca9d38ab5bca3f1ca1d
rubygeek <nola@rubygeek.com>
2014-06-11 07:35:38 -0500

    i18n the values of in direction field

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -1,6 +1,5 @@
 module SmsHelper
   def sms_messages_index_fields
-   # old columns before nola changed: %w[to from body adapter_name sent_at]
     %w[direction sent_at received_at body adapter_name]
   end
 
@@ -9,6 +8,7 @@ module SmsHelper
     when "to" then sms.to.is_a?(Array) ? sms.to.join(", ") : sms.to
     when "sent_at" then l(sms.sent_at)
     when "received_at" then l(sms.received_at) unless sms.received_at.blank?
+    when "direction" then I18n.t("common.#{sms.send(field)}") unless sms.send(field).blank?
     else sms.send(field)
     end
   end
