c6e45f52403dbc06ee71be013a47d70d85a1fbb6
Tom Smyth <tom@sassafras.coop>
2014-06-26 14:44:34 -0400

    added basic auth spec

---
+++ spec/requests/basic_authentication_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'basic authentication for xml requests' do
+
+  before(:all) do
+    @user = create(:user)
+  end
+
+  context 'when not already logged in' do
+    it 'should be required' do
+      get "/m/#{get_mission.compact_name}/formList"
+      assert_response :unauthorized
+      expect(response.headers['WWW-Authenticate']).to eq 'Basic realm="Application"'
+    end
+  end
+
+  context 'when already logged in via web' do
+    before do
+      login(@user)
+    end
+
+    it 'should not be required' do
+      get "/m/#{get_mission.compact_name}/formList"
+      assert_response :success
+    end
+  end
+
+end
