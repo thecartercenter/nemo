ec8ed8a276cd0849e2f8f7e36abc1a266ae2c833
rubygeek <nola@rubygeek.com>
2014-07-08 17:41:16 -0500

    addd app to list of disallowed sub domains

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
   include Replicable
 
-  RESERVED_SUBDOMAINS = %w(admin api assets blog calendar demo developer developers docs files ftp git imap 
+  RESERVED_SUBDOMAINS = %w(admin api assets blog calendar demo developer developers docs files ftp git imap app 
                            info lab mail manage mx pages pop sites smtp ssh ssl staging status support www)
   has_many :missions
 
