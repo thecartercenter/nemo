cdeff2aca78c5c43dfa246f23de64dd12ff5744e
Tom Smyth <tom@sassafras.coop>
2014-07-15 10:38:43 -0400

    fixed assignment validation messages

---
+++ spec/models/validation_messages/user_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe User do
+  context 'with assignment validation error' do
+    before do
+      @user = build(:user)
+      @user.assignments[0].mission = nil
+      @user.assignments[0].role = nil
+      @user.save
+    end
+
+    it 'assignment validation message should be correct' do
+      expect(@user.errors['assignments.role']).to eq ['is required.']
+      expect(@user.errors['assignments.mission']).to eq ['is required.']
+    end
+  end
+end
