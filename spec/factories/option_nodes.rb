82e89504a5cd854c4edf8cc70537552f03bb497d
Tom Smyth <tom@sassafras.coop>
2014-07-29 15:53:38 -0400

    refactored option node factory

--- spec/factories/option_nodes.rb
+++ spec/factories/option_nodes.rb
@@ -1,32 +1,31 @@
 FactoryGirl.define do
   factory :option_node do
+    mission { is_standard ? nil : get_mission }
     option
-  end
 
-  factory :option_node_with_children, class: OptionNode do
-    mission { is_standard ? nil : get_mission }
-    option nil
-    children_attribs [
-      { 'option_attribs' => { 'name_translations' => {'en' => 'Cat'} } },
-      { 'option_attribs' => { 'name_translations' => {'en' => 'Dog'} } }
-    ]
-  end
-
-  factory :option_node_with_grandchildren, class: OptionNode do
-    mission { is_standard ? nil : get_mission }
-    option nil
-    children_attribs [{
-      'option_attribs' => { 'name_translations' => {'en' => 'Animal'} },
-      'children_attribs' => [
+    factory :option_node_with_children do
+      option nil
+      children_attribs [
         { 'option_attribs' => { 'name_translations' => {'en' => 'Cat'} } },
         { 'option_attribs' => { 'name_translations' => {'en' => 'Dog'} } }
       ]
-    }, {
-      'option_attribs' => { 'name_translations' => {'en' => 'Plant'} },
-      'children_attribs' => [
-        { 'option_attribs' => { 'name_translations' => {'en' => 'Tulip'} } },
-        { 'option_attribs' => { 'name_translations' => {'en' => 'Oak'} } }
-      ]
-    }]
+    end
+
+    factory :option_node_with_grandchildren do
+      option nil
+      children_attribs [{
+        'option_attribs' => { 'name_translations' => {'en' => 'Animal'} },
+        'children_attribs' => [
+          { 'option_attribs' => { 'name_translations' => {'en' => 'Cat'} } },
+          { 'option_attribs' => { 'name_translations' => {'en' => 'Dog'} } }
+        ]
+      }, {
+        'option_attribs' => { 'name_translations' => {'en' => 'Plant'} },
+        'children_attribs' => [
+          { 'option_attribs' => { 'name_translations' => {'en' => 'Tulip'} } },
+          { 'option_attribs' => { 'name_translations' => {'en' => 'Oak'} } }
+        ]
+      }]
+    end
   end
 end
