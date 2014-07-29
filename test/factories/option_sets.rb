b02fc6dcd5b7990965a02afb6cddbd1e218647c9
Tom Smyth <tom@sassafras.coop>
2014-07-29 15:38:23 -0400

    refactored option set factory some

--- test/factories/option_sets.rb
+++ test/factories/option_sets.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :option_set do
     ignore do
       option_names %w(Yes No)
-      option_names_with_ranks nil
     end
 
     mission { is_standard ? nil : get_mission }
@@ -12,16 +11,9 @@ FactoryGirl.define do
     end
 
     optionings do
-      if option_names_with_ranks
-        # make the optioning objects according to the rank specified
-        option_names_with_ranks.each_pair.map do |name, rank|
-          Optioning.new(:rank => rank, :option => Option.new(:name => name, :mission => mission), :mission => mission)
-        end
-      else
-        # make the optioning objects, respecting the order they came in
-        option_names.each_with_index.map do |name, i|
-          Optioning.new(:rank => i + 1, :option => Option.new(:name => name, :mission => mission), :mission => mission)
-        end
+      # make the optioning objects, respecting the order they came in
+      option_names.each_with_index.map do |name, i|
+        Optioning.new(:rank => i + 1, :option => Option.new(:name => name, :mission => mission), :mission => mission)
       end
     end
   end
