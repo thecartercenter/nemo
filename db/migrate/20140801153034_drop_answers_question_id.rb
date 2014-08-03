8d7c5e720e0c412b93915b86a09f88752bcf3f7c
Tom Smyth <tom@sassafras.coop>
2014-08-03 13:14:57 -0400

    fixed other migration

--- db/migrate/20140801153034_drop_answers_question_id.rb
+++ db/migrate/20140801153034_drop_answers_question_id.rb
@@ -1,6 +1,7 @@
 class DropAnswersQuestionId < ActiveRecord::Migration
   def up
-    remove_column :answers, :question_id
+    remove_column :answers, :question_id if Answer.column_names.include?('question_id')
+    remove_column :answers, :questionable_id if Answer.column_names.include?('questionable_id')
   end
 
   def down
