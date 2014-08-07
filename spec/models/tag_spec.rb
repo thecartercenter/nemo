d26452d87342ca661246366c13c6c11d3c711a92
Adam Konner <adam.konner@gmail.com>
2014-08-07 12:50:48 -0400

    more tag tests

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,20 +1,34 @@
 require 'spec_helper'
 
 describe Tag do
-  it 'should create cleanly' do
-    create(:tag)
+  before do
+    @tag = create(:tag)
   end
 
-  context "if copy of standard object" do
+  context "abilities" do
     before do
-      @tag = create(:tag)
-      allow(@tag).to receive_messages(standard_copy?: true)
       @user = create(:user, admin: true)
       @ability = Ability.new(user: @user, mission: get_mission)
     end
 
-    it "should not allow editing" do
-      expect(@ability).not_to be_able_to :update, @tag
+    it "should normally allow editing and deleting" do
+      expect(@ability).to be_able_to :update, @tag
+      expect(@ability).to be_able_to :destroy, @tag
+    end
+
+    context "if copy of standard object" do
+      before do
+        allow(@tag).to receive_messages(standard_copy?: true)
+      end
+
+      it "should not allow editing" do
+        expect(@ability).not_to be_able_to :update, @tag
+      end
+
+      it "should not allow deleting" do
+        expect(@ability).not_to be_able_to :destroy, @tag
+      end
     end
   end
+
 end
