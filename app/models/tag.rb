212d1560005e36029f6a8590c03b56588c40e442
Adam Konner <adam.konner@gmail.com>
2014-08-07 12:47:57 -0400

    add modules to tag and tagging

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,10 @@
 class Tag < ActiveRecord::Base
-  include MissionBased
+  include MissionBased, Replicable, Standardizable
 
   belongs_to :mission
   has_many :taggings, dependent: :destroy
   has_many :questions, through: :taggings
   attr_accessible :is_standard, :name, :standard_id
+
+  replicable parent_assoc: :question
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,5 +1,9 @@
 class Tagging < ActiveRecord::Base
+  include MissionBased, Replicable, Standardizable
+
   belongs_to :question
   belongs_to :tag
   attr_accessible :is_standard, :standard_id
+
+  replicable parent_assoc: :question
 end
