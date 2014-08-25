2e8d1938174cc2bc6e1a8e31e1e8488ca46b35a0
Tom Smyth <tom@sassafras.coop>
2014-08-25 13:21:41 -0400

    added multilevel to response controller spec

--- spec/requests/responses_controller_spec.rb
+++ spec/requests/responses_controller_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 describe ResponsesController do
   before do
-    @form = create(:form, :question_types => %w(integer select_one))
+    @form = create(:form, question_types: %w(integer select_one), use_multilevel_option_set: true)
+    @option_set = OptionSet.first
+    @plant = @option_set.root_node.c[1].option
+    @tulip = @option_set.root_node.c[1].c[0].option
+    @oak = @option_set.root_node.c[1].c[1].option
     @user = get_user
     login(@user)
   end
 
   describe 'create' do
-    it 'should work' do
+    before do
       post(responses_path(mode: 'm', mission_name: get_mission.compact_name), response: {
         user_id: @user.id,
         form_id: @form.id,
@@ -21,21 +25,38 @@ describe ResponsesController do
           '1' => {
             questioning_id: @form.questionings[1].id,
             relevant: '1',
-            option_id: Option.first.id,
+            answers: { '0' => { option_id: @plant.id }, '1' => { option_id: @oak.id } }
           }
         }
       })
-      obj = Response.first
+    end
+
+    it 'should work' do
+      @obj = Response.first
       expect(response).to redirect_to responses_path
-      expect(obj.user).to eq @user
-      expect(obj.form).to eq @form
-      expect(obj.answers.size).to eq 2
+      expect(@obj.user).to eq @user
+      expect(@obj.form).to eq @form
+      expect(@obj.answers.size).to eq 3
     end
   end
 
   describe 'update' do
     before do
-      @obj = create(:response, :form => @form, :_answers => %w(42 Cat))
+      @obj = Response.create(
+        mission: get_mission,
+        user_id: @user.id,
+        form_id: @form.id,
+        answer_sets: {
+          '0' => {
+            questioning_id: @form.questionings[0].id,
+            relevant: '1',
+            value: '42',
+          },
+          '1' => {
+            questioning_id: @form.questionings[1].id,
+            relevant: '1',
+            answers: { '0' => { option_id: @plant.id }, '1' => { option_id: @oak.id } }
+      }})
     end
 
     it 'should work' do
@@ -51,17 +72,17 @@ describe ResponsesController do
           '1' => {
             questioning_id: @form.questionings[1].id,
             relevant: '1',
-            option_id: Option.last.id,
+            answers: { '0' => { option_id: @plant.id }, '1' => { option_id: @tulip.id } }
           }
         }
       })
+      expect(response).to redirect_to responses_path(mission_name: get_mission.compact_name)
       expect(Response.count).to eq 1
-      obj = Response.first
-      expect(response).to redirect_to responses_path
-      expect(obj.answers.size).to eq 2
+      @obj = Response.first
+      expect(@obj.answers.size).to eq 3
+      expect(@obj.answers[2].option.name).to eq 'Tulip'
     end
   end
-
 
   describe 'csv' do
     before do
