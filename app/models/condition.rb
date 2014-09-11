2f4c35592bd0c67687830fb790e5bff340ee8a23
Tom Smyth <tom@sassafras.coop>
2014-09-11 16:02:54 -0400

    Converted operator names to strings and refactored some things.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -4,33 +4,33 @@ class Condition < ActiveRecord::Base
   # question types that cannot be used in conditions
   NON_REFABLE_TYPES = %w(location)
 
-  belongs_to(:questioning, :inverse_of => :condition)
-  belongs_to(:ref_qing, :class_name => "Questioning", :foreign_key => "ref_qing_id", :inverse_of => :referring_conditions)
+  belongs_to(:questioning, inverse_of: :condition)
+  belongs_to(:ref_qing, class_name: "Questioning", foreign_key: "ref_qing_id", inverse_of: :referring_conditions)
 
   before_validation(:clear_blanks)
   before_validation(:clean_times)
   before_create(:set_mission)
 
   validate(:all_fields_required)
-  validates(:questioning, :presence => true)
+  validates(:questioning, presence: true)
 
-  delegate :qtype, :form, :to => :questioning, :allow_nil => true
-  delegate :has_options?, :select_options, :qtype, :rank, :code, :to => :ref_qing, :prefix => :ref_question, :allow_nil => true
+  delegate :qtype, :form, to: :questioning, allow_nil: true
+  delegate :has_options?, :select_options, :qtype, :rank, :code, to: :ref_qing, prefix: :ref_question, allow_nil: true
 
   serialize :option_ids, JSON
 
-  OPS = [
-    {:name => :eq, :types => %w(decimal integer text long_text address select_one datetime date time), :code => "="},
-    {:name => :lt, :types => %w(decimal integer datetime date time), :code => "<"},
-    {:name => :gt, :types => %w(decimal integer datetime date time), :code => ">"},
-    {:name => :leq, :types => %w(decimal integer datetime date time), :code => "<="},
-    {:name => :geq, :types => %w(decimal integer datetime date time), :code => ">="},
-    {:name => :neq, :types => %w(decimal integer text long_text address select_one datetime date time), :code => "!="},
-    {:name => :inc, :types => %w(select_multiple), :code => "="},
-    {:name => :ninc, :types => %w(select_multiple), :code => "!="}
+  OPERATORS = [
+    {name: 'eq', types: %w(decimal integer text long_text address select_one datetime date time), code: "="},
+    {name: 'lt', types: %w(decimal integer datetime date time), code: "<"},
+    {name: 'gt', types: %w(decimal integer datetime date time), code: ">"},
+    {name: 'leq', types: %w(decimal integer datetime date time), code: "<="},
+    {name: 'geq', types: %w(decimal integer datetime date time), code: ">="},
+    {name: 'neq', types: %w(decimal integer text long_text address select_one datetime date time), code: "!="},
+    {name: 'inc', types: %w(select_multiple), code: "="},
+    {name: 'ninc', types: %w(select_multiple), code: "!="}
   ]
 
-  replicable :after_copy_attribs => :copy_ref_qing_and_option, :parent_assoc => :questioning, :dont_copy => [:ref_qing_id]
+  replicable after_copy_attribs: :copy_ref_qing_and_option, parent_assoc: :questioning, dont_copy: [:ref_qing_id]
 
   def options
     # We need to sort since ar#find doesn't guarantee order
@@ -78,63 +78,43 @@ class Condition < ActiveRecord::Base
 
   # returns names of all operators that are applicable to this condition based on its referred question
   def applicable_operator_names
-    ref_qing ? OPS.select{|o| o[:types].include?(ref_question_qtype.name)}.map{|o| o[:name]} : []
-  end
-
-  # duplicates this condition
-  def duplicate
-    self.class.new(:ref_qing_id => ref_qing_id, :op => op, :value => value, :option_id => option_id)
+    ref_qing ? OPERATORS.select{|o| o[:types].include?(ref_question_qtype.name)}.map{|o| o[:name]} : []
   end
 
   def verify_ordering
     raise ConditionOrderingError.new if questioning.rank <= ref_qing.rank
   end
 
-  # gets the hash from the OPS array corresponding to self's operator
+  # Gets the definition of self's operator (self.op).
   def operator
-    @operator ||= OPS.index_by{|o| o[:name]}[op.to_sym]
-  end
-
-  # returns all known operators
-  def operators
-    OPS
+    @operator ||= OPERATORS.detect{|o| o[:name] == op}
   end
 
   def to_odk
-
     lhs = "/data/#{ref_subquestion.odk_code}"
 
     if ref_question_has_options?
 
-      xpath = "selected(#{lhs}, '#{option_ids.last}')"
-      xpath = "not(#{xpath})" if [:neq, :ninc].include?(operator[:name])
+      selected = "selected(#{lhs}, '#{option_ids.last}')"
+
+      # Apply negation if appropriate.
+      %w(neq ninc).include?(operator[:name]) ? "not(#{selected})" : selected
 
     else
 
-      # for numeric ref. questions, just convert value to string to get rhs
-      if ref_question_qtype.numeric?
-        rhs = value.to_s
-
-      # for temporal ref. questions, need to convert dates to appropriate format
-      elsif ref_question_qtype.temporal?
-
-        # get xpath compatible date type name
-        date_type = ref_question_qtype.name.gsub("datetime", "dateTime")
-        format = :"javarosa_#{date_type.downcase}"
+      # For temporal ref. questions, need to convert dates to appropriate format in xpath.
+      if ref_question_qtype.temporal?
+        format = :"javarosa_#{ref_question_qtype.name}"
         formatted = Time.zone.parse(value).to_s(format)
         lhs = "format-date(#{lhs}, '#{Time::DATE_FORMATS[format]}')"
         rhs = "'#{formatted}'"
-
-      # otherwise just quoted string
       else
-        rhs = "'#{value}'"
+        rhs = ref_question_qtype.numeric? ? value : "'#{value}'"
       end
 
-      # build the final xpath expression
-      xpath = "#{lhs} #{operator[:code]} #{rhs}"
-    end
+      "#{lhs} #{operator[:code]} #{rhs}"
 
-    return xpath
+    end
   end
 
   # generates a human readable representation of condition
