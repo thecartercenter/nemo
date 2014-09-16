d2b0f371dd911cccdb4dd411367b86fe92647013
Adam Konner <adam.konner@gmail.com>
2014-09-16 16:10:39 -0400

    refactor spec and add edge cases

--- spec/requests/controllers/questions_controller_spec.rb
+++ spec/requests/controllers/questions_controller_spec.rb
@@ -8,12 +8,32 @@ describe "Question search form", type: :feature do
     login(@user)
     visit "/en/m/#{get_mission.compact_name}/questions"
   end
+  subject { page }
 
-  it "should search questions" do
-    fill_in "search", with: "wald"
+  context "basic search" do
+    before { search_for 'wald' }
+    it { should have_content "Displaying all 2 Questions" }
+    it { should have_content @questions[0].code }
+    it { should have_content @questions[1].code }
+  end
+
+  context "no results" do
+    before { search_for 'boogeyman' }
+    it { should have_content "No Questions found" }
+  end
+
+  context "empty search" do
+    before { search_for '' }
+    it { should have_content "Displaying all 3 Questions" }
+  end
+
+  context "search error" do
+    before { search_for 'creepy:' }
+    it { should have_content "Error: Your search query could not be understood due to unexpected text near the end." }
+  end
+
+  def search_for(query)
+    fill_in "search", with: query
     click_button "Search"
-    expect(page).to have_content "Displaying all 2 Questions"
-    expect(page).to have_content @questions[0].code
-    expect(page).to have_content @questions[1].code
   end
 end
