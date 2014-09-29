96a29510019cca4d24907fddf837528de7955f8e
Tom Smyth <tom@sassafras.coop>
2014-09-29 15:52:38 -0400

    Added response ID as field for list reports

--- spec/models/report/list_report_spec.rb
+++ spec/models/report/list_report_spec.rb
@@ -4,18 +4,18 @@ describe Report::ListReport do
   context 'with multilevel option set' do
     before do
       @form = create(:form, question_types: %w(select_one integer select_one), use_multilevel_option_set: true)
-      @response = create(:response, form: @form, answer_values: [['Animal', 'Cat'], 5, ['Animal', 'Dog']])
-      @response = create(:response, form: @form, answer_values: [['Animal'], 10, ['Plant', 'Oak']])
-      @response = create(:response, form: @form, answer_values: [nil, 15, ['Plant']])
-      @report = create(:list_report, _calculations: @form.questions + ['source'])
+      @response1 = create(:response, form: @form, answer_values: [['Animal', 'Cat'], 5, ['Animal', 'Dog']])
+      @response2 = create(:response, form: @form, answer_values: [['Animal'], 10, ['Plant', 'Oak']])
+      @response3 = create(:response, form: @form, answer_values: [nil, 15, ['Plant']])
+      @report = create(:list_report, _calculations: @form.questions + ['response_id'])
     end
 
     it 'should have answer values in correct order' do
       expect(@report).to have_legacy_report_data(
-        @form.questions.map(&:name) + ['Source'],
-        ['Animal, Cat', '5',  'Animal, Dog', 'web'],
-        ['Animal',      '10', 'Plant, Oak',  'web'],
-        ['_',           '15', 'Plant',       'web']
+        @form.questions.map(&:name) + ['Response ID'],
+        ['Animal, Cat', '5',  'Animal, Dog', "##{@response1.id}"],
+        ['Animal',      '10', 'Plant, Oak',  "##{@response2.id}"],
+        ['_',           '15', 'Plant',       "##{@response3.id}"]
       )
     end
   end
