e1af49fba6b1c21dd8ecf1ebadf3bb074c883c11
Tom Smyth <tom@sassafras.coop>
2014-10-03 09:37:11 -0400

    Added another form replication test.

--- spec/models/replication/form_replication_spec.rb
+++ spec/models/replication/form_replication_spec.rb
@@ -7,21 +7,47 @@ describe Form do
   end
 
   describe 'question rank change' do
-    before do
-      @std_form = create(:form, question_types: %w(integer integer integer integer), is_standard: true)
-      @copy_form = @std_form.replicate(mode: :to_mission, dest_mission: get_mission)
-      @old_copy_qing_ids = @copy_form.questionings.map(&:id)
+    context 'without conditions' do
+      before do
+        @std_form = create(:form, question_types: %w(integer integer integer integer), is_standard: true)
+        @copy_form = @std_form.replicate(mode: :to_mission, dest_mission: get_mission)
+        @old_copy_qing_ids = @copy_form.questionings.map(&:id)
 
-      # Switch ranks on @std_form (swap ranks 1,2 and 3,4)
-      ids = @std_form.questionings.map(&:id)
-      @std_form.name = 'Updated ranks'
-      @std_form.update_ranks({ids[1] => 1, ids[0] => 2, ids[3] => 3, ids[2] => 4})
-      @std_form.save_and_rereplicate
+        # Switch ranks on @std_form (swap ranks 1,2 and 3,4)
+        ids = @std_form.questionings.map(&:id)
+        @std_form.name = 'Updated ranks' # Update name to ensure save
+        @std_form.update_ranks({ids[1] => 1, ids[0] => 2, ids[3] => 3, ids[2] => 4})
+        @std_form.save_and_rereplicate
+      end
+
+      it 'should replicate rank changes' do
+        ids = @old_copy_qing_ids
+        expect(@copy_form.reload.questionings.map(&:id)).to eq [ids[1], ids[0], ids[3], ids[2]]
+      end
     end
 
-    it 'should replicate rank changes' do
-      ids = @old_copy_qing_ids
-      expect(@copy_form.reload.questionings.map(&:id)).to eq [ids[1], ids[0], ids[3], ids[2]]
+    context 'with condition' do
+      before do
+        @std_form = create(:form, question_types: %w(select_one integer integer), is_standard: true)
+        @std_form.questionings[2].create_condition(ref_qing: @std_form.questionings[0], op: 'eq',
+          option_id: @std_form.questionings[0].options[0].id)
+        @copy_form = @std_form.replicate(mode: :to_mission, dest_mission: get_mission)
+        @old_copy_qing_ids = @copy_form.questionings.map(&:id)
+
+        # Move middle question to top.
+        ids = @std_form.questionings.map(&:id)
+        @std_form.name = 'Updated ranks' # Update name to ensure save
+        @std_form.update_ranks({ids[0] => 2, ids[1] => 1, ids[2] => 3})
+        @std_form.save_and_rereplicate!
+      end
+
+      it 'should replicate rank changes' do
+        ids = @old_copy_qing_ids
+        expect(@copy_form.reload.questionings.map(&:id)).to eq [ids[1], ids[0], ids[2]]
+
+        # Ref qing should not have changed.
+        expect(@copy_form.questionings[2].condition.ref_qing_id).to eq ids[0]
+      end
     end
   end
 
