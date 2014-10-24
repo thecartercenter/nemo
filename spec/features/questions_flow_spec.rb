134dcbb6d0050b1286aea6dab3f5c7eb5ade5bdc
Adam Konner <adam.konner@gmail.com>
2014-10-24 17:59:09 -0400

    Extend spec to cover admin mode and edge cases

--- spec/features/questions_flow_spec.rb
+++ spec/features/questions_flow_spec.rb
@@ -4,8 +4,7 @@ feature "questions flow" do
   before do
     @question1 = create(:question, name: "How many cheeses?")
     @question2 = create(:question, name: "How many wines?")
-    @user = get_user
-    # @user = create(:user, role_name: 'coordinator')
+    @user = create(:user, role_name: 'coordinator', admin: true)
     login(@user)
   end
 
@@ -42,7 +41,7 @@ feature "questions flow" do
     create(:tag, name: "thriftshop", mission_id: get_mission.id)
     create(:tag, name: "twenty", mission_id: get_mission.id)
     create(:tag, name: "dollaz", mission_id: get_mission.id)
-    create(:tag, name: "pop", mission_id: nil)
+    create(:tag, name: "awesome", mission_id: nil, is_standard: true) # Standard tag
 
     visit "/en/m/#{get_mission.compact_name}/questions/#{@question1.id}/edit"
     expect(page).to have_content "Tags:"
@@ -61,25 +60,76 @@ feature "questions flow" do
     find('li', text: "thriftshop").click
 
     # Standard tag
-    fill_in "token-input-question_tag_ids", with: "p"
-    expect(page).to have_content "pop"
-    find('li', text: "pop").click
+    fill_in "token-input-question_tag_ids", with: "a"
+    expect(page).to have_content "awesome"
+    within find('li', text: 'awesome') do
+      expect(page).to have_selector 'i.fa-certificate'
+    end
+    # Apply
+    find('li', text: "awesome").click
 
     # Create a new tag
     fill_in "token-input-question_tag_ids", with: "pocket"
     find('li', text: "pocket").click
 
+    # Add and then cancel a new tag
+    fill_in "token-input-question_tag_ids", with: "pop"
+    find('li', text: "pop").click
+    within find('div#tag_ids li', text: "pop") do
+      find('span.token-input-delete-token-elmo').click # "x" close button
+    end
+    expect(page).not_to have_content "pop"
+
     click_button "Save"
 
-    # New tags show on index page (not yet)
+    # New tag should be in database
+    expect(Tag.find_by_name('pocket').mission_id).to eq get_mission.id
+    # Canceled tag should not
+    expect(Tag.pluck(:name)).not_to include('pop')
+
+    # Tags show on index page (not yet)
     # expect(page).to have_content "Questions"
     # expect(page).to have_content "thriftshop"
     # expect(page).to have_content "pocket"
 
-    # New tags show on question page
+    # Tags show on question page
     visit "/en/m/#{get_mission.compact_name}/questions/#{@question1.id}"
-    expect(page).to have_content "thriftshop"
-    expect(page).to have_content "pop"
-    expect(page).to have_content "pocket"
+    within "div#tag_ids" do
+      expect(page).to have_selector 'li', text: "thriftshop"
+      expect(page).to have_selector 'li', text: "pocket"
+      expect(page).to have_selector 'li', text: "awesome"
+      within find('li', text: 'awesome') do
+        expect(page).to have_selector 'i.fa-certificate'
+      end
+      expect(page).not_to have_selector 'li', text: "pop"
+    end
+
+    # Admin mode
+    @question3 = create(:question, name: "How much beer?", is_standard: true, mission_id: nil)
+    visit "/en/admin/questions/#{@question3.id}/edit"
+
+    fill_in "token-input-question_tag_ids", with: "a"
+    expect(page).to have_content "awesome"
+    expect(page).not_to have_content "pocket" # Non-standard tag
+    find('li', text: "awesome").click
+
+    # Create a new tag
+    fill_in "token-input-question_tag_ids", with: "newt"
+    find('li', text: "newt").click
+
+    click_button "Save"
+
+    # Tags show on question page
+    visit "/en/admin/questions/#{@question3.id}"
+    within "div#tag_ids" do
+      expect(page).to have_selector 'li', text: "awesome"
+      within find('li', text: 'awesome') do
+        expect(page).to have_selector 'i.fa-certificate'
+      end
+    end
+
+    # Check that new tag is standard in DB
+    expect(Tag.find_by_name('newt').is_standard).to be_truthy
+    expect(Tag.find_by_name('newt').mission_id).to be_nil
   end
 end
