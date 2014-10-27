cc7905dc6fb56c2c0a32cc338da6448d4f7fac33
Tom Smyth <tom@sassafras.coop>
2014-10-27 10:15:26 -0400

    Tidied.

--- app/helpers/odk_helper.rb
+++ app/helpers/odk_helper.rb
@@ -84,6 +84,6 @@ module OdkHelper
 
   # Returns <text> tags for all first-level options.
   def odk_option_translations(form, lang)
-    form.all_first_level_option_nodes.map{ |on| %{<text id="on#{on.id}"><value>#{on.option.name(lang, strict: false)}</value></text>} }.join.html_safe
+    form.all_first_level_option_nodes.map{ |on| %Q{<text id="on#{on.id}"><value>#{on.option.name(lang, strict: false)}</value></text>} }.join.html_safe
   end
 end
