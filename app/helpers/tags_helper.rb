065e0bda697b55c5009e7b0739608926938ce1f0
Adam Konner <adam.konner@gmail.com>
2014-10-29 17:56:06 -0400

    DRY tag rendering

---
+++ app/helpers/tags_helper.rb
@@ -0,0 +1,14 @@
+module TagsHelper
+  # options[:class] - additional class(es) to add to ul
+  def render_tags(tags, options = {})
+    html = %{<ul class="tags #{options[:class]}">}
+    tags.each do |tag|
+      html << '<li class="token-input-token-elmo">'
+      html << '<i class="fa fa-fw fa-certificate"></i> ' if tag.is_standard?
+      html << tag.name
+      html << '</li>'
+    end
+    html << '</ul>'
+    raw html
+  end
+end
