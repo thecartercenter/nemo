cd40ec6712f785bd4745cb3f879fbf50f2300ed8
Adam Konner <adam.konner@gmail.com>
2014-10-30 13:21:15 -0400

    Show tags with each question in list

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,14 +1,16 @@
 module TagsHelper
   # options[:class] - additional class(es) to add to ul
   def render_tags(tags, options = {})
-    html = %{<ul class="tags #{options[:class]}">}
-    tags.each do |tag|
-      html << '<li class="token-input-token-elmo">'
-      html << '<i class="fa fa-fw fa-certificate"></i> ' if tag.is_standard?
-      html << tag.name
-      html << '</li>'
+    if tags.present?
+      html = %{<ul class="tags #{options[:class]}">}
+      tags.each do |tag|
+        html << '<li class="token-input-token-elmo">'
+        html << '<i class="fa fa-fw fa-certificate"></i> ' if tag.is_standard?
+        html << tag.name
+        html << '</li>'
+      end
+      html << '</ul>'
+      raw html
     end
-    html << '</ul>'
-    raw html
   end
 end
