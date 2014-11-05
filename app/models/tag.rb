504c4bb1f9e789365ddb601b9a9582e13ae2926a
Adam Konner <adam.konner@gmail.com>
2014-11-05 15:50:18 -0500

    optimize and simplify tag suggestions method

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,43 +7,25 @@ class Tag < ActiveRecord::Base
   attr_accessible :is_standard, :name, :standard_id, :mission_id
 
   before_save { |tag| tag.name.downcase! }
-  after_save(:invalidate_cache)
-  after_destroy(:invalidate_cache)
 
   MAX_SUGGESTIONS = 5 # The max number of suggestion matches to return
   MAX_NAME_LENGTH = 64
 
   # Returns an array of Tags matching the given mission and textual query.
   def self.suggestions(mission, query)
-    # fetch all mission tags from the cache
-    mission_id = mission ? mission.id : 'std'
-    tags = Rails.cache.fetch("mission_tags/#{mission_id}", expires_in: 2.minutes) do
-      Tag.unscoped.for_mission([mission, nil])
-    end
+    tags = Tag.for_mission([mission, nil])
 
     # Trim query to maximum length.
     query = query[0...MAX_NAME_LENGTH]
 
-    # where("name like ?", "%#{params[:q]}%").select([:id, :name])
+    exact_match = tags.find_by_name(query)
+    matches = tags.where('name like ?', "%#{query}%").order(:name).limit(MAX_SUGGESTIONS)
 
-    # scan for tags matching query
-    matches = []; exact_match = false
-    for i in 0...tags.size
-      # if an exact match, set a flag and put it at the top
-      if tags[i].name == query
-        matches.unshift tags[i]
-        exact_match = true
-        # otherwise if partial match just insert at the end
-      elsif tags[i].name && tags[i].name =~ /#{Regexp.escape(query)}/i
-        matches << tags[i]
-      end
-    end
-
-    # trim results to max size (couldn't do this earlier b/c had to search whole list for exact match)
-    matches = matches[0...MAX_SUGGESTIONS]
-
-    # if there was no exact match, we append a 'new tag' placeholder
-    unless exact_match
+    if exact_match
+      # if there was an exact match, put it at the top
+      matches.unshift(exact_match).uniq!
+    else
+      # if there was no exact match, we append a 'new tag' placeholder
       matches << Tag.new(name: query)
     end
 
@@ -64,10 +46,4 @@ class Tag < ActiveRecord::Base
         mission_id, question_ids).uniq.order(:name)
   end
 
-  private
-
-    # invalidate the mission tag cache after save, destroy
-    def invalidate_cache
-      Rails.cache.delete("mission_tags/#{mission_id}")
-    end
 end
