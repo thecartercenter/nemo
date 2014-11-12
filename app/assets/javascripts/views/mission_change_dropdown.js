84512939f8fe993a4b7349feeb3080dccbf8c6b9
Tom Smyth <tom@sassafras.coop>
2014-11-12 14:44:25 -0500

    Fixed mission change dropdown to work with poltergeist

--- app/assets/javascripts/views/mission_change_dropdown.js
+++ app/assets/javascripts/views/mission_change_dropdown.js
@@ -4,11 +4,11 @@
   // constructor
   ns.MissionChangeDropdown = klass = function(params) {
     $(params.el).on('change', function(e){
-      var new_mission_name = $(e.target).find('option:selected').val();
-      if (new_mission_name == '')
+      var new_mission_name = $(e.currentTarget).find('option:selected').val();
+      if (new_mission_name == '') {
         // Redirect straight to root if chose 'None' from dropdown.
         window.location.href = ELMO.app.url_builder.build('/', {mode: 'basic'}) + '?missionchange=1';
-      else {
+      } else {
         // If currently in basic mode, we automatically redirect to mission root.
         // This is because it doesn't currently make sense to use a path from basic mode in mission mode,
         // whereas sharing paths between missions or mission<->admin can make sense.
