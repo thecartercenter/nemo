9779a58eb0f93db40d94ef5b11419479b50f2280
Tom Smyth <tom@sassafras.coop>
2014-11-14 16:33:13 -0500

    Fixed data integrity test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -222,25 +222,4 @@ module ApplicationHelper
     end
     l.join.html_safe
   end
-
-  # Tries to get a path for the given object, returns nil if object doesn't have route
-  # Preserves the search param in the current query string, if any, unless there was a search error.
-  def path_for_with_search(obj)
-    begin
-      polymorphic_path(obj, @search_error ? {} : {search: params[:search]})
-    rescue
-      nil
-    end
-  end
-
-  def dynamic_path(obj_or_class, options = {})
-    obj = obj_or_class.is_a?(Class) ? nil : obj_or_class
-    klass = obj_or_class.is_a?(Class) ? obj_or_class : obj_or_class.class
-    action = options.delete(:action)
-    key = klass.name.demodulize.underscore
-    key = key.pluralize if action == :index
-    key = "#{action}_#{key}" if [:new, :edit].include?(action)
-    args = [:new, :index].include?(action) ? [options] : [obj, options]
-    send("#{key}_path", *args)
-  end
 end
