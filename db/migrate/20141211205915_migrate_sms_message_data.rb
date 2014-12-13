0df9e2ad1c1a657b85da3782ab28cefb012dcf33
Adam Konner <adam.konner@gmail.com>
2014-12-12 19:02:02 -0500

    Redo data migration in SQL
    A bug preventing deserialization of `to` field exposed other issues
    with the previous ruby migration

--- db/migrate/20141211205915_migrate_sms_message_data.rb
+++ db/migrate/20141211205915_migrate_sms_message_data.rb
@@ -1,49 +1,45 @@
 class MigrateSmsMessageData < ActiveRecord::Migration
   def up
-    Sms::Message.all.each do |msg|
-      if msg.direction == 'incoming' || (msg.to.in? [nil, 'null'])
-        msg.type = 'Sms::Incoming'
-        msg.to = nil
-        msg.user_id = find_user_id(msg.from)
-      elsif msg.direction == 'outgoing' || (msg.from.in? [nil, 'null'])
-        if msg.to.try(:size) > 1
-          msg.type = 'Sms::Broadcast'
-          msg.to = nil
-        else
-          msg.type = 'Sms::Reply'
-          msg.to = msg.to.first if msg.to.is_a? Array
-          msg.user_id = find_user_id(msg.to)
-        end
-        msg.from = nil
-      else
-        raise "Unable to set type for message id #{msg.id}"
-      end
-      msg.save
-    end
+    execute <<-SQL
+      UPDATE sms_messages
+      LEFT JOIN users u ON `from` IN (u.phone, u.phone2)
+      SET `type` = 'Sms::Incoming',
+        `to` = NULL,
+        user_id = u.id
+      WHERE `direction` = 'incoming'
+        OR `to` IS NULL
+        OR `to` = 'null';
+    SQL
+    execute <<-SQL
+      UPDATE sms_messages
+      SET `type` = IF(`to` LIKE '%,%', 'Sms::Broadcast', 'Sms::Reply'),
+        `from` = NULL
+      WHERE `direction` = 'outgoing'
+        OR `from` IS NULL
+        OR `from` = 'null';
+    SQL
+    execute <<-SQL
+      UPDATE sms_messages
+      SET `to` = NULL
+      WHERE `type` = 'Sms::Broadcast';
+    SQL
+    execute <<-SQL
+      UPDATE sms_messages
+      SET `to` = SUBSTRING_INDEX(TRIM(LEADING '["' FROM `to`), '"', 1)
+      WHERE `type` = 'Sms::Reply';
+    SQL
+    execute <<-SQL
+      UPDATE sms_messages
+      LEFT JOIN users u ON `to` IN (u.phone, u.phone2)
+      SET user_id = u.id
+      WHERE `type` = 'Sms::Reply';
+    SQL
 
     remove_column :sms_messages, :direction
     change_column :sms_messages, :to, :string
   end
 
   def down
-    add_column :sms_messages, :direction, :string
-    change_column :sms_messages, :to, :text
-
-    Sms::Message.all.each do |msg|
-      if msg.type == 'Sms::Incoming'
-        msg.direction = 'incoming'
-      elsif msg.type.in? %w(Sms::Broadcast Sms::Reply)
-        msg.direction = 'outgoing'
-        msg.to = [msg.to]
-      else
-        raise "Unable to read type for message id #{msg.id}"
-      end
-      msg.type = msg.user_id = ''
-      msg.save
-    end
-  end
-
-  def find_user_id(phone)
-    User.where('phone = ? OR phone2 = ?', phone, phone).first.try :id
+    raise ActiveRecord::IrreversibleMigration
   end
 end
