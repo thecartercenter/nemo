ecdc3602879422c0923d6097914a00ee7ef65e08
Tom Smyth <tom@sassafras.coop>
2015-02-23 11:01:56 -0500

    Fixed test failure.

--- test/factories/sms_messages.rb
+++ test/factories/sms_messages.rb
@@ -17,7 +17,7 @@ FactoryGirl.define do
   factory :sms_broadcast, :class => 'Sms::Broadcast', :parent => :sms_message do
   end
 
-  factory :sms_message_with_mission, :parent => :sms_message do
+  factory :sms_message_with_mission, :parent => :sms_incoming do
     mission { get_mission }
   end
 end
