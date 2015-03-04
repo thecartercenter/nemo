5f9ee65d21c7714275817105e43b9f526e276fe8
Michal Kuklis <michal.kuklis@gmail.com>
2015-03-04 13:25:23 -0500

    cleanup form item controller

--- app/controllers/form_items_controller.rb
+++ app/controllers/form_items_controller.rb
@@ -2,26 +2,18 @@ class FormItemsController < ApplicationController
   # authorization via cancan
   load_and_authorize_resource
 
-  before_filter :prepare_form_item, only: [:update]
-
   def update
+    if params[:ancestry].blank? or !@form_item.check_ancestry_integrity(params[:ancestry])
+      return render nothing: true, status: 422
+    end
+
+    @form_item.ancestry = params[:ancestry]
+    @form_item.rank = params[:rank]
+
     if @form_item.save
       render nothing: true, status: 204
     else
       render nothing: true, status: 422
     end
   end
-
-  private
-    # prepares form item
-    def prepare_form_item
-      attrs = params[:form_item]
-
-      if attrs.blank? or !@form_item.check_ancestry_integrity(attrs[:ancestry])
-        return render nothing: true, status: 422
-      end
-
-      @form_item.ancestry = attrs[:ancestry]
-      @form_item.rank = attrs[:rank]
-    end
 end
