7a51c61d92f6a90638988e6c325668af5628c56a
Michal Kuklis <michal.kuklis@gmail.com>
2015-03-06 12:51:47 -0500

    2120 - migration fix

--- db/migrate/20130829010537_copy_mission_to_join_classes.rb
+++ db/migrate/20130829010537_copy_mission_to_join_classes.rb
@@ -1,5 +1,6 @@
 class CopyMissionToJoinClasses < ActiveRecord::Migration
   def up
+    class FormItem < ActiveRecord::Base; end
     # copy mission to newly added columns
     Questioning.all.each{|q| q.mission_id = q.question.mission_id; q.save(:validate => false)}
     Condition.all.each{|c| c.mission_id = c.questioning.mission_id; c.save(:validate => false)}
