f48fcaf9fa5c3163e18813677803f3272043f525
Tom Smyth <tom@sassafras.coop>
2015-03-20 17:54:39 -0400

    Cleaned up tests and added one.

--- spec/models/form_item_spec.rb
+++ spec/models/form_item_spec.rb
@@ -24,43 +24,58 @@ describe FormItem do
   end
 
   describe "sort" do
-    before(:each) do
-      @f = create(:form, questions: ['text', 'text', 'text', 'text'])
-      @group = create(:qing_group, form: @f, parent: @f.root_group)
+    context "with flat form" do
+      before(:each) do
+        @f = create(:form, question_types: ['text', 'text', 'text', 'text'])
+        @group = create(:qing_group, form: @f, parent: @f.root_group)
+      end
+
+      it 'should create 4 questionings and one group with correct ranks' do
+        expect(@f.c.size).to eq(5)
+        expect(@f.c[0].rank).to be < @f.c[1].rank
+        expect(@f.c[1].rank).to be < @f.c[2].rank
+        expect(@f.c[2].rank).to be < @f.c[3].rank
+      end
+
+      it 'should assign a rank to a newly created group' do
+        expect(@f.c[3].rank).to be < @group.rank
+      end
+
+      it 'should adjust ranks when existing questioning moved to the empty group' do
+        @old_rank_2 = @f.c[1]
+        @old_rank_3 = @f.c[2]
+        @old_rank_4 = @f.c[3]
+        @old_rank_2.parent = @group;
+        @old_rank_2.save
+        expect(@old_rank_2.reload.rank).to eq 1
+        expect(@old_rank_3.reload.rank).to eq 2 # Should move up one.
+        expect(@old_rank_4.reload.rank).to eq 3 # Should move up one.
+      end
+
+      it 'should change order of the questioning moved higher' do
+        @qing = @f.c[3]
+        @qing.move_higher
+        expect(@f.c[3].rank).to be < @f.c[2].rank
+      end
+
+      it 'should change order of the questioning moved lower' do
+        @qing = @f.c[0]
+        @qing.move_lower
+        expect(@f.c[1].rank).to be < @f.c[0].rank
+      end
     end
 
-    it 'should create 4 questionings and one group with correct ranks' do
-      expect(@f.c.size).to eq(5)
-      expect(@f.c[0].rank).to be < @f.c[1].rank
-      expect(@f.c[1].rank).to be < @f.c[2].rank
-      expect(@f.c[2].rank).to be < @f.c[3].rank
-    end
+    context "with nested form" do
+      before do
+        @f = create(:form, question_types: ['text', ['text', 'text']])
+      end
 
-    it 'should assign a rank to a newly created group' do
-      expect(@f.c[2].rank).to be < @group.rank
-    end
-
-    it 'should adjust ranks when existing questioning moved to the empty group' do
-      @old_rank_2 = @f.c[1]
-      @old_rank_3 = @f.c[2]
-      @old_rank_4 = @f.c[3]
-      @old_rank_2.parent = @group;
-      @old_rank_2.save
-      expect(@old_rank_2.reload.rank).to eq 1
-      expect(@old_rank_3.reload.rank).to eq 2 # Should move up one.
-      expect(@old_rank_4.reload.rank).to eq 3 # Should move up one.
-    end
-
-    it 'should change order of the questioning moved higher' do
-      @qing = @f.c[3]
-      @qing.move_higher
-      expect(@f.c[3].rank).to be < @f.c[2].rank
-    end
-
-    it 'should change order of the questioning moved lower' do
-      @qing = @f.c[0]
-      @qing.move_lower
-      expect(@f.c[1].rank).to be < @f.c[0].rank
+      it 'should work when changing ranks of second level items' do
+        @q1, @q2 = @f.c[1].children
+        @q2.update_attributes!(rank: 1)
+        expect(@q1.reload.rank).to eq 2
+        expect(@q2.reload.rank).to eq 1
+      end
     end
   end
 end
