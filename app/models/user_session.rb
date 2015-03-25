03a2d4c907ba439bc7c61744f9edb14ba8c25cd8
Michal Kuklis <michal.kuklis@gmail.com>
2015-03-25 10:51:53 -0400

    Set htmlonly to true for cookie.

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -3,6 +3,7 @@ class UserSession < Authlogic::Session::Base
   # other config goes in acts_as block
   logout_on_timeout(true)
   allow_http_basic_auth(false) # We handle our own basic auth
+  httponly(true)
 
   def to_key
     new_record? ? nil : [ self.send(self.class.primary_key) ]
