f8723b7a6a6e3c2674882ed275b1b50322460f0f
Tom Smyth <tom@sassafras.coop>
2015-03-27 20:21:00 -0400

    Added explicit i18n fallback setting to development env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ ELMO::Application.configure do
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
+  config.i18n.fallbacks = false
 
   # bullet gem for query optimization
   # config.after_initialize do
