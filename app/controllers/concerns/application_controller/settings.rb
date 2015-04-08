b8523bccdf1ed4b0715f2ae85adc5b1d51d9efd6
Michal Kuklis <michal.kuklis@gmail.com>
2015-04-08 13:57:36 -0400

    Refactored setting locale

--- app/controllers/concerns/application_controller/settings.rb
+++ app/controllers/concerns/application_controller/settings.rb
@@ -3,7 +3,13 @@ module Concerns::ApplicationController::Settings
 
   # sets the locale based on the locale param (grabbed from the path by the router)
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    set_locale_or_default(params[:locale])
+  end
+
+  # sets locale based on passed preferable lang or default
+  def set_locale_or_default(pref_lang)
+    pref_lang_sym = pref_lang.blank? ? I18n.default_locale : pref_lang.to_sym
+    I18n.locale = configatron.full_locales.include?(pref_lang_sym) ? pref_lang_sym : I18n.default_locale
   end
 
   # Loads the user-specified timezone from configatron, if one exists
