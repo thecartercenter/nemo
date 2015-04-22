de239e19b5c308e200b89cbecac5bb21d364ef25
Mike Dillon <mike@appropriate.io>
2015-04-22 11:37:57 -0700

    2163: Add migration to remove unassigned users from whitelist

---
+++ db/migrate/20150422182449_remove_unassigned_users_from_whitelist.rb
@@ -0,0 +1,18 @@
+class RemoveUnassignedUsersFromWhitelist < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      DELETE wl
+      FROM whitelists wl
+        INNER JOIN forms f
+          ON wl.whitelistable_type = 'Form' AND wl.whitelistable_id = f.id
+        INNER JOIN missions m
+          ON f.mission_id = m.id
+      WHERE NOT EXISTS (
+        SELECT 1
+        FROM assignments a
+        WHERE m.id = a.mission_id
+        AND wl.user_id = a.user_id
+      )
+    SQL
+  end
+end
