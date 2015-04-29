e2d0b0a0f67b942f14f0c3b69ff9d5e0d2506b51
Tom Smyth <tom@sassafras.coop>
2015-04-29 14:58:29 -0400

    Fixed reset password migration.

--- db/migrate/20150408182742_reset_user_password.rb
+++ db/migrate/20150408182742_reset_user_password.rb
@@ -1,8 +1,9 @@
 class ResetUserPassword < ActiveRecord::Migration
   def change
+    # Standards have increased so we need to reset everyone's password.
     User.all.each do |user|
-      user.password = User.random_password
-      user.save
+      user.password = user.password_confirmation = User.random_password
+      user.save(validate: false)
     end
   end
 end
