61addc86b058581cbe829686e9699494ee7696ae
Tom Smyth <tom@sassafras.coop>
2015-04-29 13:44:27 -0400

    2169: Removed hidden mission_id field from settings form

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -46,6 +46,7 @@ class SettingsController < ApplicationController
     end
 
     def setting_params
-      params.require(:setting).permit(:mission_id, :timezone, :preferred_locales_str, :allow_unauthenticated_submissions, :incoming_sms_number, :outgoing_sms_adapter, :intellisms_username, :intellisms_password1, :intellisms_password2)
+      params.require(:setting).permit(:timezone, :preferred_locales_str, :allow_unauthenticated_submissions,
+        :incoming_sms_number, :outgoing_sms_adapter, :intellisms_username, :intellisms_password1, :intellisms_password2)
     end
 end
