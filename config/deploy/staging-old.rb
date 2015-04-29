7dd2540604c1f5612d5ecc191a12521dcf0a5ac2
Tom Smyth <tom@sassafras.coop>
2015-04-29 11:39:12 -0400

    Adjusted deploy config for new staging server

--- config/deploy/staging-old.rb
+++ config/deploy/staging-old.rb
@@ -1,21 +1,21 @@
-set :branch, 'staging'
+set :branch, 'staging_old'
 set :ping_url, "https://secure1.cceom.org"
 set :user, 'cceom'
 set :home_dir, '/home/cceom'
 set :use_sudo, false
-set(:deploy_to) {"#{home_dir}/webapps/elmo_rails/#{stage}"}
+set(:deploy_to) {"#{home_dir}/webapps/rails2/elmo_staging"}
 set :default_environment, {
-  "PATH" => "$PATH:$HOME/bin:$HOME/webapps/elmo_rails/bin",
-  "GEM_HOME" => "$HOME/webapps/elmo_rails/gems"
+  "PATH" => "$PATH:$HOME/bin:$HOME/webapps/rails2/bin",
+  "GEM_HOME" => "$HOME/webapps/rails2/gems"
 }
 
-server 'staging.getelmo.org', :app, :web, :db, :primary => true
+server 'cceom.org', :app, :web, :db, :primary => true
 
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} server"
     task command, roles: :app, except: {no_release: true} do
-      run "#{home_dir}/webapps/elmo_rails/bin/#{command}"
+      run "#{home_dir}/webapps/rails2/bin/#{command}"
     end
   end
 end
