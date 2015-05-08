2f8605bd8280809aeeb8a42089d3a3af2f6f0519
Tom Smyth <tom@sassafras.coop>
2015-05-08 11:15:33 -0400

    2172: Moved logout spec

--- spec/requests/logout_spec.rb
+++ spec/requests/logout_spec.rb
@@ -1,36 +1,32 @@
 require 'spec_helper'
 
 describe 'Logout' do
+  let(:user) { create(:user, admin: true) }
 
   before do
-    @user = create(:user, :admin => true)
+    login(user)
   end
 
   it "redirect after logout from basic mode should be correct" do
-    login(@user)
     check_logout_link_and_redirect
   end
 
   it "redirect after logout from mission mode should be correct" do
-    login(@user)
-    get("/en/m/#{get_mission.compact_name}")
-    expect(response).to be_success
+    get_s("/en/m/#{get_mission.compact_name}")
     check_logout_link_and_redirect
   end
 
   it "redirect after logout from admin mode should be correct" do
-    login(@user)
-    get('/en/admin')
-    expect(response).to be_success
+    get_s('/en/admin')
     check_logout_link_and_redirect
   end
 
   private
 
   def check_logout_link_and_redirect
-    assert_select('#logout_button[href=/en/logout]', true)
+    assert_select('#logout_button[href="/en/logout"]', true)
     delete('/en/logout')
-    assert_redirected_to('/en/logged-out')
+    expect(response).to redirect_to('/en/logged-out')
   end
 end
 
