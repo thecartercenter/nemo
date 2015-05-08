2087cba5a10e83c49357e394fb3314b4b55c8feb
Tom Smyth <tom@sassafras.coop>
2015-05-08 11:18:00 -0400

    2172: Moved response checkout spec

--- spec/requests/response_checkout_spec.rb
+++ spec/requests/response_checkout_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
-describe 'ResponseCheckout' do
-
+describe 'response checkout' do
   it "user should get a notice when response is locked by another user" do
     user = get_user
-    user_b = create(:user, :name => "first user to lock")
+    user_b = create(:user, name: "first user to lock")
     resp = create(:response)
 
     # checkout response by another user
@@ -12,17 +11,15 @@ describe 'ResponseCheckout' do
 
     # login with user and edit response
     login(user)
-    get("/en/m/#{get_mission.compact_name}/responses/#{resp.id}/edit")
-    assert_response :success
+    get_s("/en/m/#{get_mission.compact_name}/responses/#{resp.id}/edit")
 
     # check for warning that response was checked out by another user
-    assert flash[:notice]
-    assert_equal user_b.name, flash[:notice].gsub(/#{I18n.t("response.checked_out")} /, '')
+    expect(flash[:notice].gsub(/#{I18n.t("response.checked_out")} /, '')).to eq(user_b.name)
   end
 
   it "user should not get a notice when a checked out response is no longer valid" do
     user = get_user
-    user_b = create(:user, :name => "first user to lock")
+    user_b = create(:user, name: "first user to lock")
     resp = create(:response)
 
     # go just outside the valid lock time and checkout response by another user
@@ -32,10 +29,9 @@ describe 'ResponseCheckout' do
 
     # login with user and edit response
     login(user)
-    get("/en/m/#{get_mission.compact_name}/responses/#{resp.id}/edit")
-    assert_response :success
+    get_s("/en/m/#{get_mission.compact_name}/responses/#{resp.id}/edit")
 
     # check that there is no warning
-    assert_nil flash[:notice]
+    expect(flash[:notice]).to be_nil
   end
 end
