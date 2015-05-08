5c5b7651eb0b218b5cb1e3536ccbbd04f919ad88
Tom Smyth <tom@sassafras.coop>
2015-05-08 12:10:31 -0400

    2172: Moved SMS message spec

--- spec/models/sms/message_spec.rb
+++ spec/models/sms/message_spec.rb
@@ -2,21 +2,18 @@ require 'spec_helper'
 
 describe Sms::Message do
   it "creating a message should work" do
-    m = Sms::Reply.create!(:to => "14045551212", :body => "blah")
-
-    # reload to make sure serialization is working right
-    m.reload
-
+    m = Sms::Reply.create!(to: "14045551212", body: "blah")
+    m.reload # reload to make sure serialization is working right
     expect(m.to).to eq("+14045551212")
   end
 
   it "a message with no sent_at should default to now, but only when saved" do
-    m = Sms::Reply.create(:to => "14045551212", :body => "blah")
+    m = Sms::Reply.create(to: "14045551212", body: "blah")
     expect(Time.zone.now - m.sent_at < 5.seconds).to be true
   end
 
   it "a textual from field should be preserved" do
-    m = Sms::Reply.new(:from => "foo", :body => "blah")
+    m = Sms::Reply.new(from: "foo", body: "blah")
     expect(m.from).to eq("foo")
   end
 
@@ -24,7 +21,7 @@ describe Sms::Message do
     # this is just to be sure the index of length 160 doesn't mess anything up
     long = "laksdjf alsdkjf lasdfkjaf laskdsf aslkfsjflsakfda lsakl fakjsdlkfaj lskjf alksj " +
       "dfalkj sdlfaks asdaksjdafa alaskdfal alkajdasfa alsjd alasksdjf alks alfdsafdslf l lajslfdjfalsdkf dslads"
-    Sms::Reply.create(:from => "foo", :body => long)
+    Sms::Reply.create(from: "foo", body: long)
     expect(Sms::Reply.find_by_body(long + "a")).to be_nil
   end
 
