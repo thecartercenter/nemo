c743bbf9acfe33f4c6c255860a3019f61eb6c67d
Tom Smyth <tom@sassafras.coop>
2015-05-08 11:47:33 -0400

    2172: Moved odk helper spec

--- spec/helpers/odk_helper_spec.rb
+++ spec/helpers/odk_helper_spec.rb
@@ -1,17 +1,13 @@
 require 'spec_helper'
 
 describe OdkHelper do
-
   it "#required_value returns true for forms that don't allow incomplete responses" do
-    f = create(:form, :allow_incomplete => false)
-
-    expect(required_value(f)).to eq('true()')
+    f = create(:form, allow_incomplete: false)
+    expect(helper.required_value(f)).to eq('true()')
   end
 
   it "#required_value returns ODK select statement for forms that allow incomplete responses" do
-    f = create(:form, :allow_incomplete => true)
-
-    expect(required_value(f)).to eq("selected\(/data/#{IR_QUESTION}, 'no')")
+    f = create(:form, allow_incomplete: true)
+    expect(helper.required_value(f)).to eq("selected\(/data/#{OdkHelper::IR_QUESTION}, 'no')")
   end
-
 end
