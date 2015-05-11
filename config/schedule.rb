b36f83161af118c51941215afd83425919442056
Tom Smyth <tom@sassafras.coop>
2015-05-11 17:02:27 -0400

    Another whenever fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,11 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
 set :output, "log/cron.log"
 env :PATH, ENV['PATH']
 env :GEM_HOME, ENV['GEM_HOME']
 
 every 1.hour do
   # redo the indexes
-  rake_with_env "ts:index"
+  rake "ts:index"
 
   # make sure the daemon is running
-  rake_with_env "ts:start"
+  rake "ts:start"
 end
