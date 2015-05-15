8ed8504e4064102c29035f037b0482e88137a670
Mike Dillon <mike@appropriate.io>
2015-05-15 16:56:37 -0700

    2168: Enable validation for X-Twilio-Signature

--- app/models/sms/adapters/twilio_adapter.rb
+++ app/models/sms/adapters/twilio_adapter.rb
@@ -17,14 +17,10 @@ class Sms::Adapters::TwilioAdapter < Sms::Adapters::Adapter
   def receive(request)
     params = request.request_parameters.merge(request.query_parameters)
 
-    # TODO: This code needs access to the auth token of the Twilio account being
-    # used for the current mission; the validator could likely be created in
-    # initialize
-    #
-    #validator = Twilio::Util::RequestValidator.new current_mission.setting.twilio_auth_token
-    #unless validator.validate(request.original_url, params, request.headers['X-Twilio-Signature'])
-    #  raise Sms::Error.new("Could not validate incoming Twilio message from #{params[:From]}")
-    #end
+    validator = Twilio::Util::RequestValidator.new configatron.twilio_auth_token
+    unless validator.validate(request.original_url, params, request.headers['X-Twilio-Signature'])
+      raise Sms::Error.new("Could not validate incoming Twilio message from #{params[:From]}")
+    end
 
     # create and return the message
     Sms::Incoming.create(
