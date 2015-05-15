64411c93ba21e100842595d1eee577dd098b43b2
Mike Dillon <mike@appropriate.io>
2015-05-15 14:02:14 -0700

    2168: Support incoming Twilio messages

--- app/models/sms/adapters/twilio_adapter.rb
+++ app/models/sms/adapters/twilio_adapter.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class Sms::Adapters::TwilioAdapter < Sms::Adapters::Adapter
   # checks if this adapter recognizes an incoming http receive request
   def self.recognize_receive_request?(request)
@@ -12,8 +14,16 @@ class Sms::Adapters::TwilioAdapter < Sms::Adapters::Adapter
     raise NotImplementedError
   end
 
-  def receive(params)
-    raise NotImplementedError
+  def receive(request)
+    params = request.request_parameters.merge(request.query_parameters)
+
+    # create and return the message
+    Sms::Incoming.create(
+      :from => params[:From],
+      :to => params[:To],
+      :body => params[:Body],
+      :sent_at => Time.zone.now, # Twilio doesn't supply this
+      :adapter_name => service_name)
   end
 
   # Check_balance returns the balance string. Raises error if balance check failed.
