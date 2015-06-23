675587212297d33ac7b25ad1e6240886adb41c0d
Tom Smyth <tom@sassafras.coop>
2015-06-23 17:09:24 -0400

    2507: Fixed old migration

--- db/migrate/20150106145459_move_form_questionings_to_root_group.rb
+++ db/migrate/20150106145459_move_form_questionings_to_root_group.rb
@@ -11,7 +11,7 @@ end
 class MoveFormQuestioningsToRootGroup < ActiveRecord::Migration
   def up
     Form.all.each do |form|
-      group = QingGroup.create!
+      group = QingGroup.create(form_id: form.id, mission_id: form.mission_id)
       form.update_attribute(:root_id, group.id)
       # setting ancestry fields by hand so we can do them in bulk
       form.questionings.update_all(ancestry: group.id, ancestry_depth: 1)
