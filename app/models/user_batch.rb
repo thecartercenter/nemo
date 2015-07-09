23af4c50f6eae218ef6a96c1a586f09548fa2557
Mike Dillon <mike@appropriate.io>
2015-07-09 15:36:16 -0700

    2343: Stop processing spreadsheet after too many errors

--- app/models/user_batch.rb
+++ app/models/user_batch.rb
@@ -3,6 +3,8 @@ class UserBatch
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
+  IMPORT_ERROR_CUTOFF = 50
+
   attr_accessor :file
   attr_reader :users
 
@@ -58,6 +60,19 @@ class UserBatch
 
       # iterate over each row, creating users as we go
       data.each_row_streaming(offset: data.first_row).with_index do |row,row_index|
+        # excel row numbers are 1-indexed
+        row_number = 1 + data.first_row + row_index
+
+        # stop processing rows after a certain number of errors
+        # (we do this at the beginning of the loop to avoid adding a redundant
+        # :too_many_errors error on the last loop iteration)
+        if errors.count >= IMPORT_ERROR_CUTOFF
+          # we pass the previous row number to the error message formatting
+          # since the error count cutoff was surpassed by the previous rows,
+          # not this row
+          errors.add(:users, :too_many_errors, row: row_number - 1)
+          break
+        end
 
         # skip blank rows
         next if row.all?(&:blank?)
@@ -77,7 +92,7 @@ class UserBatch
         if !user.valid?
           user.errors.keys.each do |attribute|
             user.errors.full_messages_for(attribute).each do |error|
-              row_error = I18n.t('user_batch.row_error', row: data.first_row + row_index + 1, error: error)
+              row_error = I18n.t('user_batch.row_error', row: row_number, error: error)
               errors.add("users[#{row_index}].#{attribute}", row_error)
             end
           end
@@ -85,8 +100,6 @@ class UserBatch
         end
 
         users << user
-
-        # TODO: stop processing rows after a certain number of bad rows
 
       end # iteration over rows
 
