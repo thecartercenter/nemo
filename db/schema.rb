3345330f7a7f5fcf957f979183ae2ef1d227988d
Mike Dillon <mike@appropriate.io>
2015-07-11 06:30:19 -0700

    2337: Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710150907) do
+ActiveRecord::Schema.define(version: 20150710212020) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "response_id",    limit: 4
