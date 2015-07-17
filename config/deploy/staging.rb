8b34aab0ea59dbe758638cdcb44f6ab30a4e2d7b
Tom Smyth <tom@sassafras.coop>
2015-07-17 17:58:58 -0400

    Fixed staging deploy config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,6 +4,7 @@ set :user, 'cceom'
 set :home_dir, '/home/cceom'
 set :use_sudo, false
 set(:deploy_to) {"#{home_dir}/webapps/elmo_rails/#{stage}"}
+set(:bundle_dir) {"#{home_dir}/webapps/elmo_rails/gems"}
 set :default_environment, {
   "PATH" => "$HOME/bin:$HOME/webapps/elmo_rails/bin:$PATH",
   "GEM_HOME" => "$HOME/webapps/elmo_rails/gems"
