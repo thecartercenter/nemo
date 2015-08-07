ecb7bfe428051321d46fcafa5b33a40595133f1c
lmatiolis <matioli.leandro@gmail.com>
2015-08-07 13:17:09 -0300

    2783: Fix <br> tag appearing on sms index.
    The problem is that the string wasn't being tagged as html_safe.
    To avoid these kind of mistakes of just forgetting to call it, we
    can change the I18n key to end with _html and that automatically
    sets it as html_safe.
    
    Conflicts:
    
    	app/helpers/sms_helper.rb

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -11,14 +11,15 @@ module SmsHelper
     when "time" then
       if sms.sent_at <= sms.created_at - 1.minute
         time_diff = time_diff(sms.sent_at, sms.created_at)
-        t('sms.timestamp_with_diff', time: l(sms.created_at), time_diff: time_diff)
+        t('sms.timestamp_with_diff_html', time: l(sms.created_at), time_diff: time_diff)
       else
         l(sms.created_at)
       end
     when "to" then
       recips = sms.recipient_hashes(max: MAX_RECIPS_TO_SHOW).map { |r| user_with_phone(r[:user], r[:phone]) }.join('<br/>')
       extra_recipients = sms.recipient_count - MAX_RECIPS_TO_SHOW
-      recips << (extra_recipients > 0 ? t('sms.extra_recipients', count: extra_recipients) : '')
+      recips << (extra_recipients > 0 ? t('sms.extra_recipients_html', count: extra_recipients) : '')
+      recips
     when "from" then
       user_with_phone sms.sender, sms.from
     else sms.send(field)
