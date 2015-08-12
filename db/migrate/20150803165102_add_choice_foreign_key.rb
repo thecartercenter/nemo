33ba82caff44d39126c807b02647c47610fe4be4
lmatiolis <matioli.leandro@gmail.com>
2015-08-12 09:48:42 -0300

    2283: Fix previous migration (remove FK before adding it).
    This is necessary because the staging database already have it. Since
    the local one will not have it, a rescue is also needed.

--- db/migrate/20150803165102_add_choice_foreign_key.rb
+++ db/migrate/20150803165102_add_choice_foreign_key.rb
@@ -1,5 +1,6 @@
 class AddChoiceForeignKey < ActiveRecord::Migration
   def change
+    remove_foreign_key "choices", "answers" rescue "No key to remove..."
     add_foreign_key "choices", "answers"
   end
 end
