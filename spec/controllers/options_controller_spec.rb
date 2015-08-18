2722933f34acb02a9c01235ef2e48a5d8a913fa7
lmatiolis <matioli.leandro@gmail.com>
2015-08-18 20:05:14 -0300

    Fix spec params order check after schema was reordered.

--- spec/controllers/options_controller_spec.rb
+++ spec/controllers/options_controller_spec.rb
@@ -12,6 +12,6 @@ describe OptionsController, type: :request do
   it 'should return matching suggestions' do
     # The response is not super correct (e.g. id = null) but all we want to
     # test is that the controller calls suggestions and converts to json
-    expect(response.body).to eq '[{"id":null,"name_translations":{"en":"foo"},"latitude":null,"longitude":null,"name":"foo","set_names":"","in_use?":false}]'
+    expect(response.body).to eq '[{"id":null,"latitude":null,"longitude":null,"name_translations":{"en":"foo"},"name":"foo","set_names":"","in_use?":false}]'
   end
 end
