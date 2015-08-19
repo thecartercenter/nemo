ba17a37ef17d213fed0ca76de1d2046b2cfb59e9
Tom Smyth <tom@sassafras.coop>
2015-08-19 15:48:19 -0400

    Fixed canonical name migration

--- db/migrate/20150806164940_change_options_canonical_name_to_not_null.rb
+++ db/migrate/20150806164940_change_options_canonical_name_to_not_null.rb
@@ -1,5 +1,8 @@
 class ChangeOptionsCanonicalNameToNotNull < ActiveRecord::Migration
   def change
+    [Option, Question].each do |k|
+      k.where(canonical_name: nil).each{ |o| o.update_attribute(:canonical_name, o.name) }
+    end
     change_column_null :options, :canonical_name, false
   end
 end
