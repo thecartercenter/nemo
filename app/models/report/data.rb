95c9ab6065306649e96683c96189d4a39d3a73c7
lmatiolis <matioli.leandro@gmail.com>
2015-09-28 16:34:44 -0300

    3172: Limit ListReport rows to optimize speed.
    On a system with many responses, it was generating a huge table
    that was heavy for the browser (and slow). We decided to just
    limit the results for now and show a total count.

--- app/models/report/data.rb
+++ app/models/report/data.rb
@@ -2,6 +2,7 @@
 class Report::Data
 
   attr_accessor :rows
+  attr_accessor :total_rows_count
   attr_reader :totals
 
   def initialize(rows)
@@ -51,6 +52,6 @@ class Report::Data
   end
 
   def as_json(options = {})
-    {:rows => @rows, :totals => @totals}
+    {rows: @rows, totals: @totals, total_rows_count: @total_rows_count}
   end
 end
