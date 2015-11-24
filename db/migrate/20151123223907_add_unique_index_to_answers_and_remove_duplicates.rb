a95c838b257eb65e684e0f3b56cdd96a0f5ffcfc
Tom Smyth <tom@sassafras.coop>
2015-11-23 19:57:40 -0500

    3342: Added unique index on answer

---
+++ db/migrate/20151123223907_add_unique_index_to_answers_and_remove_duplicates.rb
@@ -0,0 +1,12 @@
+class AddUniqueIndexToAnswersAndRemoveDuplicates < ActiveRecord::Migration
+  def change
+    ids = execute("SELECT GROUP_CONCAT(id) FROM answers
+      GROUP BY response_id, questioning_id, rank HAVING COUNT(*) > 1").to_a
+    if ids.any?
+      ids = ids.map{ |arr| arr[0].split(",")[1..-1] }.flatten.join(",")
+      execute("DELETE FROM choices WHERE answer_id IN (#{ids})")
+      execute("DELETE FROM answers WHERE id IN (#{ids})")
+    end
+    add_index :answers, [:response_id, :questioning_id, :rank]
+  end
+end
