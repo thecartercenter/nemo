bc11acd1732305bd3f05c8ada6c71543d50b952e
Melody <meltheadorable@gmail.com>
2016-01-22 13:22:52 -0500

    3955: Updates response model to require form on mission

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,28 +1,37 @@
-require 'xml'
+require "xml"
 class Response < ActiveRecord::Base
   include MissionBased
   include Cacheable
 
   LOCK_OUT_TIME = 10.minutes
 
-  belongs_to(:form, :inverse_of => :responses, :counter_cache => true)
-  belongs_to(:checked_out_by, :class_name => "User")
-  has_many(:answers, -> { order('form_items.rank, answers.rank').includes(:questioning) }, autosave: true, dependent: :destroy, inverse_of: :response)
-  belongs_to(:user, :inverse_of => :responses)
+  belongs_to(:form, inverse_of: :responses, counter_cache: true)
+  belongs_to(:checked_out_by, class_name: "User")
+  has_many(:answers,
+    -> { order("form_items.rank, answers.rank").includes(:questioning) },
+    autosave: true,
+    dependent: :destroy,
+    inverse_of: :response
+  )
+  belongs_to(:user, inverse_of: :responses)
 
-  has_many(:location_answers, -> { where("questions.qtype_name = 'location'").order('form_items.rank').includes(questioning: :question) }, class_name: 'Answer')
+  has_many(:location_answers,
+    -> { where("questions.qtype_name = 'location'").order("form_items.rank").includes(questioning: :question) },
+    class_name: "Answer"
+  )
 
   attr_accessor(:modifier, :excerpts)
 
   # we turn off validate above and do it here so we can control the message and have only one message
   # regardless of how many answer errors there are
-  validates(:user, :presence => true)
+  validates(:user, presence: true)
   validate(:no_missing_answers)
+  validate(:form_in_mission)
 
   # This scope is DEPRECATED. Do not rely on it. Instead, call Response.unscoped and apply your own.
   default_scope( -> { includes(:form, :user).order("responses.created_at DESC") })
-  scope(:unreviewed, -> { where(:reviewed => false) })
-  scope(:by, ->(user) { where(:user_id => user.id) })
+  scope(:unreviewed, -> { where(reviewed: false) })
+  scope(:by, ->(user) { where(user_id: user.id) })
   scope :created_after, ->(date) { where("responses.created_at >= ?", date) }
   scope :created_before, ->(date) { where("responses.created_at <= ?", date) }
 
@@ -43,21 +52,21 @@ class Response < ActiveRecord::Base
   scope(:with_basic_assoc, -> { includes(:form, :user) })
 
   # loads only some answer info
-  scope(:with_basic_answers, -> { includes(:answers => {:questioning => :question}) })
+  scope(:with_basic_answers, -> { includes(answers: {questioning: :question}) })
 
   # loads only answers with location info
   scope(:with_location_answers, -> { includes(:location_answers) })
 
   accepts_nested_attributes_for(:answers)
 
-  delegate :name, :to => :checked_out_by, :prefix => true
+  delegate :name, to: :checked_out_by, prefix: true
   delegate :visible_questionings, to: :form
 
   # remove previous checkouts by a user
   def self.remove_previous_checkouts_by(user = nil)
     raise ArguementError, "A user is required" unless user
 
-    Response.where(:checked_out_by_id => user).update_all(:checked_out_at => nil, :checked_out_by_id => nil)
+    Response.where(checked_out_by_id: user).update_all(checked_out_at: nil, checked_out_by_id: nil)
   end
 
   # gets the list of fields to be searched for this class
@@ -65,29 +74,38 @@ class Response < ActiveRecord::Base
   # and whether they are searchable by a regular expression
   def self.search_qualifiers(scope)
     [
-      Search::Qualifier.new(:name => "form", :col => "forms.name", :assoc => :forms, :type => :text),
-      Search::Qualifier.new(:name => "reviewed", :col => "responses.reviewed"),
-      Search::Qualifier.new(:name => "submitter", :col => "users.name", :assoc => :users, :type => :text),
-      Search::Qualifier.new(:name => "source", :col => "responses.source"),
-      Search::Qualifier.new(:name => "submit_date", :col => "DATE(CONVERT_TZ(responses.created_at, 'UTC', '#{Time.zone.mysql_name}'))", :type => :scale),
+      Search::Qualifier.new(name: "form", col: "forms.name", assoc: :forms, type: :text),
+      Search::Qualifier.new(name: "reviewed", col: "responses.reviewed"),
+      Search::Qualifier.new(name: "submitter", col: "users.name", assoc: :users, type: :text),
+      Search::Qualifier.new(name: "source", col: "responses.source"),
+      Search::Qualifier.new(
+        name: "submit_date",
+        col: "DATE(CONVERT_TZ(responses.created_at, 'UTC', '#{Time.zone.mysql_name}'))",
+        type: :scale
+      ),
 
       # this qualifier matches responses that have answers to questions with the given option set
-      Search::Qualifier.new(:name => "option_set", :col => "option_sets.name", :assoc => :option_sets, :type => :text),
+      Search::Qualifier.new(name: "option_set", col: "option_sets.name", assoc: :option_sets, type: :text),
 
       # this qualifier matches responses that have answers to questions with the given type
       # this and other qualifiers use the 'questions' table because the join code below creates a table alias
       # the actual STI table name is 'questions'
-      Search::Qualifier.new(:name => "question_type", :col => "questions.qtype_name", :assoc => :questions),
+      Search::Qualifier.new(name: "question_type", col: "questions.qtype_name", assoc: :questions),
 
       # this qualifier matches responses that have answers to the given question
-      Search::Qualifier.new(:name => "question", :col => "questions.code", :assoc => :questions, :type => :text),
+      Search::Qualifier.new(name: "question", col: "questions.code", assoc: :questions, type: :text),
 
       # this qualifier inserts a placeholder that we replace later
-      Search::Qualifier.new(:name => "text", :col => "responses.id", :type => :indexed, :default => true),
+      Search::Qualifier.new(name: "text", col: "responses.id", type: :indexed, default: true),
 
       # support {foobar}:stuff style searches, where foobar is a question code
-      Search::Qualifier.new(:name => "text_by_code", :pattern => /\A\{(#{Question::CODE_FORMAT})\}\z/, :col => "responses.id",
-        :type => :indexed, :validator => ->(md){ Question.exists?(:mission_id => scope[:mission].id, :code => md[1]) })
+      Search::Qualifier.new(
+        name: "text_by_code",
+        pattern: /\A\{(#{Question::CODE_FORMAT})\}\z/,
+        col: "responses.id",
+        type: :indexed,
+        validator: ->(md) { Question.exists?(mission_id: scope[:mission].id, code: md[1]) }
+      )
     ]
   end
 
@@ -95,14 +113,16 @@ class Response < ActiveRecord::Base
   # relation - a Response relation upon which to build the search query
   # query - the search query string (e.g. form:polling text:interference, tomfoolery)
   # scope - the scope to pass to the search qualifiers generator
-  # options[:include_excerpts] - if true, execute the query and return the results with answer excerpts (if applicable) included;
+  # options[:include_excerpts] - if true, execute the query and return the results
+  #   with answer excerpts (if applicable) included;
   #   if false, doesn't execute the query and just returns the relation
-  # options[:dont_truncate_excerpts] - if true, excerpt length limit is very high, so full answer is returned with matches highlighted
+  # options[:dont_truncate_excerpts] - if true, excerpt length limit is very high,
+  #   so full answer is returned with matches highlighted
   def self.do_search(relation, query, scope, options = {})
     options[:include_excerpts] ||= false
 
     # create a search object and generate qualifiers
-    search = Search::Search.new(:str => query, :qualifiers => search_qualifiers(scope))
+    search = Search::Search.new(str: query, qualifiers: search_qualifiers(scope))
 
     # apply the needed associations
     relation = relation.joins(Report::Join.list_to_sql(search.associations))
@@ -119,14 +139,14 @@ class Response < ActiveRecord::Base
 
       # search all answers in this mission for a match
       # not escaping the query value because double quotes were getting escaped which makes exact phrase not work
-      attribs = {:mission_id => scope[:mission].id}
+      attribs = {mission_id: scope[:mission].id}
 
       if expression.qualifier.name == "text_by_code"
         # get qualifier text (e.g. {form}) and strip outer braces
         question_code = expression.qualifier_text[1..-2]
 
         # get the question with the given code
-        question = Question.where(:mission_id => scope[:mission].id).where(:code => question_code).first
+        question = Question.where(mission_id: scope[:mission].id).where(code: question_code).first
 
         # raising here since this shouldn't happen due to validator
         raise "question with code '#{question_code}' not found" if question.nil?
@@ -136,7 +156,7 @@ class Response < ActiveRecord::Base
       end
 
       # save the search params as we'll need them again
-      sphinx_params = [expression.values, {:with => attribs, :max_matches => 1000000, :per_page => 1000000}]
+      sphinx_params = [expression.values, {with: attribs, max_matches: 1000000, per_page: 1000000}]
       sphinx_param_sets << sphinx_params
 
       # run the sphinx search
@@ -145,11 +165,11 @@ class Response < ActiveRecord::Base
       # turn into an sql fragment
       fragment = if answer_ids.present?
         # Get all response IDs and join into string
-        Answer.select('response_id').distinct.where('id IN (?)', answer_ids).map(&:response_id).join(',')
+        Answer.select("response_id").distinct.where("id IN (?)", answer_ids).map(&:response_id).join(",")
       end
 
       # fall back to '0' if we get an empty fragment
-      fragment.presence || '0'
+      fragment.presence || "0"
     end
 
     # apply the conditions
@@ -169,20 +189,24 @@ class Response < ActiveRecord::Base
 
           # run search again
           sphinx_params[1][:with][:response_id] = responses_by_id.keys
-          sphinx_params[1][:sql] = {:include => {:questioning => :question}}
+          sphinx_params[1][:sql] = {include: {questioning: :question}}
           answers = Answer.search(*sphinx_params)
 
-          excerpter_options = {:before_match => '{{{', :after_match => '}}}', :chunk_separator => ' ... ', :query_mode => true}
+          excerpter_options = {before_match: "{{{", after_match: "}}}", chunk_separator: " ... ", query_mode: true}
           excerpter_options[:limit] = 1000000 if options[:dont_truncate_excerpts]
 
           # create excerpter
-          excerpter = ThinkingSphinx::Excerpter.new('answer_core', sphinx_params[0], excerpter_options)
+          excerpter = ThinkingSphinx::Excerpter.new("answer_core", sphinx_params[0], excerpter_options)
 
           # for each matching answer, add to excerpt to appropriate response
           answers.each do |a|
             r = responses_by_id[a.response_id]
             r.excerpts ||= []
-            r.excerpts << {:questioning_id => a.questioning_id, :code => a.questioning.code, :text => excerpter.excerpt!(a.value || a.option_name || a.option_names)}
+            r.excerpts << {
+              questioning_id: a.questioning_id,
+              code: a.questioning.code,
+              text: excerpter.excerpt!(a.value || a.option_name || a.option_names)
+            }
           end
         end
       end
@@ -224,7 +248,7 @@ class Response < ActiveRecord::Base
   # generates a cache key for the set of all responses for the given mission.
   # the key will change if the number of responses changes, or if a response is updated.
   def self.per_mission_cache_key(mission)
-    count_and_date_cache_key(:rel => unscoped.for_mission(mission), :prefix => "mission-#{mission.id}")
+    count_and_date_cache_key(rel: unscoped.for_mission(mission), prefix: "mission-#{mission.id}")
   end
 
   # We need a name field so that this class matches the Nameable duck type.
@@ -235,21 +259,21 @@ class Response < ActiveRecord::Base
   # whether the answers should validate themselves
   def validate_answers?
     # dont validate if this is an ODK submission as we don't want to lose data
-    modifier != 'odk'
+    modifier != "odk"
   end
 
   def populate_from_odk(xml)
     # Response mission should already be set
     raise "Submissions must have a mission" if mission.nil?
 
-    self.source = 'odk'
+    self.source = "odk"
 
     data = XML::Parser.string(xml).parse.root
 
-    lookup_and_check_form(:id => data['id'], :version => data['version'])
+    lookup_and_check_form(id: data["id"], version: data["version"])
 
     # Loop over each child tag and create hash of odk_code => value
-    hash = Hash[*data.children.map{|c| [c.name, c.first.try(:content)]}.flatten(1)]
+    hash = Hash[*data.children.map { |c| [c.name, c.first.try(:content)] }.flatten(1)]
 
     populate_from_hash(hash)
   end
@@ -258,9 +282,9 @@ class Response < ActiveRecord::Base
     # Response mission should already be set
     raise "Submissions must have a mission" if mission.nil?
 
-    self.source = 'j2me'
+    self.source = "j2me"
 
-    lookup_and_check_form(:id => data.delete('id'), :version => data.delete('version'))
+    lookup_and_check_form(id: data.delete("id"), version: data.delete("version"))
 
     # Get rid of other unneeded keys.
     data = data.except(*%w(uiVersion name xmlns xmlns:jrm))
@@ -271,7 +295,7 @@ class Response < ActiveRecord::Base
   def answer_set_for_questioning(questioning)
     # Build a hash of answer sets on the first call.
     @answer_sets_by_questioning ||= {}.tap do |hash|
-      answers.group_by(&:questioning).each{ |q, a| hash[q] = AnswerSet.new(questioning: q, answers: a) }
+      answers.group_by(&:questioning).each { |q, a| hash[q] = AnswerSet.new(questioning: q, answers: a) }
     end
 
     # If answer set already exists, it will be in the answer_sets_by_questioning hash, else create a new one.
@@ -293,7 +317,7 @@ class Response < ActiveRecord::Base
   # Returns an array of required questionings for which answers are missing.
   # Used in cases other than the web form where answer objects may not be created for missing answers.
   def missing_answers
-    @missing_answers ||= visible_questionings.select{ |qing| qing.required? && answer_for_qing(qing).nil? }
+    @missing_answers ||= visible_questionings.select { |qing| qing.required? && answer_for_qing(qing).nil? }
   end
 
   # if this response contains location questions, returns the gps location (as a 2 element array)
@@ -305,7 +329,7 @@ class Response < ActiveRecord::Base
 
   # indexes excerpts by questioning_id
   def excerpts_by_questioning_id
-    @excerpts_by_questioning_id ||= (excerpts || []).index_by{|e| e[:questioning_id]}
+    @excerpts_by_questioning_id ||= (excerpts || []).index_by { |e| e[:questioning_id] }
   end
 
   def check_out_valid?
@@ -343,35 +367,39 @@ class Response < ActiveRecord::Base
   end
 
   private
-    def no_missing_answers
-      errors.add(:base, :missing_answers) unless missing_answers.empty? || incomplete?
-    end
+  def no_missing_answers
+    errors.add(:base, :missing_answers) unless missing_answers.empty? || incomplete?
+  end
 
-    # Checks if form ID and version were given, if form exists, and if version is correct
-    def lookup_and_check_form(params)
-      # if either of these is nil or not an integer, error
-      raise SubmissionError.new("no form id was given") if params[:id].nil?
-      raise FormVersionError.new("form version must be specified") if params[:version].nil?
+  def form_in_mission
+    errors.add(:form, :form_unavailable) unless mission.forms.include?(form)
+  end
 
-      # try to load form (will raise activerecord error if not found)
-      self.form = Form.find(params[:id])
+  # Checks if form ID and version were given, if form exists, and if version is correct
+  def lookup_and_check_form(params)
+    # if either of these is nil or not an integer, error
+    raise SubmissionError.new("no form id was given") if params[:id].nil?
+    raise FormVersionError.new("form version must be specified") if params[:version].nil?
 
-      # if form has no version, error
-      raise "xml submissions must be to versioned forms" if form.current_version.nil?
+    # try to load form (will raise activerecord error if not found)
+    self.form = Form.find(params[:id])
 
-      # if form version is outdated, error
-      raise FormVersionError.new("form version is outdated") if form.current_version.sequence > params[:version].to_i
-    end
+    # if form has no version, error
+    raise "xml submissions must be to versioned forms" if form.current_version.nil?
 
-    # Populates response given a hash of odk-style question codes (e.g. q5, q7_1) to string values.
-    def populate_from_hash(hash)
-      form.visible_questionings.each do |qing|
-        qing.subquestions.each do |subq|
-          answer = Answer.new(questioning: qing, rank: subq.rank)
-          answer.populate_from_string(hash[subq.odk_code])
-          self.answers << answer
-        end
+    # if form version is outdated, error
+    raise FormVersionError.new("form version is outdated") if form.current_version.sequence > params[:version].to_i
+  end
+
+  # Populates response given a hash of odk-style question codes (e.g. q5, q7_1) to string values.
+  def populate_from_hash(hash)
+    form.visible_questionings.each do |qing|
+      qing.subquestions.each do |subq|
+        answer = Answer.new(questioning: qing, rank: subq.rank)
+        answer.populate_from_string(hash[subq.odk_code])
+        self.answers << answer
       end
-      self.incomplete = (hash[OdkHelper::IR_QUESTION] == 'yes')
     end
+    self.incomplete = (hash[OdkHelper::IR_QUESTION] == "yes")
+  end
 end
