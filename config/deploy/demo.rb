ea9efd226ddebc59cfdcf1f3f90548220531ace7
Tom Smyth <tom@sassafras.coop>
2016-03-05 14:38:57 -0500

    Moved demo deploy config to rails4 on webfaction

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -3,10 +3,11 @@ set :ping_url, "https://elmo.sassafras.coop"
 set :user, 'tomsmyth'
 set :home_dir, '/home/tomsmyth'
 set :use_sudo, false
-set(:deploy_to) {"#{home_dir}/webapps/rails2/#{application}_#{stage}"}
+set(:deploy_to) {"#{home_dir}/webapps/rails4/elmo_demo"}
+set(:bundle_dir) {"#{home_dir}/webapps/rails4/gems"}
 set :default_environment, {
-  "PATH" => "$PATH:$HOME/bin:$HOME/webapps/rails2/bin",
-  "GEM_HOME" => "$HOME/webapps/rails2/gems"
+  "PATH" => "$HOME/bin:$HOME/webapps/rails4/bin:$PATH",
+  "GEM_HOME" => "$HOME/webapps/rails4/gems"
 }
 
 server 'tomsmyth.ca', :app, :web, :db, :primary => true
@@ -15,7 +16,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} server"
     task command, roles: :app, except: {no_release: true} do
-      run "#{home_dir}/webapps/rails2/bin/#{command}"
+      run "#{home_dir}/webapps/rails4/bin/#{command}"
     end
   end
 end
