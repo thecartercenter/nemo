090513eea8311f88ead9d274caf1743a7ce0d9f9
Melody <meltheadorable@gmail.com>
2016-03-18 11:09:59 -0400

    3920: Changes schedule for recurring jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,13 +9,20 @@ set :output, "log/cron.log"
 env :PATH, ENV["PATH"]
 env :GEM_HOME, ENV["GEM_HOME"]
 
-every 1.hour do
-  # redo the indexes
-  rake "ts:index"
-
+# Every hour, on the hour
+every "0 * * * *" do
   # make sure the daemon is running
   rake "ts:start"
+end
 
+# Every hour, at 5 after
+every "5 * * * *" do
+  # redo the indexes
+  rake "ts:index"
+end
+
+# Every 6 hours, at 30 minutes past the hour
+every "30 */6 * * *" do
   # Clean up expired media
   runner "MediaCleanupJob.perform_later"
 end
