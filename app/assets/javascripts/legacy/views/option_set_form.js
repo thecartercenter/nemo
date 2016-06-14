64ddada078ecb52c9cb5fe1336038eb38645717d
Melody <meltheadorable@gmail.com>
2016-06-14 15:56:20 -0400

    4613: Breaks 3298, which should be fixed again soon

--- app/assets/javascripts/legacy/views/option_set_form.js
+++ app/assets/javascripts/legacy/views/option_set_form.js
@@ -85,7 +85,8 @@
     $('form.option_set_form').on('submit', function(){ return self.form_submitted(); });
 
     // hoookup hints
-    ELMO.app.hookup_hints()
+    // this causes a regression for other views, commenting out for now
+    // ELMO.app.hookup_hints()
 
     // hookup leave page warning unless ajax request
     if (!self.params.modal_mode)
