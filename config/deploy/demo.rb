723ebd53cf8a1f0fbded42249b5b6ca501b3bb74
Tom Smyth <tom@sassafras.coop>
2016-06-21 12:02:39 -0400

    Updated demo deploy script

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,22 +1,21 @@
 set :branch, 'demo'
 set :ping_url, "https://elmo.sassafras.coop"
-set :user, 'tomsmyth'
-set :home_dir, '/home/tomsmyth'
+set :user, 'ubuntu'
+set :home_dir, '/home/ubuntu'
 set :use_sudo, false
-set(:deploy_to) {"#{home_dir}/webapps/rails/elmo_demo"}
-set(:bundle_dir) {"#{home_dir}/webapps/rails/gems"}
+set(:deploy_to) {"#{home_dir}/elmo"}
+
 set :default_environment, {
-  "PATH" => "$HOME/bin:$HOME/webapps/rails/bin:$PATH",
-  "GEM_HOME" => "$HOME/webapps/rails/gems"
+  "PATH" => "$HOME/.rbenv/shims:$PATH"
 }
 
-server 'tomsmyth.ca', :app, :web, :db, :primary => true
+server 'elmo.sassafras.coop', :app, :web, :db, :primary => true
 
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} server"
     task command, roles: :app, except: {no_release: true} do
-      run "#{home_dir}/webapps/rails/bin/#{command}"
+      run "sudo service nginx #{command}"
     end
   end
 end
