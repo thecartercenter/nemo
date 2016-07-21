1a16ea7d538c4a8865f1647609e08503b15dbd43
Melody <meltheadorable@gmail.com>
2016-07-21 19:10:16 -0400

    4826: Fixes phone number normalization crashes

--- app/models/phone_normalizer.rb
+++ app/models/phone_normalizer.rb
@@ -7,7 +7,11 @@ class PhoneNormalizer
       phone_digits = phone.gsub(/\D/, "")
       return false unless phone_digits.present?
       return true if phone_digits.size <= 6
-      country_code = Phony.split(phone_digits).first
+      begin
+        country_code = Phony.split(phone_digits).first
+      rescue NoMethodError
+        country_code = ""
+      end
       phone_digits.size - country_code.size <= 6
     end
   end
@@ -16,7 +20,13 @@ class PhoneNormalizer
     return unless phone.present?
     return phone if is_shortcode?(phone)
     return unless phone.gsub(/\D/, "").present?
-    normalized = Phony.normalize(phone)
+    begin
+      normalized = Phony.normalize(phone)
+    rescue Phony::NormalizationError
+      # if something goes wrong, return + with the digits
+      normalized = phone.gsub(/\D/, "")
+      return "+#{normalized}"
+    end
     return unless normalized.present?
     Phony.format(normalized, format: :+, spaces: "", local_spaces: "", parentheses: false)
   end
