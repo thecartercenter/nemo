0375e04dfb818e27aed18ef0020eb545ccac4818
Melody <meltheadorable@gmail.com>
2016-07-27 13:45:29 -0400

    4638: Fixes repeating shortcodes

--- app/models/base36.rb
+++ app/models/base36.rb
@@ -5,10 +5,8 @@ module Base36
   end
 
   def self.to_padded_base36(n, length: nil)
-    return n.to_s(36) if (length == digits_needed(n)) || length.blank?
     raise "Length too short for number" if digits_needed(n) > length
-    return (offset(length) + n).to_s(36) if digits_needed(offset(length) + n) == length
-    n.to_s(36)
+    (offset(length) + n).to_s(36)
   end
 
   def self.offset(length)
