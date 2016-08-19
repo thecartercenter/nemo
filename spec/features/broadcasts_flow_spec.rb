6cd9b4aba9d9d7c6a4854ed0855ddbb276f33676
Tom Smyth <tom@sassafras.coop>
2016-08-19 11:39:31 -0400

    852: Moved old broadcast feature spec with new ones

--- spec/features/broadcasts_flow_spec.rb
+++ spec/features/broadcasts_flow_spec.rb
@@ -29,4 +29,18 @@ feature 'broadcasts flow', js: true do
     expect(page).to have_content("Broadcast sent successfully")
     expect(page).to have_content("All users in mission")
   end
+
+  scenario "happy path via users list with specific users selected" do
+    click_link("Users")
+    check("selected_#{user.id}")
+    check("selected_#{user2.id}")
+    click_link("Send Broadcast")
+    select("Both SMS and email", from: "broadcast_medium")
+    select("Main phone only", from: "broadcast_which_phone")
+    fill_in("Message", with: "foo bar baz")
+    click_button("Send")
+    expect(page).to have_text "Broadcast sent successfully"
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user2.name)
+  end
 end
