e35a79286d323e62579cce0cd2808ce404d87e36
Tom Smyth <tom@sassafras.coop>
2016-08-19 14:14:42 -0400

    852: Fixed recipients column in broadcast index

--- app/helpers/broadcasts_helper.rb
+++ app/helpers/broadcasts_helper.rb
@@ -4,17 +4,27 @@ module BroadcastsHelper
   end
 
   def broadcasts_index_fields
-    %w(to medium created_at errors body)
+    %w(recipients medium created_at errors body)
   end
 
   def format_broadcasts_field(broadcast, field)
     case field
-    when "to" then "#{broadcast.recipients.count} users"
-    when "medium" then t("broadcast.mediums.names." + broadcast.medium)
-    when "body" then link_to(truncate(broadcast.body, :length => 100), broadcast_path(broadcast), :title => t("common.view"))
-    when "created_at" then l(broadcast.created_at)
-    when "errors" then tbool(!broadcast.send_errors.blank?)
-    else broadcast.send(field)
+    when "recipients"
+      if broadcast.recipient_selection == "specific_users"
+        t("broadcast.user_count", count: broadcast.recipients.count)
+      else
+        t("broadcast.recipient_selection_options.#{broadcast.recipient_selection}")
+      end
+    when "medium"
+      t("broadcast.mediums.names." + broadcast.medium)
+    when "body"
+      link_to(truncate(broadcast.body, :length => 100), broadcast_path(broadcast), :title => t("common.view"))
+    when "created_at"
+      l(broadcast.created_at)
+    when "errors"
+      tbool(!broadcast.send_errors.blank?)
+    else
+      broadcast.send(field)
     end
   end
 end
