647f5ded2237582ee9df61275f8141f0b424665c
Tom Smyth <tom@sassafras.coop>
2016-08-23 16:40:50 -0400

    4648: Fixed duplicate association

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -3,7 +3,6 @@ class UserGroup < ActiveRecord::Base
 
   has_many :user_group_assignments, dependent: :destroy
   has_many :users, through: :user_group_assignments
-  has_many :broadcast_addressings, inverse_of: :addressee, dependent: :destroy
   has_many :broadcast_addressings, inverse_of: :addressee, foreign_key: :addressee_id, dependent: :destroy
   has_many :form_forwardings, inverse_of: :recipient, foreign_key: :recipient_id, dependent: :destroy
 
