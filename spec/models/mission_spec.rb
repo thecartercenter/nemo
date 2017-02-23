bf8dedf2b51cee7b867c080cac9f514f4a2baa57
Melody <meltheadorable@gmail.com>
2017-02-23 15:11:17 -0500

    5608: Adds test coverage

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -1,20 +1,25 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Mission do
-  describe 'terminate' do
-    before do
-      @mission = create(:mission_with_full_heirarchy)
+  describe "terminate" do
+    let!(:mission) { create(:mission_with_full_heirarchy) }
+    let(:expected_counts) do
+      { mission: 1, broadcast: 1, assignment: 3, form: 2, question: 6, questioning: 10, qing_group: 4, option: 10,
+        option_node: 20, option_set: 4, condition: 2, report_report: 1, response: 1, answer: 5, choice: 2,
+        sms_message: 1, user_group: 2, user_group_assignment: 3 }
     end
+    let(:deleted_counts) { expected_counts.map { |k, v| [k, 0] }.to_h }
 
-    it 'should delete all objects in mission' do
-      expect(obj_counts).to eq [1, 1, 3, 1, 6, 2, 10, 13, 3, 1, 1, 5, 2, 1, 2, 3]
-      @mission.terminate
-      expect(obj_counts).to eq [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    it "should delete all objects in mission" do
+      expect(obj_counts).to eq expected_counts
+      mission.terminate
+      expect(obj_counts).to eq deleted_counts
     end
 
     def obj_counts
-      [Mission, Broadcast, Assignment, Form, Question, QingGroup, Option, OptionNode, OptionSet,
-        Report::Report, Response, Answer, Choice, Sms::Message, UserGroup, UserGroupAssignment].map(&:count)
+      [Mission, Broadcast, Assignment, Form, Question, Questioning, QingGroup, Option, OptionNode, OptionSet, Condition,
+        Report::Report, Response, Answer, Choice, Sms::Message, UserGroup, UserGroupAssignment].map do |u| [u.model_name.param_key.to_sym, u.count]
+      end.to_h
     end
   end
 end
