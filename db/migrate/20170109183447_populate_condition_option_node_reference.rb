e12d6ce25f759a93354dff4d850453e2490f528c
Tom Smyth <tom@sassafras.coop>
2017-02-23 16:26:03 -0500

    3594: Fixed migration to create new nodes where needed

--- db/migrate/20170109183447_populate_condition_option_node_reference.rb
+++ db/migrate/20170109183447_populate_condition_option_node_reference.rb
@@ -14,30 +14,46 @@ end
 
 class OptionNode < ActiveRecord::Base
   belongs_to :option, autosave: true
+  has_ancestry
 end
 
 class PopulateConditionOptionNodeReference < ActiveRecord::Migration
   def up
     check_failed = false
 
-    # Make sure all leaf nodes match leaf options.
-    Condition.all.each do |condition|
-      if condition.option_ids.present?
-        node = condition.option_nodes.last
-        option = condition.options.last
-        if node.option_id != option.id
-          puts "Condition ##{condition.id}: OptionNode ##{node.id} didn't match Option ##{option.id}"
-          check_failed = true
+    transaction do
+
+      # Make sure all leaf nodes match leaf options.
+      Condition.all.each do |condition|
+        if condition.option_ids.present?
+          node = condition.option_nodes.last
+          option = condition.options.last
+          if option.nil?
+            puts "Couldn't find Option ##{option.id} referred to in Condition ##{condition.id}"
+            check_failed = true
+          elsif node.nil?
+            puts "For Condition ##{condition.id}, couldn't find OptionNode matching Option ##{option.id} " \
+              "in the appropriate set, but did find the Option, so creating a new node."
+            condition.ref_qing.option_set.root_node.children.create(
+              option_set_id: condition.ref_qing.option_set_id,
+              option_id: option.id,
+              mission_id: condition.mission_id,
+              rank: condition.ref_qing.option_set.root_node.children.count + 1
+            )
+          elsif node.try(:option_id) != option.id
+            puts "Condition ##{condition.id}: OptionNode ##{node.try(:id)} didn't match Option ##{option.id}"
+            check_failed = true
+          end
         end
       end
-    end
 
-    raise "OptionNode <=> Option check failed" if check_failed
+      raise "OptionNode <=> Option check failed" if check_failed
 
-    Condition.all.each do |condition|
-      if condition.option_ids.present?
-        node = condition.option_nodes.last
-        condition.update_attribute(:option_node_id, node.id)
+      Condition.all.each do |condition|
+        if condition.option_ids.present?
+          node = condition.option_nodes.last
+          condition.update_attribute(:option_node_id, node.id)
+        end
       end
     end
   end
