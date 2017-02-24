4e947ffc9fe9aadadd0e92c81c2cdb216390473e
Melody <meltheadorable@gmail.com>
2017-02-24 15:13:31 -0500

    5757: refactor specs

--- spec/controllers/odk_form_download_spec.rb
+++ spec/controllers/odk_form_download_spec.rb
@@ -2,25 +2,26 @@ require "spec_helper"
 
 # Using request spec b/c Authlogic won't work with controller spec
 describe FormsController, type: :request do
+  let!(:mission) { create(:mission) }
+  let!(:user) { create(:user, role_name: :coordinator, mission: mission) }
+  let(:form) { create(:form, mission: mission, question_types: %w(integer integer)) } # No select1's
+  let(:form_select) { create(:form, mission: mission, question_types: %w(integer select_one)) } # Regular select1
+  let(:form_multiselect) do
+    create(:form, mission: mission, question_types: %w(integer multilevel_select_one)) # Multilevel select1
+  end
+
+
   before do
-    @mission = create(:mission)
-    @user = create(:user, role_name: :coordinator, mission: @mission)
-    login(@user)
+    login(user)
+    form.publish!
+    form_select.publish!
+    form_multiselect.publish!
   end
 
   context "for regular mission" do
-    before do
-      @form1 = create(:form, mission: @mission, question_types: %w(integer integer)) # No select1's
-      @form2 = create(:form, mission: @mission, question_types: %w(integer select_one)) # Regular select1
-      @form3 = create(:form, mission: @mission, question_types: %w(integer multilevel_select_one)) # Multilevel select1
-      @form1.publish!
-      @form2.publish!
-      @form3.publish!
-    end
-
     describe "listing forms" do
       it "should succeed" do
-        get_s(odk_form_list_path, format: :xml)
+        get_s( "/en/m/#{mission.compact_name}/formList", format: :xml)
 
         # Only form3 should have a manifest.
         assert_select("xform", count: 3) do |elements|
@@ -31,21 +32,25 @@ describe FormsController, type: :request do
       end
 
       it "should succeed with no locale" do
-        get_s(odk_form_list_path(locale: nil), format: :xml)
+        get_s("/m/#{mission.compact_name}/formList", format: :xml)
       end
     end
 
     describe "showing forms" do
       it "should succeed" do
         # XML rendering details are tested elsewhere.
-        get_s(odk_form_path(@form3), format: :xml)
+        get_s("/en/m/#{mission.compact_name}/forms/#{form_multiselect.id}", format: :xml)
+      end
+
+      it "should succeed with no locale" do
+        get_s("/m/#{mission.compact_name}/forms/#{form_multiselect.id}", format: :xml)
       end
     end
 
     describe "odk manifest" do
       context "for form with no option sets" do
         it "should render empty manifest tag" do
-          get_s(odk_form_manifest_path(@form1))
+          get_s("/en/m/#{mission.compact_name}/forms/#{form.id}/manifest")
           assert_select("manifest", count: 1)
           assert_select("mediaFile", count: 0)
         end
@@ -53,8 +58,8 @@ describe FormsController, type: :request do
 
       context "for normal form" do
         it "should render regular manifest tag" do
-          get_s(odk_form_manifest_path(@form3))
-          @ifa = ItemsetsFormAttachment.new(form: @form3)
+          get_s("/m/#{mission.compact_name}/forms/#{form_multiselect.id}/manifest")
+          @ifa = ItemsetsFormAttachment.new(form: form_multiselect)
           assert_select("filename", text: "itemsets.csv")
           assert_select("hash", text: @ifa.md5)
           assert_select("downloadUrl", text: "http://www.example.com/#{@ifa.path}")
@@ -65,7 +70,7 @@ describe FormsController, type: :request do
     describe "getting itemsets file" do
       context "for form with option sets" do
         before do
-          @ifa = ItemsetsFormAttachment.new(form: @form3)
+          @ifa = ItemsetsFormAttachment.new(form: form_multiselect)
           @ifa.ensure_generated
         end
 
@@ -79,20 +84,16 @@ describe FormsController, type: :request do
   end
 
   context "for locked mission" do
-    before do
-      @mission.update_attributes!(locked: true)
-      @form1 = create(:form, mission: @mission, question_types: %w(integer integer))
-      @form1.publish!
-    end
+    let!(:mission) { create(:mission, locked: true) }
 
     it "listing forms should return 403" do
-      get(odk_form_list_path, format: :xml)
+      get("/en/m/#{mission.compact_name}/formList", format: :xml)
       expect(response.status).to eq 403
       expect(response.body.strip).to be_empty
     end
 
     it "showing form with format xml should return 403" do
-      get(odk_form_path(@form1), format: :xml)
+      get("/m/#{mission.compact_name}/forms/#{form.id}", format: :xml)
       expect(response.status).to eq 403
       expect(response.body.strip).to be_empty
     end
