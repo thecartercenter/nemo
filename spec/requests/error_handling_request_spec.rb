ef88d98b230f5ac1dfccb3fb1c4e8d210e99592a
Tom Smyth <tom@sassafras.coop>
2017-02-24 11:02:10 -0500

    5528: Tidy spec name

--- spec/requests/error_handling_request_spec.rb
+++ spec/requests/error_handling_request_spec.rb
@@ -4,7 +4,7 @@ describe "error handling" do
   let(:user) { create(:user, admin: true) }
   let(:mission) { get_mission }
 
-  it "renders 400 when a request is sent with no params" do
+  it "renders 400 when a POST request is sent with no params" do
     post(reports_path(mission_name: mission.compact_name, mode: "m"))
     expect(response).not_to be_success
     expect(response).to have_http_status 400
