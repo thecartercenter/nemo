8fd4fdd7f643eef81695bcf33fe64ca851cd9853
Tom Smyth <tom@sassafras.coop>
2017-03-31 13:45:06 -0400

    6054: Refine message search spec

--- spec/models/search/message_search_spec.rb
+++ spec/models/search/message_search_spec.rb
@@ -5,7 +5,11 @@ describe Sms::Message do
     describe "date search" do
       let(:m1) { create(:sms_incoming, created_at: "2017-01-01 22:00") }
 
-      it "should work" do
+      around do |example|
+        in_timezone("Saskatchewan") { example.run }
+      end
+
+      it "should work even if timestamp in UTC is the next day" do
         expect(search "date:2017-01-01").to eq [m1]
       end
     end
