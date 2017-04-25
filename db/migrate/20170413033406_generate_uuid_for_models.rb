6ec67e9e47e04903cb7483e54ba449c85e2893f3
Tom Smyth <tom@sassafras.coop>
2017-04-25 13:51:25 -0400

    5992: Fixed UUID migration for postgres

--- db/migrate/20170413033406_generate_uuid_for_models.rb
+++ db/migrate/20170413033406_generate_uuid_for_models.rb
@@ -1,9 +1,9 @@
-MODELS_WITH_UUIDS = [Answer, Assignment, BroadcastAddressing, Broadcast, Choice, Condition,
-  FormForwarding, FormItem, FormVersion, Form, Media::Object, Mission, Operation, OptionNode, OptionSet, Option,
-  Question, Report::Calculation, Report::OptionSetChoice, Report::Report, Response, Setting, Sms::Message,
-  Tag, Tagging, UserGroupAssignment, UserGroup, User, Whitelisting]
-
 class GenerateUuidForModels < ActiveRecord::Migration
+  MODELS_WITH_UUIDS = [Answer, Assignment, BroadcastAddressing, Broadcast, Choice, Condition,
+    FormForwarding, FormItem, FormVersion, Form, Media::Object, Mission, Operation, OptionNode, OptionSet, Option,
+    Question, Report::Calculation, Report::OptionSetChoice, Report::Report, Response, Setting, Sms::Message,
+    Tag, Tagging, UserGroupAssignment, UserGroup, User, Whitelisting]
+
   def up
     MODELS_WITH_UUIDS.each do |model|
       model.find_each do |instance|
