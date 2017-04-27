234a4122eef8f455dd1f6a9c91ffe2aac8650903
Tom Smyth <tom@sassafras.coop>
2017-04-27 14:38:57 -0400

    5992: Fixed report formatter to handle jsonb translations

--- app/models/report/formatter.rb
+++ app/models/report/formatter.rb
@@ -19,18 +19,10 @@ class Report::Formatter
     end
   end
 
-  # checks if the value looks like a translation hash. if it does, pick the appropriate one
+  # Checks if the value looks like a translation hash. If it does, pick the appropriate one.
   def self.translate(value)
-    # if it looks like a translation hash
-    if value.is_a?(String) && value[0] == "{" && value =~ /\{"[a-z]{2}"/
-      # parse it
-      dict = JSON.parse(value)
-
-      # return the appropriate value (try the current locale, then the default locale, then the first locale)
-      translated = dict[I18n.locale.to_s]
-      translated = dict[I18n.default_locale.to_s] if translated.blank?
-      translated = dict.values.first if translated.blank?
-      translated
+    if value.is_a?(Hash)
+      value[I18n.locale.to_s].presence || value[I18n.default_locale.to_s].presence || value.values.first
     else
       value
     end
