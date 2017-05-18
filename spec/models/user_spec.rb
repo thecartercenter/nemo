635dff9a6d4014bf4ed3d8bb483620227b71be14
Tom Smyth <tom@sassafras.coop>
2017-05-18 17:11:45 -0400

    6036: Marked flapping spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -92,7 +92,7 @@ describe User do
     end
   end
 
-  it "creating a user with minimal info should produce good defaults" do
+  it "creating a user with minimal info should produce good defaults", :investigate do
     user = User.create!(name: "Alpha Tester", login: "alpha", reset_password_method: "print",
                         assignments: [Assignment.new(mission: mission, role: User::ROLES.first)])
     expect(user.pref_lang).to eq("en")
