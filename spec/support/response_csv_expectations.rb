5985f5e4f96586d8183c0cf84f34e80efb5f7a34
Tom Smyth <tom@sassafras.coop>
2017-05-19 13:27:56 -0400

    6000: Fixed ordering issue in response CSV spec

--- spec/support/response_csv_expectations.rb
+++ spec/support/response_csv_expectations.rb
@@ -7,9 +7,9 @@ Sample Form 1,A User 2,2015-11-20 06:20 CST,#{uuids[0]},#{shortcodes[0]},alpha,G
 Sample Form 1,A User 1,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},foo✓,Canada,Calgary,51.045,-114.057222,\"foo\r
 \r
 \"\"bar\"\"  \r\nbaz\",100,-123.5,15.937378,44.36453,Cat,Dog,Cat,Dog;Cat,2015-10-12 12:15 CST,2014-11-09,23:15\r
-Sample Form 1,A User 3,2015-11-20 06:30 CST,#{uuids[2]},#{shortcodes[2]},foo,Canada,,,,bar,100,-123.5,15.937378,44.36453,Cat,Dog,Cat,Dog;Cat,2015-10-12 12:15 CST,2014-11-09,23:15\r
-Sample Form 1,A User 4,2015-11-20 06:30 CST,#{uuids[3]},#{shortcodes[3]},foo,Ghana,,7.1,0.4,bar,100,-123.5,15.937378,44.36453,Cat,Dog,Cat,Dog;Cat,2015-10-12 12:15 CST,2014-11-09,23:15\r
-Sample Form 2,A User 5,2015-11-20 06:30 CST,#{uuids[4]},#{shortcodes[4]},,Ghana,Accra,5.55,0.2,,,,,,,,,,,,,foo,bar,Funton,-12.9,22.7\r\n")
+Sample Form 1,A User 3,2015-11-20 06:40 CST,#{uuids[2]},#{shortcodes[2]},foo,Canada,,,,bar,100,-123.5,15.937378,44.36453,Cat,Dog,Cat,Dog;Cat,2015-10-12 12:15 CST,2014-11-09,23:15\r
+Sample Form 1,A User 4,2015-11-20 06:45 CST,#{uuids[3]},#{shortcodes[3]},foo,Ghana,,7.1,0.4,bar,100,-123.5,15.937378,44.36453,Cat,Dog,Cat,Dog;Cat,2015-10-12 12:15 CST,2014-11-09,23:15\r
+Sample Form 2,A User 5,2015-11-20 06:50 CST,#{uuids[4]},#{shortcodes[4]},,Ghana,Accra,5.55,0.2,,,,,,,,,,,,,foo,bar,Funton,-12.9,22.7\r\n")
   end
 
   def response_csv_expectation_with_repeat_groups(ordered_responses)
@@ -20,11 +20,11 @@ Sample Form 1,A User 1,2015-11-20 06:30 CST,#{uuids[0]},#{shortcodes[0]},,0,1,,,
 Sample Form 1,A User 1,2015-11-20 06:30 CST,#{uuids[0]},#{shortcodes[0]},Fruit,1,1,Apple,1,Cat;Dog,2,,,,,,\r
 Sample Form 1,A User 1,2015-11-20 06:30 CST,#{uuids[0]},#{shortcodes[0]},Fruit,1,1,Banana,2,Cat,2,,,,,,\r
 Sample Form 1,A User 1,2015-11-20 06:30 CST,#{uuids[0]},#{shortcodes[0]},Vegetable,1,1,,,,2,Asparagus,Ghana,Accra,5.55,0.2,3\r
-Sample Form 1,A User 2,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},,0,3,,,,4,,,,,,\r
-Sample Form 1,A User 2,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},Fruit,1,3,Xigua,10,Dog,4,,,,,,\r
-Sample Form 1,A User 2,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},Fruit,1,3,Yuzu,9,Cat;Dog,4,,,,,,\r
-Sample Form 1,A User 2,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},Fruit,1,3,Ugli,8,Cat,4,,,,,,\r
-Sample Form 1,A User 2,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},Vegetable,1,3,,,,4,Zucchini,Canada,Calgary,51.045,-114.057222,7\r
-Sample Form 1,A User 2,2015-11-20 06:30 CST,#{uuids[1]},#{shortcodes[1]},Vegetable,1,3,,,,4,Yam,Canada,Ottawa,45.429299,-75.629883,6\r\n")
+Sample Form 1,A User 2,2015-11-20 06:40 CST,#{uuids[1]},#{shortcodes[1]},,0,3,,,,4,,,,,,\r
+Sample Form 1,A User 2,2015-11-20 06:40 CST,#{uuids[1]},#{shortcodes[1]},Fruit,1,3,Xigua,10,Dog,4,,,,,,\r
+Sample Form 1,A User 2,2015-11-20 06:40 CST,#{uuids[1]},#{shortcodes[1]},Fruit,1,3,Yuzu,9,Cat;Dog,4,,,,,,\r
+Sample Form 1,A User 2,2015-11-20 06:40 CST,#{uuids[1]},#{shortcodes[1]},Fruit,1,3,Ugli,8,Cat,4,,,,,,\r
+Sample Form 1,A User 2,2015-11-20 06:40 CST,#{uuids[1]},#{shortcodes[1]},Vegetable,1,3,,,,4,Zucchini,Canada,Calgary,51.045,-114.057222,7\r
+Sample Form 1,A User 2,2015-11-20 06:40 CST,#{uuids[1]},#{shortcodes[1]},Vegetable,1,3,,,,4,Yam,Canada,Ottawa,45.429299,-75.629883,6\r\n")
   end
 end
