601866c89c03ab5b091083a5e79fb18173692adf
Tom Smyth <tom@sassafras.coop>
2017-06-07 16:14:26 -0400

    6461: Added performance boost for questioning.repeatable

--- app/models/response_csv.rb
+++ app/models/response_csv.rb
@@ -56,7 +56,7 @@ class ResponseCSV
         answers = response.answers.
           includes(:option, questioning: {question: {option_set: :root_node}}, choices: :option).
           order(:questioning_id, :inst_num, :rank)
-        repeatable_answers = answers.select { |a| cache[a.questioning, :parent_repeatable?] }
+        repeatable_answers = answers.select { |a| cache[a.questioning, :repeatable?] }
         non_repeat_answers = answers - repeatable_answers
 
         # Make initial row
@@ -128,7 +128,7 @@ class ResponseCSV
     if question
       return if question.multimedia?
       name = [code]
-      if cache[qing, :parent_repeatable?]
+      if cache[qing, :repeatable?]
         name = [qing.parent_group_name, code]
       end
       if cache[question, :multilevel?]
