e74be874bdbee1edff5471f5789b89a01cd3c427
Tom Smyth <tom@sassafras.coop>
2017-06-14 16:56:39 -0400

    6661: Refactored response csv spec to use new system

--- spec/models/response_csv_spec.rb
+++ spec/models/response_csv_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe ResponseCSV do
-  let(:ordered_responses) { Response.with_associations.order(:created_at) }
+  let(:responses) { Response.with_associations.order(:created_at) }
+  let(:generated_csv) { ResponseCSV.new(responses).to_s }
 
   around do |example|
     # Use a weird timezone so we know times are handled properly.
@@ -18,7 +19,7 @@ describe ResponseCSV do
 
   context "with no data" do
     it "should generate empty string" do
-      expect(ResponseCSV.new([]).to_s).to eq ""
+      expect(generated_csv).to eq ""
     end
   end
 
@@ -73,8 +74,7 @@ describe ResponseCSV do
     end
 
     it "should generate correct CSV" do
-      expected = response_csv_expectation_without_repeat_groups(ordered_responses)
-      expect(ResponseCSV.new(ordered_responses).to_s).to eq expected
+      expect(generated_csv).to eq prepare_response_csv_expectation("basic.csv")
     end
   end
 
@@ -124,10 +124,12 @@ describe ResponseCSV do
         response_a
         Timecop.freeze(10.minutes) { response_b }
       end
-
-      expected = response_csv_expectation_with_repeat_groups(ordered_responses)
-      actual = ResponseCSV.new(ordered_responses)
-      expect(actual.to_s).to eq expected
+      expect(generated_csv).to eq prepare_response_csv_expectation("with_repeat_groups.csv")
     end
+  end
+
+  def prepare_response_csv_expectation(filename)
+    prepare_expectation("response_csv/#{filename}",
+      uuid: responses.map(&:uuid), shortcode: responses.map(&:shortcode))
   end
 end
