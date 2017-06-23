c1b9b07628036f995c544a3ba9392a197612e082
Tom Smyth <tom@sassafras.coop>
2017-06-23 16:39:51 -0400

    6606: Changed log level to debug in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ ELMO::Application.configure do
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
 
-  # See everything in the log (default is :info)
-  config.log_level = :info
+  config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
