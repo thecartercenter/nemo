dd8a561d8342e5e500953a84613d2dd3ec8ab659
Devney <devneyhamilton@gmail.com>
2017-07-12 14:21:55 -0400

    6168: remove secrets.yml, add secure and expiry

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-#ELMO::Application.config.session_store :active_record_store, secure: Rails.env.production?
-ELMO::Application.config.session_store :cookie_store, key: 'elmo_session'
+ELMO::Application.config.session_store :cookie_store, key: 'elmo_session', secure: Rails.env.production?, expire_after: 1.day
