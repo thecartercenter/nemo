7a878a0814d17f67f4a05fdd75e235a5a306c13e
Tom Smyth <tom@sassafras.coop>
2017-07-14 13:55:50 -0400

    6813: Restricted rank gap check to current form only

--- app/models/form_item.rb
+++ app/models/form_item.rb
@@ -20,27 +20,6 @@ class FormItem < ApplicationRecord
 
   validate :parent_must_be_group
 
-  # Checks for gaps in ranks in the db directly.
-  def self.rank_gaps?
-    !find_by_sql("
-      SELECT id FROM form_items fi1
-      WHERE fi1.deleted_at IS NULL AND fi1.rank > 1 AND NOT EXISTS (
-        SELECT id FROM form_items fi2
-        WHERE fi2.deleted_at IS NULL AND fi2.ancestry = fi1.ancestry AND fi2.rank = fi1.rank - 1)
-    ").empty?
-  end
-
-  def self.duplicate_ranks?
-    !find_by_sql("
-      SELECT ancestry, rank
-      FROM form_items
-      WHERE deleted_at IS NULL AND ancestry is NOT NULL
-        AND ancestry != ''
-      GROUP BY ancestry, rank
-      HAVING COUNT(id) > 1
-    ").empty?
-  end
-
   # Gets an OrderedHash of the following form for the descendants of this FormItem.
   # Uses only a constant number of database queries to do so.
   # {
@@ -99,12 +78,14 @@ class FormItem < ApplicationRecord
   # Moves item to new rank and parent.
   def move(new_parent_id, new_rank)
     transaction do
-      update_attributes(parent: FormItem.find(new_parent_id), rank: new_rank)
+      new_parent = FormItem.find(new_parent_id)
+      form_id = new_parent.form_id
+      update_attributes(parent: new_parent, rank: new_rank)
 
       # Extra safeguards to make sure ranks are correct. acts_as_list should prevent these.
-      if self.class.rank_gaps?
+      if rank_gaps?
         raise "Moving Qing #{id} to parent #{new_parent_id}, rank #{new_rank} would have caused gaps in ranks."
-      elsif self.class.duplicate_ranks?
+      elsif duplicate_ranks?
         raise "Moving Qing #{id} to parent #{new_parent_id}, rank #{new_rank} would have caused duplicate ranks."
       end
     end
@@ -117,12 +98,34 @@ class FormItem < ApplicationRecord
   end
 
   private
-    # copy mission from question
-    def set_mission
-      self.mission = form.try(:mission)
-    end
 
-    def parent_must_be_group
-      errors.add(:parent, :must_be_group) unless parent.nil? || parent.is_a?(QingGroup)
-    end
+  # copy mission from question
+  def set_mission
+    self.mission = form.try(:mission)
+  end
+
+  def parent_must_be_group
+    errors.add(:parent, :must_be_group) unless parent.nil? || parent.is_a?(QingGroup)
+  end
+
+  # Checks for gaps in ranks in the db directly.
+  def rank_gaps?
+    SqlRunner.instance.run("
+      SELECT id FROM form_items fi1
+      WHERE fi1.deleted_at IS NULL AND fi1.form_id = ? AND fi1.rank > 1 AND NOT EXISTS (
+        SELECT id FROM form_items fi2
+        WHERE fi2.deleted_at IS NULL AND fi2.ancestry = fi1.ancestry AND fi2.rank = fi1.rank - 1)
+    ", form_id).any?
+  end
+
+  def duplicate_ranks?
+    SqlRunner.instance.run("
+      SELECT ancestry, rank
+      FROM form_items
+      WHERE form_id = ? AND deleted_at IS NULL AND ancestry is NOT NULL
+        AND ancestry != ''
+      GROUP BY ancestry, rank
+      HAVING COUNT(id) > 1
+    ", form_id).any?
+  end
 end
