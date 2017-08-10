182391a53ea7e54b4863723a8ef9d0819af606e4
Melody <meltheadorable@gmail.com>
2017-08-10 06:28:08 -0400

    6109: Spec for rendering

--- spec/requests/form_odk_rendering_spec.rb
+++ spec/requests/form_odk_rendering_spec.rb
@@ -214,6 +214,8 @@ describe "form rendering for odk",:odk, :reset_factory_sequences do
         ]
       )
     end
+    # set prefill pattern
+    before { form.questioning_with_code("TextQ7").update_attributes(prefill_pattern: "$IntegerQ4-$!ItemNumber") }
 
     it "should render proper xml" do
       do_request_and_expect_success
