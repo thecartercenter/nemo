26fbe9d419976edb2532e822d91e6dd25a9464ac
Tom Smyth <tom@sassafras.coop>
2017-08-24 17:24:39 -0400

    7004: Added failing spec

--- spec/decorators/odk/qing_decorator_spec.rb
+++ spec/decorators/odk/qing_decorator_spec.rb
@@ -25,6 +25,7 @@ module Odk
     let(:qing4) { described_class.decorate(form.sorted_children[1].sorted_children[0]) }
     let(:qing5) { described_class.decorate(form.sorted_children[1].sorted_children[1]) }
     let(:qing6) { described_class.decorate(form.sorted_children[1].sorted_children[2].sorted_children[0]) }
+    let(:qing9) { described_class.decorate(form.sorted_children[1].sorted_children[3].sorted_children[0]) }
     let(:qing11) { described_class.decorate(form.sorted_children[1].sorted_children[3].
       sorted_children[1].sorted_children[0]) }
 
@@ -36,20 +37,31 @@ module Odk
     end
 
     describe "xpath_to" do
-      it "returns the relative xpath when going within group" do
+      it "returns the relative xpath when going on same level within group" do
         expect(qing5.xpath_to(qing4)).to eq "../q2"
       end
 
-      it "returns the relative xpath when going from group to group" do
-        expect(qing6.xpath_to(qing11)).to eq "../../grp8/grp10/q6"
+      it "returns the relative xpath when going up within group" do
+        expect(qing6.xpath_to(qing4)).to eq "../../q2"
       end
 
       it "returns the absolute xpath when going from group to top-level" do
         expect(qing6.xpath_to(qing2)).to eq "/data/q1"
       end
 
-      it "returns the absolute xpath when going from top-level to group" do
-        expect(qing2.xpath_to(qing6)).to eq "/data/grp3/grp6/q4"
+      it "uses indexed-repeat when going down into a subgroup" do
+        expect(qing9.xpath_to(qing11)).to eq "indexed-repeat(/data/grp3/grp8/grp10/q6,"\
+          "/data/grp3,position(../..),/data/grp3/grp8,position(..),/data/grp3/grp8/grp10 1)"
+      end
+
+      it "uses indexed-repeat when going from group to group" do
+        expect(qing6.xpath_to(qing11)).to eq "indexed-repeat(/data/grp3/grp8/grp10/q6,"\
+          "/data/grp3,position(../..),/data/grp3/grp8,1,/data/grp3/grp8/grp10 1)"
+      end
+
+      it "uses indexed-repeat when going from top-level to group" do
+        expect(qing9.xpath_to(qing11)).to eq "indexed-repeat(/data/grp3/grp6/q4,"\
+          "/data/grp3,1,/data/grp3/grp6,1)"
       end
     end
   end
