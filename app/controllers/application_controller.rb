4cae5ffbaa806790047610e0f171ab1054218a22
Tom Smyth <tom@sassafras.coop>
2017-09-22 15:01:25 -0400

    6967: Added user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,17 +22,18 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
   rescue_from ActionController::InvalidAuthenticityToken, with: :handle_invalid_authenticity_token
 
-  before_filter(:check_route)
-  before_filter(:remove_missionchange_flag)
-  before_filter(:set_locale)
-  before_filter(:mailer_set_url_options)
-  before_filter(:get_mission)
-  before_filter(:get_user)
-  before_filter(:protect_admin_mode)
-  before_filter(:remember_mission)
-  before_filter(:remember_context, only: :index)
-  before_filter(:load_settings_for_mission_into_config)
-  before_filter(:load_accessible_missions)
+  before_filter :check_route
+  before_filter :remove_missionchange_flag
+  before_filter :set_locale
+  before_filter :mailer_set_url_options
+  before_filter :get_mission
+  before_filter :get_user
+  before_action :prepare_exception_notifier
+  before_filter :protect_admin_mode
+  before_filter :remember_mission
+  before_filter :remember_context, only: :index
+  before_filter :load_settings_for_mission_into_config
+  before_filter :load_accessible_missions
 
   helper_method :current_mode, :current_user, :current_mission, :current_root_path, :admin_mode?,
     :admin_mode_exit_path, :offline_mode?
