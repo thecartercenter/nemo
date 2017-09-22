ce70e1417c7404b9d09fdb76ce41302f65b33f60
Tom Smyth <tom@sassafras.coop>
2017-09-22 15:13:25 -0400

    6967: Removed secret stuff

--- config/initializers/notifier.rb
+++ config/initializers/notifier.rb
@@ -1,5 +1,8 @@
 Rails.configuration.middleware.use ExceptionNotification::Rack, email: {
   email_prefix: "[ELMO ERROR] ",
   sender_address: configatron.site_email,
-  exception_recipients: configatron.webmaster_emails
+  exception_recipients: configatron.webmaster_emails,
+
+  # Not including session because it contains user_credentials, not sure if that's secret.
+  sections: %w(request environment backtrace)
 }
