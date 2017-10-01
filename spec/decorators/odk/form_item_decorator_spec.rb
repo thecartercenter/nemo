4122c658fbdfcb9738b48f7ed83509ddbab0643d
Tom Smyth <tom@sassafras.coop>
2017-10-01 16:24:54 -0400

    7219: Moved xpath_to to form item decorator

--- spec/decorators/odk/form_item_decorator_spec.rb
+++ spec/decorators/odk/form_item_decorator_spec.rb
@@ -1,35 +1,34 @@
 require "spec_helper"
 
 module Odk
-  describe QingDecorator, :odk, :reset_factory_sequences, database_cleaner: :truncate do
+  describe FormItemDecorator, :odk, :reset_factory_sequences, database_cleaner: :truncate do
     let!(:form) do
       create(:form, question_types: [ # root (grp1)
-        "text",       # sorted_children[0] (qing2 -> q1)
-        [             # sorted_children [1] (grp3)
-          "text",     # --> sorted_children[0] (qing4 -> q2)
-          "text",     # --> sorted_children[1] (qing5 -> q3)
-          [           # --> sorted_children[2] (grp6)
-            "integer",   # --> --> sorted_children[0] (qing6 -> q4)
+        "text",       # c[0] (qing2 -> q1)
+        [             # c[1] (grp3)
+          "text",     # --> c[0] (qing4 -> q2)
+          "text",     # --> c[1] (qing5 -> q3)
+          [           # --> c[2] (grp6)
+            "integer",   # --> --> c[0] (qing6 -> q4)
           ],
-          [           # --> sorted_children[3] (grp8)
-            "text",   # --> --> sorted_children[0] (qing9 -> q5)
-            [         # --> --> sorted_children[1] (grp10)
-              "text"  # --> --> --> sorted_children[0] (qing11 -> q6)
+          [           # --> c[3] (grp8)
+            "text",   # --> --> c[0] (qing9 -> q5)
+            [         # --> --> c[1] (grp10)
+              "text"  # --> --> --> c[0] (qing11 -> q6)
             ]
           ]
         ],
-        "text"        # sorted_children[2] (qing12 -> q7)
+        "text"        # c[2] (qing12 -> q7)
       ])
     end
 
-    let(:qing2) { described_class.decorate(form.sorted_children[0]) }
-    let(:qing4) { described_class.decorate(form.sorted_children[1].sorted_children[0]) }
-    let(:qing5) { described_class.decorate(form.sorted_children[1].sorted_children[1]) }
-    let(:qing6) { described_class.decorate(form.sorted_children[1].sorted_children[2].sorted_children[0]) }
-    let(:qing9) { described_class.decorate(form.sorted_children[1].sorted_children[3].sorted_children[0]) }
-    let(:qing11) { described_class.decorate(form.sorted_children[1].sorted_children[3].
-      sorted_children[1].sorted_children[0]) }
-    let(:qing12) { described_class.decorate(form.sorted_children[2]) }
+    let(:qing2) { decorate(form.c[0]) }
+    let(:qing4) { decorate(form.c[1].c[0]) }
+    let(:qing5) { decorate(form.c[1].c[1]) }
+    let(:qing6) { decorate(form.c[1].c[2].c[0]) }
+    let(:qing9) { decorate(form.c[1].c[3].c[0]) }
+    let(:qing11) { decorate(form.c[1].c[3].c[1].c[0]) }
+    let(:qing12) { decorate(form.c[2]) }
 
     describe "absolute_xpath" do
       it do
@@ -69,6 +68,10 @@ module Odk
         expect(qing2.xpath_to(qing6)).to eq "indexed-repeat(/data/grp3/grp6/q4,"\
           "/data/grp3,1,/data/grp3/grp6,1)"
       end
+    end
+
+    def decorate(obj)
+      Odk::DecoratorFactory.decorate(obj)
     end
   end
 end
