34ad1d80a5f0c237c380420e5203d255d1c1a36a
Tom Smyth <tom@sassafras.coop>
2017-10-06 10:49:49 -0400

    Removed sphinx from Whenever schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,22 +9,6 @@ set :output, "log/cron.log"
 env :PATH, ENV["PATH"]
 env :GEM_HOME, ENV["GEM_HOME"]
 
-every :reboot do
-  rake "ts:start"
-end
-
-# Every hour, on the hour
-every 1.hour do
-  # make sure the daemon is running
-  rake "ts:start"
-end
-
-# Every hour, at 5 after
-every "5 * * * *" do
-  # redo the indexes
-  rake "ts:index"
-end
-
 # Every 6 hours, at half past
 every "30 */6 * * *" do
   runner "CleanupJob.perform_later"
