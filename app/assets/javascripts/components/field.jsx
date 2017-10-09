52567439506e6097aaa6282f8c4c23c3a19e795e
Devney <devneyhamilton@gmail.com>
2017-10-09 16:59:07 -0400

    7170: rendered hardcoded select inside form

---
+++ app/assets/javascripts/components/field.jsx
@@ -0,0 +1,11 @@
+class TestField extends React.Component {
+  render() {
+    let options =
+        [
+          <option value="1" key="1">Bir</option>,
+          <option value="2" key="2">Iki</option>,
+          <option value="3" key="3">Uc</option>
+        ]
+    return <select value="1" className="field test-select">{options}</select>
+  }
+}
