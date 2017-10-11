67a3a865bca8f110d43d03bc77b57683214eeb5d
Devney <devneyhamilton@gmail.com>
2017-10-11 12:32:24 -0400

    7170: failed to integrate with backbone. Added keys to try to get rid of warnings.

--- app/assets/javascripts/components/form_field.jsx
+++ app/assets/javascripts/components/form_field.jsx
@@ -1,7 +1,7 @@
 class FormField extends React.Component {
   render() {
 
-    let content = [<FormLabel for={this.props.for} text={this.props.label}/>, <FormSelect name={this.props.name} id={this.props.id} options={this.props.options}/>]
-    return <div id="test-field"> {content} </div>
+    let content = [<FormLabel for={this.props.for} text={this.props.label} key="label"/>, <FormSelect key="select" name={this.props.name} id={this.props.id} options={this.props.options} />]
+    return <div id="test-field" key="XYZ"> {content} </div>
   }
 }
--- app/assets/javascripts/components/form_select.jsx
+++ app/assets/javascripts/components/form_select.jsx
@@ -1,8 +1,8 @@
 class FormSelect extends React.Component {
   render() {
-    let options = [<option value=""></option>]
+    let options = [<option value="" value={this.props.id}></option>]
 
     let full_options = options.concat(this.props.options.map((o) => {return <option value={o.value} key={o.value}>{o.display}</option>}))
-    return <select className="form-control test-select" name={this.props.name} id={this.props.id}>{full_options}</select>
+    return <select className="form-control test-select" name={this.props.name} id={this.props.id} key={this.props.id}>{full_options}</select>
   }
 }
