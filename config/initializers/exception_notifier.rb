b72b414984258616292791197f47b1db9d6ac3d5
Tom Smyth <tom@sassafras.coop>
2017-10-13 11:46:21 -0400

    7273: Removed unused file

--- config/initializers/exception_notifier.rb
+++
@@ -1,8 +0,0 @@
-# Include some default data with all notifications
-module ExceptionNotifier
-  def notify_exception(e, options = {})
-    options[:data] ||= {}
-    options[:data][:host] = configatron.url.host
-    super(e, options)
-  end
-end
