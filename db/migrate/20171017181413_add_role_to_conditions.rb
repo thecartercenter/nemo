5fba656a2ac6e7cba768e656d56e57c5412472c9
Tom Smyth <tom@sassafras.coop>
2017-10-19 16:36:35 -0400

    7173: Added down migrations

--- db/migrate/20171017181413_add_role_to_conditions.rb
+++ db/migrate/20171017181413_add_role_to_conditions.rb
@@ -1,7 +1,11 @@
 class AddRoleToConditions < ActiveRecord::Migration
-  def change
+  def up
     add_column :conditions, :role, :string
     execute("UPDATE conditions SET role = 'display'")
     change_column_null :conditions, :role, false
+  end
+
+  def down
+    remove_column :conditions, :role
   end
 end
