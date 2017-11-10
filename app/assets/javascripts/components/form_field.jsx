1a540b98ab8b43ee5f6bea2be88cb1ed5d42b45d
Devney <devneyhamilton@gmail.com>
2017-11-10 15:06:27 -0500

    7170: fix form field component.

--- app/assets/javascripts/components/form_field.jsx
+++ app/assets/javascripts/components/form_field.jsx
@@ -1,15 +1,28 @@
 class FormField extends React.Component {
   render() {
-    var control = <input className="text form-control" key="input" type="text" name="questioning[condition_attributes][value]" id="questioning_condition_attributes_value" defaultValue={this.props.value} />
+    //default to text
+    var control = <input
+      className="text form-control"
+      key="input"
+      type="text"
+      name={this.props.name}
+      id={this.props.id}
+      defaultValue={this.props.value}
+     />
     if (this.props.type === "select") {
-      control = <FormSelect value={this.props.value} key="input" name={this.props.name} id={this.props.id} options={this.props.options} changeFunc={this.props.changeFunc} />
+      control = <FormSelect value={this.props.value}
+        key="input"
+        name={this.props.name}
+        id={this.props.id}
+        options={this.props.options}
+        changeFunc={this.props.changeFunc}
+      />
     }
     return (
       <div className="field">
         <label htmlFor={this.props.for} key={this.props.for}> {this.props.label} </label>
         {control}
       </div>
-
-    )
+    );
   }
 }
