c010de7f284408f3556b20c903f9d74e0e406ee4
Devney <devneyhamilton@gmail.com>
2017-11-10 14:23:36 -0500

    7170: style updates

--- app/assets/javascripts/components/conditions_form_field.jsx
+++ app/assets/javascripts/components/conditions_form_field.jsx
@@ -2,21 +2,21 @@ class ConditionsFormField extends React.Component {
 
   constructor(props) {
     super();
-    this.getFieldData = this.getFieldData.bind(this)
-    this.updateFieldData = this.updateFieldData.bind(this)
-    this.formatRefQingOptions = this.formatRefQingOptions.bind(this)
-    this.buildUrl = this.buildUrl.bind(this)
+    this.getFieldData = this.getFieldData.bind(this);
+    this.updateFieldData = this.updateFieldData.bind(this);
+    this.formatRefQingOptions = this.formatRefQingOptions.bind(this);
+    this.buildUrl = this.buildUrl.bind(this);
     this.state = props;
   }
 
-  updateFieldData(ref_qing_id) {
-    this.getFieldData(ref_qing_id);
+  updateFieldData(refQingId) {
+    this.getFieldData(refQingId);
   }
 
-  getFieldData(ref_qing_id) {
-    ELMO.app.loading(true)
+  getFieldData(refQingId) {
+    ELMO.app.loading(true);
     var self = this;
-    var url = this.buildUrl(ref_qing_id)
+    var url = this.buildUrl(refQingId);
     $.ajax(url)
       .done(function(response) {
           self.setState(response);
@@ -25,35 +25,68 @@ class ConditionsFormField extends React.Component {
           console.log(exception);
         })
         .always(function() {
-          ELMO.app.loading(false)
+          ELMO.app.loading(false);
         });
   }
 
-  buildUrl(ref_qing_id) {
-    var url = ELMO.app.url_builder.build('questionings', 'condition-form')
-    url += '?ref_qing_id=' + ref_qing_id
-    url += '&form_id=' + this.state.form_id
+  buildUrl(refQingId) {
+    var url = `${ELMO.app.url_builder.build('questionings', 'condition-form')}?ref_qing_id=${refQingId}&form_id=${this.state.form_id}`
     if (this.state.questioning_id) {
-      url += '&questioning_id=' + this.state.questioning_id
+      url += '&questioning_id=' + this.state.questioning_id;
     }
-    return url
+    return url;
   }
 
   formatRefQingOptions(reference_qing_options) {
     return reference_qing_options.map(function(o){
-      return {id: o.id, name: `${o.rank}. ${o.code}`, key: o.id}
-    })
+      return {id: o.id, name: `${o.rank}. ${o.code}`, key: o.id};
+    });
   }
 
   render() {
-    let condition_field = <input type="hidden" name="questioning[condition_attributes][id]" id="questioning_condition_attributes_id" key="questioning_condition_attributes_id" value={this.state.id}/>
-    let rq_options =  this.state.refable_qing_options
-    let ref_qing_form_field = <FormField name="questioning[condition_attributes][ref_qing_id]" for="questioning_condition_attributes_ref_qing_id" label={I18n.t('activerecord.attributes.condition.ref_qing_id')} type="select" options={this.formatRefQingOptions(rq_options)} id="questioning_condition_attributes_ref_qing_id" key="questioning_condition_attributes_ref_qing_id" changeFunc={this.updateFieldData} value={this.state.ref_qing_id}/>
-    let operator_options =  this.state.operator_options
-    let operator_form_field = <FormField name="questioning[condition_attributes][op]" for="questioning_condition_attributes_op" label={I18n.t('activerecord.attributes.condition.op')} type="select" options={operator_options} id="questioning_condition_attributes_op" key="questioning_condition_attributes_op" value={this.state.op}/>
-    let value_options =  this.state.value_options
-    let value_form_field =  <FormField name="questioning[condition_attributes][value]" for="questioning_condition_attributes_value" label={I18n.t('activerecord.attributes.condition.value')} type="text" id="questioning_condition_attributes_value" key="questioning_condition_attributes_value" options={value_options} value={this.state.value}/>
-    let fields = [ref_qing_form_field, operator_form_field, value_form_field]
-    return <div>{fields}</div>
+    let name_prefix = 'questioning[condition_attributes]';
+    let id_prefix = 'questioning_condition_attributes';
+    let condition_field_props = {
+      type: "hidden",
+      name: `${name_prefix}[id]`,
+      id: `${id_prefix}_id`,
+      key: `${id_prefix}_id`,
+      value: this.state.id ? this.state.id : ""
+    };
+    let ref_qing_field_props = {
+      type: "select",
+      name: `${name_prefix}[ref_qing_id]`,
+      for: `${id_prefix}_ref_qing_id`,
+      key: `${id_prefix}_ref_qing_id`,
+      value: this.state.ref_qing_id ? this.state.ref_qing_id : "",
+      label: I18n.t('activerecord.attributes.condition.ref_qing_id'),
+      options: this.formatRefQingOptions(this.state.refable_qing_options),
+      changeFunc: this.updateFieldData
+    };
+    let operator_field_props = {
+      type: "select",
+      name: `${name_prefix}[op]`,
+      for: `${id_prefix}_op`,
+      key: `${id_prefix}_op`,
+      value: this.state.op ? this.state.op : "",
+      label: I18n.t('activerecord.attributes.condition.op'),
+      options: this.state.operator_options
+    };
+    let value_field_props = {
+      type: "text",
+      name: `${name_prefix}[value]`,
+      for: `${id_prefix}_value`,
+      key: `${id_prefix}_value`,
+      value: this.state.value ? this.state.value : "",
+      label: I18n.t('activerecord.attributes.condition.value'),
+    };
+    return (
+      <div>
+        <input {...condition_field_props}/>
+        <FormField {...ref_qing_field_props} />
+        <FormField {...operator_field_props} />
+        <FormField {...value_field_props} />
+      </div>
+    );
   }
 }
