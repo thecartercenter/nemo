3a7ffd7dd6dd80e3eaa9bfe6fd0fc943afaff56d
Devney <devneyhamilton@gmail.com>
2017-11-20 14:11:41 -0500

    7203: added test for select one option set.

--- app/serializers/option_node_condition_view_serializer.rb
+++ app/serializers/option_node_condition_view_serializer.rb
@@ -3,14 +3,12 @@ class OptionNodeConditionViewSerializer < ActiveModel::Serializer
 
   def levels
     path = OptionNodePath.new(option_set: object.option_set, target_node: object)
-    puts "in serializer!"
-    puts object
 
     levels = path.nodes_without_root.each_with_index.map do |node, i|
       puts i
       puts path.nodes_for_depth(i + 1)
       {
-        name: path.level_name_for_depth(i + 1),
+        name: path.multilevel? ? path.level_name_for_depth(i + 1) : nil,
         selected: path.nodes[i+1].try(:option).try(:id),
         options: path.nodes_for_depth(i+1).map {|n| {name: n.option.name, id: n.option.id}} #TO DO Translation
       }
--- spec/requests/option_set_spec.rb
+++ spec/requests/option_set_spec.rb
@@ -9,56 +9,66 @@ describe "option_sets", type: :request do
   end
 
   describe "get_condition_view" do
-    #set up option sets
+    context "multilevel" do
 
-    let(:option_set) { create(:option_set, super_multilevel: true) }
+      let(:option_set) { create(:option_set, super_multilevel: true) }
 
-    it "should return json of condition view of option set" do
-      puts option_set
+      it "should return json of condition view of option set" do
+        option_node = option_set.option_nodes.select {|n| n.option && n.option.canonical_name == "Tree"}[0]
+        get "/en/m/#{get_mission.compact_name}/option-sets/#{option_set.id}/condition-form-view?node_id=#{option_node.id}"
+        expected = {
+          levels: [
+            {name: "Kingdom",
+             selected: get_node(option_set, "Plant").option.id,
+             options: [
+               {name: "Animal", id: get_node(option_set, "Animal").option.id},
+               {name: "Plant", id: get_node(option_set, "Plant").option.id}
+              ]
+            },
+            {name: "Family",
+             selected: get_node(option_set, "Tree").option.id,
+             options: [
+               {name: "Tree", id: get_node(option_set, "Tree").option.id},
+               {name: "Flower", id: get_node(option_set, "Flower").option.id}
+              ]
+            },
+            {name: "Species",
+             selected: nil,
+             options: [
+               {name: "Oak", id: get_node(option_set, "Oak").option.id},
+               {name: "Pine", id: get_node(option_set, "Pine").option.id}
+              ]
+            }
+          ]
+        }.to_json
 
-      puts option_set.option_nodes
-
-      option_node = option_set.option_nodes.select {|n| n.option && n.option.canonical_name == "Tree"}[0]
-
-      puts option_node
-
-      get "/en/m/#{get_mission.compact_name}/option-sets/#{option_set.id}/condition-form-view?node_id=#{option_node.id}"
-
-      #set up url
-
-      #make request
-      expected = {
-        levels: [
-          {name: "Kingdom",
-           selected: get_node(option_set, "Plant").option.id,
-           options: [
-             {name: "Animal", id: get_node(option_set, "Animal").option.id},
-             {name: "Plant", id: get_node(option_set, "Plant").option.id}
-            ]
-          },
-          {name: "Family",
-           selected: get_node(option_set, "Tree").option.id,
-           options: [
-             {name: "Tree", id: get_node(option_set, "Tree").option.id},
-             {name: "Flower", id: get_node(option_set, "Flower").option.id}
-            ]
-          },
-          {name: "Species",
-           selected: nil,
-           options: [
-             {name: "Oak", id: get_node(option_set, "Oak").option.id},
-             {name: "Pine", id: get_node(option_set, "Pine").option.id}
-            ]
-          }
-        ]
-      }.to_json
-      #check expectation
-      expect(response).to have_http_status(200)
-      puts(JSON.parse(response.body))
-      expect(response.body).to eq expected
+        expect(response).to have_http_status(200)
+        expect(response.body).to eq expected
+      end
     end
 
+    context "select one" do
+      let(:option_set) { create(:option_set) }
 
+      it "should return json of condition view of option set" do
+        option_node = option_set.option_nodes.select {|n| n.option && n.option.canonical_name == "Cat"}[0]
+        get "/en/m/#{get_mission.compact_name}/option-sets/#{option_set.id}/condition-form-view?node_id=#{option_node.id}"
+        expected = {
+          levels: [
+            {name: nil,
+             selected: get_node(option_set, "Cat").option.id,
+             options: [
+               {name: "Cat", id: get_node(option_set, "Cat").option.id},
+               {name: "Dog", id: get_node(option_set, "Dog").option.id}
+              ]
+            }
+          ]
+        }.to_json
+
+        expect(response).to have_http_status(200)
+        expect(response.body).to eq expected
+      end
+    end
   end
 
   def get_node(option_set, name)
