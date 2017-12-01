37d4a3e38aaca2a779cb47e07ac5ea73b56c4a41
Devney <devneyhamilton@gmail.com>
2017-12-01 12:51:52 -0500

    7253: If qing has options, display question type above options.

--- app/decorators/printable/qing_decorator.rb
+++ app/decorators/printable/qing_decorator.rb
@@ -7,5 +7,11 @@ module Printable
       str << h.reqd_sym if required?
       str << name
     end
+
+    def question_type
+      content = "#{I18n.t("question_type.#{qtype_name}")}:"
+      str = h.content_tag(:strong, content)
+      str << h.tag(:br)
+    end
   end
 end
