b4c552d819491b62d25201a726d17b63862fdd63
Tom Smyth <tom@sassafras.coop>
2017-12-08 11:07:29 -0500

    3298: Refactored form hints so they work on modals and are greatly simplified

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -42,9 +42,6 @@
     // prevent double submission of any forms on the page
     $('form').preventDoubleSubmission();
 
-    // hookup hint tooltips if any on the page
-    self.hookup_hints();
-
     self.set_alert_timeout();
   }
 
@@ -132,27 +129,6 @@
   // hides any success alerts after a delay
   klass.prototype.set_alert_timeout = function() { var self = this;
     window.setTimeout(function() {$(".alert-success").slideUp(); return false;}, 4000);
-  };
-
-  klass.prototype.hookup_hints = function() { var self = this;
-
-    // when click on the page, hints disappear
-    $('html').on('click', function(e) {
-      $('a.hint').popover('hide');
-    });
-
-    // initialize popovers
-    $('a.hint').popover({
-      html: true,
-      trigger: 'manual'
-    // toggle current tooltip
-    }).on('click', function(e) {
-      // hide previous hints
-      $('a.hint').popover('hide');
-      $(this).popover('toggle');
-      e.stopPropagation();
-      e.preventDefault();
-    });
   };
 
   // Shows/hides loading indicator.
