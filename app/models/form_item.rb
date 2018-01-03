8c6fc6cf27371c4551d82886a59f070471edbd52
Tom Smyth <tom@sassafras.coop>
2018-01-03 10:27:02 -0500

    7180: Main spec passing

--- app/models/form_item.rb
+++ app/models/form_item.rb
@@ -100,8 +100,10 @@ class FormItem < ApplicationRecord
     end
   end
 
-  def preordered_descendants
-    self.class.sort_by_ancestry(descendants.order(:rank)) { |a, b| a.rank <=> b.rank }
+  def preordered_descendants(eager_load:)
+    rel = descendants.order(:rank)
+    rel = rel.includes(eager_load) if eager_load
+    self.class.sort_by_ancestry(rel) { |a, b| a.rank <=> b.rank }
   end
 
   def sorted_children
@@ -154,6 +156,10 @@ class FormItem < ApplicationRecord
 
   def display_conditionally?
     display_if != "always" && display_conditions.any?
+  end
+
+  def condition_group
+    @condition_group ||= ConditionGroup.new(true_if: display_if, members: display_conditions)
   end
 
   def group?
