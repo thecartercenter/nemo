d8ca87eb6716ee46b03f57dd915955864792d004
Tom Smyth <tom@sassafras.coop>
2018-01-11 15:12:02 -0500

    7172: Fixed unnecessary change to serializer

--- app/serializers/condition_serializer.rb
+++ app/serializers/condition_serializer.rb
@@ -1,4 +1,4 @@
 class ConditionSerializer < ActiveModel::Serializer
   attributes :id, :conditionable_id, :conditionable_type, :ref_qing_id, :form_id,
-    :op, :value, :option_node_id, :option_set_id
+    :op, :value, :option_node_id
 end
