992df9a1f4fbcfb7e3d6803692d14f34af5d4ad9
Devney <devneyhamilton@gmail.com>
2018-01-23 14:23:23 -0500

    7727: feature spec for editing group in form

---
+++ spec/features/qing_group_form_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+feature "adding and editing qing group on form", js: true do
+  let!(:user) { create(:user) }
+  let!(:form) { create(:form, name: "Foo",
+    question_types: %w(integer multilevel_select_one select_one integer)) }
+
+  before do
+    get_mission.setting.update_attribute(:preferred_locales_str, "en,fr")
+    login(user)
+  end
+
+  scenario "add a new group to a form" do
+    visit(edit_form_path(form, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+
+    click_link("Add Group")
+    fill_in("Name (English):", with: "Foo Group")
+    fill_in("Hint (English):", with: "Bar Hint")
+    fill_in("Name (Français):", with: "Fou Groupe")
+    fill_in("Hint (Français):", with: "Barre Hinte")
+
+    expect(page).not_to have_content("Item Name (English)")
+    check("qing_group_repeatable")
+    # fill_in("Item Name (English):", with: "Test Name")
+    # fill_in("Item Name (Français):", with: "Nom d'essaie")
+    uncheck("qing_group_one_screen")
+    within(".modal") { click_button("Save") }
+
+    within(".form-items") { expect(page).to have_content("Foo Group") }
+    all(".form-items .action_link.edit")[-1].click
+    expect(page).to have_field("qing_group_group_name_en", with: "Foo Group")
+    expect(page).to have_field("qing_group_group_name_fr", with: "Fou Groupe")
+    expect(page).to have_field("qing_group_group_hint_en", with: "Bar Hint")
+    expect(page).to have_field("qing_group_group_hint_fr", with: "Barre Hinte")
+    expect(page).to have_field("qing_group_repeatable", checked: true)
+    expect(page).to have_field("qing_group_one_screen", checked: false)
+    #expect(page).to have_field("qing_group_group_item_name_en", with: "Test Name")
+    #expect(page).to have_field("qing_group_group_item_name_fr", with: "Nom d'essaie")
+
+    fill_in("Name (English):", with: "New Group Name")
+    within(".modal") { click_button("Save") }
+    within(".form-items") { expect(page).to have_content("New Group Name") }
+  end
+
+end
