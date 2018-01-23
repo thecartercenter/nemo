c9dbb21ec51a66e831f310bf33a9b00a141dd7a8
Tom Smyth <tom@sassafras.coop>
2018-01-23 17:57:58 -0500

    7878: Removed unnecessary validation check on form item position update

--- app/controllers/form_items_controller.rb
+++ app/controllers/form_items_controller.rb
@@ -8,13 +8,7 @@ class FormItemsController < ApplicationController
     # Blank parent_id means parent is root
     params[:parent_id] = @form_item.form.root_id if params[:parent_id].blank?
 
-    # Moves to new position and attempts to save.
     @form_item.move(FormItem.find(params[:parent_id]), params[:rank].to_i)
-
-    if @form_item.valid?
-      render nothing: true, status: 204
-    else
-      render @form_item.errors, status: 422
-    end
+    render nothing: true, status: 204
   end
 end
