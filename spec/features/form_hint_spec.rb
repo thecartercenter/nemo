288d7d149e52fc7bb08ed8ea5c9650f5f4589113
Tom Smyth <tom@sassafras.coop>
2018-02-07 15:14:38 -0500

    7953: Fixed failing hint spec

--- spec/features/form_hint_spec.rb
+++ spec/features/form_hint_spec.rb
@@ -22,6 +22,14 @@ feature 'form hints', js: true do
     visit(new_question_path(mode: "m", mission_name: mission_name, locale: "en"))
     select("Select One", from: "Type")
     click_link("Create New Option Set")
+    wait_modal_to_be_visible
+
+    # This is required for the spec to pass but not sure why.
+    # Best theory is that as the modal is animating down, the hints won't show.
+    # So you have to wait for the modal to be fully animated in (even after it's visible).
+    # I couldn't find a way to do this using `expect`.
+    sleep 1
+
     expect_hint_to_show_and_hide(
       selector: ".option_set_name a.hint",
       text: "A name that summarizes this set of options."
