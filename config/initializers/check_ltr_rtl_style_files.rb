c054b4bfd481b9672c2f5b5e9869b495a7a9e499
Tom Smyth <tom@sassafras.coop>
2018-02-22 09:50:17 -0500

    7866: Renamed .css.scss to .scss

--- config/initializers/check_ltr_rtl_style_files.rb
+++ config/initializers/check_ltr_rtl_style_files.rb
@@ -1,5 +1,5 @@
-ltr = Rails.root.join('app', 'assets', 'stylesheets', 'application_ltr.css.scss')
-rtl = Rails.root.join('app', 'assets', 'stylesheets', 'application_rtl.css.scss')
+ltr = Rails.root.join('app', 'assets', 'stylesheets', 'application_ltr.scss')
+rtl = Rails.root.join('app', 'assets', 'stylesheets', 'application_rtl.scss')
 unless `diff #{ltr} #{rtl}`.match(/\A\d+c\d+\n< \$dir: ltr;\n---\n> \$dir: rtl;\n\z/)
   raise "LTR and RTL stylesheets must be identical except for $dir variable. "\
     "Use `diff` to determine differences."
