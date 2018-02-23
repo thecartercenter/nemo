31540c377720fbe76f92d010a954d1d0ebfe904d
Tom Smyth <tom@sassafras.coop>
2018-02-23 15:58:23 -0500

    7483: Refactored old use of inject

--- app/models/option_set.rb
+++ app/models/option_set.rb
@@ -211,7 +211,7 @@ class OptionSet < ApplicationRecord
     if is_standard?
       copies.to_a.sum(&:answer_count)
     else
-      questionings.inject(0) { |sum, q| sum += q.answers.count }
+      questionings.to_a.sum { |q| q.answers.count }
     end
   end
 
