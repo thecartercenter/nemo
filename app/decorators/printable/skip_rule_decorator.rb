1137edd5fd3c15f6273495ad5d4be2a8fc186cc8
Devney <devneyhamilton@gmail.com>
2018-02-23 17:37:50 -0500

    7178: this does the basic idea, could use some tweaking.

---
+++ app/decorators/printable/skip_rule_decorator.rb
@@ -0,0 +1,16 @@
+module Printable
+  class SkipRuleDecorator < ApplicationDecorator
+    delegate_all
+    # Generates a human readable representation of a skip rule.
+
+    def human_readable(prefs = {})
+      "Skip to #{dest_item.code} if #{decorate_conditions}"
+    end
+
+    def decorate_conditions
+      decorated_conditions = ConditionDecorator.decorate_collection(condition_group.members)
+      concatenator = (condition_group.true_if == "all_met") ? I18n.t("common.AND") : I18n.t("common.OR")
+      decorated_conditions.map{ |c| c.human_readable}.join(" #{concatenator} ")
+    end
+  end
+end
