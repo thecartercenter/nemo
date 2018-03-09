b8d3aea1d5b309910824212136e278b6e77df59c
Tom Smyth <tom@sassafras.coop>
2018-03-09 14:59:20 -0500

    7822: Added theme flag file migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_307_154_526) do
+ActiveRecord::Schema.define(version: 20_180_309_195_347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
