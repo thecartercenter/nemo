fe52b4cbdb8f1a43bcc2da93c64d69c05a66c86b
Tom Smyth <tom@sassafras.coop>
2018-03-20 11:53:43 -0400

    Added creation of tmp dir to spec before suite hook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,17 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/collection_matchers'
+# frozen_string_literal: true
 
-# Add this to load Capybara integration:
-require 'capybara/rspec'
-require 'capybara/rails'
-require 'selenium-webdriver'
-require 'capybara-screenshot/rspec'
-require 'paperclip/matchers'
-require 'cancan/matchers'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rspec/rails"
+require "rspec/collection_matchers"
+require "capybara/rspec"
+require "capybara/rails"
+require "selenium-webdriver"
+require "capybara-screenshot/rspec"
+require "paperclip/matchers"
+require "cancan/matchers"
+require "fileutils"
 
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new(
@@ -32,7 +33,7 @@ end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   # persist example status
@@ -77,6 +78,11 @@ RSpec.configure do |config|
     # In CI environments, the SCSS preprocessor won't have been run because the developer won't have
     # done it and assets:precompile won't have been run. This could lead to issues with feature specs.
     Themeing::ScssPreprocessor.new.run
+  end
+
+  # Make sure we have a tmp dir as some specs rely on it.
+  config.before(:suite) do
+    FileUtils.mkdir_p(Rails.root.join("tmp"))
   end
 
   # We have to use around so that this block runs before arounds and befores in actual specs.
