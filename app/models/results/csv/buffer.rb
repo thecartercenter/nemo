0030d4380de01079eda062cf5919d59728a885a6
Tom Smyth <tom@sassafras.coop>
2018-03-20 14:15:17 -0400

    7338: Refactor more responsibility into buffer

--- app/models/results/csv/buffer.rb
+++ app/models/results/csv/buffer.rb
@@ -4,26 +4,27 @@ module Results
   # Temporarily holds a set of rows from the CSV output while they are being populated.
   module Csv
     class Buffer
-      attr_accessor :cells, :header_map, :empty
+      attr_accessor :csv, :cells, :header_map, :empty, :cur_group_path
       alias empty? empty
 
       def initialize
         init_header_map
+        init_cur_group_path
         self.cells = []
         self.empty = true
       end
 
       # Takes a row from the DB result and sets the appropriate cells in the buffer.
       def process_row(row)
+        # If path to group has changed, it's time to dump the CSV row and start a new one!
+        dump_row if group_path_changed?(row)
         add_common_cells(row) if empty?
         header = row["question_code"]
         cells[col_index_for_header(header)] = row["answer_value"]
       end
 
-      def dump_to(csv)
-        return if empty?
-        csv << cells
-        clear
+      def finish
+        dump_row
       end
 
       def headers
@@ -33,6 +34,34 @@ module Results
       end
 
       private
+
+      def dump_row
+        return if empty?
+        csv << cells
+        clear
+      end
+
+      def init_cur_group_path
+        # -1s ensure it will be changed on first loop.
+        self.cur_group_path = group_path_keys.map { |key| [key, -1] }.to_h
+      end
+
+      # Keys/col names from a row that uniquely identify the row's group path.
+      def group_path_keys
+        @group_path_keys ||= %w[response_id group1_rank group1_item_num]
+      end
+
+      # Checks if the path to the group for the current answer row has changed from the previous row.
+      def group_path_changed?(row)
+        changed = false
+        group_path_keys.each do |key|
+          if row[key] != cur_group_path[key]
+            changed = true
+            cur_group_path[key] = row[key]
+          end
+        end
+        changed
+      end
 
       def init_header_map
         self.header_map = ActiveSupport::OrderedHash.new
--- app/models/results/csv/generator2.rb
+++ app/models/results/csv/generator2.rb
@@ -6,10 +6,9 @@ module Results
     class Generator2
       UUID_LENGTH = 36 # This should never change.
 
-      attr_accessor :cur_group_path, :buffer
+      attr_accessor :buffer
 
       def initialize(responses)
-        init_cur_group_path
         self.buffer = Buffer.new
       end
 
@@ -28,37 +27,12 @@ module Results
 
       def csv_body
         CSV.generate(row_sep: configatron.csv_row_separator) do |csv|
-          set_db_timezone do
-            query.each do |row|
-              # If path to group has changed, it's time to dump the CSV row and start a new one!
-              buffer.dump_to(csv) if group_path_changed?(row)
-              buffer.process_row(row)
-            end
-            buffer.dump_to(csv)
+          buffer.csv = csv
+          query.each do |row|
+            buffer.process_row(row)
           end
+          buffer.finish
         end
-      end
-
-      def init_cur_group_path
-        # -1s ensure it will be changed on first loop.
-        self.cur_group_path = group_path_keys.map { |key| [key, -1] }.to_h
-      end
-
-      # Checks if the path to the group for the current answer row has changed from the previous row.
-      def group_path_changed?(row)
-        changed = false
-        group_path_keys.each do |key|
-          if row[key] != cur_group_path[key]
-            changed = true
-            cur_group_path[key] = row[key]
-          end
-        end
-        changed
-      end
-
-      # Keys/col names from a row that uniquely identify the row's group path.
-      def group_path_keys
-        @group_path_keys ||= %w[response_id group1_rank group1_item_num]
       end
 
       def query
