f4e14cec621365a4d018d0d860089193b75ebdcc
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-21 17:12:50 +0100

    remove comments

--- config/initializers/verify_host.rb
+++ config/initializers/verify_host.rb
@@ -1,7 +1,3 @@
-puts '--------------------'
-puts !configatron.offline_mode, 'mode'
-puts configatron.url.host.nil?, 'host'
-puts '--------------------'
 if !configatron.offline_mode && configatron.url.host.nil?
   raise "Host is required when offline mode is false. See local_config.rb.example for configuration guidance."
 end
