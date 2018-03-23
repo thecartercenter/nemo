2527cf95f75cbf75ab51530beb92a9a78ce3c444
Devney <devneyhamilton@gmail.com>
2018-03-23 13:56:06 -0400

    7201: added Response Condition Serializer back in

---
+++ app/serializers/response_condition/condition_serializer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module ResponseCondition
+  # Serializes condition for response web form display logic
+  class ConditionSerializer < ActiveModel::Serializer
+    #attributes :ref_qing_id, :op, :value, :option_node_id
+    format_keys :lower_camel
+  end
+end
