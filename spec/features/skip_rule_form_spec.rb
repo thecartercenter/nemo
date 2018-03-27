3569c92d8cf1d268cc0a0e9eb1525d3bd6efe82d
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-03-27 12:23:55 +0100

    update skip logic spec

--- spec/features/skip_rule_form_spec.rb
+++ spec/features/skip_rule_form_spec.rb
@@ -16,9 +16,9 @@ feature "skip rule form", js: true do
         question_types: %w[integer integer integer integer integer])
     end
 
-    let(:dest_qing_str) { "Question ##{form.c[4].full_dotted_rank} #{form.c[4].code}" }
-    let(:first_cond_str) { "Question ##{form.c[0].full_dotted_rank} #{form.c[0].code} is equal to 5" }
-    let(:second_cond_str) { "Question ##{form.c[1].full_dotted_rank} #{form.c[1].code} is equal to 10" }
+    let(:dest_qing_str) { dest_qing_str }
+    let(:first_cond_str) { first_cond_str }
+    let(:second_cond_str) { second_cond_str }
 
     include_examples :form_logic do
       subject { form }
@@ -30,9 +30,18 @@ feature "skip rule form", js: true do
           {ref_qing_id: form.c[0].id, op: "eq", value: "5"},
           {ref_qing_id: form.c[1].id, op: "eq", value: "10"}
         ])
+      form.c[2].skip_rules.create!(destination: "item", dest_item: form.c[4], skip_if: "all_met",
+        conditions_attributes: [
+          {ref_qing_id: form.c[0].id, op: "eq", value: "25"},
+          {ref_qing_id: form.c[1].id, op: "neq", value: "20"}
+        ])
     end
 
     context "with existing skip rule" do
+      let(:dest_qing_str) { "Question ##{form.c[4].full_dotted_rank} #{form.c[4].code}" }
+      let(:first_cond_str) { "Question ##{form.c[0].full_dotted_rank} #{form.c[0].code} is equal to 5" }
+      let(:second_cond_str) { "Question ##{form.c[1].full_dotted_rank} #{form.c[1].code} is equal to 10" }
+
       scenario "read-only mode" do
         expected = "Skip to #{dest_qing_str} if any of these conditions are met "\
         "#{first_cond_str} "\
@@ -98,6 +107,40 @@ feature "skip rule form", js: true do
           expect_selected_operator("< less than")
           expect_filled_in_value("20")
         end
+      end
+    end
+
+    context "existing questioning" do
+      let(:dest_qing_str) { "Question ##{form.c[4].full_dotted_rank} #{form.c[4].code}" }
+      let(:first_cond_str) { "Question ##{form.c[0].full_dotted_rank} #{form.c[0].code} is less than 1000" }
+      let(:second_cond_str) { "Question ##{form.c[1].full_dotted_rank} #{form.c[1].code} is equal to 10" }
+
+      scenario do
+        visit("/en/m/#{form.mission.compact_name}/questionings/#{form.c[2].id}/edit")
+
+        # edit first rule
+        within(all(".skip-rule")[0]) do
+          find('select[name*="\\[skip_if\\]"]').select("if all of these conditions are met")
+          within(all(".condition-fields")[0]) do
+            select_question(form.c[0].code)
+            select_operator("< less than")
+            fill_in_value("1000")
+          end
+        end
+
+        # delete second rule
+        within(".rule-2") do
+          click_delete_link
+        end
+
+        click_on "Save"
+
+        rule_2 = "Skip to #{dest_qing_str} if all of these conditions are met "\
+        "#{first_cond_str} "\
+        "#{second_cond_str}"
+
+        visit("/en/m/#{form.mission.compact_name}/questionings/#{form.c[2].id}")
+        expect(page).to have_content(rule_2)
       end
     end
   end
