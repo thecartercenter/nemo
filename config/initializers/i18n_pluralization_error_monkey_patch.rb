e9ed2f9ab3cab3a6b52a845ef45e8e78a0e418b9
Tom Smyth <tom@sassafras.coop>
2018-04-03 14:20:33 -0400

    8231: Added monkey patch and spec to fix arabic pluralization issue

---
+++ config/initializers/i18n_pluralization_error_monkey_patch.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module I18n
+  module Backend
+    # Overriding the pluralization method so if the proper plural form is missing we will try
+    # to fallback to the default gettext plural form (which is the `germanic` one).
+    # From: https://github.com/svenfuchs/i18n/issues/123
+    # See spec/helpers/arabic_missing_plural_spec.rb for spec that fails without this.
+    module Pluralization
+      def pluralize(locale, entry, count)
+        return entry unless entry.is_a?(Hash) && count
+
+        pluralizer = pluralizer(locale)
+        if pluralizer.respond_to?(:call)
+          return entry[:zero] if count.zero? && entry.key?(:zero)
+
+          plural_key = pluralizer.call(count)
+          return entry[plural_key] if entry.key?(plural_key)
+
+          # fallback to the default gettext plural forms if real entry is missing (for example :few)
+          default_gettext_key = count == 1 ? :one : :other
+          return entry[default_gettext_key] if entry.key?(default_gettext_key)
+
+          # If nothing is found throw the classic exception
+          raise InvalidPluralizationData.new(entry, count)
+        else
+          super
+        end
+      end
+    end
+  end
+end
