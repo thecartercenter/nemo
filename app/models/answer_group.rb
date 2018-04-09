e57cc25bbccc4d6bae7d4ed3ed778d241596b88c
Devney <devneyhamilton@gmail.com>
2018-04-09 16:31:45 -0400

    8182: stub classes

---
+++ app/models/answer_group.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Corresponds with a QingGroup
+# An AnswerGroups's parent is an AnswerGroupSet or an AnswerGroup.
+# Its children can be Answers, AnswerSets, AnswerGroups, or AnswerGroupSets.
+class AnswerGroup < ResponseNode
+end
---
+++ app/models/response_node.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Parent class for nodes in the answer hierarchy
+class ResponseNode
+  self.table_name = "Answers"
+end
