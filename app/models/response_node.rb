3966366d5ed7c6f6e1d205d4fe26b21b59502678
Devney <devneyhamilton@gmail.com>
2018-04-10 14:40:12 -0400

    8182: use rank for deterministic ordering

--- app/models/response_node.rb
+++ app/models/response_node.rb
@@ -4,7 +4,5 @@
 class ResponseNode < ApplicationRecord
   self.table_name = "answers"
 
-  has_closure_tree(dependent: :destroy)
-  # TODO: add ordering using rank? or add ordering column that is populated based on form item rank?
-  # TODO: we might want to add touch since the trees aren't very deep
+  has_closure_tree(dependent: :destroy, order: :rank)
 end
