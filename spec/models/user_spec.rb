15f983b4e7a3eb59bfcb9e2a0826f8aa41755856
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-04-17 13:42:04 +0100

    update specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -96,10 +96,16 @@ describe User do
     end
 
     describe "uniqueness" do
-      before { @user = create(:user, login: "jayita") }
+      let!(:user) { create(:user, login: "jayita") }
+
+      it "returns an error when the login is not unique" do
+        @user2 = build(:user, login: "jayita")
+        expect(@user2).not_to be_valid
+        expect(@user2.errors.full_messages.join).to match(/Username: Please enter a unique value/)
+      end
 
       it "can create a user with the same login after deleting" do
-        @user.destroy
+        user.destroy
         @user2 = build(:user, login: "jayita")
         expect(@user2).to be_valid
       end
