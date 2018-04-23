728c1d3f2b88f3cad835179a262ef9563a27bdb4
Tom Smyth <tom@sassafras.coop>
2018-04-23 12:58:04 -0400

    Added deleted_at awareness to group validations

--- app/models/user_group_assignment.rb
+++ app/models/user_group_assignment.rb
@@ -4,6 +4,6 @@ class UserGroupAssignment < ApplicationRecord
   belongs_to :user
   belongs_to :user_group
 
-  validates :user, uniqueness: { scope: :user_group }
+  validates :user, uniqueness: {scope: %i[user_group deleted_at]}
   validates :user, :user_group, presence: true
 end
