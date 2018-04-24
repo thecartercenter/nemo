4ee09e8e95c11f9feeb1fde8282dbdad3cf7b124
Tom Smyth <tom@sassafras.coop>
2018-04-24 16:25:10 -0400

    8336: Add migration to fix nulls in level_names column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_423_205_259) do
+ActiveRecord::Schema.define(version: 20_180_424_202_221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
