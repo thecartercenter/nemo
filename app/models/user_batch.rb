22eeea2dacd541f093b0c20917c4b5a1a4100335
Tom Smyth <tom@sassafras.coop>
2018-04-27 14:27:10 -0400

    8339: Fix test and add new expectations

--- app/models/user_batch.rb
+++ app/models/user_batch.rb
@@ -226,7 +226,7 @@ class UserBatch
   # Takes a happy, uncoerced gender and stuffs it into a recognized box
   def coerce_gender(gender_string)
     return nil unless gender_string.present?
-    gender_options = GENDER_OPTIONS.map { |g| [g, I18n.t("user.gender_options.#{g}")] }.to_h
+    gender_options = User::GENDER_OPTIONS.map { |g| [g, I18n.t("user.gender_options.#{g}")] }.to_h
     gender_selection = gender_options.find { |k, v| gender_string == v }
     gender = gender_selection.try(:first) || :specify
     gender_custom = gender_string if gender == :specify
