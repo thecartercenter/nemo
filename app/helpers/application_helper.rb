bca30782ed01a9ad756193dbb1ed5436529fad15
Tom Smyth <tom@sassafras.coop>
2018-05-08 11:16:37 -0400

    8392: Fix ODK manifest URL on HTTPS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,8 @@ module ApplicationHelper
   # Builds a URL with the exact given path based on the system's host, protocol, and port.
   def url_for_path(path)
     path = "/#{path}" if path[0] != "/"
-    URI::HTTP.build(configatron.url.to_h.merge(path: path))
+    builder = configatron.url.protocol == "https" ? URI::HTTPS : URI::HTTP
+    builder.build(configatron.url.to_h.merge(path: path))
   end
 
   # hackish way of getting the route key identical to what would be returned by model_name.route_key on a model
--- spec/requests/odk_form_download_spec.rb
+++ spec/requests/odk_form_download_spec.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 # Using request spec b/c Authlogic won't work with controller spec
 describe FormsController, :odk, type: :request do
   let!(:mission) { create(:mission) }
   let!(:user) { create(:user, role_name: :coordinator, mission: mission) }
-  let!(:form) { create(:form, :published, mission: mission, question_types: %w(integer integer)) }
-  let!(:form_select) { create(:form, :published, mission: mission, question_types: %w(integer select_one)) }
+  let!(:form) { create(:form, :published, mission: mission, question_types: %w[integer integer]) }
+  let!(:form_select) { create(:form, :published, mission: mission, question_types: %w[integer select_one]) }
   let!(:form_multiselect) do
-    create(:form, :published, mission: mission, question_types: %w(integer multilevel_select_one))
+    create(:form, :published, mission: mission, question_types: %w[integer multilevel_select_one])
   end
-
 
   before do
     login(user)
@@ -18,7 +19,8 @@ describe FormsController, :odk, type: :request do
   context "for regular mission" do
     describe "listing forms" do
       it "should succeed" do
-        get_s("/en/m/#{mission.compact_name}/formList", format: :xml)
+        get("/en/m/#{mission.compact_name}/formList", format: :xml)
+        expect(response).to be_success
 
         # Only form_multiselect should have a manifest.
         assert_select("xform", count: 3) do |elements|
@@ -30,50 +32,71 @@ describe FormsController, :odk, type: :request do
       end
 
       it "should succeed with no locale" do
-        get_s("/m/#{mission.compact_name}/formList", format: :xml)
+        get("/m/#{mission.compact_name}/formList", format: :xml)
+        expect(response).to be_success
       end
     end
 
     describe "showing forms" do
       it "should succeed" do
         # XML rendering details are tested elsewhere.
-        get_s("/en/m/#{mission.compact_name}/forms/#{form_multiselect.id}", format: :xml)
+        get("/en/m/#{mission.compact_name}/forms/#{form_multiselect.id}", format: :xml)
+        expect(response).to be_success
       end
 
       it "should succeed with no locale" do
-        get_s("/m/#{mission.compact_name}/forms/#{form_multiselect.id}", format: :xml)
+        get("/m/#{mission.compact_name}/forms/#{form_multiselect.id}", format: :xml)
+        expect(response).to be_success
       end
     end
 
     describe "odk manifest" do
       context "for form with no option sets" do
         it "should render empty manifest tag" do
-          get_s("/en/m/#{mission.compact_name}/forms/#{form.id}/manifest")
+          get("/en/m/#{mission.compact_name}/forms/#{form.id}/manifest")
+          expect(response).to be_success
           assert_select("manifest", count: 1)
           assert_select("mediaFile", count: 0)
         end
       end
 
       context "for normal form" do
+        let(:ifa) { ItemsetsFormAttachment.new(form: form_multiselect) }
+
         it "should render regular manifest tag" do
-          get_s("/m/#{mission.compact_name}/forms/#{form_multiselect.id}/manifest")
-          @ifa = ItemsetsFormAttachment.new(form: form_multiselect)
+          get("/m/#{mission.compact_name}/forms/#{form_multiselect.id}/manifest")
+          expect(response).to be_success
           assert_select("filename", text: "itemsets.csv")
-          assert_select("hash", text: @ifa.md5)
-          assert_select("downloadUrl", text: "http://www.example.com/#{@ifa.path}")
+          assert_select("hash", text: ifa.md5)
+          assert_select("downloadUrl", text: "http://www.example.com/#{ifa.path}")
+        end
+
+        context "on https" do
+          around do |example|
+            configatron.url.protocol = "https"
+            example.run
+            configatron.url.protocol = "http"
+          end
+
+          it "should use https in URL" do
+            get("/m/#{mission.compact_name}/forms/#{form_multiselect.id}/manifest")
+            expect(response).to be_success
+            assert_select("downloadUrl", text: "https://www.example.com/#{ifa.path}")
+          end
         end
       end
     end
 
     describe "getting itemsets file" do
       context "for form with option sets" do
+        let(:ifa) { ItemsetsFormAttachment.new(form: form_multiselect) }
+
         before do
-          @ifa = ItemsetsFormAttachment.new(form: form_multiselect)
-          @ifa.ensure_generated
+          ifa.ensure_generated
         end
 
         it "should succeed" do
-          get_s("/#{@ifa.path}")
+          get("/#{ifa.path}")
           expect(response).to be_success
           expect(response.body).to match(/,Cat/)
         end
