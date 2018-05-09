13e213db8f24328abfb0fe5127e842c99eaf3126
Tom Smyth <tom@sassafras.coop>
2018-05-09 12:53:30 -0400

    8394: Fix issue with handling of OGG files in new paperclip version

--- app/models/media/audio.rb
+++ app/models/media/audio.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class Media::Audio < Media::Object
-  validates_attachment_content_type :item, content_type: [%r{\Aaudio/.*\Z}, 'video/webm']
+  # For some reason, the mime-magic gem returns video/ogg for audio OGG files. Same for WEBM.
+  validates_attachment_content_type :item, content_type: [%r{\Aaudio/.*\Z}, "video/ogg", "video/webm"]
   validates_attachment_file_name :item, matches: /\.(mp3|ogg|webm|wav)\Z/i
 
   def thumb_path
