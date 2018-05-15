5766fadf66a16af3ff0211171fa2a0c6891fac39
Tom Smyth <tom@sassafras.coop>
2018-05-15 15:15:00 -0400

    8125: Reduce inserts in hierarchy insert

--- db/migrate/20180511173513_add_answer_hierarchies.rb
+++ db/migrate/20180511173513_add_answer_hierarchies.rb
@@ -7,7 +7,11 @@ class AddAnswerHierarchies < ActiveRecord::Migration
     remove_index "answer_hierarchies", name: "answer_desc_idx"
 
     execute("DELETE FROM answer_hierarchies")
+
+    @inserts = []
     add_for([])
+
+    do_inserts
 
     add_index "answer_hierarchies", %w[ancestor_id descendant_id generations],
       name: "answer_anc_desc_idx", unique: true, using: :btree
@@ -17,14 +21,30 @@ class AddAnswerHierarchies < ActiveRecord::Migration
   private
 
   def add_for(ancestor_ids)
-    parent_where_expr = ancestor_ids.empty? ? "IS NULL" : "= '#{ancestor_ids.last}'"
+    answers = answers_by_parent_id[ancestor_ids.last]
+    return if answers.blank?
     (ancestor_ids + [:self]).reverse.each_with_index do |aid, i|
-      aid_expr = aid == :self ? "id" : "'#{aid}'"
-      execute("INSERT INTO answer_hierarchies(ancestor_id, descendant_id, generations)
-        SELECT #{aid_expr}, id, #{i} FROM answers WHERE parent_id #{parent_where_expr}")
+      answers.each do |answer|
+        aid_str = aid == :self ? answer["id"] : aid
+        @inserts << "('#{aid_str}', '#{answer['id']}', #{i})"
+      end
     end
-    execute("SELECT id FROM answers WHERE parent_id #{parent_where_expr}").each do |row|
-      add_for(ancestor_ids + [row["id"]])
+    answers.each { |r| add_for(ancestor_ids + [r["id"]]) }
+  end
+
+  def answers_by_parent_id
+    @answers_by_parent_id ||= execute("SELECT id, parent_id FROM answers WHERE deleted_at IS NULL")
+      .to_a.group_by { |r| r["parent_id"] }
+  end
+
+  def do_inserts
+    batched_insert("answer_hierarchies", "ancestor_id,descendant_id,generations", @inserts)
+  end
+
+  def batched_insert(tbl, cols, rows)
+    rows.each_slice(1000) do |slice|
+      execute("INSERT INTO #{tbl} (#{cols})
+        VALUES #{slice.join(',')}")
     end
   end
 end
