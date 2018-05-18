5be3a69e6b95f96e8dbf7a4864eaa378a5603f99
Tom Smyth <tom@sassafras.coop>
2018-05-18 13:12:36 -0400

    8125: Fix migration reversibility

--- db/migrate/20180504182310_add_new_rank_to_answers.rb
+++ db/migrate/20180504182310_add_new_rank_to_answers.rb
@@ -1,8 +1,12 @@
 class AddNewRankToAnswers < ActiveRecord::Migration
-  def change
+  def up
     add_column :answers, :new_rank, :integer
     execute("UPDATE answers SET new_rank = 0")
     change_column_null :answers, :new_rank, false
     add_index :answers, :new_rank
+  end
+
+  def down
+    remove_column :answers, :new_rank
   end
 end
