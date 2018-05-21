c5d957d05b258ed1a93a8e677e4be01424693d26
Jill Dimond <jill@sassafrastech.com>
2018-05-21 15:39:51 -0400

    8438: added recursive groups

--- app/models/results/response_tree_builder.rb
+++ app/models/results/response_tree_builder.rb
@@ -9,13 +9,18 @@ module Results
 
     def build
       @rn = AnswerGroup.new
-      add_children
+      add_children(@form.root_group, @rn)
       @rn
     end
 
-    def add_children
-      @form.sorted_children.each do |q|
-        @rn.children << Answer.new(questioning_id: q.id, new_rank: q.rank)
+    def add_children(form_node, response_node)
+      form_node.children.each do |q|
+        if q.class == QingGroup
+          response_node.children << AnswerGroup.new(questioning_id: q.id, new_rank: q.rank)
+          add_children(q, response_node.children.last) if q.children.present?
+        else
+          response_node.children << Answer.new(questioning_id: q.id, new_rank: q.rank)
+        end
       end
     end
   end
--- spec/models/results/response_tree_builder_spec.rb
+++ spec/models/results/response_tree_builder_spec.rb
@@ -14,14 +14,39 @@ describe "response tree" do
 
       # expect to be the same qing_ids
       expect(response_tree.root_answers.map(&:questioning_id))
-        .to eq(form1.root_questionings.map(&:id))
+        .to include(form1.root_questionings[0].id, form1.root_questionings[1].id,
+          form1.root_questionings[2].id)
 
       # same ranks
       expect(response_tree.root_answers.map(&:new_rank))
-        .to eq(form1.root_questionings.map(&:rank))
+        .to include(form1.root_questionings[0].rank, form1.root_questionings[1].rank,
+          form1.root_questionings[2].rank)
     end
   end
 
-  # context "more complex forms" do
-  # end
+  context "forms with groups" do
+    let(:form2) { create(:form, question_types: ["text", %w[select_one text], "multilevel_select_one"]) }
+
+    it "should produce the correct tree" do
+      response_tree = Results::ResponseTreeBuilder.new(form2).build
+
+      puts response_tree.children.inspect
+      # same number of children at the root
+      expect(response_tree.children.length).to eq form2.root_group.children.length
+
+      # expect to include one answer group
+      expect(response_tree.children.map(&:class)).to include(AnswerGroup)
+
+      # expect to be the same qing_ids
+      expect(response_tree.children.map(&:questioning_id))
+        .to eq(form2.root_group.children.map(&:id))
+
+      # expect to be the same qing_ids
+      expect(response_tree.children.map(&:new_rank))
+        .to eq(form2.root_group.children.map(&:rank))
+
+      # expect appropriate number of children in the group
+      expect(response_tree.children.descendants.length).to eq 2
+    end
+  end
 end
