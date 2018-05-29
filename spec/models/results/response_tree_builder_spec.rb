632fa8e601e51e7453c14b2f70db234f4c0db8ee
Jill Dimond <jill@sassafrastech.com>
2018-05-29 09:40:07 -0400

    8438: refactored tests

--- spec/models/results/response_tree_builder_spec.rb
+++ spec/models/results/response_tree_builder_spec.rb
@@ -3,81 +3,55 @@
 require "spec_helper"
 
 describe "response tree" do
-  # form with repeat group
   context "simple form" do
-    let(:form1) { create(:form, question_types: %w[text text text]) }
+    let(:form) { create(:form, question_types: %w[text text text]) }
 
     it "should produce a simple tree from a form with three children" do
-      response_tree = Results::ResponseTreeBuilder.new(form1).build
-      # same number of leaves at the root
-      expect(response_tree.root_answers.count).to eq form1.root_questionings.count
-
-      # expect to be the same qing_ids
-      expect(response_tree.root_answers.map(&:questioning_id))
-        .to include(form1.root_questionings[0].id, form1.root_questionings[1].id,
-          form1.root_questionings[2].id)
-
-      # same ranks
-      expect(response_tree.root_answers.map(&:new_rank))
-        .to include(form1.root_questionings[0].rank, form1.root_questionings[1].rank,
-          form1.root_questionings[2].rank)
+      response_tree = Results::ResponseTreeBuilder.new(form).build
+      expect_children(response_tree, %w[Answer Answer Answer], form.children.map(&:id))
     end
   end
 
-  context "forms with groups" do
-    let(:form2) { create(:form, question_types: ["text", %w[select_one text], "text"]) }
+  context "forms with a group" do
+    let(:form) { create(:form, question_types: ["text", %w[select_one text], "text"]) }
 
     it "should produce the correct tree" do
-      response_tree = Results::ResponseTreeBuilder.new(form2).build
+      response_tree = Results::ResponseTreeBuilder.new(form).build
 
-      # same number of children at the root
-      expect(response_tree.children.length).to eq form2.root_group.children.length
-
-      # expect to include one answer group
-      expect(response_tree.children.map(&:class)).to include(AnswerGroup)
-
-      # expect to be the same qing_ids
-      expect(response_tree.children.map(&:questioning_id))
-        .to eq(form2.root_group.children.map(&:id))
-
-      # expect to be the same qing_ids
-      expect(response_tree.children.map(&:new_rank))
-        .to eq(form2.root_group.children.map(&:rank))
-
-      # expect appropriate number of children in the group
-      expect(response_tree.children.descendants.length).to eq 2
+      expect_children(response_tree, %w[Answer AnswerGroup Answer], form.children.map(&:id))
+      expect_children(response_tree.children[1], %w[Answer Answer], form.children[1].children.map(&:id))
     end
   end
 
   context "multilevel answer forms" do
-    let(:form3) { create(:form, question_types: %w[text multilevel_select_one]) }
+    let(:form) { create(:form, question_types: %w[text multilevel_select_one]) }
 
     it "should create the appropriate multilevel answer tree" do
-      response_tree = Results::ResponseTreeBuilder.new(form3).build
+      response_tree = Results::ResponseTreeBuilder.new(form).build
 
-      expect(response_tree.children[0].class).to eq AnswerSet
-      expect(response_tree.children[1].class).to eq Answer
-
-      expect(response_tree.children[0].children[0].class).to eq Answer
-      expect(response_tree.children[0].children[1].class).to eq Answer
-      expect(response_tree.children[0].children.length).to eq 2
-
-      # test rank and questioning id, takes a node, array (types), array (qing_ids)
+      expect_children(response_tree, %w[AnswerSet Answer], form.children.map(&:id))
+      expect_children(response_tree.children[0], %w[Answer Answer], form.children[0].children.map(&:id))
     end
   end
 
   context "repeat group forms" do
-    let(:form4) { create(:form, question_types: ["text", {repeating: {items: ["text", "text"]}}]) }
+    let(:form) { create(:form, question_types: ["text", {repeating: {items: %w[text text]}}]) }
 
     it "should create the appropriate repeating group tree" do
-      response_tree = Results::ResponseTreeBuilder.new(form4).build
+      response_tree = Results::ResponseTreeBuilder.new(form).build
 
-      expect(response_tree.children[0].class).to eq AnswerGroupSet
-      expect(response_tree.children[0].children[0].class).to eq AnswerGroup
-      expect(response_tree.children[0].children.length).to eq 1
-      expect(response_tree.children[0].children[0].children.length).to eq 2
-      expect(response_tree.children[0].children[0].children[0].class).to eq Answer
-      expect(response_tree.children[0].children[0].children[1].class).to eq Answer
+      expect_children(response_tree, %w[AnswerGroupSet Answer], form.children.map(&:id))
+      expect_children(response_tree.children[0], %w[AnswerGroup], [form.children[0].id])
+      expect_children(response_tree.children[0].children[0], %w[Answer Answer],
+        form.children[0].children.map(&:id))
     end
+  end
+
+  private
+
+  def expect_children(node, types, qing_ids)
+    expect(node.children.map(&:type)).to eq types
+    expect(node.children.map(&:questioning_id)).to eq qing_ids
+    expect(node.children.map(&:new_rank).sort).to eq((1..node.children.size).to_a)
   end
 end
