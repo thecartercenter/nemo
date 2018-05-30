f07f68bc91920fea82aec784dc555276db1e2f04
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-05-30 18:27:41 +0100

    7196 update spec

--- spec/models/batch_destroy_spec.rb
+++ spec/models/batch_destroy_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 describe BatchDestroy, type: :model do
@@ -15,61 +17,61 @@ describe BatchDestroy, type: :model do
         users << current_user
         destroyer.destroy!
 
-        # current user
-        expect(User.count).to eq(1)
-        expect(current_user.active).to be_truthy
+        expect(User.all.to_a).to contain_exactly(current_user)
       end
 
-      it "deactivates everyone but the current user" do
+      it "deactivates user with mission and skips current user" do
         users << current_user << user
         destroyer.destroy!
 
         # users that exist but are not the current user should be deactivated
         User.where.not(email: "current@user.com").each do |u|
-          expect(u.active).to be_falsey
+          expect(u).not_to be_active
         end
 
         # current user and user with a mission
         expect(User.count).to eq(2)
-        expect(current_user.active).to be_truthy
+        expect(User.all.to_a).to contain_exactly(current_user, user)
       end
     end
 
     describe "question" do
-      let(:questions) { create_list(:question, 3) }
-      let(:batch) { questions }
+      let(:q1) { create(:question) }
+      let(:q2) { create(:question) }
+      let(:q3) { create(:question) }
+      let(:batch) { [q1, q2, q3] }
 
       describe "published forms" do
         before do
-          allow(questions.first).to receive(:published?) { true }
-          allow(questions.second).to receive(:published?) { true }
-          allow(questions.third).to receive(:published?) { false }
+          allow(q1).to receive(:published?) { true }
+          allow(q2).to receive(:published?) { true }
+          allow(q3).to receive(:published?) { false }
         end
 
         it "skips questions that are on published forms" do
           destroyer.destroy!
-          expect(Question.count).to eq(2)
+          expect(Question.all.to_a).to contain_exactly(q1, q2)
         end
       end
 
       describe "answers" do
         before do
-          allow(questions.first).to receive(:has_answers?) { false }
-          allow(questions.second).to receive(:has_answers?) { false }
-          allow(questions.third).to receive(:has_answers?) { true }
+          allow(q1).to receive(:has_answers?) { false }
+          allow(q2).to receive(:has_answers?) { false }
+          allow(q3).to receive(:has_answers?) { true }
         end
 
         it "skips questions that have answers" do
           destroyer.destroy!
-          expect(Question.count).to eq(1)
+          expect(Question.all.to_a).to contain_exactly(q3)
         end
       end
 
       describe "no answers and no published forms" do
         before do
-          allow(questions.first).to receive(:has_answers?) { false }
-          allow(questions.second).to receive(:has_answers?) { false }
-          allow(questions.third).to receive(:published?) { false }
+          allow(q1).to receive(:has_answers?) { false }
+          allow(q2).to receive(:has_answers?) { false }
+          allow(q3).to receive(:published?) { false }
         end
 
         it "skips questions that have answers" do
