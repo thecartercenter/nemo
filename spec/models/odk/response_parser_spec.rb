a7adfad2f055be2fce092806f17b982179ee6a7d
Devney <devneyhamilton@gmail.com>
2018-06-15 11:04:01 -0700

    6441: consolidate specs more

--- spec/models/odk/response_parser_spec.rb
+++ spec/models/odk/response_parser_spec.rb
@@ -60,8 +60,8 @@ describe Odk::ResponseParser do
 
     context "response with select multiple" do
       let(:question_types) { %w[select_multiple select_multiple text] }
-      let(:opt1) {form.c[0].option_set.sorted_children[0]}
-      let(:opt2) {form.c[0].option_set.sorted_children[1]}
+      let(:opt1) { form.c[0].option_set.sorted_children[0] }
+      let(:opt2) { form.c[0].option_set.sorted_children[1] }
       let(:xml_values) { ["on#{opt1.id} on#{opt2.id}", "none", "A"] }
       let(:expected_values) { ["#{opt1.option.name};#{opt2.option.name}", nil, "A"] }
 
@@ -80,7 +80,7 @@ describe Odk::ResponseParser do
     context "with location type" do
       let(:question_types) { %w[location location text] }
       let(:xml_values) { ["12.345600 -76.993880", "12.3456 -76.99388 123.456 20.0", "A"] }
-      let(:expected_values) {xml_values}
+      let(:expected_values) { xml_values }
 
       it "parses location answers correctly" do
         Odk::ResponseParser.new(response: response, files: files).populate_response
@@ -88,33 +88,31 @@ describe Odk::ResponseParser do
       end
     end
 
-    context "with date, time, and datetime types" do
-      let(:question_types) { %w[datetime date time] }
-      let(:xml_values) { ["2017-07-12T16:40:00.000+03", "2017-07-01", "14:30:00.000+03"] }
-      let(:expected_values) { ["2017-07-12 07:40:00 -0600", "2017-07-01", "2000-01-01 14:30:00 UTC"] }
-
+    context "with time-related questions" do
       around do |example|
         in_timezone("Saskatchewan") { example.run } # Saskatchewan is -06
       end
+      
+      context "with date, time, and datetime types" do
+        let(:question_types) { %w[datetime date time] }
+        let(:xml_values) { ["2017-07-12T16:40:00.000+03", "2017-07-01", "14:30:00.000+03"] }
+        let(:expected_values) { ["2017-07-12 07:40:00 -0600", "2017-07-01", "2000-01-01 14:30:00 UTC"] }
 
-      it "retains timezone information for datetime but not time" do
-        Odk::ResponseParser.new(response: response, files: files).populate_response
-        expect_children(response.root_node, %w[Answer Answer Answer], form.c.map(&:id), expected_values)
-      end
-    end
-
-    context "with prefilled timestamps" do
-      let(:question_types) { %w[formstart text formend] }
-      let(:xml_values) { ["2017-07-12T16:40:12.000-06", "A", "2017-07-12T16:42:43.000-06"] }
-      let(:expected_values) { ["2017-07-12 16:40:12 -06", "A", "2017-07-12 16:42:43 -06"] }
-
-      around do |example|
-        in_timezone("Saskatchewan") { example.run } # Saskatchewan is -06
+        it "retains timezone information for datetime but not time" do
+          Odk::ResponseParser.new(response: response, files: files).populate_response
+          expect_children(response.root_node, %w[Answer Answer Answer], form.c.map(&:id), expected_values)
+        end
       end
 
-      it "accepts data normally" do
-        Odk::ResponseParser.new(response: response, files: files).populate_response
-        expect_children(response.root_node, %w[Answer Answer Answer], form.c.map(&:id), expected_values)
+      context "with prefilled timestamps" do
+        let(:question_types) { %w[formstart text formend] }
+        let(:xml_values) { ["2017-07-12T16:40:12.000-06", "A", "2017-07-12T16:42:43.000-06"] }
+        let(:expected_values) { ["2017-07-12 16:40:12 -06", "A", "2017-07-12 16:42:43 -06"] }
+
+        it "accepts data normally" do
+          Odk::ResponseParser.new(response: response, files: files).populate_response
+          expect_children(response.root_node, %w[Answer Answer Answer], form.c.map(&:id), expected_values)
+        end
       end
     end
   end
@@ -151,7 +149,7 @@ describe Odk::ResponseParser do
       expect_children(response.root_node.c[1].c[1], %w[Answer Answer], form.c[1].c.map(&:id), %w[D E])
     end
   end
-  
+
   context "form with multilevel answer" do
     let(:question_types) { %w[text multilevel_select_one multilevel_select_one multilevel_select_one] }
     let(:level1_opt) { form.c[1].option_set.sorted_children[1] }
