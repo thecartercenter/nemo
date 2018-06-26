f3d42420ca676903cef1e5d6e69cca058e015b25
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-06-26 17:36:32 +0100

    7446 add audio prompt column to question

---
+++ db/migrate/20180626162616_add_audio_prompt_to_questions.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Add audio prompt to question table for paperclip
+class AddAudioPromptToQuestions < ActiveRecord::Migration
+  def up
+    add_attachment :questions, :audio_prompt
+  end
+
+  def down
+    remove_attachment :questions, :audio_prompt
+  end
+end
