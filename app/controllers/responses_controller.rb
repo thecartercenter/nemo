0f2776508715925cccbf28b3c1b313eac31e0709
Tom Smyth <tom@sassafras.coop>
2018-06-28 14:46:46 -0400

    7824: Fixed response form strong params

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -282,43 +282,39 @@ class ResponsesController < ApplicationController
 
   def response_params
     if params[:response]
-      reviewer_only = if @response.present? && can?(:review, @response)
-        [:reviewed, :reviewer_notes, :reviewer_id]
-      else
-        []
-      end
+      to_permit = [:form_id, :user_id, :incomplete]
 
-      permitted = params.require(:response).permit(:form_id, :user_id, :incomplete, *reviewer_only)
+      if @response.present? && can?(:review, @response)
+        to_permit << [:reviewed, :reviewer_notes, :reviewer_id]
+      end
 
       # In some rare cases, create or update can occur without answers_attributes. Not sure how.
       # Also need to respect the modify_answers permission here.
-      if params[:response][:answers_attributes] &&
-        (action_name != "update" || can?(:modify_answers, @response))
-        permitted = permitted.merge(params.require(:response).permit(permitted_answer_attributes))
+      if (ans_attribs = params[:response][:answers_attributes]) &&
+          (action_name != "update" || can?(:modify_answers, @response))
+
+        # We need to permit each possible answer attribute key for each given hash key in answers_attributes.
+        # See https://stackoverflow.com/a/36779535/2066866.
+        to_permit << {
+          answers_attributes: ans_attribs.keys.map { |k| [k, permitted_answer_attributes] }.to_h
+        }
       end
 
-      permitted
+      params.require(:response).permit(to_permit)
     end
   end
 
+  # Returns the permitted keys for an answer and its choices.
   def permitted_answer_attributes
-    # The answers_attributes hash might look like {'2746' => { ... }, '2731' => { ... }, ... }
-    # The keys are irrelevant so we permit all of them, but we only want to permit certain attribs
-    # on the answers.
-    permitted_answer_attribs = %w(id value option_id option_node_id questioning_id relevant rank
+    @permitted_answer_attributes ||= %w(
+      id value option_id option_node_id questioning_id relevant rank
       time_value(1i) time_value(2i) time_value(3i) time_value(4i) time_value(5i) time_value(6i)
       datetime_value
       datetime_value(1i) datetime_value(2i) datetime_value(3i)
       datetime_value(4i) datetime_value(5i) datetime_value(6i)
       date_value(1i) date_value(2i) date_value(3i) inst_num media_object_id _destroy
-      date_value)
-
-    {
-      answers_attributes: [
-        *permitted_answer_attribs,
-        choices_attributes: [:id, :option_id, :option_node_id, :checked]
-      ]
-    }
+      date_value
+    ) << {choices_attributes: %i[id option_id option_node_id checked]}
   end
 
   def check_form_exists_in_mission
