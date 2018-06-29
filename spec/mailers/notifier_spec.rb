397b824e9daee03e0022d3bb1964bb24f538e0f0
Amandeep Singh Sapra <sapraaman@gmail.com>
2018-06-29 19:54:05 +0300

    6729 Lint issues fixed

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -6,13 +6,12 @@ describe Notifier do
   let!(:mission_1) { create(:mission, name: "Test Mission") }
   let!(:mission_2) { create(:mission) }
   let!(:mission_3) { create(:mission) }
-  
+
   let!(:user) { create(:user, role_name: :staffer, mission: mission_1) }
-  let!(:mission_2_coordinator) { create(:user, role_name: :coordinator, mission: mission_2)}
-  let!(:mission_3_coordinator) { create(:user, role_name: :coordinator, mission: mission_3)}
+  let!(:mission_2_coordinator) { create(:user, role_name: :coordinator, mission: mission_2) }
+  let!(:mission_3_coordinator) { create(:user, role_name: :coordinator, mission: mission_3) }
 
   context "password reset email" do
-
     let(:mail) { described_class.password_reset_instructions(user).deliver_now }
 
     it "should have user's email in to field" do
@@ -20,7 +19,6 @@ describe Notifier do
     end
 
     context "user belongs to 1 mission" do
-
       context "user's mission does not have any coordinator" do
         it "should not have anyone in reply-to" do
           expect(mail.reply_to).to be_empty
@@ -28,9 +26,8 @@ describe Notifier do
       end
 
       context "user's mission has 1 coordinator" do
-        let!(:coordinator) { create(:user, role_name: :coordinator, mission: mission_1)}
-        let!(:staffer) { create(:user, role_name: :staffer, mission: mission_1)}
-        
+        let!(:coordinator) { create(:user, role_name: :coordinator, mission: mission_1) }
+        let!(:staffer) { create(:user, role_name: :staffer, mission: mission_1) }
         it "should have coordinator's email in reply-to" do
           expect(mail.reply_to).to eq [coordinator.email]
         end
@@ -41,24 +38,22 @@ describe Notifier do
       end
 
       context "user's mission has multiple coordinators" do
-        let!(:coordinator_1) { create(:user, role_name: :coordinator, mission: mission_1)}
-        let!(:coordinator_2) { create(:user, role_name: :coordinator, mission: mission_1)}
-
+        let!(:coordinator_1) { create(:user, role_name: :coordinator, mission: mission_1) }
+        let!(:coordinator_2) { create(:user, role_name: :coordinator, mission: mission_1) }
         it "should have all coordinator's email in reply-to" do
           expect(mail.reply_to.length).to eq 2
           expect(mail.reply_to).to include coordinator_1.email
           expect(mail.reply_to).to include coordinator_2.email
         end
       end
-
     end
 
     context "user belongs to multiple missions" do
-      let!(:mission_1_coordinator_1) { create(:user, role_name: :coordinator, mission: mission_1)}
-      let!(:mission_1_coordinator_2) { create(:user, role_name: :coordinator, mission: mission_1)}
+      let!(:mission_1_coordinator_1) { create(:user, role_name: :coordinator, mission: mission_1) }
+      let!(:mission_1_coordinator_2) { create(:user, role_name: :coordinator, mission: mission_1) }
 
-      before { user.assignments.create!(mission: mission_2, role: :staffer) } 
-      
+      before { user.assignments.create!(mission: mission_2, role: :staffer) }
+
       it "should have all mission coordinator's email in reply-to" do
         expect(mail.reply_to.length).to eq 3
         expect(mail.reply_to).to include mission_1_coordinator_1.email
@@ -70,6 +65,5 @@ describe Notifier do
         expect(mail.reply_to).to_not include mission_3_coordinator.email
       end
     end
-
   end
 end
