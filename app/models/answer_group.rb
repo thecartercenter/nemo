3b98199f775852302d73593e868833d240f4f39d
Scott Nelson <scott@scottnelson.co>
2018-07-01 21:36:00 -0400

    Render answer tree when editing or viewing response

--- app/models/answer_group.rb
+++ app/models/answer_group.rb
@@ -4,4 +4,5 @@
 # An AnswerGroups's parent is an AnswerGroupSet or an AnswerGroup.
 # Its children can be Answers, AnswerSets, AnswerGroups, or AnswerGroupSets.
 class AnswerGroup < ResponseNode
+  belongs_to :questioning, class_name: 'QingGroup'
 end
