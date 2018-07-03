b6404e4d86cbefe4234b435c0078b1f72cb9d193
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-07-03 15:59:44 +0100

    7446 clean up code

--- spec/decorators/odk/form_decorator_spec.rb
+++ spec/decorators/odk/form_decorator_spec.rb
@@ -2,25 +2,21 @@
 
 require "spec_helper"
 
-# Form decorator spec
-describe Odk::FormDecorator, :odk, :reset_factory_sequences do
+describe Odk::FormDecorator, :odk do
   include_context "odk rendering"
 
   describe "needs_manifest?" do
     let(:decorated_form) { decorate(form) }
+    subject(:needs_manifest?) { decorated_form.needs_manifest? }
 
     context "for form with single level option sets only" do
       let(:form) { create(:form, question_types: %w[select_one]) }
-      it "should return false" do
-        expect(decorated_form.needs_manifest?).to be_falsey
-      end
+      it { is_expected.to be_falsey }
     end
 
     context "for form with multi level option set" do
       let(:form) { create(:form, question_types: %w[select_one multilevel_select_one]) }
-      it "should return true" do
-        expect(decorated_form.needs_manifest?).to be_truthy
-      end
+      it { is_expected.to be_truthy }
     end
   end
 end
