d6f8c8b77ad450fb0ce07929942c098c3daa4994
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-07-03 13:21:11 +0100

    7446 refactor specs

--- spec/decorators/odk/form_decorator_spec.rb
+++ spec/decorators/odk/form_decorator_spec.rb
@@ -3,24 +3,22 @@
 require "spec_helper"
 
 # Form decorator spec
-module Odk
-  describe FormDecorator, :odk, :reset_factory_sequences do
-    describe "needs_manifest?", :odk do
-      context "for form with single level option sets only" do
-        before { @form = create(:form, question_types: %w(select_one)) }
-        it "should return false" do
-          decorated_form = decorate(@form)
-          expect(decorated_form.needs_manifest?).to be false
-        end
+describe Odk::FormDecorator, :odk, :reset_factory_sequences do
+  describe "needs_manifest?" do
+    let(:decorated_form) { decorate(form) }
+
+    context "for form with single level option sets only" do
+      let(:form) { create(:form, question_types: %w[select_one]) }
+      it "should return false" do
+        expect(decorated_form.needs_manifest?).to be_falsey
       end
-      context "for form with multi level option set" do
-        before { @form = create(:form, question_types: %w[select_one multilevel_select_one]) }
-        it "should return true" do
-          decorated_form = decorate(@form)
-          expect(decorated_form.needs_manifest?).to be true
-        end
+    end
+
+    context "for form with multi level option set" do
+      let(:form) { create(:form, question_types: %w[select_one multilevel_select_one]) }
+      it "should return true" do
+        expect(decorated_form.needs_manifest?).to be_truthy
       end
     end
   end
 end
-
