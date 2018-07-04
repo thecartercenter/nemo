d524ce340b08d109d5964aa07ad1a1e98f9b0a07
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-07-04 18:08:49 +0100

    7446 failing spec

--- spec/decorators/odk/form_decorator_spec.rb
+++ spec/decorators/odk/form_decorator_spec.rb
@@ -9,14 +9,30 @@ describe Odk::FormDecorator, :odk do
     let(:decorated_form) { decorate(form) }
     subject(:needs_manifest?) { decorated_form.needs_manifest? }
 
-    context "for form with single level option sets only" do
-      let(:form) { create(:form, question_types: %w[select_one]) }
-      it { is_expected.to be false }
+    context "multilevel option sets" do
+      describe "for form with single level option sets only" do
+        let(:form) { create(:form, question_types: %w[select_one]) }
+        it { is_expected.to be false }
+      end
+
+      describe "for form with multi level option set" do
+        let(:form) { create(:form, question_types: %w[select_one multilevel_select_one]) }
+        it { is_expected.to be true }
+      end
     end
 
-    context "for form with multi level option set" do
-      let(:form) { create(:form, question_types: %w[select_one multilevel_select_one]) }
-      it { is_expected.to be true }
+    context "audio prompts" do
+      context "visible questions" do
+        let(:form) { create(:form, question_types: %w[text]) }
+
+        describe "for form with audio prompt in question" do
+          before do
+            form.c[0].question.update!(audio_prompt: audio_fixture("powerup.mp3"))
+          end
+
+          it { is_expected.to be true }
+        end
+      end
     end
   end
 end
