0d28421b1c1a40513c209b6c08196347b58050aa
Devney <devneyhamilton@gmail.com>
2018-07-05 14:52:06 -0400

    8602: refactor getting item from code

--- app/models/odk/code_mapper.rb
+++ app/models/odk/code_mapper.rb
@@ -20,18 +20,22 @@ module Odk
     # format that was q#{questioning.question.id}
     def item_id_for_code(code, form)
       clean_code = code.split("_").first # remove suffix for multilevel subqings.
-      if /qing\S*/.match? clean_code
-        qing_id = clean_code.remove("qing")
-        Questioning.where(id: qing_id).pluck(:id).first
-      elsif /grp\S*/.match? clean_code
-        grp_id = clean_code.remove("grp")
-        FormItem.where(id: grp_id).pluck(:id).first
-      elsif /q\S*/.match? clean_code # fallback for older style qing odk code
-        question_id = clean_code.remove "q"
-        Questioning.where(question_id: question_id, form_id: form.id).pluck(:id).first
-      else
-        raise SubmissionError, "Code format unknown: #{code}."
-      end
+      qing_id = remove_prefix_if_matches(clean_code, "qing")
+      return Questioning.where(id: qing_id).pluck(:id).first if qing_id
+      grp_id = remove_prefix_if_matches(clean_code, "grp")
+      return FormItem.where(id: grp_id).pluck(:id).first if grp_id
+      # fallback for older style qing odk code
+      question_id = remove_prefix_if_matches(clean_code, "q")
+      return Questioning.where(question_id: question_id, form_id: form.id).pluck(:id).first if question_id
+      # if we get here, code is not a known format
+      raise SubmissionError, "Code format unknown: #{code}."
+    end
+
+    private
+
+    def remove_prefix_if_matches(string, prefix)
+      md =  string.match(/\A#{Regexp.quote(prefix)}(.+)\z/)
+      md.present? && md.length == 2 ? md[1] : nil
     end
   end
 end
