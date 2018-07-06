283a218e39ad1946b61f79da99af9d4e56aa4c61
Devney <devneyhamilton@gmail.com>
2018-07-06 13:15:15 -0400

    8606: save answer values

--- app/models/results/web_response_parser.rb
+++ app/models/results/web_response_parser.rb
@@ -4,27 +4,29 @@ module Results
   # Builds and saves response tree, with all blank answers, from a form object.
   class WebResponseParser
     def initialize
+
     end
 
     def parse(data)
-      root_questioning_id = data[:root][:questioning_id]
       root = new_node(data[:root], 0)
       add_children(data[:root][:children], root)
     end
 
     def add_children(children, parent_node)
-      children.keys.each do |k|
-        parent_node.children << new_node(children[k], k)
+      children.each do |k, v|
+        parent_node.children << new_node(v, k)
       end
       parent_node
     end
 
     def new_node(data_node, new_rank)
       type = data_node[:type].constantize
-      type.new(
+      attrs = {
         questioning_id: data_node[:questioning_id],
         new_rank: new_rank
-      )
+      }
+      attrs[:value] = data_node[:value] if type == Answer
+      type.new(attrs)
     end
   end
 end
--- spec/models/results/web_response_parser_spec.rb
+++ spec/models/results/web_response_parser_spec.rb
@@ -8,8 +8,9 @@ describe Results::WebResponseParser do
     let(:form) { create(:form, question_types: %w[text text text]) }
 
     it "builds matching tree" do
+      ActionController::Parameters.action_on_unpermitted_parameters = :raise
 
-      input = {
+      input = ActionController::Parameters.new({
         root: {
           id: "",
           type: "AnswerGroup",
@@ -39,11 +40,68 @@ describe Results::WebResponseParser do
             }
           }
         }
-      }
+      })
       tree = Results::WebResponseParser.new.parse(input)
       puts tree.debug_tree
       expect(tree.questioning_id).to eq form.root_group.id
-      expect_children(tree, %w[Answer Answer Answer], form.c.map(&:id))
+      expect_children(tree, %w[Answer Answer Answer], form.c.map(&:id), %w[A B C])
     end
+
+    context "forms with a group" do
+      let(:form) { create(:form, question_types: ["text", %w[text text], "text"]) }
+
+      xit "should produce the correct tree" do
+        input = {
+          root: {
+            id: "",
+            type: "AnswerGroup",
+            questioning_id: form.root_group.id,
+            relevant: "true",
+            children: {
+              "0" => {
+                id: "",
+                type: "Answer",
+                questioning_id: form.c[0].id,
+                relevant: "true",
+                value: "A"
+              },
+              "1" => {
+                id: "",
+                type: "AnswerGroup",
+                questioning_id: form.c[1].id,
+                relevant: "true",
+                children:  {
+                  "0" => {
+                    id: "",
+                    type: "Answer",
+                    questioning_id: form.c[0].id,
+                    relevant: "true",
+                    value: "B"
+                  },
+                  "1" => {
+                    id: "",
+                    type: "Answer",
+                    questioning_id: form.c[1].id,
+                    relevant: "true",
+                    children: "C"
+                  }
+                }
+              },
+              "2" => {
+                id: "",
+                type: "Answer",
+                questioning_id: form.c[2].id,
+                relevant: "true",
+                value: "D"
+              }
+            }
+          }
+        }
+        expect_root(response_tree, form)
+        expect_children(response_tree, %w[Answer AnswerGroup Answer], form.c.map(&:id))
+        expect_children(response_tree.c[1], %w[Answer Answer], form.c[1].c.map(&:id))
+      end
+    end
+
   end
 end
