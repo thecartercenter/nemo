62656213dffa2dfe7221b40b81969002fb8388fb
Devney <devneyhamilton@gmail.com>
2018-07-09 16:01:51 -0400

    8606: added first attempt at strong params based on current style web request; will need to update based on feature specs

--- app/models/results/web_response_parser.rb
+++ app/models/results/web_response_parser.rb
@@ -4,8 +4,28 @@ module Results
   # Builds and saves response tree, with all blank answers, from a form object.
   class WebResponseParser
 
-    PERMITTED_TOP_LEVEL_PARAMS = [:id, :questioning_id, :value]
-    #OTHER_PERMITTED = [:choices_attributes: . . . .]
+    PERMITTED_TOP_LEVEL_PARAMS = [:id, :questioning_id, :value].freeze
+    # TODO: choices, media, time and date,
+    OTHER_PERMITTED_PARAMS = [
+      :option_node_id,
+      "datetime_value(1i)",
+      "datetime_value(2i)",
+      "datetime_value(3i)",
+      "datetime_value(4i)",
+      "datetime_value(5i)",
+      "datetime_value(6i)",
+      "date_value(1i)",
+      "date_value(2i)",
+      "date_value(3i)",
+      "time_value(1i)",
+      "time_value(2i)",
+      "time_value(3i)",
+      "time_value(4i)",
+      "time_value(5i)",
+      "time_value(6i)",
+      :media_object_id,
+      {choices_attributes: %i[option_node_id checked]}
+    ].freeze
 
     def initialize
     end
@@ -30,7 +50,7 @@ module Results
       type = data_node[:type].constantize
 
       clean_params = data_node.slice(*PERMITTED_TOP_LEVEL_PARAMS).permit(
-        PERMITTED_TOP_LEVEL_PARAMS
+        [].concat(PERMITTED_TOP_LEVEL_PARAMS, OTHER_PERMITTED_PARAMS)
       )
 
       type.new(clean_params.merge(new_rank: new_rank))
