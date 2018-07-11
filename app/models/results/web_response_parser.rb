9e0298063e806f7e01d87e018f6b84065ffd025c
Devney <devneyhamilton@gmail.com>
2018-07-11 14:05:29 -0400

    8606: style improvements

--- app/models/results/web_response_parser.rb
+++ app/models/results/web_response_parser.rb
@@ -3,32 +3,31 @@
 module Results
   # Builds and saves response tree, with all blank answers, from a form object.
   class WebResponseParser
-
-    TOP_LEVEL_PARAMS = [
-      :id,
-      :questioning_id,
-      :value,
-      :option_node_id,
-      :"datetime_value(1i)",
-      :"datetime_value(2i)",
-      :"datetime_value(3i)",
-      :"datetime_value(4i)",
-      :"datetime_value(5i)",
-      :"datetime_value(6i)",
-      :"date_value(1i)",
-      :"date_value(2i)",
-      :"date_value(3i)",
-      :"time_value(1i)",
-      :"time_value(2i)",
-      :"time_value(3i)",
-      :"time_value(4i)",
-      :"time_value(5i)",
-      :"time_value(6i)",
-      :media_object_id,
+    TOP_LEVEL_PARAMS = %i(
+      :id
+      :questioning_id
+      :value
+      :option_node_id
+      :"datetime_value(1i)"
+      :"datetime_value(2i)"
+      :"datetime_value(3i)"
+      :"datetime_value(4i)"
+      :"datetime_value(5i)"
+      :"datetime_value(6i)"
+      :"date_value(1i)"
+      :"date_value(2i)"
+      :"date_value(3i)"
+      :"time_value(1i)"
+      :"time_value(2i)"
+      :"time_value(3i)"
+      :"time_value(4i)"
+      :"time_value(5i)"
+      :"time_value(6i)"
+      :media_object_id
       :choices_attributes
-    ].freeze
+    ).freeze
 
-    #TODO: fix? not quite working right
+    # TODO: fix? not quite working right
     PERMITTED_PARAMS = (TOP_LEVEL_PARAMS.dup - [:choices_attributes]).append(choices_attributes: %w[option_node_id checked])
 
     def initialize
