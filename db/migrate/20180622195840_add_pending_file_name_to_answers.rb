41b6ccab95cd93c5d8d54e6abe6a8203711cb4a6
Tom Smyth <tom@sassafras.coop>
2018-07-11 10:47:04 -0400

    Add migration version to AddPendingFileNameToAnswers

--- db/migrate/20180622195840_add_pending_file_name_to_answers.rb
+++ db/migrate/20180622195840_add_pending_file_name_to_answers.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddPendingFileNameToAnswers < ActiveRecord::Migration
+class AddPendingFileNameToAnswers < ActiveRecord::Migration[4.2]
   def change
     add_column :answers, :pending_file_name, :string
   end
