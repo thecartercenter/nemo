b3ef24748cda5ad79a02fc4c611ee2a3d92bb3fd
Devney <devneyhamilton@gmail.com>
2018-07-13 08:10:33 -0400

    8602: refactor regex usage in codemapper

--- app/models/odk/code_mapper.rb
+++ app/models/odk/code_mapper.rb
@@ -19,16 +19,16 @@ module Odk
     # Form passed in because used for fall-back to older qing odk code
     # format that was q#{questioning.question.id}
     def item_id_for_code(code, form)
-      clean_code = code.split("_").first # remove suffix for multilevel subqings.
-      qing_id = remove_prefix_if_matches(clean_code, "qing")
-      return Questioning.where(id: qing_id).pluck(:id).first if qing_id
-      grp_id = remove_prefix_if_matches(clean_code, "grp")
-      return FormItem.where(id: grp_id).pluck(:id).first if grp_id
-      # fallback for older style qing odk code
-      question_id = remove_prefix_if_matches(clean_code, "q")
-      return Questioning.where(question_id: question_id, form_id: form.id).pluck(:id).first if question_id
-      # if we get here, code is not a known format
-      raise SubmissionError, "Code format unknown: #{code}."
+      # look for prefix and id, and remove "_#{rank}" suffix for multilevel subqings.
+      md = code.match(/\A(grp|qing|q|os|on)([a-f0-9\-]+)/)
+      raise SubmissionError, "Code format unknown: #{code}." if md.blank? || md.length != 3
+      prefix = md[1]
+      id = md[2]
+      case prefix
+      when "grp", "qing" then return FormItem.where(id: id).pluck(:id).first
+      # when prefix is q, fallback for older style qing odk code
+      when "q" then return Questioning.where(question_id: id, form_id: form.id).pluck(:id).first
+      end
     end
 
     def code_for_option_node_id(node_id)
--- spec/models/odk/code_mapper_spec.rb
+++ spec/models/odk/code_mapper_spec.rb
@@ -4,22 +4,19 @@ require "rails_helper"
 
 describe Odk::CodeMapper do
   let(:form) { create(:form, question_types: ["text", %w[text text], "multilevel_select_one"]) }
+  let(:mapper) { Odk::CodeMapper.new }
 
   context "item_id_for_code" do
     it "retrieves group for group code" do
       group = form.c[1]
       code = "grp#{group.id}"
-      expected = group.id
-      actual = Odk::CodeMapper.new.item_id_for_code(code, form)
-      expect(expected).to eq actual
+      expect(mapper.item_id_for_code(code, form)).to eq group.id
     end
 
     it "retrieves questioning for questioning with code starting with qing" do
       form_item = form.c[0]
       code = "qing#{form_item.id}"
-      expected = form_item.id
-      actual = Odk::CodeMapper.new.item_id_for_code(code, form)
-      expect(expected).to eq actual
+      expect(mapper.item_id_for_code(code, form)).to eq form_item.id
     end
 
     it "retrieves correct questioning when it receives old style q{question.id} code" do
@@ -28,23 +25,22 @@ describe Odk::CodeMapper do
       assert form.c[0].question_id == other_form.c[0].question_id
       form_item = form.c[0]
       code = "q#{form_item.question.id}"
-      expected = form_item.id
-      actual = Odk::CodeMapper.new.item_id_for_code(code, form)
-      expect(expected).to eq actual
+      expect(mapper.item_id_for_code(code, form)).to eq form_item.id
     end
 
     it "handles multilevel codes" do
       multilevel_qing = form.c[2]
       codes = multilevel_qing.subqings.map { |sq| Odk::SubqingDecorator.decorate(sq).odk_code }
+      puts "codes: #{codes}"
       codes.each do |c|
-        expect(Odk::CodeMapper.new.item_id_for_code(c, form)).to eq multilevel_qing.id
+        expect(mapper.item_id_for_code(c, form)).to eq multilevel_qing.id
       end
     end
 
     it "errors when code has unknown format" do
       code = "group123"
       expect do
-        Odk::CodeMapper.new.item_id_for_code(code, form)
+        mapper.item_id_for_code(code, form)
       end.to raise_error(SubmissionError, "Code format unknown: #{code}.")
     end
   end
