fb2b278c44dee75aae604748fa3b2e81e11ef5f8
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-07-24 18:31:41 +0100

    7446 update specs

--- spec/features/questionings_form_spec.rb
+++ spec/features/questionings_form_spec.rb
@@ -2,6 +2,14 @@ require "rails_helper"
 
 describe "questionings form", js: true  do
   let(:user) { create(:user, role_name: "coordinator") }
+  let(:edit_qing_path) do
+    edit_questioning_path(
+      qing,
+      locale: "en",
+      mode: "m",
+      mission_name: get_mission.compact_name
+    )
+  end
 
   before do
     login(user)
@@ -13,7 +21,7 @@ describe "questionings form", js: true  do
 
     context "when unpublished" do
       it "should display all fields as editable" do
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         expect_editable("required", true)
         expect_editable("hidden", true)
         expect_editable("display_logic", true, field_type: "select")
@@ -21,7 +29,7 @@ describe "questionings form", js: true  do
       end
 
       it "should display logic iff metadata is not selected on a metadata type question" do
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         select "Date/Time", from: "Type"
         expect_visible("display_logic", true)
         expect_visible("skip_logic", true)
@@ -36,7 +44,7 @@ describe "questionings form", js: true  do
       end
 
       it "should hide hidden option when metadata field has a value" do
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         select "Select One", from: "questioning_question_attributes_qtype_name"
         expect_visible("hidden", true)
         select "Date/Time", from: "Type"
@@ -50,7 +58,7 @@ describe "questionings form", js: true  do
       end
 
       it "should display default only if question type is defaultable" do
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         select "Select One", from: "questioning_question_attributes_qtype_name"
         expect_editable("default", false)
         select "Text", from: "Type" # Text is defaultable
@@ -60,7 +68,7 @@ describe "questionings form", js: true  do
       end
 
       it "should display readonly only if default is not empty" do
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         select "Text", from: "Type" # Text is defaultable
         expect_editable("read_only", false)
         fill_in "Default Answer", with: "Test"
@@ -71,17 +79,20 @@ describe "questionings form", js: true  do
       end
 
       it "should be able to add audio prompt to through questioning form" do
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         attach_file("Audio Prompt", audio_fixture("powerup.mp3").path)
         click_on "Save"
-        expect(page).to have_content("Question updated successfully.")
+
+        # we can still see the audio prompt file
+        visit edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name)
+        expect(page).to have_content("powerup.mp3")
       end
     end
 
     context "when published" do
       it "should display all fields as not editable" do
         form.publish!
-        visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+        visit edit_qing_path
         expect_editable("required", false)
         expect_editable("hidden", false)
         expect_editable("display_logic", false, field_type: "select")
@@ -96,7 +107,7 @@ describe "questionings form", js: true  do
     let(:qing) { copied_form.questionings.last }
 
     it "should display all fields as editable" do
-      visit(edit_questioning_path(qing, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+      visit edit_qing_path
       expect_editable("required", true)
       expect_editable("hidden", true)
       expect_editable("display_logic", true, field_type: "select")
