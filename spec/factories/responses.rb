27f7f552a8b0ebed2a69472fa33e8246e3c91167
Devney <devneyhamilton@gmail.com>
2018-07-25 17:08:17 -0400

    8646: geopoint specs passing but take another look at model spec helpers

--- spec/factories/responses.rb
+++ spec/factories/responses.rb
@@ -1,7 +1,6 @@
 module ResponseFactoryHelper
   # Returns a potentially nested array of answers.
   def self.build_answers(response, answer_values)
-    puts "response class: #{response.class}"
     root = response.build_root_node(type: "AnswerGroup", form_item: response.form.root_group, new_rank: 0, rank: 1, inst_num: 1)
     add_level(response.form.sorted_children, answer_values, root)
     root
@@ -15,8 +14,9 @@ module ResponseFactoryHelper
           case item
           when Questioning
             add_answer(parent, item, answer_values[i], i)
-          #when QingGroup
-            #add_group(parent, item, answer_values[i], i) # not repeating
+          when QingGroup
+            add_group(parent, item, answer_values[i], i)
+          # TODO handle repeating
           end
         end
       end
@@ -25,10 +25,91 @@ module ResponseFactoryHelper
   end
 
   def self.add_answer(parent, questioning, value, new_rank)
-    parent.children.build(type: "Answer", form_item: questioning, value: value, new_rank: new_rank, rank: new_rank + 1, inst_num: parent.inst_num)
+    if questioning.multilevel?
+      build_answer_set(parent, questioning, value, new_rank)
+    else
+      parent.children.build(build_answer_attrs(parent, questioning, value, new_rank))
+    end
   end
 
-  def self.build_answer(qing, value, inst_num)
+  def self.add_group(parent, form_group, answer_values, new_rank)
+    answer_group = parent.children.build(
+      type: "AnswerGroup",
+      form_item: form_group,
+      new_rank: new_rank,
+      inst_num: new_rank,
+      rank: new_rank + 1
+    )
+    add_level(form_group.c, answer_values, answer_group)
+  end
+
+  def self.build_answer_set(parent, qing, values, new_rank)
+    set = parent.children.build(type: "AnswerSet", form_item: qing, new_rank: new_rank, inst_num: new_rank, rank: new_rank + 1)
+    unless values.blank?
+      options_by_name = qing.all_options.index_by(&:name)
+      values.each do |v|
+        option = qing.all_options.select { |o| o.canonical_name == v }.first
+        option_id = option.present? ? option.id : nil
+        set.children.build(
+          type: "Answer",
+          questioning: qing,
+          option_id: option_id,
+          new_rank: new_rank,
+          inst_num: inst_num("Answer", parent),
+          rank: new_rank + 1
+        )
+      end
+    end
+    set
+  end
+
+  def self.build_answer_attrs(parent, qing, value, new_rank)
+    attrs = {
+      type: "Answer",
+      form_item: qing
+    }
+    attrs.merge!(rank_attributes("Answer", parent))
+    case qing.qtype_name
+    when "select_one" # not multilevel
+      option = qing.all_options.select { |o| o.canonical_name == value }.first
+      attrs[:option_id] = option.id
+    when "select_multiple"
+      options_by_name = qing.options.index_by(&:name)
+      choices = value.map do |c|
+        Choice.new(option: options_by_name[c]) || raise("could not find option with name '#{c}'")
+      end
+      attrs[:choices] = choices
+    when "date", "time", "datetime"
+      attrs["#{qing.qtype_name}_value"] = value
+    when "image", "annotated_image", "signature", "sketch", "audio", "video"
+      attrs[:media_object] = value
+    else
+      attrs[:value] = value
+    end
+    attrs
+  end
+
+  # Rank and inst_num will go away at end of answer refactor
+  def self.rank_attributes(type, tree_parent)
+    {
+      new_rank: tree_parent.present? ? tree_parent.children.length : 0,
+      rank: tree_parent.is_a?(AnswerSet) ? tree_parent.children.length + 1 : 1,
+      inst_num: inst_num(type, tree_parent)
+    }
+  end
+
+  # Inst num will go away at end of answer refactor; this makes it work with answer arranger
+  def self.inst_num(type, tree_parent)
+    if tree_parent.is_a?(AnswerGroupSet) # repeat group
+      tree_parent.children.length + 1
+    elsif %w[Answer AnswerSet AnswerGroupSet].include?(type)
+      tree_parent.inst_num
+    else
+      1
+    end
+  end
+
+  def self.build_answer(qing, value, new_rank)
     answers = case qing.qtype_name
     when "select_one"
       options_by_name = qing.all_options.index_by(&:name)
@@ -87,7 +168,6 @@ FactoryGirl.define do
 
     # Ensure unpublished form associations have been published at least once
     after(:build) do |response, evaluator|
-      puts "response class in after build: #{response.class}"
       form = response.form
       unless form.published? && form.current_version.present?
         form.publish!
--- spec/models/results/geopoint_processing_spec.rb
+++ spec/models/results/geopoint_processing_spec.rb
@@ -6,13 +6,16 @@ describe "geopoint processing" do
   let(:form) { create(:form, question_types: %w[location]) }
   let(:questioning) { form.questionings.first }
   let(:response) { build(:response, form: form, answer_values: [value]) }
-  let(:answer) { response.answers[0] }
+  let(:answer) { response.root_node.c[0] }
 
   context "with no value" do
     let(:value) { "" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: nil, lat: nil, lng: nil, alt: nil, acc: nil)
+      pp response
+      puts response.root_node.debug_tree
+      puts "rsponse answers: #{response.answers[0]}"
+      expect_location_answer(response, answer, val: nil, lat: nil, lng: nil, alt: nil, acc: nil)
     end
 
     it "should be valid" do
@@ -24,7 +27,7 @@ describe "geopoint processing" do
     let(:value) { "-2.366030 -0.039825" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: value, lat: -2.366030, lng: -0.039825, alt: nil, acc: nil)
+      expect_location_answer(response, answer, val: value, lat: -2.366030, lng: -0.039825, alt: nil, acc: nil)
     end
 
     it "should be valid" do
@@ -36,7 +39,7 @@ describe "geopoint processing" do
     let(:value) { "-2.366030 -0.039825 100.235" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: value, lat: -2.366030, lng: -0.039825, alt: 100.235, acc: nil)
+      expect_location_answer(response, answer, val: value, lat: -2.366030, lng: -0.039825, alt: 100.235, acc: nil)
     end
 
     it "should be valid" do
@@ -48,7 +51,7 @@ describe "geopoint processing" do
     let(:value) { "-2.366030 -0.039825 100.235 20.000" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: value, lat: -2.366030, lng: -0.039825, alt: 100.235, acc: 20.0)
+      expect_location_answer(response, answer, val: value, lat: -2.366030, lng: -0.039825, alt: 100.235, acc: 20.0)
     end
 
     it "should be valid" do
@@ -60,7 +63,7 @@ describe "geopoint processing" do
     let(:value) { "-2.3660309999 -0.0398259999 100.2350001 20.00001" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: "-2.366031 -0.039826 100.235 20.000",
+      expect_location_answer(response, answer, val: "-2.366031 -0.039826 100.235 20.000",
                                      lat: -2.366030, lng: -0.039825, alt: 100.235, acc: 20.0)
     end
 
@@ -73,7 +76,7 @@ describe "geopoint processing" do
     let(:value) { "-92.366030 -0.039825" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: value, lat: -92.366030, lng: -0.039825, alt: nil, acc: nil)
+      expect_location_answer(response, answer, val: value, lat: -92.366030, lng: -0.039825, alt: nil, acc: nil)
     end
 
     it "should be invalid" do
@@ -86,7 +89,7 @@ describe "geopoint processing" do
     let(:value) { "-4000000000000000.366030 -29.039825" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: "0.000000 -29.039825", lat: 0, lng: -29.039825, alt: nil, acc: nil)
+      expect_location_answer(response, answer, val: "0.000000 -29.039825", lat: 0, lng: -29.039825, alt: nil, acc: nil)
     end
 
     it "should be invalid" do
@@ -99,7 +102,7 @@ describe "geopoint processing" do
     let(:value) { "-4.366030 -181.039825" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: value, lat: -4.366030, lng: -181.039825, alt: nil, acc: nil)
+      expect_location_answer(response, answer, val: value, lat: -4.366030, lng: -181.039825, alt: nil, acc: nil)
     end
 
     it "should be invalid" do
@@ -112,7 +115,7 @@ describe "geopoint processing" do
     let(:value) { "-4.366030" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: value, lat: -4.366030, lng: nil, alt: nil, acc: nil)
+      expect_location_answer(response, answer, val: value, lat: -4.366030, lng: nil, alt: nil, acc: nil)
     end
 
     it "should be invalid" do
@@ -125,7 +128,7 @@ describe "geopoint processing" do
     let(:value) { "-4.366030 -22.039825 1000009.9" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: "-4.366030 -22.039825 0.000",
+      expect_location_answer(response, answer, val: "-4.366030 -22.039825 0.000",
                                      lat: -4.366030, lng: -22.039825, alt: 0.0, acc: nil)
     end
 
@@ -139,7 +142,7 @@ describe "geopoint processing" do
     let(:value) { "-4.366030 -22.039825 33.9 -1.0" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: "-4.366030 -22.039825 33.900 0.000",
+      expect_location_answer(response, answer, val: "-4.366030 -22.039825 33.900 0.000",
                                      lat: -4.366030, lng: -22.039825, alt: 33.9, acc: 0.0)
     end
 
@@ -153,7 +156,7 @@ describe "geopoint processing" do
     let(:value) { "-4.366030 -22.039825 33.9 1000007.5" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: "-4.366030 -22.039825 33.900 0.000",
+      expect_location_answer(response, answer, val: "-4.366030 -22.039825 33.900 0.000",
                                      lat: -4.366030, lng: -22.039825, alt: 33.9, acc: 0.0)
     end
 
@@ -167,7 +170,7 @@ describe "geopoint processing" do
     let(:value) { "-2.366030 -0.039825 100.235 20.000 35.2" }
 
     it "should process properly" do
-      expect_location_answer(answer, val: "-2.366030 -0.039825 100.235 20.000",
+      expect_location_answer(response, answer, val: "-2.366030 -0.039825 100.235 20.000",
                                      lat: -2.366030, lng: -0.039825, alt: 100.235, acc: 20.0)
     end
 
@@ -180,14 +183,14 @@ describe "geopoint processing" do
     let(:value) { "-2.366030 -0.039825 100.235 20.000" }
 
     it "should update properly" do
-      expect_location_answer(answer, val: "-2.366030 -0.039825 100.235 20.000",
+      expect_location_answer(response, answer, val: "-2.366030 -0.039825 100.235 20.000",
                                      lat: -2.366030, lng: -0.039825, alt: 100.235, acc: 20.0)
 
       # Need a full reload or we trip the `location_values_replicated` flag
       id = answer.id
       answer = Answer.find(id)
       answer.value = "12.34 56.78"
-      expect_location_answer(answer, val: "12.340000 56.780000",
+      expect_location_answer(response, answer, val: "12.340000 56.780000",
                                      lat: 12.34, lng: 56.78, alt: nil, acc: nil)
     end
   end
