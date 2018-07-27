3f93dee5f1693f978b24bce8856cf7aa8a3127b5
Tom Smyth <tom@sassafras.coop>
2018-07-27 12:17:22 -0400

    Fix migration to add version

--- db/migrate/20180626162616_add_audio_prompt_to_questions.rb
+++ db/migrate/20180626162616_add_audio_prompt_to_questions.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Add audio prompt to question table for paperclip
-class AddAudioPromptToQuestions < ActiveRecord::Migration
+class AddAudioPromptToQuestions < ActiveRecord::Migration[5.1]
   def up
     add_attachment :questions, :audio_prompt
   end
