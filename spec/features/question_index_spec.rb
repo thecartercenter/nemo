a45bddfd3396daa5c56a75eb1fe6b1c3f03347e4
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-08-22 16:25:08 +0100

    8659 start failing spec

--- spec/features/question_index_spec.rb
+++ spec/features/question_index_spec.rb
@@ -13,11 +13,17 @@ feature "question index", js: true do
   describe "batch delete" do
     let!(:questions) { create_list(:question, 3, mission: mission) }
 
-    scenario do
+    scenario "works" do
       visit("/en/m/#{mission.compact_name}/questions")
       all("input.batch_op").each { |b| b.set(true) }
       accept_confirm { click_on("Delete Multiple Questions") }
       expect(page).to have_content("3 questions deleted successfully")
+    end
+
+    scenario "redirects correctly after batch delete" do
+      visit("/en/m/#{mission.compact_name}/questions")
+
+      save_and_open_page
     end
   end
 end
