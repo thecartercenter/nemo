d3174f6019541beb2333256e55c34959f72a57f5
Jill Dimond <jill@sassafrastech.com>
2018-08-28 15:11:50 -0400

    8804: return html instead of usign ejs for summary report

--- app/assets/javascripts/legacy/views/report/form_summary_display.js
+++ app/assets/javascripts/legacy/views/report/form_summary_display.js
@@ -12,19 +12,7 @@
   klass.prototype.parent = ns.Display.prototype;
 
   klass.prototype.render = function() { var self = this;
-    var template = JST['legacy/templates/report/form_summary_display'];
-    $('.report_body').empty().append(template({
-      report: this.report.attribs,
-      max_result_cols: this.report.attribs.question_labels == 'title' ? 5 : 8,
-      helper: self
-    }));
-  }
-
-  klass.prototype.partial = function(name, params) {
-    params.helper = this;
-    params.report = this.report.attribs;
-    var template = JST['legacy/templates/report/form_summary_' + name];
-    return template(params);
+    $('.report_body').empty().append(this.report.attribs.erb_view);
   }
 
 }(ELMO.Report));
