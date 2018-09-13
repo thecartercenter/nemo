6b270335ae8de9d21dbe24548ca7064e1f4557d9
Devney <devneyhamilton@gmail.com>
2018-09-13 14:08:38 -0400

    6423: move new spec to forms_flow_spec

--- spec/features/forms_flow_spec.rb
+++ spec/features/forms_flow_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 feature "forms flow", js: true do
   let!(:user) { create(:user) }
-  let!(:form) { create(:sample_form) }
+  let!(:form) { create(:form, name: "Foo",
+    question_types: %w(integer multilevel_select_one select_one integer)) }
   let(:forms_path) { "/en/m/#{form.mission.compact_name}/forms" }
 
   before do
@@ -31,5 +32,57 @@ feature "forms flow", js: true do
     #     expect(page).not_to have_css('h4', text: 'Print Format Tips')
     #   end
     # end
+  end
+
+  scenario "add nested groups to a form" do
+    outer_name = "Outer Group"
+    middle_name = "Middle Group"
+    inner_name = "Inner Group"
+    question_name = "[IntegerQ1] Integer Question Title 1"
+
+    visit(edit_form_path(form, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+    create_group(outer_name)
+    create_group(middle_name)
+    create_group(inner_name)
+
+    click_button("Save") # save form
+
+    # Uses DOM manipulation to simulate moving items in the draggable list.
+    # Capybara's drag_to doesn't work because the 'droppable' el isn't available
+    # until the draggable item is clicked.
+
+    nest_groups = <<~SCRIPT
+      function itemLi(name) {
+        return $('li .inner:contains(' + name + ')').parent();
+      }
+      function moveChildToParent(childName, newParentName) {
+        var newParent = itemLi(newParentName);
+        var newOl = $('<ol class=\"item-list ui-sortable\"></ol>');
+        newParent.append(newOl);
+        var item = itemLi(childName);
+        item.detach().appendTo(newOl);
+        ELMO.formItemsView.draggable.drop_happened(null, {item: item});
+      }
+      moveChildToParent('#{middle_name}', '#{outer_name}');
+      setTimeout(function() {moveChildToParent('#{inner_name}', '#{middle_name}');}, 100)
+      setTimeout(function() {moveChildToParent('#{question_name}', '#{inner_name}');}, 100)
+    SCRIPT
+    execute_script(nest_groups)
+    click_button("Save") # save form
+    outer_css = ".draggable-list-wrapper ol li"
+    middle_css = ".draggable-list-wrapper ol li ol li"
+    inner_css = ".draggable-list-wrapper ol li ol li ol li"
+    question_css = ".draggable-list-wrapper ol li ol li ol li ol li"
+    within(".form-items") { expect(page).to have_css(outer_css, text: outer_name) }
+    within(".form-items") { expect(page).to have_css(middle_css, text: middle_name) }
+    within(".form-items") { expect(page).to have_css(inner_css, text: inner_name) }
+    within(".form-items") { expect(page).to have_css(question_css, text: question_name) }
+  end
+
+  def create_group(name)
+    click_link("Add Group")
+    fill_in("Name (English)", with: name)
+    check("qing_group_repeatable")
+    within(".modal") { click_button("Save") }
   end
 end
