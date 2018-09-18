36423e867415dfc0bfd160232539a286426aa6b2
Scott Nelson <scott@scottnelson.co>
2018-09-18 12:10:32 -0400

    8793 updated schema file w/ migration timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822145730) do
+ActiveRecord::Schema.define(version: 20180918154928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
