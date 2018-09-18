94132d796fa282f4680b8dcfb92f5301fe39198a
Jill Dimond <jill@sassafrastech.com>
2018-09-18 09:52:52 -0400

    8811: cleaned up code pre code review

--- app/models/questioning.rb
+++ app/models/questioning.rb
@@ -8,7 +8,8 @@ class Questioning < FormItem
     :min_max_error_msg, :multilevel?, :multimedia?, :name, :numeric?, :odk_constraint, :odk_name,
     :option_set_id, :option_set_id=, :option_set, :option_set=, :options, :preordered_option_nodes,
     :printable?, :qtype_name, :qtype_name=, :qtype, :select_options, :sms_formatting_as_appendix?,
-    :sms_formatting_as_text?, :standardized?, :subqings, :tags, :temporal?, :title, :metadata_type, :reference,
+    :sms_formatting_as_text?, :standardized?, :subqings, :tags, :temporal?, :title, :metadata_type,
+    :reference,
     to: :question
   delegate :published?, to: :form
   delegate :smsable?, to: :form, prefix: true
