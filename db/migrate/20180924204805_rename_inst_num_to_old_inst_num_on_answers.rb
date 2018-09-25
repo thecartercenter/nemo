cd30fd1cb947bbfc84b8c15902615e7fee6e6906
Devney <devneyhamilton@gmail.com>
2018-09-25 10:19:15 -0400

    8665: fix more hound errors

--- db/migrate/20180924204805_rename_inst_num_to_old_inst_num_on_answers.rb
+++ db/migrate/20180924204805_rename_inst_num_to_old_inst_num_on_answers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameInstNumToOldInstNumOnAnswers < ActiveRecord::Migration[5.1]
   def change
     rename_column(:answers, :inst_num, :old_inst_num)
--- db/migrate/20180924213302_rename_rank_to_old_rank_in_answers.rb
+++ db/migrate/20180924213302_rename_rank_to_old_rank_in_answers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameRankToOldRankInAnswers < ActiveRecord::Migration[5.1]
   def change
     rename_column(:answers, :rank, :old_rank)
