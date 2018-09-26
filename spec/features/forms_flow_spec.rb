ab809116df8c92a29b0c97a1ca685d5ad90e4f95
Tom Smyth <tom@sassafras.coop>
2018-09-26 13:49:43 -0400

    6423: Fix (hopefully) feature spec issues

--- spec/features/forms_flow_spec.rb
+++ spec/features/forms_flow_spec.rb
@@ -48,38 +48,36 @@ feature "forms flow", js: true do
     create_group(middle_name)
     create_group(inner_name)
 
-    click_button("Save") # save form
+    execute_script(move_item_js_functions)
 
-    # Uses DOM manipulation to simulate moving items in the draggable list.
-    # Capybara's drag_to doesn't work because the 'droppable' el isn't available
-    # until the draggable item is clicked.
+    # Move the items one by one, waiting for the move to complete each time.
+    within(".form-items .draggable-list-wrapper") do
+      expect(page).to have_nested_item(depth: 1, name: outer_name)
 
-    nest_groups = <<~SCRIPT
-      function itemLi(name) {
-        return $('li .inner:contains(' + name + ')').parent();
-      }
-      function moveChildToParent(childName, newParentName) {
-        var newParent = itemLi(newParentName);
-        var newOl = $('<ol class=\"item-list ui-sortable\"></ol>');
-        newParent.append(newOl);
-        var item = itemLi(childName);
-        item.detach().appendTo(newOl);
-        ELMO.formItemsView.draggable.drop_happened(null, {item: item});
-      }
-      moveChildToParent('#{middle_name}', '#{outer_name}');
-      setTimeout(function() {moveChildToParent('#{inner_name}', '#{middle_name}');}, 100)
-      setTimeout(function() {moveChildToParent('#{question_name}', '#{inner_name}');}, 100)
-    SCRIPT
-    execute_script(nest_groups)
+      move_item(middle_name, outer_name)
+      expect(page).to have_nested_item(depth: 2, name: middle_name)
+
+      move_item(inner_name, middle_name)
+      expect(page).to have_nested_item(depth: 3, name: inner_name)
+
+      move_item(question_name, inner_name)
+      expect(page).to have_nested_item(depth: 4, name: question_name)
+    end
+
     click_button("Save")
-    outer_css = ".draggable-list-wrapper ol li"
-    middle_css = ".draggable-list-wrapper ol li ol li"
-    inner_css = ".draggable-list-wrapper ol li ol li ol li"
-    question_css = ".draggable-list-wrapper ol li ol li ol li ol li"
-    within(".form-items") { expect(page).to have_css(outer_css, text: outer_name) }
-    within(".form-items") { expect(page).to have_css(middle_css, text: middle_name) }
-    within(".form-items") { expect(page).to have_css(inner_css, text: inner_name) }
-    within(".form-items") { expect(page).to have_css(question_css, text: question_name) }
+    expect(page).to have_content("Form updated successfully.")
+
+    # Ensure the changes were persisted.
+    within(".form-items .draggable-list-wrapper") do
+      expect(page).to have_nested_item(depth: 1, name: outer_name)
+      expect(page).to have_nested_item(depth: 2, name: middle_name)
+      expect(page).to have_nested_item(depth: 3, name: inner_name)
+      expect(page).to have_nested_item(depth: 4, name: question_name)
+    end
+  end
+
+  def have_nested_item(depth:, name:)
+    have_css((%w[ol li] * depth).join(" > "), text: name)
   end
 
   def create_group(name)
@@ -87,5 +85,35 @@ feature "forms flow", js: true do
     fill_in("Name (English)", with: name)
     check("qing_group_repeatable")
     within(".modal") { click_button("Save") }
+  end
+
+  def move_item_js_functions
+    # Uses DOM manipulation to simulate moving items in the draggable list.
+    # Capybara's drag_to doesn't work because the 'droppable' el isn't available
+    # until the draggable item is clicked.
+    <<~SCRIPT
+      window.itemLi = function(name) {
+        return $('li .inner:contains(' + name + ')').parent();
+      }
+      window.moveChildToParent = function(childName, newParentName) {
+        var newParent = itemLi(newParentName);
+        var newOl = $('<ol class=\"item-list ui-sortable\"></ol>');
+        newParent.append(newOl);
+        var item = itemLi(childName);
+        item.detach().appendTo(newOl);
+        ELMO.formItemsView.draggable.drop_happened(null, {item: item});
+      }
+    SCRIPT
+  end
+
+  def move_item(name, parent_name)
+    execute_script("moveChildToParent('#{name}', '#{parent_name}');")
+    wait_for_ajax # Wait for AJAX saving to complete or we sometimes get timing errors.
+
+    # Trying this after much difficulty to see if it removes flakiness.
+    # Suspicion is that if another item is attempted to be moved before this AJAX call finishes,
+    # trouble happens.
+    # If this does fix things, maybe try a better way later.
+    sleep(5)
   end
 end
