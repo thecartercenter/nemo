29da74db795816d8373353d56f53cdd584801d07
Tom Smyth <tom@sassafras.coop>
2018-10-01 10:34:52 -0400

    8197: Add more assertions

--- docs/answer_assertions.sql
+++ docs/answer_assertions.sql
@@ -1,5 +1,6 @@
+-- Execute these queries by pasting into PSQL console. All should return no rows.
+
 -- Exactly one root AnswerGroup per response_id
--- Below should return no rows.
 select response_id, count(*)
   from answers
   where parent_id is null and deleted_at is null
@@ -7,7 +8,6 @@ select response_id, count(*)
   having count(*) > 1;
 
 -- Exactly one AnswerGroup per non-repeat group and response_id
--- Below should return no rows.
 select response_id, questioning_id, count(a.id)
   from answers a inner join form_items f on f.id = a.questioning_id
   where a.type = 'AnswerGroup' and f.repeatable = 'f'
@@ -15,7 +15,6 @@ select response_id, questioning_id, count(a.id)
   having count(a.id) > 1;
 
 -- Exactly one AnswerGroupSet per repeat group and response_id
--- Below should return no rows.
 select response_id, questioning_id, count(a.id)
   from answers a inner join form_items f on f.id = a.questioning_id
   where a.type = 'AnswerGroupSet' and f.repeatable = 't'
@@ -23,7 +22,6 @@ select response_id, questioning_id, count(a.id)
   having count(a.id) > 1;
 
 -- Contiguous new_rank
--- Below should return no rows.
 SELECT a1.id, a1.new_rank
   FROM answers a1
   WHERE a1.new_rank > 1 AND a1.deleted_at IS NULL AND NOT EXISTS (
@@ -33,7 +31,6 @@ SELECT a1.id, a1.new_rank
   );
 
 -- Non-duplicate ranks
--- Below should return no rows.
 SELECT parent_id, new_rank, COUNT(id)
   FROM answers
   WHERE deleted_at IS NULL AND parent_id is NOT NULL
@@ -41,13 +38,11 @@ SELECT parent_id, new_rank, COUNT(id)
   HAVING COUNT(id) > 1;
 
 -- Roots have new_rank 1.
--- Below should return no rows.
 SELECT a1.id, a1.new_rank
   FROM answers a1
   WHERE a1.new_rank != 1 AND a1.parent_id IS NULL AND a1.deleted_at IS NULL;
 
 -- Old inst_num is the same for all Answers in an AnswerGroup
--- Below should return no rows.
 SELECT parent_id, COUNT(DISTINCT inst_num)
   FROM answers
   WHERE answers.deleted_at IS NULL AND answers.type = 'Answer'
@@ -55,10 +50,20 @@ SELECT parent_id, COUNT(DISTINCT inst_num)
   HAVING COUNT(DISTINCT inst_num) > 1;
 
 -- Answers with old_rank != 1 should have AnswerSet as parent
--- Below should return no rows.
 SELECT answers.id
   FROM answers INNER JOIN answers parents ON answers.parent_id = parents.id
   WHERE answers.old_rank IS NOT NULL
     AND answers.deleted_at IS NULL
     AND answers.old_rank > 1
     AND parents.type != 'AnswerSet';
+
+-- All Answer, AnswerSet, AnswerGroupSet have parent_id
+SELECT id
+  FROM answers
+  WHERE deleted_at IS NULL AND type != 'AnswerGroup' AND parent_id IS NULL;
+
+-- Answers never have children
+SELECT answers.id
+  FROM answers
+  WHERE answers.deleted_at IS NULL AND type = 'Answer'
+    AND EXISTS (SELECT children.id FROM answers children WHERE children.parent_id = answers.id);
