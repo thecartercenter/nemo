eb7ecb4553418d88756cbc5bd51bb3f2f3a4f4f0
Devney <devneyhamilton@gmail.com>
2018-10-04 15:58:30 -0400

    8103: create spec file, move route

---
+++ spec/features/operations/operations_panel_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "operations panel" do
+  let(:user) { create(:admin) }
+
+  context "in mission mode" do
+  end
+
+  context "in admin mode" do
+  end
+end
