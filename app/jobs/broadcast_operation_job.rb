d292abbf3f1d971e8afd354a89a903b711e19952
Tom Smyth <tom@sassafras.coop>
2018-10-19 14:01:38 -0400

    8946: Save broadcast ID instead of full broadcast

--- app/jobs/broadcast_operation_job.rb
+++ app/jobs/broadcast_operation_job.rb
@@ -2,18 +2,21 @@
 
 # Delivers a broadcast in the context of a background operation
 class BroadcastOperationJob < OperationJob
-  def perform(operation, broadcast)
+  def perform(operation, broadcast_id)
+    broadcast = Broadcast.find(broadcast_id)
     broadcast.deliver
     broadcast.update!(sent_at: Time.current)
+  rescue ActiveRecord::RecordNotFound
+    save_failure(I18n.t("operation.errors.broadcast.not_found"))
   rescue Sms::Adapters::PartialSendError
     # this is considered a success but we'd still like to show there were some errors
     operation.update!(
-      job_error_report: I18n.t("operation.errors.broadcast_partial", url: error_url(broadcast))
+      job_error_report: I18n.t("operation.errors.broadcast.partial_send", url: error_url(broadcast))
     )
     broadcast.update!(sent_at: Time.current)
   rescue Sms::Adapters::FatalSendError
     # this is considered an full operation failure
-    save_failure(I18n.t("operation.errors.broadcast_fatal", url: error_url(broadcast)))
+    save_failure(I18n.t("operation.errors.broadcast.fatal_send", url: error_url(broadcast)))
   end
 
   private
