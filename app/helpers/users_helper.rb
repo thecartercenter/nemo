4f22fbe42f0eab3fe4ba27a992ef5925a520c307
Jill Dimond <jill@sassafrastech.com>
2018-11-07 10:50:25 -0500

    8814: fixing helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -76,8 +76,10 @@ module UsersHelper
   end
 
   def assignment_data
-    { missions: Mission.all.map { |m| MissionSerializer.new(m) },
+    {
+      missions: Mission.all.map { |m| MissionSerializer.new(m) },
       roles: User::ROLES.map { |r| r },
-      assignments: @user.assignments.map { |a| AssignmentSerializer.new(a) } }
+      assignments: @user.assignments.map { |a| AssignmentSerializer.new(a) }
+    }
   end
 end
