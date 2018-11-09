26c4088b8d55a003018668aeeb5da437a80cdcdb
Scott Nelson <scott@scottnelson.co>
2018-11-09 10:46:21 -0500

    9048 remove token from media objects

--- app/models/media/object.rb
+++ app/models/media/object.rb
@@ -13,21 +13,6 @@ module Media
 
     delegate :mission, to: :answer
 
-    before_save :generate_token
-
     scope :expired, -> { where(answer_id: nil).where("created_at < ?", 12.hours.ago) }
-
-    private
-
-    # We need to create a persisted token to use for authorization
-    # because when a response fails to save with a media object uploaded
-    # the media object is persisted with no reference back to the response
-    # and no way to check if you're authorized to see it.
-    #
-    # Storing the token with the object and authorizing against it
-    # keeps a user from spoofing the URL to see unauthorized media
-    def generate_token
-      self.token = SecureRandom.hex
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181109141200) do
+ActiveRecord::Schema.define(version: 20181109154424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,11 +97,11 @@ ActiveRecord::Schema.define(version: 20181109141200) do
     t.integer "old_id"
     t.string "recipient_selection", limit: 255, null: false
     t.text "send_errors"
+    t.datetime "sent_at"
     t.string "source", limit: 255, default: "manual", null: false
     t.string "subject", limit: 255
     t.datetime "updated_at"
     t.string "which_phone", limit: 255
-    t.datetime "sent_at"
     t.index ["mission_id"], name: "index_broadcasts_on_mission_id"
   end
 
@@ -267,7 +267,6 @@ ActiveRecord::Schema.define(version: 20181109141200) do
     t.integer "item_file_size"
     t.datetime "item_updated_at"
     t.integer "old_id"
-    t.string "token", limit: 255
     t.string "type", limit: 255
     t.datetime "updated_at", null: false
     t.index ["answer_id"], name: "index_media_objects_on_answer_id"
@@ -526,11 +525,11 @@ ActiveRecord::Schema.define(version: 20181109141200) do
   end
 
   create_table "saved_uploads", force: :cascade do |t|
-    t.string "file_file_name"
+    t.datetime "created_at", null: false
     t.string "file_content_type"
+    t.string "file_file_name"
     t.integer "file_file_size"
     t.datetime "file_updated_at"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
@@ -585,11 +584,11 @@ ActiveRecord::Schema.define(version: 20181109141200) do
     t.uuid "broadcast_id"
     t.integer "broadcast_old_id"
     t.datetime "created_at", null: false
-    t.string "reply_error_message"
     t.string "from", limit: 255
     t.uuid "mission_id"
     t.integer "mission_old_id"
     t.integer "old_id"
+    t.string "reply_error_message"
     t.uuid "reply_to_id"
     t.integer "reply_to_old_id"
     t.datetime "sent_at"
