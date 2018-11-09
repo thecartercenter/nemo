ac7487e09a8f2f58378f2834e19178d28005fdf2
Tom Smyth <tom@sassafras.coop>
2018-11-09 15:40:59 -0500

    9067: Refactor sms message factory and spec

--- spec/models/sms/message_spec.rb
+++ spec/models/sms/message_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 describe Sms::Message, :sms do
   it "creating a message should work" do
@@ -9,7 +11,7 @@ describe Sms::Message, :sms do
 
   it "a message with no sent_at should default to now, but only when saved" do
     m = Sms::Reply.create(to: "14045551212", body: "blah")
-    expect(Time.zone.now - m.sent_at < 5.seconds).to be true
+    expect(Time.zone.now - m.sent_at < 5.seconds).to be(true)
   end
 
   it "a textual from field should be preserved" do
@@ -19,15 +21,16 @@ describe Sms::Message, :sms do
 
   it "long messages with slight difference at end should not be returned as equal" do
     # this is just to be sure the index of length 160 doesn't mess anything up
-    long = "laksdjf alsdkjf lasdfkjaf laskdsf aslkfsjflsakfda lsakl fakjsdlkfaj lskjf alksj " +
-      "dfalkj sdlfaks asdaksjdafa alaskdfal alkajdasfa alsjd alasksdjf alks alfdsafdslf l lajslfdjfalsdkf dslads"
+    long = "laksdjf alsdkjf lasdfkjaf laskdsf aslkfsjflsakfda lsakl fakjsdlkfaj lskjf alksj "\
+      "dfalkj sdlfaks asdaksjdafa alaskdfal alkajdasfa alsjd alasksdjf alks"\
+      "alfdsafdslf l lajslfdjfalsdkf dslads"
     Sms::Reply.create(from: "foo", body: long)
-    expect(Sms::Reply.find_by_body(long + "a")).to be_nil
+    expect(Sms::Reply.find_by(body: long + "a")).to be_nil
   end
 
   it "when a mission is deleted, remove all sms messages from that mission" do
-    sms = create(:sms_message_with_mission)
+    sms = create(:sms_incoming)
     Sms::Message.mission_pre_delete(sms.mission)
-    expect(Sms::Message.find_by_mission_id(sms.id)).to be_nil
+    expect(Sms::Message.find_by(mission_id: sms.id)).to be_nil
   end
 end
