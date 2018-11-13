8eff1c4d23113b0d8886cf916d8ce37e333240ea
Tom Smyth <tom@sassafras.coop>
2018-11-13 11:37:17 -0500

    9048: Tidy objects controller

--- app/controllers/media/objects_controller.rb
+++ app/controllers/media/objects_controller.rb
@@ -1,79 +1,76 @@
 # frozen_string_literal: true
 
-class Media::ObjectsController < ApplicationController
-  before_action :set_media_object, only: %i[show destroy]
-  skip_authorization_check
+module Media
+  # Creating, getting, and deleting media attached to responses.
+  class ObjectsController < ApplicationController
+    before_action :set_media_object, only: %i[show destroy]
+    skip_authorization_check
 
-  def show
-    style = params[:style] || "original"
-    @answer = @media_object.answer
-    @response = @answer.try(:response)
-    disposition = params[:dl] == "1" ? "attachment" : "inline"
+    def show
+      style = params[:style] || "original"
+      @answer = @media_object.answer
+      @response = @answer.try(:response)
+      disposition = params[:dl] == "1" ? "attachment" : "inline"
 
-    authorize!(:show, @response) if @response
+      authorize!(:show, @response) if @response
 
-    if @media_object.item.options[:storage] == "fog"
-      redirect_to(@media_object.item.expiring_url)
-    else
-      send_file(@media_object.item.path(style),
-        type: @media_object.item_content_type,
-        disposition: disposition,
-        filename: media_filename)
+      if @media_object.item.options[:storage] == "fog"
+        redirect_to(@media_object.item.expiring_url)
+      else
+        send_file(@media_object.item.path(style), type: @media_object.item_content_type,
+                                                  disposition: disposition, filename: media_filename)
+      end
     end
-  end
 
-  def create
-    media = media_class(params[:type]).new(item: params[:upload])
-    # answer_id can be blank because creation is asynchronous and
-    # will be assigned when the response is submitted
-    media.answer = Answer.find(params[:answer_id]) if params[:answer_id]
+    def create
+      media = media_class(params[:type]).new(item: params[:upload])
+      # answer_id can be blank because creation is asynchronous and
+      # will be assigned when the response is submitted
+      media.answer = Answer.find(params[:answer_id]) if params[:answer_id]
 
-    if media.save
-      # Json keys match hidden input names that contain the key in dropzone form.
-      # See ELMO.Views.FileUploaderView for more info.
-      render(json: {media_object_id: media.id}, status: :created)
-    else
-      # Currently there is only one type of validation failure: incorrect type.
-      # The default paperclip error messages are heinous, which is why we're doing this.
-      msg = I18n.t("activerecord.errors.models.media/object.invalid_format")
-      render(json: {errors: [msg]}, status: :unprocessable_entity)
+      if media.save
+        # Json keys match hidden input names that contain the key in dropzone form.
+        # See ELMO.Views.FileUploaderView for more info.
+        render(json: {media_object_id: media.id}, status: :created)
+      else
+        # Currently there is only one type of validation failure: incorrect type.
+        # The default paperclip error messages are heinous, which is why we're doing this.
+        msg = I18n.t("activerecord.errors.models.media/object.invalid_format")
+        render(json: {errors: [msg]}, status: :unprocessable_entity)
+      end
     end
-  end
 
-  def destroy
-    @media_object.destroy
-    render(body: nil, status: :no_content)
-  end
-
-  private
-
-  def set_media_object
-    @media_object = Media::Object.find(params[:id])
-  end
-
-  def media_object_params
-    params.require(:media_object).permit(:answer_id, :annotation)
-  end
-
-  def media_filename
-    extension = File.extname(@media_object.item_file_name)
-    if @response && @answer
-      "elmo-#{@response.shortcode}-#{@answer.id}#{extension}"
-    else
-      "elmo-unsaved_response-#{@media_object.id}#{extension}"
+    def destroy
+      @media_object.destroy
+      render(body: nil, status: :no_content)
     end
-  end
 
-  def media_class(type)
-    case type
-    when "audios"
-      Media::Audio
-    when "videos"
-      Media::Video
-    when "images"
-      Media::Image
-    else
-      raise "A valid media type must be specified"
+    private
+
+    def set_media_object
+      @media_object = Media::Object.find(params[:id])
+    end
+
+    def media_object_params
+      params.require(:media_object).permit(:answer_id, :annotation)
+    end
+
+    def media_filename
+      extension = File.extname(@media_object.item_file_name)
+      if @response && @answer
+        "elmo-#{@response.shortcode}-#{@answer.id}#{extension}"
+      else
+        "elmo-unsaved_response-#{@media_object.id}#{extension}"
+      end
+    end
+
+    def media_class(type)
+      case type
+      when "audios" then Media::Audio
+      when "videos" then Media::Video
+      when "images" then Media::Image
+      else raise "A valid media type must be specified"
+      end
     end
   end
 end
