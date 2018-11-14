b97dc0b38fccaadd0773c04e68eee3061076bcf9
Devney <devneyhamilton@gmail.com>
2018-11-14 17:04:22 -0500

    8933: consolidate operations file upload spec

--- spec/features/operations/file_upload_spec.rb
+++ spec/features/operations/file_upload_spec.rb
@@ -11,30 +11,20 @@ feature "user batch file upload", js: true do
   let(:params) { {locale: "en", mode: "m", mission_name: get_mission.compact_name} }
 
   before { login(admin) }
+
   scenario "uploading user batch" do
     visit new_user_batch_path(params)
     node = find(".user_batch_form")
-
-    # try hitting submit with no file, expect error
-    click_button("Import")
-    expect(page).to have_content("No file selected for import.")
-
-    # invalid file
-    drop_in_dropzone(invalid_file, 0)
-    expect_no_preview(node)
-    expect(page).to have_content("The uploaded file was not an accepted format.")
-    expect(page).to have_button("Import")
-
-    # try uploading valid file
-    drop_in_dropzone(user_batch, 0)
-    expect_preview(node)
-    expect(page).to have_content("batch_of_3.csv")
+    run_scenario(node, user_batch, "batch_of_3.csv")
   end
 
   scenario "uploading option set import" do
     visit new_option_set_imports_path(params)
     node = find(".option_set_import_form")
+    run_scenario(node, option_set_import, "simple.csv")
+  end
 
+  def run_scenario(node, correct_file, correct_file_name)
     # try hitting submit with no file, expect error
     click_button("Import")
     expect(page).to have_content("No file selected for import.")
@@ -46,8 +36,8 @@ feature "user batch file upload", js: true do
     expect(page).to have_button("Import")
 
     # try uploading valid file
-    drop_in_dropzone(option_set_import, 0)
+    drop_in_dropzone(correct_file, 0)
     expect_preview(node)
-    expect(page).to have_content("simple.csv")
+    expect(page).to have_content(correct_file_name)
   end
 end
