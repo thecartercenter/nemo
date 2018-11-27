deaaaff949197c08e26c99457f0a979999f98f17
Devney <devneyhamilton@gmail.com>
2018-11-26 16:10:15 -0800

    1472: basic promotion spec added, passing without changes

--- spec/models/replication/tag_replication_spec.rb
+++ spec/models/replication/tag_replication_spec.rb
@@ -8,7 +8,6 @@ describe "replicating questions with tags" do
   describe "to_mission" do
     let(:orig_tag1) { create(:tag, name: "a", mission: nil) }
     let(:orig_tag2) { create(:tag, name: "b", mission: nil) }
-    # add a second tag to question
     let(:orig_q) { create(:question, qtype_name: "text", is_standard: true, tags: [orig_tag1, orig_tag2]) }
     let(:copy_q) { orig_q.replicate(mode: :to_mission, dest_mission: mission) }
     let(:new_tag) { find_tag_by_name(copy_q, "b") }
@@ -42,6 +41,34 @@ describe "replicating questions with tags" do
         expect(b_tag_for_copy.id).not_to eq(orig_tag2.id)
         expect(b_tag_for_copy.name).to eq(orig_tag2.name)
         expect(b_tag_for_copy.mission).to eq(mission)
+      end
+    end
+  end
+
+  describe "promote" do
+    let(:orig_tag1) { create(:tag, name: "a", mission: mission) }
+    let(:orig_tag2) { create(:tag, name: "b", mission: mission) }
+    let(:orig_q) do
+      create(:question, mission: mission, qtype_name: "text", tags: [orig_tag1, orig_tag2])
+    end
+    let(:std) { orig_q.replicate(mode: :promote) }
+
+    context "basic" do
+      it "should create new tag with mission nil" do
+        orig_q.reload
+        expect(orig_q.id).not_to eq(std.id)
+        expect(std.tags.count).to eq(2)
+        [orig_tag1, orig_tag2].each_with_index do |orig_tag, _i|
+          new_tag = find_tag_by_name(std, orig_tag.name)
+          expect(new_tag.id).not_to eq(orig_tag.id)
+          expect(new_tag.name).to eq(orig_tag.name)
+          expect(new_tag.mission).to be_nil
+        end
+      end
+    end
+
+    context "conflicting standard tag exists" do
+      it "should use the existing tag" do
       end
     end
   end
