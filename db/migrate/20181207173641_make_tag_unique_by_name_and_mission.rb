9af83c7f156dcd51283abe24a31bbe5e893fb094
Devney <devneyhamilton@gmail.com>
2018-12-07 14:54:26 -0500

    9126: working for tags with missions

--- db/migrate/20181207173641_make_tag_unique_by_name_and_mission.rb
+++ db/migrate/20181207173641_make_tag_unique_by_name_and_mission.rb
@@ -1,5 +1,7 @@
 class MakeTagUniqueByNameAndMission < ActiveRecord::Migration[5.1]
   def up
+    select_sql = "SELECT array_agg(id), name, mission_id FROM tags GROUP BY name, mission_id HAVING count(*) > 1"
+    duplicate_tags = execute(select_sql)
     remove_index(:tags, column: [:name, :mission_id])
     add_index :tags, [:name, :mission_id], unique: true, name: "index_tags_on_name_and_mission_id", where: "(deleted_at IS NULL)"
     add_index :tags,[:name], unique: true, name: "index_tags_on_name_where_mission_id_null", where: "(deleted_at IS NULL AND mission_id IS NULL)"
