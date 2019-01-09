ad766a8c52b0cb2cae787ff5dabeed3431aa1bc4
Jill Dimond <jill@sassafrastech.com>
2019-01-09 10:17:04 -0500

    9226 returned controller to accept select all

--- app/controllers/concerns/batch_processable.rb
+++ app/controllers/concerns/batch_processable.rb
@@ -3,7 +3,9 @@ module BatchProcessable
 
   def load_selected_objects(klass)
     klass = klass.accessible_by(current_ability)
-    if params[:selected].present?
+    if params[:select_all].present?
+      klass.all.to_a
+    elsif params[:selected].present?
       params[:selected].keys.collect{ |id| klass.find_by_id(id) }.compact
     else
       []
