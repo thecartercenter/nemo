b4c85b83b6cac2abc6ce53fd7da7535e0a739e81
Tom Smyth <tom@sassafras.coop>
2019-01-25 16:50:05 -0500

    9280: Stub pagination constant to speed up test

--- spec/features/response_index_spec.rb
+++ spec/features/response_index_spec.rb
@@ -35,26 +35,27 @@ feature "response index", js: true do
   end
 
   describe "bulk destroy paginated" do
-    let!(:responses) { create_list(:response, 50, mission: mission, reviewed: true) }
-    let!(:responses_un) { create_list(:response, 50, mission: mission) }
+    let!(:responses) { create_list(:response, 5, mission: mission, reviewed: true) }
+    let!(:responses_un) { create_list(:response, 5, mission: mission) }
+
+    before do
+      stub_const(ResponsesController, "PER_PAGE", 2)
+    end
 
     context "unfiltered select page" do
-      let!(:preserved_obj) { Response.limit(80).last.shortcode.upcase }
-      it_behaves_like "select all on page", link: "Delete Selected", klass: "responses", num: 20
+      let!(:preserved_obj) { Response.limit(8).last.shortcode.upcase }
+      it_behaves_like "select all on page", link: "Delete Selected", klass: "responses", num: 2
     end
 
     context "unfiltered select all" do
       let!(:preserved_obj) { nil }
-      it_behaves_like "select all that exist", klass: "responses", num: 100,
-                                               link: "Delete Selected"
+      it_behaves_like "select all that exist", klass: "responses", num: 10, link: "Delete Selected"
     end
 
     context "filtered select all" do
       let!(:preserved_obj) { nil }
-      it_behaves_like "select all that exist", klass: "responses",
-                                               num: 50,
-                                               link: "Delete Selected",
-                                               query: "reviewed:1"
+      it_behaves_like "select all that exist", klass: "responses", num: 5,
+                                               link: "Delete Selected", query: "reviewed:1"
     end
   end
 end
