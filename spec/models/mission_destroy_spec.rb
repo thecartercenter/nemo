901a33ddc93344f34fc22c70a7b4815b793e576b
Tom Smyth <tom@sassafras.coop>
2019-01-31 13:58:45 -0500

    9335: Move mission with full hierarchy creation logic into spec file and clean up

--- spec/models/mission_destroy_spec.rb
+++ spec/models/mission_destroy_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe "mission destroy" do
-  let!(:mission) { create(:mission_with_full_heirarchy) }
+  let!(:mission) { create(:mission) }
   let(:before_counts) do
     {
       "Answer": 5,
@@ -29,6 +29,33 @@ describe "mission destroy" do
     }
   end
   let(:after_counts) { before_counts.map { |k, _| [k, 0] }.to_h }
+
+  before do
+    Setting.load_for_mission(mission)
+
+    users = create_list(:user, 3, mission: mission)
+    user_groups = create_list(:user_group, 2, mission: mission)
+    users.each { |u| create(:user_group_assignment, user: u, user_group: user_groups.sample) }
+
+    broadcast = create(:broadcast, mission: mission, medium: "sms", recipients: users)
+    broadcast.deliver # Creates Sms::Broadcast
+
+    opt_set = create(:option_set, option_names: :multilevel, mission: mission)
+    opt_set.replicate(mode: :clone)
+
+    form = create(:form, mission: mission,
+                         question_types: ["integer", "select_one", %w[integer integer], "select_multiple"])
+    form.publish! # Creates version
+    create(:question, qtype_name: "select_one", option_set: opt_set, mission: mission)
+    create(:condition, ref_qing: form.c[0], conditionable: form.c[3], mission: mission)
+    create(:skip_rule, source_item: form.c[1], destination: "item", dest_item: form.c[3],
+                       conditions_attributes: [{ref_qing_id: form.c[0].id, op: "eq", value: "5"}])
+    form.replicate(mode: :clone) # Tests that cloned objects can be deleted
+
+    create(:report, mission: mission)
+    create(:response, user: users.first, mission: mission, form: form,
+                      answer_values: [3, "Cat", [5, 6], %w[Cat Dog]])
+  end
 
   it "should delete all objects in mission" do
     expect(actual_counts).to eq(before_counts)
