74fff0b6b49e01e2dd278bb4c5c8785113e13b3b
Kevin Cooper <cooperka92@gmail.com>
2019-02-14 14:09:40 -0500

    9395: Add Enzyme to simulate dropdown select

--- spec/javascript/components/FormSelect.test.js
+++ spec/javascript/components/FormSelect.test.js
@@ -1,17 +1,27 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import Component from "../../../app/javascript/components/FormSelect";
 
+const changeFunc = jest.fn();
+
 const defaultProps = {
-  id: "1",
+  id: "root",
   options: [
-    { id: "1", name: "First" },
-    { id: "2", name: "Second" },
+    { id: "item1", name: "First" },
+    { id: "item2", name: "Second" },
   ],
+  changeFunc,
 };
 
 it("renders as expected", () => {
   const tree = renderer.create(<Component {...defaultProps} />);
   expect(tree.toJSON()).toMatchSnapshot();
+});
+
+it("calls changeFunc on change", () => {
+  const wrapper = shallow(<Component {...defaultProps} />);
+  wrapper.find("select").simulate("change", { target: { value: defaultProps.options[0].id } });
+  expect(changeFunc).toMatchSnapshot();
 });
