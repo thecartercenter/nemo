1a46c95d587a40ba783b9bea380ef3bcd86df2fb
Kevin Cooper <cooperka92@gmail.com>
2019-02-21 15:51:31 -0500

    9396: Display FormFilter in 'searches' view

--- spec/javascript/components/search/FormFilter.test.js
+++ spec/javascript/components/search/FormFilter.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {shallow} from "enzyme";
 
-import Component from "../../../../app/javascript/components/search_filters/FormFilter";
+import Component from "../../../../app/javascript/components/search/FormFilter";
 
 const defaultProps = {
   allForms: [
