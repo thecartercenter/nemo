1c9d64b9b02b2798c7b83537ce3f130ce7b90024
Kevin Cooper <cooperka92@gmail.com>
2019-02-22 15:01:59 -0500

    9396: Show popover on click

--- app/javascript/components/search/FormFilter.js
+++ app/javascript/components/search/FormFilter.js
@@ -2,12 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class FormFilter extends React.Component {
+  componentDidMount() {
+    // Initialize all popovers on the page.
+    $(function() {
+      // TODO: Be more selective about which ones to initialize.
+      $("[data-toggle=\"popover\"]").popover();
+    });
+  }
+
   render() {
     return (
-      <input
+      <button
         className="btn btn-default"
-        type="button"
-        value={I18n.t("filter.form")} />
+        data-content="List!"
+        data-placement="bottom"
+        data-toggle="popover"
+        title="Choose form"
+        type="button">
+        {I18n.t("filter.form")}
+      </button>
     );
   }
 }
--- spec/javascript/setupTests.js
+++ spec/javascript/setupTests.js
@@ -2,6 +2,11 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import I18n from "i18n-js";
 
+// Stub out jquery.
+// eslint-disable-next-line no-undef
+window.$ = () => {};
+
+// Provide translations.
 // eslint-disable-next-line no-undef
 window.I18n = I18n;
 
@@ -12,6 +17,7 @@ try {
   console.error("Failed to find translations. Did you run `rails i18n:js:export` yet?");
 }
 
+// Initialize Enzyme.
 Enzyme.configure({
   adapter: new Adapter(),
 });
