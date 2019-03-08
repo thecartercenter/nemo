99372952b4097844afd310c1920ca76797b8701c
Kevin Cooper <cooperka92@gmail.com>
2019-03-08 12:11:37 -0500

    9396: Add integration test for form filter

--- spec/javascript/components/search/Filters.test.js
+++ spec/javascript/components/search/Filters.test.js
@@ -1,14 +1,39 @@
 import React from "react";
-import {shallow} from "enzyme";
+import {shallow, mount} from "enzyme";
 
 import Component from "../../../../app/javascript/components/search/Filters";
 
 const defaultProps = {
-  allForms: [],
-  selectedFormIds: []
+  allForms: [
+    {
+      id: "1",
+      displayName: "One"
+    },
+    {
+      id: "2",
+      displayName: "Two"
+    }
+  ],
+  selectedFormIds: [
+    "2"
+  ],
 };
 
 it("renders as expected", () => {
   const wrapper = shallow(<Component {...defaultProps} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+describe("integration", () => {
+  const wrapper = mount(<Component {...defaultProps} />);
+
+  it("navigates on apply form filter", () => {
+    wrapper.find("Button.btn-form-filter").simulate("click");
+    const overlay = shallow(wrapper.find("OverlayTrigger").prop("overlay"));
+    overlay.find("Select2").simulate("change", {target: {value: defaultProps.allForms[0].id}});
+
+    expect(window.location.assign).toMatchSnapshot();
+    overlay.find("Button.btn-apply").simulate("click");
+    expect(window.location.assign).toMatchSnapshot();
+  });
 });
