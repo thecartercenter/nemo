ef963cedbd27de00e7a0103cd4d263f866ff84ad
Tom Smyth <tom@sassafras.coop>
2019-03-11 11:31:22 -0400

    9434: Fix report index and set consistent row height

--- app/decorators/report_decorator.rb
+++ app/decorators/report_decorator.rb
@@ -3,6 +3,11 @@
 class ReportDecorator < ApplicationDecorator
   delegate_all
 
+  # Without this, if decorator.class is called, it returns the Report module for some reason.
+  def class
+    Report::Report
+  end
+
   def default_path
     @default_path ||= h.report_path(object)
   end
