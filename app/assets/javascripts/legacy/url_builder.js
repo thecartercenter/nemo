7d54245028123b9742297f699298d8d4e2e936fd
Kevin Cooper <cooperka92@gmail.com>
2019-04-04 20:01:07 -0400

    9397: Manually fix remaining url_builder lints

--- app/assets/javascripts/legacy/url_builder.js
+++ app/assets/javascripts/legacy/url_builder.js
@@ -1,29 +1,29 @@
+/* eslint-disable no-param-reassign */
+
 // ELMO.UrlBuilder
 (function (ns, klass) {
-  // constructor
+  // eslint-disable-next-line no-multi-assign
   ns.UrlBuilder = klass = function (params) {
     this.locale = params.locale;
     this.mode = params.mode;
     this.mission_name = params.mission_name;
   };
 
-  klass.prototype.build = function () {
-    // we need some funky magic to turn the arguments object into an array
-    let args = Array.prototype.slice.call(arguments, 0);
+  klass.prototype.build = function (...args) {
     let options = {};
 
-    // if the last arg is an options hash, extract it
-    if (typeof (args[args.length - 1]) === 'object') {
+    // If the last arg is an options hash, extract it.
+    if (typeof args[args.length - 1] === 'object') {
       options = args[args.length - 1];
       args = args.slice(0, args.length - 1);
     }
 
-    // default to the current locale and mission name
+    // Default to the current locale and mission name.
     options.locale = options.locale || this.locale;
     options.mode = options.mode || this.mode;
     options.mission_name = options.mission_name || this.mission_name;
 
-    suffix = this.strip_scope(args.join('/'));
+    const suffix = this.strip_scope(args.join('/'));
 
     let result;
     switch (options.mode) {
@@ -37,23 +37,25 @@
         result = `/${options.locale}/admin/${suffix}`;
         break;
       default:
-        throw 'invalid mode';
+        throw new Error(`Invalid mode: ${options.mode}`);
     }
 
-    // return, fixing any double or trailing slashes
-    return result.replace(/[\/]{2,}/g, '/').replace(/\/$/, '');
+    // Return, fixing any double or trailing slashes.
+    return result.replace(/[/]{2,}/g, '/').replace(/\/$/, '');
   };
 
-  // Strips the locale and mission_name from the given path.
+  // Strip the locale and mission_name from the given path.
   klass.prototype.strip_scope = function (path) {
-    let mode_mission;
-    if (this.mode == 'mission') mode_mission = `m/${this.mission_name}`;
-    else if (this.mode == 'admin') mode_mission = 'admin';
-    else mode_mission = '';
+    let modeMission;
+    if (this.mode === 'mission') modeMission = `m/${this.mission_name}`;
+    else if (this.mode === 'admin') modeMission = 'admin';
+    else modeMission = '';
 
-    // replace the "/en/", "/en/m/mission", "/en/m/mission/", "/en", and "/" variants with "/"
-    if (path == '' || path.match(new RegExp(`^\/([a-z]{2}(\/${mode_mission})?(\/)?)?$`))) return '/';
-    // else fix the "/en/foo" or "/en/m/mission/foo" variants
-    return path.replace(new RegExp(`^\/[a-z]{2}(\/${mode_mission})?\/(.+)`), (m, $1, $2) => { return `/${$2}`; });
+    // Replace the "/en/", "/en/m/mission", "/en/m/mission/", "/en", and "/" variants with "/"...
+    if (!path || path.match(new RegExp(`^/([a-z]{2}(/${modeMission})?(/)?)?$`))) {
+      return '/';
+    }
+    // ...else fix the "/en/foo" or "/en/m/mission/foo" variants.
+    return path.replace(new RegExp(`^/[a-z]{2}(/${modeMission})?/(.+)`), (m, $1, $2) => `/${$2}`);
   };
 }(ELMO));
