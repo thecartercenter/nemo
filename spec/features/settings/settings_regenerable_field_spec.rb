5a601dbe186bd0584573b0ffdd87aee82d5d1895
Kevin Cooper <cooperka92@gmail.com>
2019-05-13 11:27:31 -0400

    9540: Add settings regenerable fields spec

---
+++ spec/features/settings/settings_regenerable_field_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "settings regenerable fields", js: true do
+  include_context "regenerable fields"
+
+  let!(:user) { create(:user) }
+  let!(:mission) { get_mission }
+
+  before do
+    login(user)
+  end
+
+  describe "override code" do
+    scenario "can regenerate" do
+      visit("/en/m/#{mission.compact_name}/settings")
+      expect_token_generated(".setting_override_code")
+      expect_token_regenerated(".setting_override_code")
+    end
+  end
+
+  describe "incoming sms token" do
+    scenario "can regenerate" do
+      visit("/en/m/#{mission.compact_name}/settings")
+      emails = emails_sent_by { expect_token_regenerated(".setting_incoming_sms_token") }
+      expect(emails.length).to eq(1)
+
+      email = emails.first
+      expect(email.body.to_s).to match(/#{mission.name}/)
+      expect(email.body.to_s).not_to match(/translation_missing/)
+    end
+  end
+end
--- spec/support/contexts/regenerable_fields.rb
+++ spec/support/contexts/regenerable_fields.rb
@@ -2,6 +2,15 @@
 
 # Provides spec helper methods for dealing with regenerable fields.
 shared_context "regenerable fields" do
+  def expect_token_generated(class_name)
+    within(class_name) do
+      token = find(".regenerable-field span").text
+      click_on("Generate")
+      expect(page).to have_css(".fa-check-circle")
+      expect(find(".regenerable-field span").text).not_to eq(token)
+    end
+  end
+
   def expect_token_regenerated(class_name)
     within(class_name) do
       token = find(".regenerable-field span").text
