eda919b50f64f45d3d6fbb1bc5c7bdd97736fd3c
Tom Smyth <tom@sassafras.coop>
2019-05-14 16:33:33 -0400

    9690: Tidy matchers file

--- spec/support/matchers/matchers.rb
+++ spec/support/matchers/matchers.rb
@@ -1,29 +1,31 @@
-require 'rspec/expectations'
+# frozen_string_literal: true
 
-RSpec::Matchers.define :end_with do |expected|
+require "rspec/expectations"
+
+RSpec::Matchers.define(:end_with) do |expected|
   match do |actual|
     actual[-expected.size..-1] == expected
   end
 end
 
-RSpec::Matchers.define :be_destroyed do
+RSpec::Matchers.define(:be_destroyed) do
   match do |actual|
-    raise 'expected nil to not be destroyed' if actual.nil?
+    raise "expected nil to not be destroyed" if actual.nil?
     !actual.class.exists?(actual.id)
   end
 end
 
 # Compares gridable report with expected data given as array of arrays representing grid.
-RSpec::Matchers.define :have_data_grid do |*expected|
+RSpec::Matchers.define(:have_data_grid) do |*expected|
   match do |report|
     raise "Report errors: " + report.errors.full_messages.join(", ") unless report.errors.empty?
 
     # if nil is expected, compute the right expected value
-    expected = report.data.totals ? [["TTL"], ["TTL", "0"]] : [] if expected.first.nil?
+    expected = report.data.totals ? [["TTL"], %w[TTL 0]] : [] if expected.first.nil?
 
     # Convert _ to ' '
     expected.each do |row|
-      row.map!{ |c| c.is_a?(String) ? c.gsub(/_(?=.)/, " ") : c }
+      row.map! { |c| c.is_a?(String) ? c.gsub(/_(?=.)/, " ") : c }
     end
 
     expected == to_grid(report)
@@ -35,42 +37,45 @@ RSpec::Matchers.define :have_data_grid do |*expected|
 
   def to_grid(report)
     # Add header row.
-    actual = [report.header_set[:col].collect{|cell| cell.name}]
+    actual = [report.header_set[:col].collect(&:name)]
     actual[0] << "TTL" if report.data.totals
 
-    report.data.rows.each_with_index do |row, i|
-      actual_row = []
-
-      # Add col header.
-      if report.header_set[:row] && report.header_set[:row].cells[i]
-        actual_row << report.header_set[:row].cells[i].name
-      end
-
-      actual_row += row
-
-      # Add row total.
-      actual_row << report.data.totals[:row][i] if report.data.totals
-
-      actual << actual_row
-    end
+    # Add main rows.
+    report.data.rows.each_with_index { |row, i| actual << actual_row(report, row, i) }
 
     # Add column total row.
     actual += [["TTL"] + report.data.totals[:col] + [report.data.totals[:grand]]] if report.data.totals
 
     # Convert everything to string; convert empty strings to "_".
-    actual.map{|row| row.map{|cell| cell.to_s.empty? ? "_" : cell.to_s}}
+    actual.map { |row| row.map { |cell| cell.to_s.empty? ? "_" : cell.to_s } }
+  end
+
+  def actual_row(report, row)
+    result = []
+
+    # Add col header.
+    if report.header_set[:row] && report.header_set[:row].cells[row_idx]
+      result << report.header_set[:row].cells[row_idx].name
+    end
+
+    result += row
+
+    # Add row total.
+    result << report.data.totals[:row][row_idx] if report.data.totals
+
+    result
   end
 end
 
-RSpec::Matchers.define :match_csv do |expected|
+RSpec::Matchers.define(:match_csv) do |expected|
   match do |actual|
     # Strip BOM from actual
-    doc = actual.gsub("\xEF\xBB\xBF", "")
+    doc = actual.delete("\xEF\xBB\xBF")
     doc.to_s == expected
   end
 end
 
-RSpec::Matchers.define :have_errors do |errors|
+RSpec::Matchers.define(:have_errors) do |errors|
   match do |object|
     object.invalid? && errors.all? do |field, pattern|
       object.errors[field].join.match?(pattern)
