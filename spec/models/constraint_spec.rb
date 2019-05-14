53987c9892675cba23a66703e7738dd53c5f80f3
Tom Smyth <tom@sassafras.coop>
2019-05-14 14:37:20 -0400

    9690: Add condition validation passthrough

--- spec/models/constraint_spec.rb
+++ spec/models/constraint_spec.rb
@@ -20,5 +20,22 @@ describe Constraint do
         it { is_expected.to have_errors(conditions: "There must be at least one condition.") }
       end
     end
+
+    describe "condition validation passthrough" do
+      let(:form) { create(:form, question_types: %w[integer]) }
+
+      context "with incomplete condition" do
+        let(:constraint) do
+          build(:constraint, questioning: form.c[0],
+                             conditions_attributes: [{ref_qing_id: form.c[0].id, op: "eq", value: ""}])
+        end
+
+        it "should set validation error" do
+          expect(constraint).not_to be_valid
+          expect(constraint.errors["conditions.base"].join).to eq("All condition fields are required.")
+          expect(constraint.conditions[0].errors[:base].join).to eq("All condition fields are required.")
+        end
+      end
+    end
   end
 end
