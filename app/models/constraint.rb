1d483fc461e867255a3dcccf132682b7bf2ff93a
Tom Smyth <tom@sassafras.coop>
2019-05-14 11:49:17 -0400

    9690: Add acts_as_list

--- app/models/constraint.rb
+++ app/models/constraint.rb
@@ -6,6 +6,10 @@ class Constraint < ApplicationRecord
 
   translates :rejection_msg
 
+  # Constraint ranks are currently not editable, but they provide a source of deterministic ordering
+  # which is useful in tests and in UI consistency.
+  acts_as_list column: :rank, scope: [:questioning_id]
+
   belongs_to :mission
   belongs_to :questioning
 end
