2321e8fd83a4ce5f88315690d94194f9a155a995
Tom Smyth <tom@sassafras.coop>
2019-05-14 16:25:40 -0400

    9690: Comment [skip ci]

--- db/migrate/20190514142053_enable_pgcrypto.rb
+++ db/migrate/20190514142053_enable_pgcrypto.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Newer versions of Postgres us pgcrypto for UUID generation instead of uuid-ossp.
 class EnablePgcrypto < ActiveRecord::Migration[5.2]
   def change
     enable_extension "pgcrypto"
