e97c3eea3a350d7788a26a5b0a5f47290a2de2f0
Kevin Cooper <cooperka92@gmail.com>
2019-05-15 11:52:18 -0400

    9722: Revert ES6 changes to url_builder
    Webpacker can't seem to compile any assets outside of app/javascript
    (and only supports one single entry point).
    
    Similar issue: https://stackoverflow.com/q/50982724/763231

--- app/assets/javascripts/legacy/url_builder.js
+++ app/assets/javascripts/legacy/url_builder.js
@@ -9,8 +9,10 @@
     this.mission_name = params.mission_name;
   };
 
-  klass.prototype.build = function (...args) {
-    let options = {};
+  klass.prototype.build = function () {
+    // we need some funky magic to turn the arguments object into an array
+    var args = Array.prototype.slice.call(arguments, 0);
+    var options = {};
 
     // If the last arg is an options hash, extract it.
     if (typeof args[args.length - 1] === 'object') {
@@ -23,21 +25,21 @@
     options.mode = options.mode || this.mode;
     options.mission_name = options.mission_name || this.mission_name;
 
-    const suffix = this.strip_scope(args.join('/'));
+    var suffix = this.strip_scope(args.join('/'));
 
-    let result;
+    var result;
     switch (options.mode) {
       case 'basic':
-        result = `/${options.locale}/${suffix}`;
+        result = '/' + options.locale + '/' + suffix;
         break;
       case 'mission':
-        result = `/${options.locale}/m/${options.mission_name}/${suffix}`;
+        result = '/' + options.locale + '/m/' + options.mission_name + '/' + suffix;
         break;
       case 'admin':
-        result = `/${options.locale}/admin/${suffix}`;
+        result = '/' + options.locale + '/admin/' + suffix;
         break;
       default:
-        throw new Error(`Invalid mode: ${options.mode}`);
+        throw new Error('Invalid mode: ' + options.mode);
     }
 
     // Return, fixing any double or trailing slashes.
@@ -46,16 +48,16 @@
 
   // Strip the locale and mission_name from the given path.
   klass.prototype.strip_scope = function (path) {
-    let modeMission;
-    if (this.mode === 'mission') modeMission = `m/${this.mission_name}`;
+    var modeMission;
+    if (this.mode === 'mission') modeMission = 'm/' + this.mission_name;
     else if (this.mode === 'admin') modeMission = 'admin';
     else modeMission = '';
 
     // Replace the "/en/", "/en/m/mission", "/en/m/mission/", "/en", and "/" variants with "/"...
-    if (!path || path.match(new RegExp(`^/([a-z]{2}(/${modeMission})?(/)?)?$`))) {
+    if (!path || path.match(new RegExp('^/([a-z]{2}(/' + modeMission + ')?(/)?)?$'))) {
       return '/';
     }
     // ...else fix the "/en/foo" or "/en/m/mission/foo" variants.
-    return path.replace(new RegExp(`^/[a-z]{2}(/${modeMission})?/(.+)`), (m, $1, $2) => `/${$2}`);
+    return path.replace(new RegExp('^/[a-z]{2}(/' + modeMission + ')?/(.+)'), function (m, $1, $2) { return '/' + $2; });
   };
 }(ELMO));
