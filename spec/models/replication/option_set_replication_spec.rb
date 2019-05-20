7e6eaf63a53163a536d6bff562b9fafa359cceb8
Tom Smyth <tom@sassafras.coop>
2019-05-20 10:58:49 -0400

    9730: Add spec covering option set clone in admin mode

--- spec/models/replication/option_set_replication_spec.rb
+++ spec/models/replication/option_set_replication_spec.rb
@@ -14,7 +14,7 @@ describe OptionSet do
       expect(copy.mission).to eq(mission2)
       expect(copy.name).to eq(orig.name)
       expect(copy.standard).to eq(orig)
-      expect(copy.is_standard).to eq(false)
+      expect(copy.is_standard).to be(false)
       expect(copy.total_options).to eq(6)
 
       # Ensure options are correct.
@@ -32,20 +32,20 @@ describe OptionSet do
 
       # Ensure option set ID gets copied all the way down.
       expect(copy.root_node.option_set_id).to eq(copy.id)
-      expect(copy.root_node.c[0].option_set_id).to eq(copy.id)
-      expect(copy.root_node.c[0].c[0].option_set_id).to eq(copy.id)
+      expect(copy.c[0].option_set_id).to eq(copy.id)
+      expect(copy.c[0].c[0].option_set_id).to eq(copy.id)
 
       # Ensure option nodes get correct attributes
-      expect(copy.root_node.c[0].is_standard).to be(false)
-      expect(copy.root_node.c[0].c[0].is_standard).to be(false)
-      expect(copy.root_node.c[0].standard_copy).to be(true)
-      expect(copy.root_node.c[0].c[0].standard_copy).to be(true)
+      expect(copy.c[0].is_standard).to be(false)
+      expect(copy.c[0].c[0].is_standard).to be(false)
+      expect(copy.c[0].standard_copy).to be(true)
+      expect(copy.c[0].c[0].standard_copy).to be(true)
 
       # Ensure option nodes get correct original references
-      expect(copy.root_node.c[0].original_id).to eq(orig.root_node.c[0].id)
-      expect(copy.root_node.c[0].c[0].original_id).to eq(orig.root_node.c[0].c[0].id)
-      expect(copy.root_node.c[0].original).to eq(orig.root_node.c[0])
-      expect(copy.root_node.c[0].c[0].original).to eq(orig.root_node.c[0].c[0])
+      expect(copy.c[0].original_id).to eq(orig.c[0].id)
+      expect(copy.c[0].c[0].original_id).to eq(orig.c[0].c[0].id)
+      expect(copy.c[0].original).to eq(orig.c[0])
+      expect(copy.c[0].c[0].original).to eq(orig.c[0].c[0])
 
       # Ensure no duplicates.
       expect(Option.count).to eq(12)
@@ -76,49 +76,87 @@ describe OptionSet do
   end
 
   describe "clone" do
-    let(:orig) { create(:option_set, option_names: :multilevel, mission: mission1) }
     let(:copy) { orig.replicate(mode: :clone) }
 
-    it "should make correct copy" do
-      expect(copy.mission).to eq(mission1)
-      expect(copy.is_standard).to eq(false)
-      expect(copy.original).to eq(orig)
-      expect(copy.total_options).to eq(6)
-      expect(copy.options).to eq(orig.options)
+    context "within mission" do
+      let(:orig) { create(:option_set, option_names: :multilevel, mission: mission1) }
 
-      # Ensure options are correct.
-      expect(copy.c[0].option.name).to eq(orig.c[0].option.name)
-      expect(copy.c[0].c[0].option.name).to eq(orig.c[0].c[0].option.name)
+      it "should make correct copy" do
+        expect(copy.mission).to eq(mission1)
+        expect(copy.is_standard).to be(false)
+        expect(copy.original).to eq(orig)
+        expect(copy.total_options).to eq(6)
+        expect(copy.options).to eq(orig.options)
 
-      # Ensure ancestry is correct.
-      expect(copy.root_node.parent).to be_nil
-      expect(copy.c[0].parent).to eq(copy.root_node)
-      expect(copy.c[0].c[0].parent).to eq(copy.c[0])
+        # Ensure options are correct.
+        expect(copy.c[0].option.name).to eq(orig.c[0].option.name)
+        expect(copy.c[0].c[0].option.name).to eq(orig.c[0].c[0].option.name)
 
-      # Ensure option set gets correct root node id.
-      expect(copy.root_node_id).not_to be_nil
-      expect(copy.root_node_id).not_to eq(orig.root_node_id)
+        # Ensure ancestry is correct.
+        expect(copy.root_node.parent).to be_nil
+        expect(copy.c[0].parent).to eq(copy.root_node)
+        expect(copy.c[0].c[0].parent).to eq(copy.c[0])
 
-      # Ensure option set ID gets copied all the way down.
-      expect(copy.root_node.option_set_id).to eq(copy.id)
-      expect(copy.root_node.c[0].option_set_id).to eq(copy.id)
-      expect(copy.root_node.c[0].c[0].option_set_id).to eq(copy.id)
+        # Ensure option set gets correct root node id.
+        expect(copy.root_node_id).not_to be_nil
+        expect(copy.root_node_id).not_to eq(orig.root_node_id)
 
-      # Ensure option nodes get correct attributes
-      expect(copy.root_node.c[0].is_standard).to be(false)
-      expect(copy.root_node.c[0].c[0].is_standard).to be(false)
-      expect(copy.root_node.c[0].standard_copy).to be(false)
-      expect(copy.root_node.c[0].c[0].standard_copy).to be(false)
+        # Ensure option set ID and mission gets copied all the way down.
+        expect(copy.root_node.option_set_id).to eq(copy.id)
+        expect(copy.c[0].option_set_id).to eq(copy.id)
+        expect(copy.c[0].c[0].option_set_id).to eq(copy.id)
+        expect(copy.root_node.mission).to eq(mission1)
+        expect(copy.c[0].mission).to eq(mission1)
+        expect(copy.c[0].c[0].mission).to eq(mission1)
 
-      # Ensure option nodes get correct original references
-      expect(copy.root_node.c[0].original_id).to eq(orig.root_node.c[0].id)
-      expect(copy.root_node.c[0].c[0].original_id).to eq(orig.root_node.c[0].c[0].id)
-      expect(copy.root_node.c[0].original).to eq(orig.root_node.c[0])
-      expect(copy.root_node.c[0].c[0].original).to eq(orig.root_node.c[0].c[0])
+        # Ensure option nodes get correct attributes
+        expect(copy.c[0].is_standard).to be(false)
+        expect(copy.c[0].c[0].is_standard).to be(false)
+        expect(copy.c[0].standard_copy).to be(false)
+        expect(copy.c[0].c[0].standard_copy).to be(false)
 
-      # Ensure no duplicates (options are not duplicated in :clone mode)
-      expect(Option.count).to eq(6)
-      expect(OptionNode.count).to eq(14)
+        # Ensure option nodes get correct original references
+        expect(copy.c[0].original_id).to eq(orig.c[0].id)
+        expect(copy.c[0].c[0].original_id).to eq(orig.c[0].c[0].id)
+        expect(copy.c[0].original).to eq(orig.c[0])
+        expect(copy.c[0].c[0].original).to eq(orig.c[0].c[0])
+
+        # Ensure no duplicates (options are not duplicated in :clone mode)
+        expect(Option.count).to eq(6)
+        expect(OptionNode.count).to eq(14)
+      end
+    end
+
+    context "within admin mode" do
+      let(:orig) { create(:option_set, option_names: :multilevel, is_standard: true) }
+
+      it "should make correct copy" do
+        expect(copy.mission).to be_nil
+        expect(copy.is_standard).to be(true)
+        expect(copy.original).to eq(orig)
+        expect(copy.total_options).to eq(6)
+        expect(copy.options).to eq(orig.options)
+
+        # Ensure option nodes get correct attributes
+        expect(copy.c[0].is_standard).to be(true)
+        expect(copy.c[0].c[0].is_standard).to be(true)
+        expect(copy.c[0].standard_copy).to be(false)
+        expect(copy.c[0].c[0].standard_copy).to be(false)
+
+        # Ensure option set ID and mission gets copied all the way down.
+        expect(copy.root_node.option_set_id).to eq(copy.id)
+        expect(copy.c[0].option_set_id).to eq(copy.id)
+        expect(copy.c[0].c[0].option_set_id).to eq(copy.id)
+        expect(copy.root_node.mission).to be_nil
+        expect(copy.c[0].mission).to be_nil
+        expect(copy.c[0].c[0].mission).to be_nil
+
+        # Ensure option nodes get correct original references
+        expect(copy.c[0].original_id).to eq(orig.c[0].id)
+        expect(copy.c[0].c[0].original_id).to eq(orig.c[0].c[0].id)
+        expect(copy.c[0].original).to eq(orig.c[0])
+        expect(copy.c[0].c[0].original).to eq(orig.c[0].c[0])
+      end
     end
   end
 end
