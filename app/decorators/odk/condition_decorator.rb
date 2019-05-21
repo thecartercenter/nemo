1692e2295ea5bfd938a4a469c47ec2b98d150c9f
Tom Smyth <tom@sassafras.coop>
2019-05-21 14:19:48 -0400

    9687: Make odk rendering ignore right_side_is_qing conditions for now

--- app/decorators/odk/condition_decorator.rb
+++ app/decorators/odk/condition_decorator.rb
@@ -17,22 +17,9 @@ module Odk
     }.freeze
 
     def to_odk
+      return "true()" if right_side_is_qing?
       lhs = questioning.xpath_to(ref_subqing)
-
-      if left_qing.has_options?
-        selected = "selected(#{lhs}, '#{option_node.odk_code}')"
-        %w[neq ninc].include?(op) ? "not(#{selected})" : selected
-      else
-        if left_qing.temporal?
-          format = :"javarosa_#{left_qing.qtype_name}"
-          formatted = Time.zone.parse(value).to_s(format)
-          lhs = "format-date(#{lhs}, '#{Time::DATE_FORMATS[format]}')"
-          rhs = "'#{formatted}'"
-        else
-          rhs = left_qing.numeric? ? value : "'#{value}'"
-        end
-        "#{lhs} #{OP_XPATH[op.to_sym]} #{rhs}"
-      end
+      left_qing.has_options? ? select_to_odk(lhs) : non_select_to_odk(lhs)
     end
 
     def questioning
@@ -41,6 +28,25 @@ module Odk
 
     def left_qing
       decorate(object.left_qing)
+    end
+
+    private
+
+    def select_to_odk(lhs)
+      selected = "selected(#{lhs}, '#{option_node.odk_code}')"
+      %w[neq ninc].include?(op) ? "not(#{selected})" : selected
+    end
+
+    def non_select_to_odk(lhs)
+      if left_qing.temporal?
+        format = :"javarosa_#{left_qing.qtype_name}"
+        formatted = Time.zone.parse(value).to_s(format)
+        lhs = "format-date(#{lhs}, '#{Time::DATE_FORMATS[format]}')"
+        rhs = "'#{formatted}'"
+      else
+        rhs = left_qing.numeric? ? value : "'#{value}'"
+      end
+      "#{lhs} #{OP_XPATH[op.to_sym]} #{rhs}"
     end
   end
 end
