7cb34eae54e3ebcc78ad81c532df80eb5607b230
Tom Smyth <tom@sassafras.coop>
2019-05-22 14:43:13 -0400

    9693: Basic cases

--- app/decorators/odk/condition_decorator.rb
+++ app/decorators/odk/condition_decorator.rb
@@ -17,9 +17,13 @@ module Odk
     }.freeze
 
     def to_odk
-      return "true()" if right_side_is_qing?
-      lhs = questioning.xpath_to(ref_subqing)
-      left_qing.has_options? ? select_to_odk(lhs) : non_select_to_odk(lhs)
+      if left_qing.has_options?
+        select_to_odk
+      elsif left_qing.temporal? && right_side_is_literal?
+        temporal_to_odk
+      else
+        other_types_to_odk
+      end
     end
 
     def questioning
@@ -32,21 +36,39 @@ module Odk
 
     private
 
-    def select_to_odk(lhs)
-      selected = "selected(#{lhs}, '#{option_node.odk_code}')"
+    def select_to_odk
+      selected = "selected(#{left_xpath}, '#{option_node.odk_code}')"
       %w[neq ninc].include?(op) ? "not(#{selected})" : selected
     end
 
-    def non_select_to_odk(lhs)
-      if left_qing.temporal?
-        format = :"javarosa_#{left_qing.qtype_name}"
-        formatted = Time.zone.parse(value).to_s(format)
-        lhs = "format-date(#{lhs}, '#{Time::DATE_FORMATS[format]}')"
-        rhs = "'#{formatted}'"
-      else
-        rhs = left_qing.numeric? ? value : "'#{value}'"
-      end
-      "#{lhs} #{OP_XPATH[op.to_sym]} #{rhs}"
+    def temporal_to_odk
+      format = :"javarosa_#{left_qing.qtype_name}"
+      formatted = Time.zone.parse(value).to_s(format)
+      left = "format-date(#{left_xpath}, '#{Time::DATE_FORMATS[format]}')"
+      right = "'#{formatted}'"
+      join_with_operator(left, right)
+    end
+
+    def other_types_to_odk
+      left = left_xpath
+      right = if right_side_is_qing?
+                right_xpath
+              else
+                left_qing.numeric? ? value : "'#{value}'"
+              end
+      join_with_operator(left, right)
+    end
+
+    def join_with_operator(left, right)
+      "#{left} #{OP_XPATH[op.to_sym]} #{right}"
+    end
+
+    def left_xpath
+      questioning.xpath_to(ref_subqing)
+    end
+
+    def right_xpath
+      questioning.xpath_to(right_qing)
     end
   end
 end
--- spec/decorators/odk/condition_decorator_spec.rb
+++ spec/decorators/odk/condition_decorator_spec.rb
@@ -6,153 +6,131 @@ describe Odk::ConditionDecorator do
   include_context "odk rendering"
 
   describe "to_odk", :odk do
-    let(:qing1) { decorate(form.c[0]) }
+    let(:q1) { decorate(form.c[0]) }
     let(:opt_set) { form.c[0].option_set }
     let(:option_node) { form.c[0].option_set.c[0] }
     let(:hostq) { decorate(form.c.last) }
     let(:condition) { Condition.new({conditionable: hostq.object}.merge(params)) }
     subject(:xpath) { decorate(condition).to_odk }
 
-    context "for single level select one question" do
-      let(:form) { create(:form, question_types: %w[select_one text]) }
+    context "when right_side_type is literal" do
+      context "for single level select one question" do
+        let(:form) { create(:form, question_types: %w[select_one text]) }
 
-      context "with eq operator" do
-        let(:params) { {left_qing: qing1.object, op: "eq", option_node: option_node} }
+        context "with eq operator" do
+          let(:params) { {left_qing: q1.object, op: "eq", option_node: option_node} }
+          it { is_expected.to eq("selected(/data/#{q1.odk_code}, 'on#{option_node.id}')") }
+        end
 
-        it do
-          expect(xpath).to eq("selected(/data/#{qing1.odk_code}, 'on#{option_node.id}')")
+        context "with neq operator" do
+          let(:params) { {left_qing: q1.object, op: "neq", option_node: option_node} }
+          it { is_expected.to eq("not(selected(/data/#{q1.odk_code}, 'on#{option_node.id}'))") }
         end
       end
 
-      context "with neq operator" do
-        let(:params) { {left_qing: qing1.object, op: "neq", option_node: option_node} }
+      context "for multilevel select one question" do
+        let(:form) { create(:form, question_types: %w[multilevel_select_one]) }
+        let(:subq1) { decorate(q1.subqings[0]) }
+        let(:subq2) { decorate(q1.subqings[1]) }
 
-        it do
-          expect(xpath).to eq("not(selected(/data/#{qing1.odk_code}, 'on#{option_node.id}'))")
+        context "for first level" do
+          let(:params) { {left_qing: q1.object, op: "eq", option_node: opt_set.c[0]} }
+          it { is_expected.to eq("selected(/data/#{subq1.odk_code}, 'on#{opt_set.c[0].id}')") }
         end
-      end
-    end
 
-    context "for multilevel select one question" do
-      let(:form) { create(:form, question_types: %w[multilevel_select_one]) }
-      let(:subqing1) { decorate(qing1.subqings[0]) }
-      let(:subqing2) { decorate(qing1.subqings[1]) }
-
-      context "for first level" do
-        let(:params) { {left_qing: qing1.object, op: "eq", option_node: opt_set.c[0]} }
-
-        it do
-          expect(xpath).to eq("selected(/data/#{subqing1.odk_code}, 'on#{opt_set.c[0].id}')")
+        context "for second level" do
+          let(:params) { {left_qing: q1.object, op: "eq", option_node: opt_set.c[0].c[1]} }
+          it { is_expected.to eq("selected(/data/#{subq2.odk_code}, 'on#{opt_set.c[0].c[1].id}')") }
         end
       end
 
-      context "for second level" do
-        let(:params) { {left_qing: qing1.object, op: "eq", option_node: opt_set.c[0].c[1]} }
+      context "for select multiple question" do
+        let(:form) { create(:form, question_types: %w[select_multiple text]) }
 
-        it do
-          expect(xpath).to eq("selected(/data/#{subqing2.odk_code}, 'on#{opt_set.c[0].c[1].id}')")
+        context "with inc operator" do
+          let(:params) { {left_qing: q1.object, op: "inc", option_node: option_node} }
+          it { is_expected.to eq("selected(/data/#{q1.odk_code}, 'on#{option_node.id}')") }
         end
-      end
-    end
 
-    context "for select multiple question" do
-      let(:form) { create(:form, question_types: %w[select_multiple text]) }
-
-      context "with inc operator" do
-        let(:params) { {left_qing: qing1.object, op: "inc", option_node: option_node} }
-
-        it do
-          expect(xpath).to eq("selected(/data/#{qing1.odk_code}, 'on#{option_node.id}')")
+        context "with ninc operator" do
+          let(:params) { {left_qing: q1.object, op: "ninc", option_node: option_node} }
+          it { is_expected.to eq("not(selected(/data/#{q1.odk_code}, 'on#{option_node.id}'))") }
         end
       end
 
-      context "with ninc operator" do
-        let(:params) { {left_qing: qing1.object, op: "ninc", option_node: option_node} }
+      context "for non-select question" do
+        let(:form) { create(:form, question_types: %w[integer text date time datetime text]) }
+        let(:int_q) { decorate(form.c[0]) }
+        let(:text_q) { decorate(form.c[1]) }
+        let(:date_q) { decorate(form.c[2]) }
+        let(:time_q) { decorate(form.c[3]) }
+        let(:datetime_q) { decorate(form.c[4]) }
 
-        it do
-          expect(xpath).to eq("not(selected(/data/#{qing1.odk_code}, 'on#{option_node.id}'))")
+        context "with eq operator and int question" do
+          let(:params) { {left_qing: int_q.object, op: "eq", value: "5"} }
+          it { is_expected.to eq("/data/#{int_q.odk_code} = 5") }
         end
-      end
-    end
 
-    context "for non-select question" do
-      let(:form) { create(:form, question_types: %w[integer text date time datetime text]) }
-      let(:int_q) { decorate(form.c[0]) }
-      let(:text_q) { decorate(form.c[1]) }
-      let(:date_q) { decorate(form.c[2]) }
-      let(:time_q) { decorate(form.c[3]) }
-      let(:datetime_q) { decorate(form.c[4]) }
-
-      context "with eq operator and int question" do
-        let(:params) { {left_qing: int_q.object, op: "eq", value: "5"} }
-
-        it do
-          expect(xpath).to eq("/data/#{int_q.odk_code} = 5")
+        context "with neq operator and text question" do
+          let(:params) { {left_qing: text_q.object, op: "neq", value: "foo"} }
+          it { is_expected.to eq("/data/#{text_q.odk_code} != 'foo'") }
         end
-      end
 
-      context "with neq operator and text question" do
-        let(:params) { {left_qing: text_q.object, op: "neq", value: "foo"} }
-
-        it do
-          expect(xpath).to eq("/data/#{text_q.odk_code} != 'foo'")
+        context "with date question and geq operator" do
+          let(:params) { {left_qing: date_q.object, op: "geq", value: "1981-10-26"} }
+          it { is_expected.to eq("format-date(/data/#{date_q.odk_code}, '%Y%m%d') >= '19811026'") }
         end
-      end
 
-      context "with date question and geq operator" do
-        let(:params) { {left_qing: date_q.object, op: "geq", value: "1981-10-26"} }
+        context "with time question and leq operator" do
+          let(:params) { {left_qing: time_q.object, op: "leq", value: "3:56pm"} }
+          it { is_expected.to eq("format-date(/data/#{time_q.odk_code}, '%H%M') <= '1556'") }
+        end
 
-        it do
-          expect(xpath).to eq("format-date(/data/#{date_q.odk_code}, '%Y%m%d') >= '19811026'")
+        context "with datetime question and gt operator" do
+          let(:params) { {left_qing: datetime_q.object, op: "gt", value: "Dec 3 2003 11:56"} }
+          it { is_expected.to eq("format-date(/data/#{datetime_q.odk_code}, '%Y%m%d%H%M') > '200312031156'") }
         end
       end
 
-      context "with time question and leq operator" do
-        let(:params) { {left_qing: time_q.object, op: "leq", value: "3:56pm"} }
+      context "for intra-group reference" do
+        let(:form) { create(:form, question_types: [%w[multilevel_select_one text text]]) }
+        let(:q1) { decorate(form.c[0].c[0]) }
+        let(:q2) { decorate(form.c[0].c[1]) }
 
-        it do
-          expect(xpath).to eq("format-date(/data/#{time_q.odk_code}, '%H%M') <= '1556'")
+        context "for regular ref qing" do
+          let(:params) { {left_qing: q2.object, op: "eq", value: "foo"} }
+          it { is_expected.to eq("../#{q2.odk_code} = 'foo'") }
         end
-      end
 
-      context "with datetime question and gt operator" do
-        let(:params) { {left_qing: datetime_q.object, op: "gt", value: "Dec 3 2003 11:56"} }
-
-        it do
-          expect(xpath).to eq("format-date(/data/#{datetime_q.odk_code}, '%Y%m%d%H%M') > '200312031156'")
+        context "for multilevel ref qing" do
+          let(:option_node) { q1.option_set.c[0].c[0] }
+          let(:params) { {left_qing: q1.object, op: "eq", option_node: option_node} }
+          it { is_expected.to eq("selected(../#{q1.odk_code}_2, '#{option_node.odk_code}')") }
         end
       end
     end
 
-    context "for intra-group reference" do
-      let(:form) { create(:form, question_types: [%w[multilevel_select_one text text]]) }
-      let(:q1) { decorate(form.sorted_children[0].sorted_children[0]) }
-      let(:q2) { decorate(form.sorted_children[0].sorted_children[1]) }
+    context "when right_side_type is qing" do
+      let(:q2) { decorate(form.c[1]) }
 
-      context "for regular ref qing" do
-        let(:params) { {left_qing: q2.object, op: "eq", value: "foo"} }
-
-        it do
-          expect(xpath).to eq("../#{q2.odk_code} = 'foo'")
-        end
+      context "with straight equality" do
+        let(:form) { create(:form, question_types: %w[text text]) }
+        let(:params) { {left_qing: q2.object, op: "eq", right_side_type: "qing", right_qing: q1.object} }
+        it { is_expected.to eq("/data/#{q2.odk_code} = /data/#{q1.odk_code}") }
       end
 
-      context "for multilevel ref qing" do
-        let(:option_node) { q1.option_set.sorted_children[0].sorted_children[0] }
-        let(:params) { {left_qing: q1.object, op: "eq", option_node: option_node} }
-
-        it do
-          expect(xpath).to eq("selected(../#{q1.odk_code}_2, '#{option_node.odk_code}')")
-        end
+      context "with temporal questions" do
+        let(:form) { create(:form, question_types: %w[date date]) }
+        let(:params) { {left_qing: q2.object, op: "eq", right_side_type: "qing", right_qing: q1.object} }
+        it { is_expected.to eq("/data/#{q2.odk_code} = /data/#{q1.odk_code}") }
       end
-    end
 
-    context "for right_side_type qing" do
-      let(:form) { create(:form, question_types: %w[text text]) }
-      let(:params) { {left_qing: hostq.object, op: "eq", right_side_type: "qing", right_qing: qing1.object} }
-
-      it "temporarily ignores" do
-        expect(xpath).to eq("true()")
+      context "with intra group references" do
+        let(:form) { create(:form, question_types: [%w[integer integer]]) }
+        let(:q1) { decorate(form.c[0].c[0]) }
+        let(:q2) { decorate(form.c[0].c[1]) }
+        let(:params) { {left_qing: q2.object, op: "gt", right_side_type: "qing", right_qing: q1.object} }
+        it { is_expected.to eq("../#{q2.odk_code} > ../#{q1.odk_code}") }
       end
     end
   end
