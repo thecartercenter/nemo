e57ed44f2720638be38da22ec7f1e4bcd5c75be5
Kevin Cooper <cooperka92@gmail.com>
2019-06-03 17:12:38 -0400

    9745: More hounds

--- app/models/search/parser.rb
+++ app/models/search/parser.rb
@@ -41,40 +41,39 @@ class Search::Parser
     token = Search::Token.new(@search, kind, parent)
 
     token.children = case kind
-      when :query
-        [take(token, :expression)] + (next_is?(:eot) ? [] : [take(token, :query)])
+    when :query
+      [take(token, :expression)] + (next_is?(:eot) ? [] : [take(token, :query)])
 
-      when :expression
-        raise Search::ParseError, I18n.t("search.or_not_allowed_between") if next_is?(:or)
-        [next_is?(:chunk) && next2_is?(*COMP_OP) ? take(token, :qualified_expression) : take(token, :unqualified_expression)]
+    when :expression
+      raise Search::ParseError, I18n.t("search.or_not_allowed_between") if next_is?(:or)
+      [next_is?(:chunk) && next2_is?(*COMP_OP) ? take(token, :qualified_expression) : take(token, :unqualified_expression)]
 
-      when :unqualified_expression
-        [take(token, :values)]
+    when :unqualified_expression
+      [take(token, :values)]
 
-      when :qualified_expression
-        [take_terminal(token, :chunk), take_terminal(token, *COMP_OP), take(token, :rhs)]
+    when :qualified_expression
+      [take_terminal(token, :chunk), take_terminal(token, *COMP_OP), take(token, :rhs)]
 
-      when :rhs
-        if next_is?(:lparen)
-          [take_terminal(token, :lparen), take(token, :values), take_terminal(token, :rparen)]
+    when :rhs
+      if next_is?(:lparen)
+        [take_terminal(token, :lparen), take(token, :values), take_terminal(token, :rparen)]
+      else
+        [take(token, :value)]
+      end
+
+    when :values
+      [take(token, :value)] +
+        if next_is?(:eot, :rparen) || next2_is?(*COMP_OP)
+          []
+        elsif next_is?(:or)
+          [take_terminal(token, :or), take(token, :values)]
         else
-          [take(token, :value)]
+          [take(token, :values)]
         end
 
-      when :values
-        [take(token, :value)] +
-          if next_is?(:eot, :rparen) || next2_is?(*COMP_OP)
-            []
-          elsif next_is?(:or)
-            [take_terminal(token, :or), take(token, :values)]
-          else
-            [take(token, :values)]
-          end
-
-      when :value
-        [take_terminal(token, :chunk, :string)]
-
-      end
+    when :value
+      [take_terminal(token, :chunk, :string)]
+    end
 
     token
   end
