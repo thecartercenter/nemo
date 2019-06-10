54353416e52c942fdb39e228294595cd66809cdc
Tom Smyth <tom@sassafras.coop>
2019-06-10 10:59:11 -0400

    9689: Add right side stuff to permitted params

---
+++ app/controllers/concerns/condition_formable.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module ConditionFormable
+  extend ActiveSupport::Concern
+
+  def condition_params
+    [:id, :left_qing_id, :right_qing_id, :right_side_type, :op, :value, :_destroy, option_node_ids: []]
+  end
+end
