43ab920822c8444d9c33bce478280dd975dcc950
Tom Smyth <tom@sassafras.coop>
2019-06-25 15:03:19 -0400

    9696: Combine SkipLogicFormField and SkipRuleSetFormField

--- app/javascript/components/conditions/SkipLogicFormField/component.js
+++ app/javascript/components/conditions/SkipLogicFormField/component.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import SkipRuleSetFormField from './SkipRuleSetFormField/component';
+import SkipRuleFormField from './SkipRuleFormField';
 
 class SkipLogicFormField extends React.Component {
   static propTypes = {
     type: PropTypes.string.isRequired,
     skipRules: PropTypes.arrayOf(PropTypes.object).isRequired,
+    formId: PropTypes.string.isRequired,
+    laterItems: PropTypes.arrayOf(PropTypes.object),
+    refableQings: PropTypes.arrayOf(PropTypes.object),
   };
 
   constructor(props) {
     super(props);
     const { skipRules } = this.props;
     const skip = skipRules.length === 0 ? 'dont_skip' : 'skip';
-    this.state = { skip };
+    this.state = { skip, skipRules };
+  }
+
+  handleAddClick = () => {
+    const { laterItems } = this.props;
+    const laterItemsExist = laterItems.length > 0;
+    this.setState(({ skipRules }) => ({
+      skipRules: skipRules.concat([{
+        key: Math.round(Math.random() * 100000000),
+        destination: laterItemsExist ? 'item' : 'end',
+        skipIf: 'always',
+        conditions: [],
+      }]),
+    }));
   }
 
   skipOptionChanged = (event) => {
+    const { skipRules } = this.state;
     this.setState({ skip: event.target.value });
+    if (event.target.value === 'skip' && skipRules.length === 0) {
+      this.handleAddClick();
+    }
   }
 
   skipOptionTags = () => {
@@ -33,8 +53,8 @@ class SkipLogicFormField extends React.Component {
   }
 
   render() {
-    const { type } = this.props;
-    const { skip } = this.state;
+    const { formId, laterItems, type, refableQings } = this.props;
+    const { skip, skipRules } = this.state;
     const selectProps = {
       className: 'form-control skip-or-not',
       value: skip,
@@ -48,10 +68,38 @@ class SkipLogicFormField extends React.Component {
         <select {...selectProps}>
           {this.skipOptionTags()}
         </select>
-        <SkipRuleSetFormField
-          hide={skip === 'dont_skip'}
-          {...this.props}
-        />
+        <div
+          className="rule-set"
+          style={{ display: skip === 'dont_skip' ? 'none' : '' }}
+        >
+          {skipRules.map((rule, index) => (
+            <SkipRuleFormField
+              formId={formId}
+              hide={skip === 'dont_skip'}
+              key={rule.key || rule.id}
+              laterItems={laterItems}
+              ruleId={`rule-${index + 1}`}
+              namePrefix={`${type}[skip_rules_attributes][${index}]`}
+              refableQings={refableQings}
+              {...rule}
+            />
+          ))}
+          <div
+            className="rule-add-link-wrapper"
+          >
+            {/* TODO: Improve a11y. */}
+            {/* eslint-disable */}
+            <a
+              onClick={this.handleAddClick}
+              tabIndex="0"
+            >
+            {/* eslint-enable */}
+              <i className="fa fa-plus" />
+              {' '}
+              {I18n.t('form_item.add_rule')}
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
