b7be0039c18c7d50ecddf6b956f2e1d7bf4b4d86
Kevin Cooper <cooperka92@gmail.com>
2019-07-05 14:24:18 -0700

    9393: Factor out filter_by_boolean

--- app/searchers/responses_searcher.rb
+++ app/searchers/responses_searcher.rb
@@ -174,11 +174,7 @@ class ResponsesSearcher < Searcher
     when "form"
       return filter_by_names(token_values, Form, current_ids: form_ids)
     when "reviewed"
-      return false unless token_values.length == 1
-      value = token_values[0].downcase
-      return false unless %w[1 0 yes no].include?(value)
-      self.is_reviewed = %w[1 yes].include?(value)
-      return true
+      return filter_by_boolean(token_values, filter_name: :is_reviewed)
     when "submitter"
       return filter_by_names(token_values, User, current_ids: submitters, include_name: true)
     when "group"
@@ -188,12 +184,24 @@ class ResponsesSearcher < Searcher
     end
   end
 
-  # Given a list of names, find all instances of this class that match,
+  # Given a list of values, find all instances of this class that match,
   # and append their IDs to the existing list of IDs to filter by.
+  # Returns false if unable to handle this case.
   def filter_by_names(names, klass, current_ids: [], include_name: false)
     matches = klass.where("name ILIKE ANY (array[?])", names).pluck(:id, :name)
     return false if matches.empty?
     current_ids.concat(matches.map { |id, name| include_name ? {id: id, name: name} : id })
+    true
+  end
+
+  # Filter by a valid boolean.
+  # Returns false if unable to handle this case.
+  def filter_by_boolean(token_values, filter_name:)
+    return false unless token_values.length == 1
+    value = token_values[0].downcase
+    return false unless %w[1 0 yes no].include?(value)
+    self[filter_name] = %w[1 yes].include?(value)
+    true
   end
 
   def equality_op?(op_kind)
