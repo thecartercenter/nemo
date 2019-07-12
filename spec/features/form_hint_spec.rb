b667887a2dbae164efde33bb27c0d0d946f57885
Tom Smyth <tom@sassafras.coop>
2019-07-12 09:49:35 -0400

    9863: Add failing spec

--- spec/features/form_hint_spec.rb
+++ spec/features/form_hint_spec.rb
@@ -1,20 +1,30 @@
-# Tests the hint popovers in regular forms and modal forms.
-require 'rails_helper'
+# frozen_string_literal: true
 
-feature 'form hints', js: true do
-  let(:form) { create(:form, question_types: %w(integer image image)) }
+# Tests the hint popovers in regular forms and modal forms.
+require "rails_helper"
+
+feature "form hints", js: true do
+  let(:form) { create(:form, question_types: %w[integer image image]) }
   let(:mission_name) { get_mission.compact_name }
+  let(:user) { create(:user, role_name: "coordinator") }
 
   before do
-    @user = create(:user, role_name: "coordinator")
-    login(@user)
+    login(user)
   end
 
-  scenario "regular form" do
+  scenario "question form" do
     visit(new_question_path(mode: "m", mission_name: mission_name, locale: "en"))
     expect_hint_to_show_and_hide(
       selector: ".question_code a.hint",
       text: "A short codename (between 2-20 characters)"
+    )
+  end
+
+  scenario "form form" do
+    visit(new_form_path(mode: "m", mission_name: mission_name, locale: "en"))
+    expect_hint_to_show_and_hide(
+      selector: ".form_name a.hint",
+      text: "A descriptive name"
     )
   end
 
@@ -28,7 +38,7 @@ feature 'form hints', js: true do
     # Best theory is that as the modal is animating down, the hints won't show.
     # So you have to wait for the modal to be fully animated in (even after it's visible).
     # I couldn't find a way to do this using `expect`.
-    sleep 1
+    sleep(1)
 
     expect_hint_to_show_and_hide(
       selector: ".option_set_name a.hint",
