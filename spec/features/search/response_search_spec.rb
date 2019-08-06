e0109abcfa8bafd91f7cc53f4cc5f5afd581e50b
Kevin Cooper <cooperka92@gmail.com>
2019-08-05 20:51:00 -0700

    9775: Add basic response_search filter specs

--- spec/features/search/response_search_spec.rb
+++ spec/features/search/response_search_spec.rb
@@ -7,11 +7,12 @@ feature "response search", js: true do
 
   let!(:mission) { get_mission }
   let!(:user) { create(:user, role_name: "coordinator", admin: true) }
+  let(:user2) { create(:user) }
   let!(:form) { create(:form, name: "Form 1", question_types: %w[text]) }
   let!(:response1) { create(:response, user: user, form: form, answer_values: ["foo"]) }
   let!(:response2) { create(:response, user: user, form: form, answer_values: ["bar"]) }
 
-  scenario "search" do
+  scenario "search results" do
     r1_code = response1.shortcode.upcase
     r2_code = response2.shortcode.upcase
 
@@ -40,5 +41,26 @@ feature "response search", js: true do
     expect(page).to have_content(
       "Error: Your search query could not be understood due to unexpected text near the end."
     )
+  end
+
+  scenario "search filters" do
+    login(user)
+    visit "/en/m/#{mission.compact_name}/responses"
+
+    search_for(%(foo))
+    expect(page).to have_field("search", with: "foo")
+
+    new_search_for(%(form:"Form 1"))
+    expect(page).to have_field("search", with: "")
+    expect(page).to have_css("#form-filter.active-filter", text: "Form (Form 1)")
+
+    new_search_for(%(reviewed:YES))
+    expect(page).to have_field("search", with: "")
+    expect(page).to have_css("#reviewed-filter.active-filter", text: "Reviewed (Yes)")
+
+    new_search_for(%(submitter:("#{user.name}" | "#{user2.name}") reviewed:0))
+    expect(page).to have_field("search", with: "")
+    expect(page).to have_css("#submitter-filter.active-filter", text: "Submitter (2 filters)")
+    expect(page).to have_css("#reviewed-filter.active-filter", text: "Reviewed (No)")
   end
 end
