0d7bc913056ca4c04fde6e2fe8283c14c955f0ef
Tom Smyth <tom@sassafras.coop>
2019-09-11 13:33:42 -0400

    9949: Remove unused helper methods

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -18,44 +18,6 @@ module ReportsHelper
     end
   end
 
-  # converts the given report to CSV format
-  def report_to_csv(report)
-    # We use \r\n because Excel seems to prefer it.
-    CSV.generate(row_sep: configatron.csv_row_separator) do |csv|
-      # determine if we need blank cell for row headers
-      blank = report.header_set[:row] ? [""] : []
-
-      # add header row
-      csv << blank + report.header_set[:col].collect { |c| c.name || "NULL" } if report.header_set[:col]
-
-      # add data rows
-      report.data.rows.each_with_index do |row, idx|
-        # get row header if exists
-        row_header = report.header_set[:row] ? [report.header_set[:row].cells[idx].name || "NULL"] : []
-
-        # Add the data. All report data has the potential to be paragraph style text so we run it through
-        # the formatter.
-        csv << row_header + row.map { |c| format_csv_para_text(c) }
-      end
-    end
-  end
-
-  # Formats paragraph style textual data in CSV to play nice with Excel.
-  def format_csv_para_text(text)
-    return text unless text.is_a?(String) && !text.blank?
-
-    # We convert to Markdown since there is a gem to do it and it's much more
-    # readable. Conversion also strips unknown tags.
-    text = ReverseMarkdown.convert(text, unknown_tags: :drop)
-
-    # Excel seems to like \r\n, so replace all plain \ns with \r\n in all string-type cells.
-    # Also ReverseMarkdown adds extra whitespace -- trim it.
-    text = text.split(/\r?\n/).map(&:strip).join("\r\n")
-
-    # Also remove html entities.
-    text.gsub(/&(?:[a-z\d]+|#\d+|#x[a-f\d]+);/i, "")
-  end
-
   # javascript includes for the report view
   def report_js_includes
     javascript_include_tag("https://www.google.com/jsapi") +
