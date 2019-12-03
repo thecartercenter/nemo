6eda497557e83031050aa277e73aac4c90d2e8e6
Tom Smyth <tom@sassafras.coop>
2019-12-03 16:52:39 -0500

    10245: Lint spec files

--- spec/requests/sms_auth_code_field_spec.rb
+++ spec/requests/sms_auth_code_field_spec.rb
@@ -1,32 +1,33 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 # TODO: Most of these tests should live in authorization specs instead.
 #   Please move at the next opportunity.
 describe "sms auth code field", :sms, database_cleaner: :all do
-
   before do
     @user = FactoryGirl.create(:user)
     @staffer = FactoryGirl.create(:user, role_name: :staffer)
     @enumerator = FactoryGirl.create(:user, role_name: :enumerator)
   end
 
-  context 'in show mode for staffer' do
+  context "in show mode for staffer" do
     before do
       login(@staffer)
       get("/en/m/#{get_mission.compact_name}/users/#{@user.id}")
       assert_response(:success)
     end
 
-    it 'should be visible' do
-      assert_select('div.user_sms_auth_code', true)
+    it "should be visible" do
+      assert_select("div.user_sms_auth_code", true)
     end
 
-    it 'should not have regenerate button' do
-      assert_select('div.user_sms_auth_code a', :text => /Regenerate/, :count => 0)
+    it "should not have regenerate button" do
+      assert_select("div.user_sms_auth_code a", text: /Regenerate/, count: 0)
     end
   end
 
-  context 'in edit mode for same user' do
+  context "in edit mode for same user" do
     before do
       login(@user)
       get("/en/m/#{get_mission.compact_name}/users/#{@user.id}/edit")
@@ -34,88 +35,88 @@ describe "sms auth code field", :sms, database_cleaner: :all do
 
     old_key = nil
 
-    it 'should be visible' do
+    it "should be visible" do
       # Store for later
-      assert_select('div.user_sms_auth_code', true) do |e|
+      assert_select("div.user_sms_auth_code", true) do |e|
         old_key = e.to_s
       end
     end
 
-    it 'should have regenerate button' do
-      assert_select('div.user_sms_auth_code a', :text => /Regenerate/, :count => 1)
+    it "should have regenerate button" do
+      assert_select("div.user_sms_auth_code a", text: /Regenerate/, count: 1)
     end
 
-    context 'on regenerate' do
-      it 'should return the new sms_auth_code value as json' do
+    context "on regenerate" do
+      it "should return the new sms_auth_code value as json" do
         patch(regenerate_sms_auth_code_user_path(@user))
         expect(response).to be_successful
 
         @user.reload
-        expect(response.body).to eq({ value: @user.sms_auth_code }.to_json)
+        expect(response.body).to eq({value: @user.sms_auth_code}.to_json)
       end
 
-      it 'should have a new key' do
+      it "should have a new key" do
         get "/en/m/#{get_mission.compact_name}/users/#{@user.id}"
-        assert_select('div.user_sms_auth_code') do |e|
-          expect(old_key).not_to eq e.to_s
+        assert_select("div.user_sms_auth_code") do |e|
+          expect(old_key).not_to eq(e.to_s)
         end
       end
     end
   end
 
-  context 'in show mode for enumerator' do
+  context "in show mode for enumerator" do
     before do
       login(@enumerator)
       get("/en/m/#{get_mission.compact_name}/users/#{@user.id}")
     end
 
-    it 'should not be visible' do
-      assert_select('div.user_sms_auth_code', false)
+    it "should not be visible" do
+      assert_select("div.user_sms_auth_code", false)
     end
   end
 
-  context 'in edit mode for enumerator' do
+  context "in edit mode for enumerator" do
     before do
       login(@enumerator)
     end
 
-    context 'with other user' do
+    context "with other user" do
       before { get("/en/m/#{get_mission.compact_name}/users/#{@user.id}/edit") }
 
-      it 'should not be visible' do
-        assert_select('div.user_sms_auth_code', false)
+      it "should not be visible" do
+        assert_select("div.user_sms_auth_code", false)
       end
     end
 
-    context 'with own user' do
+    context "with own user" do
       before { get("/en/m/#{get_mission.compact_name}/users/#{@enumerator.id}/edit") }
 
       old_key = nil
 
-      it 'should be visible' do
+      it "should be visible" do
         # Store for later
-        assert_select('div.user_sms_auth_code', true) do |e|
+        assert_select("div.user_sms_auth_code", true) do |e|
           old_key = e.to_s
         end
       end
 
-      it 'should have regenerate button' do
-        assert_select('div.user_sms_auth_code a', :text => /Regenerate/, :count => 1)
+      it "should have regenerate button" do
+        assert_select("div.user_sms_auth_code a", text: /Regenerate/, count: 1)
       end
 
-      context 'on regenerate' do
-        it 'should return the new sms_auth_code value as json' do
+      context "on regenerate" do
+        it "should return the new sms_auth_code value as json" do
           patch(regenerate_sms_auth_code_user_path(@enumerator))
           expect(response).to be_successful
 
           @enumerator.reload
-          expect(response.body).to eq({ value: @enumerator.sms_auth_code }.to_json)
+          expect(response.body).to eq({value: @enumerator.sms_auth_code}.to_json)
         end
 
-        it 'should have a new key' do
+        it "should have a new key" do
           get "/en/m/#{get_mission.compact_name}/users/#{@enumerator.id}"
-          assert_select('div.user_sms_auth_code') do |e|
-            expect(old_key).not_to eq e.to_s
+          assert_select("div.user_sms_auth_code") do |e|
+            expect(old_key).not_to eq(e.to_s)
           end
         end
       end
