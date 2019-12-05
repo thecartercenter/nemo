45b47bd61fa32bc82c4b301d32b8b4518ea379d4
Kevin Cooper <cooperka92@gmail.com>
2019-12-04 16:27:50 -0800

    10285: Auto-cop app/models/concerns/

--- app/models/concerns/tree_traverseable.rb
+++ app/models/concerns/tree_traverseable.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 module TreeTraverseable
   extend ActiveSupport::Concern
 
   def lowest_common_ancestor(other)
-    ancestor_id = (self.self_and_ancestor_ids & other.self_and_ancestor_ids).last
-    ancestor_id == id ? self : self.ancestors.find(ancestor_id)
+    ancestor_id = (self_and_ancestor_ids & other.self_and_ancestor_ids).last
+    ancestor_id == id ? self : ancestors.find(ancestor_id)
   end
 
   # Gets path from ancestor to self. ancestor may be self.
   def path_from_ancestor(ancestor, include_ancestor: false, include_self: false)
     return [self] if ancestor == self
 
-    ancestor_list = self.ancestors.to_a
+    ancestor_list = ancestors.to_a
     # find ancestor
     i = ancestor_list.find_index { |a| a.id == ancestor.id }
 
