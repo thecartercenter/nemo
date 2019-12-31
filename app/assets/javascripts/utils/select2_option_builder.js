cce31297c96e47b018d0b9f9897574b6af878687
Kevin Cooper <cooperka92@gmail.com>
2019-12-31 15:25:13 -0800

    10356: Clean up select2_option_builder.js

--- app/assets/javascripts/utils/select2_option_builder.js
+++ app/assets/javascripts/utils/select2_option_builder.js
@@ -1,16 +1,8 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // Builds options for select2 controls so that common options can be reused without duplication.
 // It's designed like this, instead of as a wrapper of select2, so it can be used in React as well
 // as Backbone.
 ELMO.Utils.Select2OptionBuilder = class Select2OptionBuilder {
-  ajax(url, resultsKey, textKey) {
-    if (resultsKey == null) { resultsKey = 'results'; }
-    if (textKey == null) { textKey = 'text'; }
+  ajax(url, resultsKey = 'results', textKey = 'text') {
     return {
       url,
       dataType: 'json',
