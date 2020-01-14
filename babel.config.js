32f05bcb3df3d870c86d68be7eabbd14f92fcf73
Kevin Cooper <cooperka92@gmail.com>
2020-01-13 17:55:41 -0800

    10375: Upgrade configs based on new default examples

--- babel.config.js
+++ babel.config.js
@@ -1,102 +1,117 @@
-{
-  "env": {
-    "production": {
-      "presets": [
-        [
-          "@babel/env",
-          {
-            "modules": false,
-            "targets": {
-              "browsers": "> 1%",
-              "uglify": true
-            },
-            "useBuiltIns": "entry"
-          }
-        ],
-        "@babel/react"
-      ],
-      "plugins": [
-        "@babel/syntax-dynamic-import",
-        "@babel/transform-runtime",
-        "@babel/plugin-proposal-object-rest-spread",
-        [
-          "@babel/plugin-proposal-decorators",
-          {
-            "legacy": true
-          }
-        ],
-        [
-          "@babel/plugin-proposal-class-properties",
-          {
-            "loose": true
-          }
-        ]
-      ]
-    },
-    "development": {
-      "presets": [
-        [
-          "@babel/env",
-          {
-            "modules": false,
-            "targets": {
-              "browsers": "> 1%",
-              "uglify": true
-            },
-            "useBuiltIns": "entry"
-          }
-        ],
-        "@babel/react"
-      ],
-      "plugins": [
-        "@babel/syntax-dynamic-import",
-        "@babel/transform-runtime",
-        "@babel/plugin-proposal-object-rest-spread",
-        [
-          "@babel/plugin-proposal-decorators",
-          {
-            "legacy": true
-          }
-        ],
-        [
-          "@babel/plugin-proposal-class-properties",
-          {
-            "loose": true
-          }
-        ]
-      ]
-    },
-    "test": {
-      "presets": [
-        [
-          "@babel/env",
-          {
-            "targets": {
-              "browsers": "> 1%",
-              "uglify": true
-            },
-            "useBuiltIns": "entry"
-          }
-        ],
-        "@babel/react"
-      ],
-      "plugins": [
-        "@babel/syntax-dynamic-import",
-        "@babel/transform-runtime",
-        "@babel/plugin-proposal-object-rest-spread",
-        [
-          "@babel/plugin-proposal-decorators",
-          {
-            "legacy": true
-          }
-        ],
-        [
-          "@babel/plugin-proposal-class-properties",
-          {
-            "loose": true
-          }
-        ]
-      ]
-    }
+module.exports = function (api) {
+  const validEnv = ['development', 'test', 'production'];
+  const currentEnv = api.env();
+
+  if (!validEnv.includes(currentEnv)) {
+    throw new Error(
+      `${'Please specify a valid `NODE_ENV` or '
+      + '`BABEL_ENV` environment variables. Valid values are "development", '
+      + '"test", and "production". Instead, received: '}${
+        JSON.stringify(currentEnv)
+      }.`,
+    );
   }
-}
+
+  return {
+    env: {
+      production: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              modules: false,
+              targets: {
+                browsers: '> 1%',
+                uglify: true,
+              },
+              useBuiltIns: 'entry',
+            },
+          ],
+          '@babel/react',
+        ],
+        plugins: [
+          '@babel/syntax-dynamic-import',
+          '@babel/transform-runtime',
+          '@babel/plugin-proposal-object-rest-spread',
+          [
+            '@babel/plugin-proposal-decorators',
+            {
+              legacy: true,
+            },
+          ],
+          [
+            '@babel/plugin-proposal-class-properties',
+            {
+              loose: true,
+            },
+          ],
+        ],
+      },
+      development: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              modules: false,
+              targets: {
+                browsers: '> 1%',
+                uglify: true,
+              },
+              useBuiltIns: 'entry',
+            },
+          ],
+          '@babel/react',
+        ],
+        plugins: [
+          '@babel/syntax-dynamic-import',
+          '@babel/transform-runtime',
+          '@babel/plugin-proposal-object-rest-spread',
+          [
+            '@babel/plugin-proposal-decorators',
+            {
+              legacy: true,
+            },
+          ],
+          [
+            '@babel/plugin-proposal-class-properties',
+            {
+              loose: true,
+            },
+          ],
+        ],
+      },
+      test: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              targets: {
+                browsers: '> 1%',
+                uglify: true,
+              },
+              useBuiltIns: 'entry',
+            },
+          ],
+          '@babel/react',
+        ],
+        plugins: [
+          '@babel/syntax-dynamic-import',
+          '@babel/transform-runtime',
+          '@babel/plugin-proposal-object-rest-spread',
+          [
+            '@babel/plugin-proposal-decorators',
+            {
+              legacy: true,
+            },
+          ],
+          [
+            '@babel/plugin-proposal-class-properties',
+            {
+              loose: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
-plugins:
-  postcss-import: {}
-  postcss-cssnext: {}
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+    }),
+  ],
+};
