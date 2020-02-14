5783c0204dfdd7d8726ab85e6a3caa1276cbf00b
Kevin Cooper <cooperka92@gmail.com>
2020-02-14 14:37:38 -0800

    10451: Add Scout context to every request

---
+++ app/controllers/concerns/application_controller/monitoring.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# ApplicationController methods related to Scout monitoring/analytics.
+module Concerns::ApplicationController::Monitoring
+  extend ActiveSupport::Concern
+
+  def set_scout_context
+    ScoutApm::Context.add_user(username: current_user&.login)
+    ScoutApm::Context.add(locale: I18n.locale,
+                          mode: params[:mode],
+                          mission_name: current_mission&.compact_name)
+  end
+end
