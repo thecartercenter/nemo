530d2f2e3e31db9140cf4d43fe4d75889e4fda95
Kevin Cooper <cooperka92@gmail.com>
2020-03-16 15:21:32 -0700

    10446: Add specs for editing group name

--- spec/features/forms/form_item/qing_group_form_spec.rb
+++ spec/features/forms/form_item/qing_group_form_spec.rb
@@ -14,56 +14,80 @@ feature "adding and editing qing group on form", js: true do
     login(user)
   end
 
-  scenario "add a new group to a form" do
-    visit(edit_form_path(form, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+  context "adding a new group to a form" do
+    before do
+      visit(edit_form_path(form, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+      click_link("Add Group")
+    end
 
-    click_link("Add Group")
-    fill_in("Name (English)", with: "Foo Group")
-    fill_in("Hint (English)", with: "Bar Hint")
-    fill_in("Name (Français)", with: "Fou Groupe")
-    fill_in("Hint (Français)", with: "Barre Hinte")
+    scenario "happy path" do
+      fill_in("Name (English)", with: "Foo Group")
+      fill_in("Hint (English)", with: "Bar Hint")
+      fill_in("Name (Français)", with: "Fou Groupe")
+      fill_in("Hint (Français)", with: "Barre Hinte")
 
-    # Test display conditions when creating new group
-    select("Display this group if all of these conditions are met", from: "qing_group_display_logic")
-    find(".add-condition")
-    find('select[name$="[left_qing_id]"]').select(form.c[0].code)
-    find('select[name$="[op]"]').select("= equals")
-    find('input[name$="[value]"]').set("3")
+      # Test display conditions when creating new group
+      select("Display this group if all of these conditions are met", from: "qing_group_display_logic")
+      find(".add-condition")
+      find('select[name$="[left_qing_id]"]').select(form.c[0].code)
+      find('select[name$="[op]"]').select("= equals")
+      find('input[name$="[value]"]').set("3")
 
-    expect(page).not_to have_content("Item Name (English)")
-    check("qing_group_repeatable")
-    fill_in("Item Name (English)", with: "Test Name")
-    fill_in("Item Name (Français)", with: "Nom d'essaie")
-    uncheck("qing_group_one_screen")
-    within(".modal") { click_button("Save") }
+      expect(page).not_to have_content("Item Name (English)")
+      check("qing_group_repeatable")
+      fill_in("Item Name (English)", with: "Test Name")
+      fill_in("Item Name (Français)", with: "Nom d'essaie")
+      uncheck("qing_group_one_screen")
+      within(".modal") { click_button("Save") }
 
-    within(".form-items") { expect(page).to have_content("Foo Group") }
-    all(".form-items .action-link.action-link-edit")[-1].click
-    expect(page).to have_field("qing_group_group_name_en", with: "Foo Group")
-    expect(page).to have_field("qing_group_group_name_fr", with: "Fou Groupe")
-    expect(page).to have_field("qing_group_group_hint_en", with: "Bar Hint")
-    expect(page).to have_field("qing_group_group_hint_fr", with: "Barre Hinte")
-    expect(page).to have_field("qing_group_repeatable", checked: true)
-    expect(page).to have_field("qing_group_one_screen", checked: false)
-    expect(page).to have_field("qing_group_group_item_name_en", with: "Test Name")
-    expect(page).to have_field("qing_group_group_item_name_fr", with: "Nom d'essaie")
+      within(".form-items") { expect(page).to have_content("Foo Group") }
+      all(".form-items .action-link.action-link-edit")[-1].click
+      expect(page).to have_field("qing_group_group_name_en", with: "Foo Group")
+      expect(page).to have_field("qing_group_group_name_fr", with: "Fou Groupe")
+      expect(page).to have_field("qing_group_group_hint_en", with: "Bar Hint")
+      expect(page).to have_field("qing_group_group_hint_fr", with: "Barre Hinte")
+      expect(page).to have_field("qing_group_repeatable", checked: true)
+      expect(page).to have_field("qing_group_one_screen", checked: false)
+      expect(page).to have_field("qing_group_group_item_name_en", with: "Test Name")
+      expect(page).to have_field("qing_group_group_item_name_fr", with: "Nom d'essaie")
 
-    # Test display condition saved. Detailed test is in display_conditions_form_spec
-    expect(page).to have_select("qing_group_display_logic",
-      selected: "Display this group if all of these conditions are met")
+      # Test display condition saved. Detailed test is in display_conditions_form_spec
+      expect(page).to have_select("qing_group_display_logic",
+        selected: "Display this group if all of these conditions are met")
 
-    fill_in("Name (English)", with: "New Group Name")
-    within(".modal") { click_button("Save") }
-    within(".form-items") { expect(page).to have_content("New Group Name") }
-  end
+      fill_in("Name (English)", with: "New Group Name")
+      within(".modal") { click_button("Save") }
+      within(".form-items") { expect(page).to have_content("New Group Name") }
+    end
 
-  scenario "add a new group to a form (blank name)" do
-    visit(edit_form_path(form, locale: "en", mode: "m", mission_name: get_mission.compact_name))
+    scenario "with blank name" do
+      fill_in("Name (English)", with: "")
+      within(".modal") { click_button("Save") }
 
-    click_link("Add Group")
-    fill_in("Name (English)", with: "")
-    within(".modal") { click_button("Save") }
+      expect(page).to have_content("This field is required")
+    end
 
-    expect(page).to have_content("This field is required")
+    context "then editing" do
+      before do
+        fill_in("Name (English)", with: "Foo Group")
+        within(".modal") { click_button("Save") }
+
+        find("li.form-item-group").click
+      end
+
+      scenario "happy path" do
+        fill_in("Name (English)", with: "something else")
+        within(".modal") { click_button("Save") }
+
+        within(".form-items") { expect(page).to have_content("something else") }
+      end
+
+      scenario "with blank name" do
+        fill_in("Name (English)", with: "")
+        within(".modal") { click_button("Save") }
+
+        expect(page).to have_content("This field is required")
+      end
+    end
   end
 end
