7644af20e0112b4b297eae6f4de0bd8c038945d0
Kevin Cooper <cooperka92@gmail.com>
2020-03-27 16:39:41 -0700

    9429: Add passthrough odata transformers

--- config/initializers/odata_server.rb
+++ config/initializers/odata_server.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
-schema = OData::ActiveRecordSchema::Base.new("NEMO", classes: [Response], group_by_form: true)
+transform_schema_for_metadata = lambda do |schema|
+  schema
+end
+
+transform_json_for_resource = lambda do |json|
+  json
+end
+
+schema = OData::ActiveRecordSchema::Base.new("NEMO", classes: [Response],
+                                                     group_by_form: true,
+                                                     transform_schema_for_metadata: transform_schema_for_metadata,
+                                                     transform_results_for_resource: nil,
+                                                     transform_json_for_resource: transform_json_for_resource)
 OData::Edm::DataServices.schemas << schema
