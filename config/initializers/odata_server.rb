efee76c22defba75653b10b5a60f26dfdc89e453
Kevin Cooper <cooperka92@gmail.com>
2020-03-27 17:41:59 -0700

    Override schema with simple stubs for metadata

--- config/initializers/odata_server.rb
+++ config/initializers/odata_server.rb
@@ -1,7 +1,53 @@
 # frozen_string_literal: true
 
+class SimpleSchema
+  attr_reader :namespace, :entity_types
+
+  def initialize
+    @namespace = "NEMO"
+    @entity_types = SimpleEntities.new
+  end
+end
+
+class SimpleEntities
+  attr_reader :values
+
+  def initialize
+    @values = Response.distinct.pluck(:form_id).map do |id|
+      name = Form.find(id).name
+      SimpleEntity.new("Response#{name}")
+    end
+  end
+end
+
+class SimpleEntity
+  attr_reader :name, :plural_name, :qualified_name, :key_property, :properties, :navigation_properties
+
+  def initialize(name)
+    @name = name
+    @plural_name = name
+    @qualified_name = name
+    @key_property = SimpleProperty.new("Id")
+    @properties = {Shortcode: SimpleProperty.new("Shortcode")}
+    @navigation_properties = []
+  end
+end
+
+class SimpleProperty
+  attr_reader :name, :return_type
+
+  def initialize(name)
+    @name = name
+    @return_type = :text
+  end
+
+  def nullable?
+    true
+  end
+end
+
 transform_schema_for_metadata = lambda do |schema|
-  schema
+  SimpleSchema.new
 end
 
 transform_json_for_resource = lambda do |json|
