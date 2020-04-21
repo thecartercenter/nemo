bcbbbb2dde1116306be730b34d7d07c14e1dc34d
Kevin Cooper <cooperka92@gmail.com>
2020-04-20 18:55:28 -0700

    10592: Clean up add_entity_types logic

--- config/initializers/odata/server.rb
+++ config/initializers/odata/server.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module OData
+  # Initializer for the odata_server engine.
   class Server
     @transform_json_for_root = lambda do |json|
       json
@@ -22,27 +23,35 @@ module OData
                      transform_schema_for_metadata: @transform_schema_for_metadata,
                      transform_json_for_resource_feed: @transform_json_for_resource_feed)
 
-      # Manually add our entity types with some extra options.
-      # TODO: Clean this up and make more efficient.
+      add_entity_types(schema)
+
+      ODataController.data_services.clear_schemas
+      ODataController.data_services.append_schemas([schema])
+    end
+
+    # Manually add our entity types, grouping responses by form.
+    def self.add_entity_types(schema)
       # TODO: Scope to current mission
       forms = Response
         .distinct
         .pluck(:form_id)
         .map { |id| {id: id, name: Form.find(id).name} }
 
-      forms.each do |id:, name:|
-        name = "Responses: #{name}"
-        entity = schema.add_entity_type(Response, where: {form_id: id},
-                                                  name: name,
-                                                  reflect_on_associations: false)
-        # We don't want to double-pluralize since it already says "Responses".
-        def entity.plural_name
-          name
-        end
-      end
+      forms.each { |id:, name:| add_form_entity_type(schema, id, name) }
+    end
 
-      ODataController.data_services.clear_schemas
-      ODataController.data_services.append_schemas([schema])
+    # Add an entity type to the schema for a given form.
+    def self.add_form_entity_type(schema, id, name)
+      name = "Responses: #{name}"
+      entity = schema.add_entity_type(Response, where: {form_id: id},
+                                                name: name,
+                                                reflect_on_associations: false)
+
+      # We don't want to double-pluralize since it already says "Responses",
+      # so override this method.
+      def entity.plural_name
+        name
+      end
     end
   end
 end
