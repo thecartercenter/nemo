6b18bc1a8d20913fe7abfae6cc2b117e994f20ea
Kevin Cooper <cooperka92@gmail.com>
2020-04-29 16:17:14 -0700

    10594: Share distinct_forms logic

--- app/models/o_data/simple_entities.rb
+++ app/models/o_data/simple_entities.rb
@@ -4,11 +4,9 @@ module OData
   class SimpleEntities
     attr_reader :values
 
-    def initialize
-      # TODO: All published forms in the missions, regardless of if they have response
-      @values = Response.distinct.pluck(:form_id).map do |id|
-        name = Form.find(id).name
-        SimpleEntity.new("Responses: #{name}")
+    def initialize(distinct_forms)
+      @values = distinct_forms.map do |form|
+        SimpleEntity.new("Responses: #{form.name}")
       end
     end
   end
