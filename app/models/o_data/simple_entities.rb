6caaabf26800dabab53e2bd9266fb9a1da592542
Kevin Cooper <cooperka92@gmail.com>
2020-04-29 21:22:48 -0700

    10594: Map question types to OData types

--- app/models/o_data/simple_entities.rb
+++ app/models/o_data/simple_entities.rb
@@ -4,11 +4,6 @@ module OData
   class SimpleEntities
     attr_reader :values
 
-    # TODO:
-    #       <EntityType Name="Response.Form_A" BaseType="Demo.Response">
-    #         <Property Name="FullName" Type="Edm.String"/>
-    #         <Property Name="Age" Type="Edm.Int64"/>
-    #       </EntityType>
     def initialize(distinct_forms)
       base_entity = SimpleEntity.new("Response", key_name: "ResponseID",
                                                  property_types: {
@@ -21,10 +16,13 @@ module OData
                                                  })
       # TODO: Fix `warning: toplevel constant NAMESPACE referenced by ODataController::NAMESPACE`
       base_type = "#{ODataController::NAMESPACE}.Response"
+
       response_entities = distinct_forms.map do |form|
+        property_types = form.questions.map do |q|
+          [q.name, q.qtype.odata_type.to_sym]
+        end.to_h
         SimpleEntity.new("Responses: #{form.name}", extra_tags: {BaseType: base_type},
-                                                    property_types: {
-                                                    })
+                                                    property_types: property_types)
       end
 
       @values = [base_entity] + response_entities
