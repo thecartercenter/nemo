aac026e540d0ed679788d91ec843bec897a910d9
Kevin Cooper <cooperka92@gmail.com>
2020-04-29 21:44:14 -0700

    10594: Navigate all form children, not just qings

--- app/models/o_data/simple_entities.rb
+++ app/models/o_data/simple_entities.rb
@@ -4,6 +4,16 @@ module OData
   class SimpleEntities
     attr_reader :values
 
+    # TODO:
+    #       <EntityType Name="Response.Form_C" BaseType="Demo.Response">
+    #         <Property Name="City" Type="Edm.String"/>
+    #         <Property Name="HouseholdMembers" Type="Collection(Demo.Repeat.Form_C.R1)"/>
+    #       </EntityType>
+    #       <EntityType Name="Repeat.Form_B.R1">
+    #         <Property Name="FullName" Type="Edm.String"/>
+    #         <Property Name="Age" Type="Edm.Int64"/>
+    #         <Property Name="Eyes" Type="Collection(Demo.Repeat.Form_B.R2)"/>
+    #       </EntityType>
     def initialize(distinct_forms)
       base_entity = SimpleEntity.new("Response", key_name: "ResponseID",
                                                  property_types: {
@@ -18,8 +28,11 @@ module OData
       base_type = "#{ODataController::NAMESPACE}.Response"
 
       response_entities = distinct_forms.map do |form|
-        property_types = form.questions.map do |q|
-          [q.name, q.qtype.odata_type.to_sym]
+        repeat_number = 0
+        property_types = form.c.map do |c|
+          c.is_a?(Questioning) ?
+            [c.name, c.qtype.odata_type.to_sym] :
+            [c.group_name, "Collection(#{ODataController::NAMESPACE}.Repeat.#{form.name}.R#{repeat_number += 1})"]
         end.to_h
         SimpleEntity.new("Responses: #{form.name}", extra_tags: {BaseType: base_type},
                                                     property_types: property_types)
