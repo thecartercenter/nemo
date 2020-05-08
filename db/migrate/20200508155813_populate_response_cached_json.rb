3355856912430e2380fcbf740dea0c5986a30ce9
Kevin Cooper <cooperka92@gmail.com>
2020-05-08 09:06:56 -0700

    10595: Some migration performance improvements

---
+++ db/migrate/20200508155813_populate_response_cached_json.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class PopulateResponseCachedJson < ActiveRecord::Migration[5.2]
+  # Disable transaction wrapper so that the update queries will be committed even if we
+  # have to fail the transaction part-way through.
+  disable_ddl_transaction!
+
+  def up
+    # TODO: Temporary filter; sandbox haitimalaria riverblindnessnigeria2017
+    mission_ids = %w[sandbox haitimalaria riverblindnessnigeria2017].map do |compact_name|
+      Mission.find_by(compact_name: compact_name).id
+    end
+    responses = Response.where(mission_id: mission_ids)
+    remaining_responses = ENV["FORCE_REDO"] ? responses : responses.where(cached_json: nil)
+
+    cache_responses(remaining_responses)
+  end
+
+  def cache_responses(responses)
+    # Disable logging for this db-heavy migration.
+    old_level = ActiveRecord::Base.logger.level
+    ActiveRecord::Base.logger.level = 1
+
+    total = responses.count
+    curr = 0
+    responses.find_each do |response|
+      json = Results::ResponseJsonGenerator.new(response).as_json
+      puts "Updating #{response.shortcode}... (#{curr += 1} / #{total})"
+      response.update!(cached_json: json)
+    end
+
+    ActiveRecord::Base.logger.level = old_level
+  end
+end
