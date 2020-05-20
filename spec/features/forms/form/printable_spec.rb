595808bdcd8eb8fcd2a0a9b7e16ce233628d64bf
Kevin Cooper <cooperka92@gmail.com>
2020-05-20 09:33:16 -0700

    Pump up the wait time just in case it helps Travis

--- spec/features/forms/form/printable_spec.rb
+++ spec/features/forms/form/printable_spec.rb
@@ -10,7 +10,7 @@ feature "forms", js: true do
   let(:forms_path) { "/en/m/#{form.mission.compact_name}/forms" }
 
   # Allow longer delays because specs were failing on Travis.
-  let(:longer_wait_time) { 20 }
+  let(:longer_wait_time) { 120 }
 
   before do
     login(user)
