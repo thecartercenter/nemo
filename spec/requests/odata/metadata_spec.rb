982c139acdbe6e3864226ea8600aaab6e84312f5
Kevin Cooper <cooperka92@gmail.com>
2020-05-27 21:21:45 -0700

    10595: Remove group name indexes from metadata
    This may cause Power BI to error when two groups
    have the same name, but that seems preferable to the
    complexity these indexes have when trying to match
    metadata with resource output. It's temporary until we
    have unique group codes anyway.

--- spec/requests/odata/metadata_spec.rb
+++ spec/requests/odata/metadata_spec.rb
@@ -16,11 +16,8 @@ describe "OData $metadata" do
     it { expect_fixture("basic_metadata.xml", forms: [form, form_with_no_responses]) }
   end
 
-  context "with nested groups" do
+  context "with nested groups", :reset_factory_sequences do
     include_context "odata with nested groups"
-    it do
-      substitutions = {group_name: ["Group 3 (1)", "Group 1 (1)", "Group 2 (2)"]}
-      expect_fixture("nested_groups_metadata.xml", forms: [form], substitutions: substitutions)
-    end
+    it { expect_fixture("nested_groups_metadata.xml", forms: [form]) }
   end
 end
