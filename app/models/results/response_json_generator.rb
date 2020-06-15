d3bb4f090893139e3881978204951aad79a809f0
Kevin Cooper <cooperka92@gmail.com>
2020-06-15 09:48:26 -0700

    10827: Rename object => json

--- app/models/results/response_json_generator.rb
+++ app/models/results/response_json_generator.rb
@@ -12,75 +12,75 @@ module Results
     end
 
     def as_json
-      object = {}
-      object["ResponseID"] = response.id
-      object["ResponseShortcode"] = response.shortcode
-      object["FormName"] = form.name
-      object["ResponseSubmitterName"] = user.name
-      object["ResponseSubmitDate"] = response.created_at.iso8601
-      object["ResponseReviewed"] = response.reviewed?
+      json = {}
+      json["ResponseID"] = response.id
+      json["ResponseShortcode"] = response.shortcode
+      json["FormName"] = form.name
+      json["ResponseSubmitterName"] = user.name
+      json["ResponseSubmitDate"] = response.created_at.iso8601
+      json["ResponseReviewed"] = response.reviewed?
       root = response.root_node_including_tree(:choices, form_item: :question, option_node: :option_set)
-      add_answers(root, object) unless root.nil?
-      add_nil_answers(response.form, object)
-      object
+      add_answers(root, json) unless root.nil?
+      add_nil_answers(response.form, json)
+      json
     end
 
     private
 
     delegate :form, :user, to: :response
 
-    # Adds data for the given Response node to the given object.
+    # Adds data for the given Response node to the given json object.
     # Object may be an array or hash.
-    def add_answers(response_node, object)
+    def add_answers(response_node, json)
       response_node.children.each do |child_node|
         if child_node.is_a?(Answer)
-          object[child_node.question_code] = value_for(child_node)
+          json[child_node.question_code] = value_for(child_node)
         elsif child_node.is_a?(AnswerSet)
-          object[child_node.question_code] = answer_set_value(child_node)
+          json[child_node.question_code] = answer_set_value(child_node)
         elsif child_node.is_a?(AnswerGroup)
-          add_group_answers(child_node, object)
+          add_group_answers(child_node, json)
         elsif child_node.is_a?(AnswerGroupSet)
-          set = object[node_key(child_node)] = []
+          set = json[node_key(child_node)] = []
           add_answers(child_node, set)
         end
       end
     end
 
-    def add_group_answers(group, object)
+    def add_group_answers(group, json)
       if group.repeatable?
-        object << (item = {})
+        json << (item = {})
         add_answers(group, item)
       else
-        subgroup = object[node_key(group)] = {}
+        subgroup = json[node_key(group)] = {}
         add_answers(group, subgroup)
       end
     end
 
     # Make sure we include everything specified by metadata in our output,
     # even if an older Response didn't include that qing/group originally.
-    def add_nil_answers(form_item, object)
+    def add_nil_answers(form_item, json)
       # Note: This logic is similar to add_answers, but with Form instead of Response.
       # The logic seems more readable when they're independent like this.
       # It also preserves old data better.
       form_item.children.map do |child_item|
         if child_item.is_a?(QingGroup)
-          add_nil_group_answers(child_item, object)
+          add_nil_group_answers(child_item, json)
         else
-          object[node_key(child_item)] ||= nil
+          json[node_key(child_item)] ||= nil
         end
       end
     end
 
-    def add_nil_group_answers(group, object)
+    def add_nil_group_answers(group, json)
       if group.repeatable?
-        entries = object[node_key(group)] ||= []
+        entries = json[node_key(group)] ||= []
         # Make sure it's an array of hashes, not a hash (in case 'repeatable' changed).
-        entries = object[node_key(group)] = [entries] unless entries.is_a?(Array)
+        entries = json[node_key(group)] = [entries] unless entries.is_a?(Array)
         entries.each { |entry| add_nil_answers(group, entry) }
       else
-        subgroup = object[node_key(group)] ||= {}
+        subgroup = json[node_key(group)] ||= {}
         # Make sure it's a hash, not an array of hashes (in case 'repeatable' changed).
-        subgroup = object[node_key(group)] = subgroup.first if subgroup.is_a?(Array)
+        subgroup = json[node_key(group)] = subgroup.first if subgroup.is_a?(Array)
         add_nil_answers(group, subgroup)
       end
     end
