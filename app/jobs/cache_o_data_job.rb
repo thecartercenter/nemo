465f746a944c202721b26224a0747110e9c4bd8b
Kevin Cooper <cooperka92@gmail.com>
2020-08-04 18:46:04 -0700

    10981: Create Operation if number of responses is above threshold

--- app/jobs/cache_o_data_job.rb
+++ app/jobs/cache_o_data_job.rb
@@ -6,21 +6,27 @@ class CacheODataJob < ApplicationJob
   # In practice, 100 responses take ~10-30 seconds to cache on a small VM.
   BATCH_SIZE = 500
 
+  # A user-facing Operation should be created if there are so many responses to cache
+  # that it's worth tracking progress.
+  OPERATION_THRESHOLD = 1000
+
   # Default to lower-priority queue.
   queue_as :odata
 
   def perform
-    enqueued_jobs = Delayed::Job.where("handler LIKE '%job_class: CacheODataJob%'").where(failed_at: nil)
+    enqueued = Delayed::Job.where("handler LIKE '%job_class: CacheODataJob%'").where(failed_at: nil).count
     # Wait to get called again by the scheduler if something else is already in progress.
-    return if enqueued_jobs.count > 1
+    return if enqueued > 1
 
-    responses = Response.where(dirty_json: true).limit(BATCH_SIZE)
-    responses.each do |response|
-      CacheODataJob.cache_response(response, logger: Delayed::Worker.logger)
-    end
+    create_or_update_operation
+    cache_batch
 
     # Self-enqueue if there are responses left to cache after this batch.
-    self.class.set(wait: 1.second).perform_later if Response.exists?(dirty_json: true)
+    if Response.exists?(dirty_json: true)
+      self.class.set(wait: 1.second).perform_later
+    else
+      complete_operation
+    end
   end
 
   # This can be invoked synchronously or asynchronously, depending on need.
@@ -39,5 +45,39 @@ class CacheODataJob < ApplicationJob
     # Phone home without failing the entire operation.
     ExceptionNotifier.notify_exception(e, data: {shortcode: response.shortcode})
     {error: e.class.name}
+  end
+
+  private
+
+  # Update the existing operation, if found;
+  # otherwise create an operation only if the number of responses exceeds the threshold.
+  def create_or_update_operation
+    ongoing = Operation.find_by(job_class: CacheODataOperationJob.name, job_completed_at: nil)
+    return if ongoing.nil? && Response.where(dirty_json: true).count < OPERATION_THRESHOLD
+    ongoing = enqueue_operation if ongoing.nil?
+  end
+
+  def enqueue_operation
+    operation = Operation.new(
+      creator: nil,
+      mission: nil,
+      job_class: CacheODataOperationJob,
+      details: I18n.t("operation.details.cache_odata"),
+      job_params: {}
+    )
+    operation.enqueue
+    operation
+  end
+
+  def complete_operation
+    Operation.where(job_class: CacheODataOperationJob.name, job_completed_at: nil)
+      .update(job_completed_at: Time.current)
+  end
+
+  def cache_batch
+    responses = Response.where(dirty_json: true).limit(BATCH_SIZE)
+    responses.each do |response|
+      CacheODataJob.cache_response(response, logger: Delayed::Worker.logger)
+    end
   end
 end
