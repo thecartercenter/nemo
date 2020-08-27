9c127732e015ca20c140acf6b1fbf55c03f4b48f
Kevin Cooper <cooperka92@gmail.com>
2020-08-26 18:36:14 -0700

    10981: Log cron errors separately (e.g. deprecation warnings)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 # Use this file to easily define all of your cron jobs.
+# Docs: https://github.com/javan/whenever
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-set(:output, "log/cron.log")
+set(:output, {error: "log/cron-error.log", standard: "log/cron.log"})
+
 env(:PATH, ENV["PATH"])
 env(:GEM_HOME, ENV["GEM_HOME"])
 
