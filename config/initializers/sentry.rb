05ccb37bddf4687b940680a9c75fa9a5a8f3a495
Kevin Cooper <cooperka92@gmail.com>
2020-09-15 19:04:24 -0700

    11106: Don't block requests on synchronous error reporting

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,11 @@ Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.release = "nemo@#{configatron.system_version}"
   config.current_environment = Rails.env.to_s
+
+  if Rails.env.production?
+    # Don't block requests on synchronous error reporting.
+    config.async = lambda { |event|
+      SentryJob.perform_later(event)
+    }
+  end
 end
