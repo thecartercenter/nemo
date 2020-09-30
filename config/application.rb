d36e420edd9e6a34753d745b365c1ffa0d00ef7b
Kevin Cooper <cooperka92@gmail.com>
2020-09-29 17:36:19 -0700

    Disable Sentry reporting for CI specs

--- config/application.rb
+++ config/application.rb
@@ -113,6 +113,7 @@ module ELMO
 
     # Error reporting via Sentry (formerly Raven).
     Raven.configure do |config|
+      next if Rails.env.test?
       config.dsn = "https://a81af08ff85042f3ae314e6c685853a3@o448595.ingest.sentry.io/5430181"
     end
 
