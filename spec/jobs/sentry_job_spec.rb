91ee70b17b24da3199efdfb9c10708e040511c98
Kevin Cooper <cooperka92@gmail.com>
2020-10-07 16:14:12 -0700

    11106: Fix sentry spec now that it's disabled for specs

--- spec/jobs/sentry_job_spec.rb
+++ spec/jobs/sentry_job_spec.rb
@@ -4,6 +4,12 @@ require "rails_helper"
 require "json"
 
 describe SentryJob do
+  before do
+    Raven.configure do |config|
+      config.dsn = "https://fake@fake.ingest.sentry.io/fake"
+    end
+  end
+
   it "gets created automatically" do
     expect(Delayed::Job.count).to eq(0)
     Raven.capture_message("Test")
