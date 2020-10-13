40665f48de68a00682ba39aa25cad9ecd775baba
Kevin Cooper <cooperka92@gmail.com>
2020-10-13 12:49:44 -0700

    11180: Add OData responses_slug feature flag

--- config/initializers/o_data.rb
+++ config/initializers/o_data.rb
@@ -3,4 +3,27 @@
 module OData
   NAMESPACE = "NEMO"
   BASE_PATH = "/odata/v1"
+
+  # User-friendly, doesn't need to be URL safe.
+  def self.responses_name(form_id, form_name)
+    if Settings.use_data_factory_slugs.present?
+      responses_slug(form_id, form_name)
+    else
+      "Responses: #{form_name}"
+    end
+  end
+
+  # URL safe, doesn't need to be user-friendly.
+  def self.responses_url(form_id, form_name)
+    if Settings.use_data_factory_slugs.present?
+      responses_slug(form_id, form_name)
+    else
+      "Responses-#{form_id}"
+    end
+  end
+
+  # Both user-friendly-ish and URL safe.
+  def self.responses_slug(form_id, form_name)
+    "Responses-#{form_name.gsub(/[^a-z1-9]/i, '-')}-#{form_id}"
+  end
 end
