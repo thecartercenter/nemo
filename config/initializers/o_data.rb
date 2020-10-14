4e240ca5134deee153062fbbbd40b7c673e58c16
Kevin Cooper <cooperka92@gmail.com>
2020-10-14 15:56:07 -0700

    11180: Move new methods to form decorator

--- config/initializers/o_data.rb
+++ config/initializers/o_data.rb
@@ -3,27 +3,4 @@
 module OData
   NAMESPACE = "NEMO"
   BASE_PATH = "/odata/v1"
-
-  # User-friendly, doesn't need to be URL safe.
-  def self.responses_name(form)
-    if Settings.use_data_factory_slugs.present?
-      responses_slug(form)
-    else
-      "Responses: #{form.name}"
-    end
-  end
-
-  # URL safe, doesn't need to be user-friendly.
-  def self.responses_url(form)
-    if Settings.use_data_factory_slugs.present?
-      responses_slug(form)
-    else
-      "Responses-#{form.id}"
-    end
-  end
-
-  # Both user-friendly-ish and URL safe.
-  def self.responses_slug(form)
-    "Responses-#{form.name.gsub(/[^a-z1-9]/i, '-')}-#{form.id}"
-  end
 end
