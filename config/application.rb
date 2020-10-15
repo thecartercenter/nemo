1cbffcd6d59e6bff3942417c70cfde22ff941984
Kevin Cooper <cooperka92@gmail.com>
2020-10-14 17:09:52 -0700

    11091: Small Zeitwerk performance improvement

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,12 @@ module ELMO
     ]
     Rails.autoloaders.main.ignore("#{config.root}/app/overrides")
 
+    # Zeitwerk uses absolute paths internally, and applications running in :zeitwerk mode
+    # do not need require_dependency, so models, controllers, jobs, etc. do not need to be
+    # in $LOAD_PATH. Setting this to false saves Ruby from checking these directories when
+    # resolving require calls with relative paths.
+    config.add_autoload_paths_to_load_path = false
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
