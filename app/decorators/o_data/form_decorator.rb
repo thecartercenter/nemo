067f5d79db8ca7703018ef17a6103e7c8d2c6704
Kevin Cooper <cooperka92@gmail.com>
2020-10-21 16:17:04 -0700

    11180: Refactor into OData::FormDecorator

---
+++ app/decorators/o_data/form_decorator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module OData
+  # Decorates forms for the OData API.
+  class FormDecorator < ApplicationDecorator
+    delegate_all
+
+    # User-friendly, doesn't need to be URL safe.
+    def responses_name
+      return responses_slug if Settings.use_data_factory_slugs.present?
+      "Responses: #{name}"
+    end
+
+    # URL safe, doesn't need to be user-friendly.
+    def responses_url
+      return responses_slug if Settings.use_data_factory_slugs.present?
+      "Responses-#{id}"
+    end
+
+    # Both user-friendly-ish and URL safe.
+    def responses_slug
+      "Responses-#{name.gsub(/[^a-z1-9]/i, '-')}-#{id}"
+    end
+  end
+end
