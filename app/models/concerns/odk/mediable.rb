f1acb9184000d806490d6599a3d52c499e70cacd
Kevin Cooper <cooperka92@gmail.com>
2020-12-08 14:15:31 -0800

    11092: [Breaking] Update attachment syntax and validations

--- app/models/concerns/odk/mediable.rb
+++ app/models/concerns/odk/mediable.rb
@@ -9,24 +9,25 @@ module ODK
     ODK_MEDIA_MIME_TYPES = %w[audio/mpeg audio/ogg audio/wave audio/wav audio/x-wav audio/x-pn-wav
                               audio/flac video/ogg application/ogg video/mp4 image/png image/jpeg].freeze
     ODK_MEDIA_EXTS = {audio: %w[mp3 ogg wav flac], video: %w[mp4], image: %w[png jpg jpeg]}.freeze
+    ODK_EXTS_REGEX = /\.(#{ODK_MEDIA_EXTS.values.flatten.join('|')})\z/i.freeze
 
     class_methods do
       def odk_media_attachment(col_name)
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          has_attached_file :#{col_name}
+          has_one_attached :#{col_name}
 
           # The purpose of these validations is twofold:
           # 1. to prevent malicious content from being uploaded to the server (e.g. executable code)
           # 2. to prevent people from uploading content that won't be playable on an Android device
+          #
           # #1 is mostly handled by content type validation and #2 is mostly handled by extension validation.
           # We don't care too much if people upload e.g. a video with the wrong extension. This may sneak
           # through the validations (e.g. an ogg video file with an .ogg extension (should be .ogv))
           # and it may not play back properly but this should be rare and is not a security risk.
           # Executable code should definitely be caught by the content type validation.
-          validates_attachment_content_type :#{col_name}, content_type: ODK_MEDIA_MIME_TYPES,
-                                                          message: :invalid_type
-          validates_attachment_file_name :#{col_name},
-            matches: /\\.(#{ODK_MEDIA_EXTS.values.flatten.join('|')})\\z/i, message: :invalid_type
+          #
+          validates :#{col_name}, content_type: { in: ODK_MEDIA_MIME_TYPES, message: :invalid_type }
+          validate :#{col_name}, :extension_works_on_android
 
           def #{col_name}?
             #{col_name}_file_name.present?
@@ -38,6 +39,13 @@ module ODK
             @#{col_name}_media_type ||= ODK_MEDIA_EXTS.map do |type, exts|
               type if exts.include?(extension)
             end.compact.first
+          end
+
+          private
+
+          def extension_works_on_android
+            extension = File.extname(#{col_name}.filename.to_s)
+            errors.add(:#{col_name}, :invalid_type) unless ODK_EXTS_REGEX.match?(extension)
           end
         RUBY
       end
--- app/models/saved_tabular_upload.rb
+++ app/models/saved_tabular_upload.rb
@@ -17,5 +17,5 @@
 
 # A model for a spreadsheet upload.
 class SavedTabularUpload < SavedUpload
-  validates_attachment_file_name :file, matches: /\.(csv)\z/i
+  validates :file, content_type: %w[text/csv]
 end
