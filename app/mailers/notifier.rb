1cef470ba1d072a6a4fdbf4b4e06f667c571c746
Tom Smyth <tom@sassafras.coop>
2020-12-30 23:02:14 -0500

    10398: Change Notifier mailer to get site_name from Setting class

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,29 +3,34 @@
 # Email notifier, to send various emails
 class Notifier < ApplicationMailer
   def password_reset_instructions(user, mission: nil)
-    build_reset_url(user)
+    @reset_url = reset_url(user)
+    @site_name = site_name(mission)
     mail(to: user.email, reply_to: coordinator_emails(mission),
          subject: t("notifier.password_reset_instructions"))
   end
 
   def intro(user, mission: nil)
     @user = user
-    build_reset_url(user)
+    @reset_url = reset_url(user)
+    @site_name = site_name(mission)
     mail(to: user.email, reply_to: coordinator_emails(mission),
-         subject: t("notifier.welcome", site: Settings.site_name))
+         subject: t("notifier.welcome", site: @site_name))
   end
 
   def sms_token_change_alert(mission)
     raise ArgumentError, "Mission must not be nil" unless mission
-
     @mission = mission
-    @site_name = Settings.site_name
+    @site_name = site_name(mission)
     all_emails = (coordinator_emails(mission) + admin_emails).uniq
     mail(to: all_emails, reply_to: all_emails,
          subject: t("notifier.sms_token_change.subject", mission_name: mission.name))
   end
 
   private
+
+  def site_name(mission)
+    Setting.for_mission(mission).site_name
+  end
 
   def coordinator_emails(mission)
     return [] if mission.nil?
@@ -36,8 +41,7 @@ class Notifier < ApplicationMailer
     User.where(admin: true).active.pluck(:email)
   end
 
-  def build_reset_url(user)
-    @reset_url = edit_password_reset_url(user.perishable_token,
-      mode: nil, mission_name: nil, locale: user.pref_lang)
+  def reset_url(user)
+    edit_password_reset_url(user.perishable_token, mode: nil, mission_name: nil, locale: user.pref_lang)
   end
 end
