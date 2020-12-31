43031473f44218d68033508a56482804fc45a9f7
Tom Smyth <tom@sassafras.coop>
2020-12-30 23:08:07 -0500

    10398: Remove unused code in application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,11 +3,4 @@
 # App-wide parent for all mailers.
 class ApplicationMailer < ActionMailer::Base
   default from: configatron.site_email
-
-  def broadcast(recips, subj, msg)
-    @msg = msg
-    # TODO: We should send a separate email to each recipient
-    # like we do with an SMS broadcast
-    mail(to: recips, subject: "[#{Settings.broadcast_tag}] #{subj}")
-  end
 end
