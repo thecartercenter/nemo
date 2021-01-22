60588e8d12c507da488570f1daf9b7a1f595cc1d
Kevin Cooper <cooperka92@gmail.com>
2021-01-21 17:22:45 -0800

    11092: Clean up answers_helper before a refactor

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -7,33 +7,33 @@ module AnswersHelper
   end
 
   def thumb_path(object, params = {})
-    if object.dynamic_thumb?
-      media_path(object, params.merge(style: :thumb))
-    else
-      object.static_thumb_path
-    end
+    return object.static_thumb_path unless object.dynamic_thumb?
+    media_path(object, params.merge(style: :thumb))
   end
 
   # Creates a media thumbnail link
   def media_link(object, show_delete: false)
-    if object.nil?
-      content_tag(:div, "[#{t('common.none')}]", class: "no-value")
-    else
-      content_tag(:div, class: "media-thumbnail") do
-        concat(link_to(image_tag(thumb_path(object)), media_path(object), target: "_blank", rel: "noopener"))
+    return content_tag(:div, "[#{t('common.none')}]", class: "no-value") if object.nil?
 
-        concat(content_tag(:div, class: "links") do
-          concat(link_to(content_tag(:i, "", class: "fa fa-download"),
-            media_path(object, dl: "1"), class: "download"))
+    content_tag(:div, class: "media-thumbnail") do
+      concat(
+        link_to(image_tag(thumb_path(object)), media_path(object), target: "_blank", rel: "noopener")
+      )
+
+      concat(
+        content_tag(:div, class: "links") do
+          concat(
+            link_to(content_tag(:i, "", class: "fa fa-download"), media_path(object, dl: "1"), class: "download")
+          )
 
           if show_delete
-            concat(link_to(content_tag(:i, "", class: "fa fa-trash-o"),
-              "#",
-              class: "delete",
-              data: {"confirm-msg" => t("response.remove_media_object_confirm.#{object.kind}")}))
+            data = {"confirm-msg" => t("response.remove_media_object_confirm.#{object.kind}")}
+            concat(
+              link_to(content_tag(:i, "", class: "fa fa-trash-o"), "#", class: "delete", data: data)
+            )
           end
-        end)
-      end
+        end
+      )
     end
   end
 end
