03d8bca71ee0c1a64158f196fbbc49c17ab989f0
Kevin Cooper <cooperka92@gmail.com>
2021-01-27 23:05:18 -0800

    11092: Add set_unique_media_prompt_filenames migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_195531) do
+ActiveRecord::Schema.define(version: 2021_01_27_195532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
