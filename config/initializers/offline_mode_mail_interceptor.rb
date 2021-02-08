c074e17b8bfcff8392990b8f03c45fa46951ef64
Tom Smyth <tom@sassafras.coop>
2021-02-08 13:26:03 -0500

    debug

--- config/initializers/offline_mode_mail_interceptor.rb
+++ config/initializers/offline_mode_mail_interceptor.rb
@@ -2,6 +2,8 @@
 
 class OfflineModeMailInterceptor
   def self.delivering_email(message)
+    puts ENV["NEMO_OFFLINE_MODE"].inspect
+    puts "CHECKING DELIVERIES #{Cnfg.offline_mode?.inspect}"
     message.perform_deliveries = false if Cnfg.offline_mode?
   end
 end
