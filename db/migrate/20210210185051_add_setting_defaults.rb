1248b8d53b493912fad3a01fe70d3107aeb46324
Tom Smyth <tom@gather.coop>
2021-02-17 14:26:20 -0500

    10398: Fix settings migration

--- db/migrate/20210210185051_add_setting_defaults.rb
+++ db/migrate/20210210185051_add_setting_defaults.rb
@@ -3,6 +3,12 @@
 class AddSettingDefaults < ActiveRecord::Migration[6.1]
   def change
     change_column_default(:settings, :timezone, from: nil, to: "UTC")
+    reversible do |dir|
+      dir.up do
+        execute("UPDATE settings SET incoming_sms_numbers = '[]' WHERE incoming_sms_numbers IS NULL")
+        execute("UPDATE settings SET preferred_locales = '[]' WHERE preferred_locales IS NULL")
+      end
+    end
     change_column_null(:settings, :incoming_sms_numbers, false)
     change_column_null(:settings, :preferred_locales, false)
   end
