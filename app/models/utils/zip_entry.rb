bcf3d6e6f28d1be665f93870f669c9ecf80fe0a9
Jill Dimond <jpdimond@gmail.com>
2021-03-15 11:43:44 -0400

    hound clean up

--- app/models/utils/zip_entry.rb
+++ app/models/utils/zip_entry.rb
@@ -5,11 +5,10 @@ require "zip"
 require "fileutils"
 
 module Utils
+  # Extends ::Zip::Entry to include clean up method
   class ZipEntry < ::Zip::Entry
-
     def clean_up
-      FileUtils.rm @zipfile
+      FileUtils.rm(@zipfile)
     end
-
   end
 end
