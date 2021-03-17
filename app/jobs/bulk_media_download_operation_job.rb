58f1b608c64b984bd0313ae81f2a2fb1d6ad6875
Jill Dimond <jpdimond@gmail.com>
2021-03-17 09:15:47 -0400

    code review changes

--- app/jobs/bulk_media_download_operation_job.rb
+++ app/jobs/bulk_media_download_operation_job.rb
@@ -8,13 +8,11 @@ class BulkMediaDownloadOperationJob < OperationJob
     ability = Ability.new(user: operation.creator, mission: mission)
     packager = Utils::BulkMediaPackager.new(ability: ability, search: search, operation: operation)
 
-    if packager.space_on_disk?
-      result = packager.download_and_zip_images
-      save_attachment(File.open(result.to_s), result.basename)
-      FileUtils.remove_file(result)
-    else
-      raise "Not enough space on disk for media export"
-    end
+    raise "Not enough space on disk for media export" unless packer.space_on_disk?
+
+    result = packager.download_and_zip_images
+    save_attachment(File.open(result.to_s), result.basename)
+    FileUtils.remove_file(result)
   rescue Search::ParseError => e
     operation_failed(e.to_s)
   end
