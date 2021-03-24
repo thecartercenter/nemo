67ee459772d73b1af28ceac2964248a6af49ab5f
Kevin Cooper <cooperka92@gmail.com>
2021-03-24 15:38:54 -0700

    11593: Don't load Sentry in offline mode

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  next if Rails.env.test?
+  # If the Sentry DSN remains nil, Sentry will be disabled.
+  next if Rails.env.test? || Cnfg.offline_mode?
 
   config.dsn = "https://a81af08ff85042f3ae314e6c685853a3@o448595.ingest.sentry.io/5430181"
   config.breadcrumbs_logger = [:active_support_logger]
