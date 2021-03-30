1690fde22ebcc507ffd89df6f7b89d18b1740418
Kevin Cooper <cooperka92@gmail.com>
2021-03-30 15:20:13 -0700

    11637: Allow for migrate:redo

--- db/migrate/20210317233456_drop_sessions_table.rb
+++ db/migrate/20210317233456_drop_sessions_table.rb
@@ -5,4 +5,8 @@ class DropSessionsTable < ActiveRecord::Migration[6.1]
     # Hasn't been used since ~2017.
     drop_table :sessions
   end
+
+  def down
+    create_table :sessions # rubocop:disable Rails/CreateTableWithTimestamps
+  end
 end
