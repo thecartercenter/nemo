93f838ba2ccf1808554f5bbd8424eb790bdb04ae
Jill Dimond <jpdimond@gmail.com>
2021-04-20 10:21:44 -0400

    11488: fixed xpath to work on odk

--- spec/decorators/odk/form_decorator_spec.rb
+++ spec/decorators/odk/form_decorator_spec.rb
@@ -60,7 +60,7 @@ describe ODK::FormDecorator, :odk do
     let(:likert_options2) { create(:option_set, option_names: %w[OK Whatever Bad], option_values: [4, 5, 6]) }
     let(:likert_question) { create(:question, code: "likert1", qtype_name: "select_one", option_set: likert_options) }
     let(:likert_question2) { create(:question, code: "likert2", qtype_name: "select_one", option_set: likert_options2) }
-    let(:score) { create(:question, code: "score1", qtype_name: "integer") }
+    let(:score) { create(:question, code: "score1", qtype_name: "text") }
     let(:form) { create(:form, :live, name: "Dynamic answers for option sets", questions: [likert_question, likert_question2, score]) }
     let(:decorated_form) { decorate(form) }
 
