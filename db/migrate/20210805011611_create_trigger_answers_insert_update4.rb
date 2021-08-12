f9e35273e49a08d7d982d4da4b5fe3b71d551cbc
Kevin Cooper <cooperka92@gmail.com>
2021-08-11 22:14:08 -0700

    11878: Update legacy answers during migration

--- db/migrate/20210805011611_create_trigger_answers_insert_update4.rb
+++ db/migrate/20210805011611_create_trigger_answers_insert_update4.rb
@@ -13,6 +13,33 @@ class CreateTriggerAnswersInsertUpdate4 < ActiveRecord::Migration[6.1]
       .before(:insert, :update) do
       "new.tsv := TO_TSVECTOR('simple', COALESCE( new.value, to_char(new.date_value, 'YYYY-MM-DD'), to_char(new.time_value, 'HH24hMImSSs'), to_char(new.datetime_value, 'YYYY-MM-DD HH24hMImSSs'), (SELECT STRING_AGG(opt_name_translation.value, ' ') FROM options, option_nodes, JSONB_EACH_TEXT(options.name_translations) opt_name_translation WHERE options.id = option_nodes.option_id AND (option_nodes.id = new.option_node_id OR option_nodes.id IN (SELECT option_node_id FROM choices WHERE answer_id = new.id))), '' ));"
     end
+
+    update_legacy_answers
+  end
+
+  def update_legacy_answers
+    puts "Updating legacy answers."
+    answers_to_update = Answer.where("answers.date_value IS NOT NULL OR answers.time_value IS NOT NULL OR answers.datetime_value IS NOT NULL")
+    puts "Found #{answers_to_update.count}..."
+
+    SqlRunner.instance.run("ALTER TABLE answers DISABLE TRIGGER answers_before_insert_update_row_tr")
+    SqlRunner.instance.run(update_answers_with_date)
+    SqlRunner.instance.run("ALTER TABLE answers ENABLE TRIGGER answers_before_insert_update_row_tr")
+  end
+
+  # Simplified TSV copied from answer_search_vector_updater#trigger_expression.
+  def update_answers_with_date
+    <<-SQL.squish
+        UPDATE answers SET tsv = TO_TSVECTOR('simple', COALESCE(
+            answers.value,
+            to_char(answers.date_value, 'YYYY-MM-DD'),
+            to_char(answers.time_value, 'HH24hMImSSs'),
+            to_char(answers.datetime_value, 'YYYY-MM-DD HH24hMImSSs')
+          ))
+          WHERE answers.date_value IS NOT NULL
+          OR answers.time_value IS NOT NULL
+          OR answers.datetime_value IS NOT NULL
+    SQL
   end
 
   def down
