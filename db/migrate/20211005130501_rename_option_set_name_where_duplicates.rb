cafd54ddaef43d7f07bea92b9973663440676a83
Kevin Cooper <cooperka92@gmail.com>
2021-10-13 14:38:56 -0700

    11992: Output logs when renaming; use save!

--- db/migrate/20211005130501_rename_option_set_name_where_duplicates.rb
+++ db/migrate/20211005130501_rename_option_set_name_where_duplicates.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 class RenameOptionSetNameWhereDuplicates < ActiveRecord::Migration[6.1]
   def up
-    # for each mission
     OptionSet.find_each do |os|
-      sets = OptionSet.where(mission_id: os.mission_id).where(name: os.name)
+      sets = OptionSet.where(mission_id: os.mission_id, name: os.name)
       next if sets.count < 2
       sets.each do |set|
-        set.name = "#{set.name} #{set.created_at}"
-        set.save
+        puts "Renaming duplicate option set ##{set.id}: #{set.name}"
+        set.update!(name: "#{set.name} #{set.created_at}")
       end
     end
 
