e97da06618d877a3bcb48b0bab5f777a3ddaa69d
Jill Dimond <jpdimond@gmail.com>
2021-11-02 15:13:17 -0400

    11997: basic xml export functionality

---
+++ app/jobs/xml_download_operation_job.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "fileutils"
+
+# Operation for exporting all media on a form.
+class XmlDownloadOperationJob < OperationJob
+  def perform(operation, search: nil, options: {})
+    ability = Ability.new(user: operation.creator, mission: mission)
+
+    packager = Utils::XmlPackager.new(
+      ability: ability, search: search, selected: options[:selected], operation: operation
+    )
+
+    raise "Not enough space on disk for media export" unless packager.space_on_disk?
+
+    result = packager.download_and_zip_xml
+    save_attachment(File.open(result.to_s), result.basename)
+    FileUtils.remove_file(result)
+  rescue Search::ParseError => e
+    operation_failed(e.to_s)
+  end
+end
---
+++ app/models/utils/xml_packager.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require "sys/filesystem"
+require "zip"
+require "fileutils"
+
+module Utils
+  # Utility to support a bulk image download operation
+  class XmlPackager < Packager
+    TMP_DIR = "tmp/odk_xml"
+
+    def responses_scope
+      responses = Response.accessible_by(@ability, :export)
+      responses = apply_search_scope(responses, @search, @operation.mission) if @search.present?
+
+      responses.joins("INNER JOIN active_storage_attachments ON
+        active_storage_attachments.record_id = responses.id")
+        .joins("INNER JOIN active_storage_blobs ON
+        active_storage_blobs.id = active_storage_attachments.blob_id")
+    end
+
+    def xml_size
+      @xml_size ||= responses_scope.sum("active_storage_blobs.byte_size")
+    end
+
+    def xml_meta
+      {space_on_disk: space_on_disk?, xml_size: bytes_to_mb(xml_size)}
+    end
+
+    def download_and_zip_xml
+      FileUtils.mkdir_p(Rails.root.join(TMP_DIR))
+      responses_ids = responses_scope.pluck("id")
+      filename = "#{@operation.mission.compact_name}-xml-responses-"\
+        "#{Time.current.to_s(:filename_datetime)}.zip"
+      zipfile_name = Rails.root.join(TMP_DIR, filename)
+      zip(zipfile_name, responses_ids)
+    end
+
+    def make_human_readable(response)
+      xml_string = response.odk_xml.download
+      questions = response.form.questionings
+      # replace qing id with human readable name
+      questions.each { |q| xml_string.gsub!("qing#{q.id}", q.name) }
+      xml_string
+    end
+
+    private
+
+    def zip(zipfile_name, responses_ids)
+      Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+        responses_ids.each do |rid|
+          response = Response.find(rid)
+          next unless response.odk_xml.present?
+          zip_xml(zipfile, response, rid)
+        rescue Zip::EntryExistsError => e
+          report_error(e)
+          next
+        end
+      end
+      zipfile_name
+    end
+
+    def zip_xml(zipfile, response, rid)
+      xml_filename = "nemo-response-#{rid}.xml"
+      xml_filepath = Rails.root.join(TMP_DIR, xml_filename)
+      File.write(xml_filepath, make_human_readable(response))
+      zip_entry = Utils::ZipEntry.new(xml_filepath, xml_filename)
+      zipfile.add(zip_entry, xml_filepath)
+    end
+
+    def report_error(error)
+      Sentry.add_breadcrumb(Sentry::Breadcrumb.new(
+        message: "Mission: #{@operation.mission.compact_name}. Response ID: #{rid}"
+      ))
+      notify_admins(error)
+    end
+  end
+end
