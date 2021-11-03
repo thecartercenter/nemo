5d4624ccb58a39c32ebf333a771ec4ccb124a817
Jill Dimond <jpdimond@gmail.com>
2021-11-03 10:25:28 -0400

    11997: added more tests

--- spec/models/utils/xml_packager_spec.rb
+++ spec/models/utils/xml_packager_spec.rb
@@ -12,10 +12,12 @@ describe Utils::XmlPackager do
   let(:operation) { create(:operation, creator: user) }
   let(:response) { Response.new(form: form, mission: form.mission, user: user, source: "odk") }
   let(:formver) { "abc" } # Don't override the version by default
-  let(:form) { create(:form, :live, questions: questions) }
   let(:xml) { prepare_odk_response_fixture(fixture_name, form, values: xml_values, formver: formver) }
+  let(:ability) { Ability.new(user: operation.creator, mission: operation.mission) }
+  let(:packager) { described_class.new(ability: ability, search: nil, selected: [], operation: operation) }
 
   context "simple form" do
+    let(:form) { create(:form, :live, questions: questions) }
     let(:fixture_name) { "simple_response" }
     let(:questions) do
       [
@@ -25,7 +27,6 @@ describe Utils::XmlPackager do
         create(:question, qtype_name: "text", name: "catname")
       ]
     end
-    let(:question_types) { %w[text text text text] }
     let(:xml_values) { %w[rhys dimond alfred honey] }
 
     before do
@@ -34,33 +35,43 @@ describe Utils::XmlPackager do
     end
 
     it "should calculate the size of the files" do
-      ability = Ability.new(user: operation.creator, mission: operation.mission)
-      packager = described_class.new(
-        ability: ability, search: nil, selected: [], operation: operation
-      )
       expect(packager.xml_size).to eq(674)
     end
 
     it "should replace the xml file with question names" do
-      ability = Ability.new(user: operation.creator, mission: operation.mission)
-      packager = described_class.new(
-        ability: ability, search: nil, selected: [], operation: operation
-      )
-      human_readable_xml = packager.make_human_readable(response)
+      human_readable_xml = packager.human_readable_xml(response)
       expect(human_readable_xml).to include("<firstname>rhys</firstname>")
       expect(human_readable_xml).to include("<lastname>dimond</lastname>")
     end
 
     it "should zip one xml response" do
-      ability = Ability.new(user: operation.creator, mission: operation.mission)
-      packager = described_class.new(
-        ability: ability, search: nil, selected: [], operation: operation
-      )
       results = packager.download_and_zip_xml
-
       expect(results.basename.to_s).to match(/#{operation.mission.compact_name}-xml-responses.+.zip/)
       expect(File.exist?(results.to_s)).to be(true)
+    end
+  end
 
+  context "response with repeat groups" do
+    let(:fixture_name) { "repeat_group_form_response" }
+    let(:question_types) { ["text", {repeating: {items: %w[text text]}}] }
+    let(:xml_values) { %w[Hastings Wynn Dimond Rhys Dimond] }
+    let(:form) { create(:form, :live, question_types: question_types) }
+
+    before do
+      names = %w[city firstname lastname]
+      form.questions.each_with_index do |q, i|
+        q.name = names[i]
+        q.save
+      end
+      response.odk_xml.attach(io: StringIO.new(xml), filename: "odk-response.xml", content_type: "xml")
+      response.save
+    end
+
+    it "should have the correct xml" do
+      human_readable_xml = packager.human_readable_xml(response)
+      puts human_readable_xml
+      expect(human_readable_xml).to include("<firstname>Rhys</firstname>")
+      expect(human_readable_xml).to include("<firstname>Wynn</firstname>")
     end
   end
 end
