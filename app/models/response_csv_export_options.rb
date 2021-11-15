be26f8770e5f88553da888ab61e610f927fddb01
Jill Dimond <jpdimond@gmail.com>
2021-11-15 10:08:06 -0500

    11997: xml export frontend

--- app/models/response_csv_export_options.rb
+++ app/models/response_csv_export_options.rb
@@ -4,7 +4,7 @@
 class ResponseCSVExportOptions
   include ActiveModel::Model
 
-  attr_accessor :long_text_behavior, :download_media
+  attr_accessor :long_text_behavior, :download_media, :download_xml, :download_csv
 
   LONG_TEXT_BEHAVIOR_OPTIONS = %w[exclude truncate include].freeze
 
