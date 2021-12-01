b4c82129e541653b24b00710f35a54ac79425539
Jill Dimond <jpdimond@gmail.com>
2021-12-01 16:01:40 -0500

    11968: saving code review changes

--- spec/models/forms/export_spec.rb
+++ spec/models/forms/export_spec.rb
@@ -25,7 +25,7 @@ describe Forms::Export do
     end
   end
 
-  context "repeat group form" do
+  context "repeat group form with question outside repeat group before" do
     let(:repeatgroupform) do
       create(
         :form,
@@ -43,9 +43,162 @@ describe Forms::Export do
         "#{headers}"\
         "1,text,#{q1.code},#{q1.name},false,false,\"\",\"\",always,\"\",,false\n"\
         "2,integer,#{q2.code},#{q2.name},false,false,\"\",\"\",always,\"\",,false\n"\
-        "3,,Group 1,Repeat Group,false,true,\"\",\"\",always,\"\",,false\n"\
+        "3,,#{q31.parent.code},Repeat Group,false,true,\"\",\"\",always,\"\",,false\n"\
         "3.1,text,#{q31.code},#{q31.name},false,true,\"\",\"\",always,\"\",,false\n"\
         "3.2,text,#{q32.code},#{q32.name},false,true,\"\",\"\",always,\"\",,false\n"\
+      )
+    end
+  end
+
+  context "repeat group form with question outside repeat group after" do
+    let(:repeatgroupform) do
+      create(
+        :form,
+        question_types: [{repeating: {items: %w[text text]}}, "text"]
+      )
+    end
+
+    it "should produce the correct csv" do
+      exporter = Forms::Export.new(repeatgroupform)
+      q1 = repeatgroupform.questionings[0]
+      q2 = repeatgroupform.questionings[1]
+      q3 = repeatgroupform.questionings[2]
+      expect(exporter.to_csv).to eq(
+        "#{headers}"\
+        "1,,#{q1.parent.code},Repeat Group,false,true,\"\",\"\",always,\"\",,false\n"\
+        "1.1,text,#{q1.code},#{q1.name},false,true,\"\",\"\",always,\"\",,false\n"\
+        "1.2,text,#{q2.code},#{q2.name},false,true,\"\",\"\",always,\"\",,false\n"\
+        "2,text,#{q3.code},#{q3.name},false,false,\"\",\"\",always,\"\",,false\n"\
+      )
+    end
+  end
+
+  context "regular group at start of form followed by a question outside of the group" do
+    let(:groupform) do
+      create(
+        :form,
+        question_types: [%w[text text], "integer"]
+      )
+    end
+
+    it "should produce the correct csv" do
+      exporter = Forms::Export.new(groupform)
+
+      q1 = groupform.questionings[0]
+      q2 = groupform.questionings[1]
+      q3 = groupform.questionings[2]
+
+      expect(exporter.to_csv).to eq(
+        "#{headers}"\
+        "1,,#{q1.parent.code},Group,false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.1,text,#{q1.code},#{q1.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.2,text,#{q2.code},#{q2.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2,integer,#{q3.code},#{q3.name},false,false,\"\",\"\",always,\"\",,false\n"\
+      )
+    end
+  end
+
+  context "regular group at end of form preceded by a queston outside the group" do
+    let(:groupform) do
+      create(
+        :form,
+        question_types: ["integer", %w[text text]]
+      )
+    end
+
+    it "should produce the correct csv" do
+      exporter = Forms::Export.new(groupform)
+
+      q1 = groupform.questionings[0]
+      q2 = groupform.questionings[1]
+      q3 = groupform.questionings[2]
+
+      expect(exporter.to_csv).to eq(
+        "#{headers}"\
+        "1,integer,#{q1.code},#{q1.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2,,#{q2.parent.code},Group,false,false,\"\",\"\",always,\"\",,false\n"\
+        "2.1,text,#{q2.code},#{q2.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2.2,text,#{q3.code},#{q3.name},false,false,\"\",\"\",always,\"\",,false\n"\
+      )
+    end
+  end
+
+  context "two groups in a row" do
+    let(:groupform) do
+      create(
+        :form,
+        question_types: [%w[text text], %w[text text]]
+      )
+    end
+
+    it "should produce the correct csv" do
+      exporter = Forms::Export.new(groupform)
+
+      q1 = groupform.questionings[0]
+      q2 = groupform.questionings[1]
+      q3 = groupform.questionings[2]
+      q4 = groupform.questionings[3]
+
+      expect(exporter.to_csv).to eq(
+        "#{headers}"\
+        "1,,#{q1.parent.code},Group,false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.1,text,#{q1.code},#{q1.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.2,text,#{q2.code},#{q2.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2,,#{q3.parent.code},Group,false,false,\"\",\"\",always,\"\",,false\n"\
+        "2.1,text,#{q3.code},#{q3.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2.2,text,#{q4.code},#{q4.name},false,false,\"\",\"\",always,\"\",,false\n"\
+      )
+    end
+  end
+
+  context "two repeat groups in a row" do
+    let(:groupform) do
+      create(
+        :form,
+        question_types: [{repeating: {items: %w[text text]}}, {repeating: {items: %w[text text]}}]
+      )
+    end
+
+    it "should produce the correct csv" do
+      exporter = Forms::Export.new(groupform)
+      q1 = groupform.questionings[0]
+      q2 = groupform.questionings[1]
+      q3 = groupform.questionings[2]
+      q4 = groupform.questionings[3]
+
+      expect(exporter.to_csv).to eq(
+        "#{headers}"\
+        "1,,#{q1.parent.code},Repeat Group,false,true,\"\",\"\",always,\"\",,false\n"\
+        "1.1,text,#{q1.code},#{q1.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.2,text,#{q2.code},#{q2.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2,,#{q3.parent.code},Repeat Group,false,true,\"\",\"\",always,\"\",,false\n"\
+        "2.1,text,#{q3.code},#{q3.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "2.2,text,#{q4.code},#{q4.name},false,false,\"\",\"\",always,\"\",,false\n"\
+      )
+    end
+  end
+
+  context "a group in a group" do
+    let(:groupform) do
+      create(
+        :form,
+        question_types: [["text", ["text", "integer"]]]
+      )
+    end
+
+    it "should produce the correct csv" do
+      exporter = Forms::Export.new(groupform)
+      q1 = groupform.questionings[0]
+      q2 = groupform.questionings[1]
+      q3 = groupform.questionings[2]
+
+      expect(exporter.to_csv).to eq(
+        "#{headers}"\
+        "1,,#{q1.parent.code},Group,false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.1,text,#{q1.code},#{q1.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.2,,#{q2.parent.code},Group,false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.2.1,text,#{q2.code},#{q2.name},false,false,\"\",\"\",always,\"\",,false\n"\
+        "1.2.2,integer,#{q3.code},#{q3.name},false,false,\"\",\"\",always,\"\",,false\n"\
       )
     end
   end
@@ -67,4 +220,6 @@ describe Forms::Export do
       )
     end
   end
+
+  context "repeat group"
 end
