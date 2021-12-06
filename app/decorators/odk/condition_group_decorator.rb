cf77340e5d74fea9671427cdbc334a8da4e43d04
Kevin Cooper <cooperka92@gmail.com>
2021-12-06 13:44:23 -0800

    12056: Hard-code and/or without caps

--- app/decorators/odk/condition_group_decorator.rb
+++ app/decorators/odk/condition_group_decorator.rb
@@ -7,7 +7,7 @@ module ODK
       if members.empty?
         negate? ? "false()" : nil
       else
-        conjunction = true_if == "all_met" ? I18n.t("common.AND") : I18n.t("common.OR")
+        conjunction = true_if == "all_met" ? "and" : "or"
         result = decorated_members.map { |m| "(#{m.to_odk})" }.join(" #{conjunction} ")
         negate? ? "not(#{result})" : result
       end
