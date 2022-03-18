3f19985a4c96438755c8c962641f4f178ef5624d
Kevin Cooper <cooperka92@gmail.com>
2022-03-18 14:42:27 -0700

    12143: Add short random delay

--- app/models/odk/response_saver.rb
+++ app/models/odk/response_saver.rb
@@ -3,7 +3,8 @@
 module ODK
   # Saves responses and is responsible for handling serialization and duplicate errors
   class ResponseSaver
-    MAX_TRIES = 10
+    # The more tries, the more likely it is to eventually succeed
+    MAX_TRIES = 12
 
     def self.save_with_retries!(params)
       tries = 0
@@ -18,7 +19,14 @@ module ODK
         break
       rescue ActiveRecord::SerializationFailure => e
         tries += 1
+
+        # Note for future: Do we still need to destroy the blank response in this case?
+        # See notes related to https://redmine.sassafras.coop/issues/12142.
         raise e if tries >= MAX_TRIES
+
+        # Delay a short, random number of milliseconds
+        # to allow different requests to try in various orders.
+        sleep(rand(1..100) / 1000.0)
       end
     end
   end
