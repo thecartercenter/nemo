0bfde0aa8fcd5d92e2aee5c9e95f6470a3fe1f5a
Kevin Cooper <cooperka92@gmail.com>
2022-03-18 11:52:07 -0700

    12143: Log more accurately to sentry; speed up spec timer

--- app/models/odk/response_saver.rb
+++ app/models/odk/response_saver.rb
@@ -5,14 +5,17 @@ module ODK
   class ResponseSaver
     MAX_TRIES = 10
     # For testing race conditions, stubbed in tests
-    SLEEP_TIMER = 0
+    TEST_SLEEP_TIMER = 0
 
     def self.save_with_retries!(params)
       tries = 0
       loop do
         ActiveRecord::Base.transaction(isolation: :serializable) do
-          return if ODK::ResponseParser.duplicate?(params[:submission_file], params[:user_id])
-          sleep(SLEEP_TIMER)
+          if ODK::ResponseParser.duplicate?(params[:submission_file], params[:user_id])
+            Sentry.capture_message("Ignored parallel duplicate")
+            return
+          end
+          sleep(TEST_SLEEP_TIMER)
           params[:response].save!(validate: false)
         end
         break
