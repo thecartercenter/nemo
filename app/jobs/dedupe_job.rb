ecf8183f2340f378546df76079460f3b5a0a83c1
Jill Dimond <jpdimond@gmail.com>
2022-06-29 11:49:03 -0400

    12165: added more tests

--- app/jobs/dedupe_job.rb
+++ app/jobs/dedupe_job.rb
@@ -15,16 +15,26 @@ class DedupeJob < ApplicationJob
 
   def dupe_codes
     return false if Response.dirty_dupe.empty?
-    @dupe_codes ||= tuples.group_by { |t| t[3] }.values.reject { |set| set.size < 2 }
-                           .map { |set| set[1..].map { |t| t[0] } }.flatten
+    # Make hashtable with checksum as key, reject sets where there is only 1
+    @dupe_checksums ||= tuples.group_by { |t| t[3] }.values.reject { |set| set.size < 2 }
+    @dupe_codes ||= check_unique_users
+  end
+
+  def check_unique_users
+    @dupe_checksums.map do |dc|
+      # Build hash table with user key (need unique users)
+      user_sets = dc.group_by { |t| t[2] }.values.reject { |set| set.size < 2 }
+      # Get the sets that were submitted later and only get the code
+      user_sets.map { |set| set[1..].map { |t| t[0] } }.flatten
+    end.flatten
   end
 
   def tuples
     @tuples ||= ActiveStorage::Attachment.where(record_id: Response.dirty_dupe.map(&:id))
-                                         .where(record_type: "Response")
-                                         .includes(record: :user)
-                                         .order(created_at: :desc)
-                                         .filter { |attachment| attachment.record.presence }.map do |attachment|
+      .where(record_type: "Response")
+      .includes(record: :user)
+      .order(created_at: :desc)
+      .filter { |attachment| attachment.record.presence }.map do |attachment|
       response = attachment.record
       [response.shortcode, response.created_at, response.user.name, attachment.checksum, response.mission_id]
     end.compact
@@ -32,7 +42,6 @@ class DedupeJob < ApplicationJob
 
   def backup_duplicate_xml
     # see submission error, save to tmp/duplicates
-    puts "dupe codes #{dupe_codes}"
     dupe_responses = Response.where(shortcode: dupe_codes).map(&:id)
     attachments = ActiveStorage::Attachment.where(record_id: dupe_responses)
     FileUtils.mkdir_p(TMP_DUPE_BACKUPS_PATH)
@@ -43,7 +52,6 @@ class DedupeJob < ApplicationJob
 
   def copy_attachment(attachment)
     File.open("#{TMP_DUPE_BACKUPS_PATH}/#{attachment.filename}", "w") do |f|
-      puts "downloading file #{TMP_DUPE_BACKUPS_PATH}/#{attachment.filename}"
       attachment.download { |chunk| f.write(chunk) }
     end
   rescue StandardError => e
@@ -52,6 +60,7 @@ class DedupeJob < ApplicationJob
   end
 
   def destroy_duplicates!
+    puts "Destroying #{dupe_codes}"
     ResponseDestroyer.new(scope: Response.where(shortcode: dupe_codes)).destroy!
   end
 
--- spec/jobs/dedupe_job_spec.rb
+++ spec/jobs/dedupe_job_spec.rb
@@ -13,6 +13,8 @@ describe DedupeJob do
   let(:nemo_response) { Response.first }
   let(:save_fixtures) { true }
   let(:r1_path) { "tmp/odk/responses/simple_response/simple_response.xml" }
+  let(:r2_path) { "tmp/odk/responses/simple_response/simple_response2.xml" }
+
   let(:xml_values) { [1, 2, 3, 4] }
 
   before(:all) do
@@ -75,7 +77,6 @@ describe DedupeJob do
     end
 
     it "should remove a duplicate and create a copy" do
-      puts "Response count #{Response.count}"
       expect(Response.dirty_dupe.count).to eq(3)
       described_class.perform_later
       expect(Response.all.count).to eq(2)
@@ -85,7 +86,94 @@ describe DedupeJob do
     end
   end
 
-  context "more complex dedupe" do
-    
+  context "more complex dedupe with different users" do
+    # create two identical responses (with same xml)
+    let!(:form2) { create(:form, :live, mission: mission, question_types: question_types) }
+    let!(:form3) { create(:form, :live, mission: mission, question_types: question_types) }
+    let(:user2) { create(:user, role_name: "enumerator", mission: mission) }
+    let(:user3) { create(:user, role_name: "enumerator", mission: mission) }
+    let(:user4) { create(:user, role_name: "enumerator", mission: mission) }
+    let(:user5) { create(:user, role_name: "enumerator", mission: mission) }
+
+    let!(:r1) do
+      create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r1_path,
+        form: form1,
+        answer_values: xml_values,
+        user_id: user.id
+      )
+    end
+
+    let!(:r2) do
+      create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r1_path,
+        form: form1,
+        answer_values: xml_values,
+        user_id: user.id
+      )
+    end
+
+    let!(:r3) do
+      create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r2_path,
+        form: form2,
+        answer_values: xml_values,
+        user_id: user2.id
+      )
+    end
+
+    let!(:r4) do
+      create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r2_path,
+        form: form2,
+        answer_values: xml_values,
+        user_id: user2.id
+      )
+    end
+
+    let!(:r5) do
+      create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r1_path,
+        form: form2,
+        answer_values: ["test", 2, "test", 4],
+        user_id: user3.id
+      )
+    end
+
+    let!(:r6) do
+      create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r1_path,
+        form: form2,
+        answer_values: [2, 2, 3, 4],
+        user_id: user4.id
+      )
+    end
+
+    it "should remove two duplicates and create two copies" do
+      expect(Response.dirty_dupe.count).to eq(6)
+      described_class.perform_later
+      expect(Response.all.count).to eq(4)
+      expect(Response.dirty_dupe.count).to eq(0)
+      expect(Response.find_by(shortcode: r1.shortcode)).to eq(nil)
+      expect(Response.find_by(shortcode: r2.shortcode)).to_not(eq(nil))
+      expect(Response.find_by(shortcode: r3.shortcode)).to eq(nil)
+      expect(Response.find_by(shortcode: r4.shortcode)).to_not(eq(nil))
+      # Check for backups
+      expect(File.directory?(DedupeJob::TMP_DUPE_BACKUPS_PATH)).to eq(true)
+      expect(File.file?("#{DedupeJob::TMP_DUPE_BACKUPS_PATH}/simple_response.xml")).to eq(true)
+      expect(File.file?("#{DedupeJob::TMP_DUPE_BACKUPS_PATH}/simple_response2.xml")).to eq(true)
+    end
   end
 end
