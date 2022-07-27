1bde8c1c8ead1f51e8abcb65c3601dac0c792324
Kevin Cooper <cooperka92@gmail.com>
2022-07-27 14:55:14 -0700

    12029: Get params working for both spec routes

--- spec/features/responses/enketo_spec.rb
+++ spec/features/responses/enketo_spec.rb
@@ -5,20 +5,25 @@ require "rails_helper"
 feature "enketo form rendering and submission", js: true do
   let(:user) { create(:user) }
   let(:form) { create(:form, :live, question_types: %w[text]) }
-  let(:params) do
-    {locale: "en", mode: "m", mission_name: get_mission.compact_name,
-     form_id: form.id, enketo: 1}
-  end
-
-  let(:r1) { create(:response, form: form, answer_values: %w[foo]) }
+  let(:params) { {locale: "en", mode: "m", mission_name: get_mission.compact_name, enketo: 1} }
+  let(:form_params) { params.merge(form_id: form.id) }
+  let(:response_params) { params.merge(id: r1.shortcode) }
+  let(:r1) { create(:response, form: form, answer_values: %w[foo-bar]) }
 
   before do
     login(user)
   end
 
-  it "renders" do
-    visit(new_response_path(params))
+  it "renders blank" do
+    visit(new_response_path(form_params))
     expect(page).to have_content(form.name)
     expect(page).to have_content(form.c[0].name)
+  end
+
+  it "shows existing response" do
+    visit(response_path(response_params))
+    expect(page).to have_content(form.name)
+    expect(page).to have_content(form.c[0].name)
+    expect(page).to have_content("foo-bar")
   end
 end
