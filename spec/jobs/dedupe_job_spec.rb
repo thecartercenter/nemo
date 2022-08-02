a5abbfd993e44c2fd027e89c561ae22468046f28
Jill Dimond <jpdimond@gmail.com>
2022-08-02 10:50:08 -0400

    12165: completely refactored to simplify

--- spec/jobs/dedupe_job_spec.rb
+++ spec/jobs/dedupe_job_spec.rb
@@ -9,7 +9,6 @@ describe DedupeJob do
   let!(:question_types) { %w[text text text text] }
   let(:form1) { create(:form, :live, mission: mission, question_types: question_types) }
   let(:fixture_name) { "single_question" }
-  let(:nemo_response) { Response.first }
   let(:save_fixtures) { true }
   let(:r1_path) { "tmp/odk/responses/simple_response/simple_response.xml" }
   let(:r2_path) { "tmp/odk/responses/simple_response/simple_response2.xml" }
@@ -87,6 +86,47 @@ describe DedupeJob do
     end
   end
 
+  # context "Duplicates with different missions" do
+  #   let!(:mission1) { create(:mission) }
+  #   let!(:mission2) { create(:mission) }
+  #   let!(:formabc) { create(:form, :live, mission: mission1, question_types: question_types) }
+  #   let!(:formxyz) { create(:form, :live, mission: mission2, question_types: question_types) }
+  #
+  #   let!(:r1) do
+  #     create(
+  #       :response,
+  #       :with_odk_attachment,
+  #       xml_path: r1_path,
+  #       form: formabc,
+  #       answer_values: xml_values,
+  #       user_id: user.id,
+  #       dirty_dupe: false
+  #     )
+  #   end
+  #
+  #   let(:r2) do
+  #     create(
+  #       :response,
+  #       :with_odk_attachment,
+  #       xml_path: r1_path,
+  #       form: formxyz,
+  #       answer_values: xml_values,
+  #       user_id: user.id
+  #     )
+  #   end
+  #
+  #   it "should not delete the responses" do
+  #     expect(Response.count).to eq(1)
+  #
+  #     r2.created_at = DateTime.now + 20.minutes
+  #     r2.save!
+  #
+  #     expect(Response.count).to eq(2)
+  #     described_class.perform_now
+  #     expect(Response.count).to eq(1)
+  #   end
+  # end
+
   context "dedupe over time" do
     let!(:form2) { create(:form, :live, mission: mission, question_types: question_types) }
     let(:user2) { create(:user, role_name: "enumerator", mission: mission) }
@@ -115,47 +155,23 @@ describe DedupeJob do
       )
     end
 
-    let(:new_dupe1) do
-      create(
-        :response,
-        :with_odk_attachment,
-        xml_path: r1_path,
-        form: form1,
-        answer_values: xml_values,
-        user_id: user.id
-      )
-    end
-
-    let(:new_orig1) do
-      create(
-        :response,
-        :with_odk_attachment,
-        xml_path: r2_path,
-        form: form1,
-        answer_values: %w[cat dog bark meow],
-        user_id: user.id
-      )
-    end
-
-    let(:new_dupe2) do
-      create(
-        :response,
-        :with_odk_attachment,
-        xml_path: r1_path,
-        form: form1,
-        answer_values: xml_values,
-        user_id: user.id
-      )
-    end
-
     it "should dedupe correctly" do
       # Start with two existing dupes, but are clean
       described_class.perform_now
       expect(Response.all.count).to eq(2)
+      puts "first test"
 
       # One new duplicate response that is dirty
-      new_dupe1.created_at = DateTime.now + 10.minutes
-      new_dupe1.save!
+
+      new_dupe1 = create(
+        :response,
+        :with_odk_attachment,
+        xml_path: r1_path,
+        form: form1,
+        answer_values: xml_values,
+        user_id: user.id,
+        dirty_dupe: true
+      )
 
       expect(Response.all.count).to eq(3)
       described_class.perform_now
@@ -163,8 +179,15 @@ describe DedupeJob do
       expect(Response.where(id: new_dupe1.id)).to_not(be_present)
 
       # New original response (note a duplicate)
-      new_orig1.created_at = DateTime.now + 20.minutes
-      new_orig1.save!
+      new_orig1 =
+        create(
+          :response,
+          :with_odk_attachment,
+          xml_path: r2_path,
+          form: form1,
+          answer_values: %w[cat dog bark meow],
+          user_id: user.id
+        )
 
       described_class.perform_now
 
@@ -173,9 +196,15 @@ describe DedupeJob do
       expect(Response.find(new_orig1.id).dirty_dupe).to be(false)
 
       # A new duplicate to the original ones
-      new_dupe2.created_at = DateTime.now
-      new_dupe2.save!
-
+      new_dupe2 =
+        create(
+          :response,
+          :with_odk_attachment,
+          xml_path: r1_path,
+          form: form1,
+          answer_values: xml_values,
+          user_id: user.id
+        )
       expect(Response.all.count).to eq(4)
       described_class.perform_now
       expect(Response.all.count).to eq(3)
@@ -262,10 +291,10 @@ describe DedupeJob do
       described_class.perform_now
       expect(Response.all.count).to eq(4)
       expect(Response.dirty_dupe.count).to eq(0)
-      expect(Response.find_by(shortcode: r1.shortcode)).to eq(nil)
-      expect(Response.find_by(shortcode: r2.shortcode)).to_not(eq(nil))
-      expect(Response.find_by(shortcode: r3.shortcode)).to eq(nil)
-      expect(Response.find_by(shortcode: r4.shortcode)).to_not(eq(nil))
+      expect(Response.find_by(shortcode: r2.shortcode)).to eq(nil)
+      expect(Response.find_by(shortcode: r1.shortcode)).to_not(eq(nil))
+      expect(Response.find_by(shortcode: r4.shortcode)).to eq(nil)
+      expect(Response.find_by(shortcode: r3.shortcode)).to_not(eq(nil))
       # Check for backups
       expect(File.directory?(DedupeJob::TMP_DUPE_BACKUPS_PATH)).to eq(true)
       expect(File.file?("#{DedupeJob::TMP_DUPE_BACKUPS_PATH}/simple_response.xml")).to eq(true)
