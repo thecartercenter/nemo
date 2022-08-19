1536ec1b2ea0aebc2047b045fe46ce1a0b53b69c
Kevin Cooper <cooperka92@gmail.com>
2022-08-18 21:59:18 -0700

    12165: Update responses more efficiently in migration

--- db/migrate/20220621161053_add_dirty_dupe_to_response.rb
+++ db/migrate/20220621161053_add_dirty_dupe_to_response.rb
@@ -3,16 +3,10 @@
 class AddDirtyDupeToResponse < ActiveRecord::Migration[6.1]
   def up
     add_column :responses, :dirty_dupe, :boolean, default: true, null: false
-
-    # Set existing responses to not dirty
-    Response.all.each do |r|
-      r.dirty_dupe = false
-      # Dont update timestamps
-      r.save!(touch: false)
-    end
+    Response.update_all(dirty_dupe: false)
   end
 
   def down
-    remove_column :responses, :dirty_dupe, :boolean, default: true
+    remove_column :responses, :dirty_dupe
   end
 end
