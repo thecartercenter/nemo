ee66935e7dd470a0e494310db0d18b60c42f3b2b
Kevin Cooper <cooperka92@gmail.com>
2022-09-13 11:22:28 -0700

    12272: Upgrade babel and related dependencies

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,10 @@
 const { environment } = require('@rails/webpacker');
 
+// This file is effectively the same as what you'd normally have
+// inside webpack.config.js for a Node project.
+//
+// See also https://webpack.js.org/configuration/
+
 // Ignore JS test files.
 environment.loaders.append('ignore', {
   test: /\.test\.js$/,
