743cbf7afea4377afbb8a6fd0e65e4142898c805
Kevin Cooper <cooperka92@gmail.com>
2022-09-20 21:08:38 -0700

    12272: Try installing missing plugins, no luck yet

--- babel.config.js
+++ babel.config.js
@@ -43,21 +43,14 @@ module.exports = function (api) {
     plugins: [
       '@babel/syntax-dynamic-import',
       '@babel/transform-runtime',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-optional-chaining',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      // proposal-private-property needs to come AFTER proposal-decorators.
       ['@babel/plugin-proposal-private-methods', { loose: true }],
       ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-      [
-        '@babel/plugin-proposal-decorators',
-        {
-          legacy: true,
-        },
-      ],
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true,
-        },
-      ],
     ],
   };
 };
