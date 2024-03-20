136cc5f6453cf4e69815c32fe8c91d14d151f220
Melody <meltheadorable@gmail.com>
2024-03-20 14:12:45 -0400

    fix hound warning

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Delayed::Worker.logger = Logger.new(Rails.root.join("log/dj.log"))
-Delayed::Worker.logger = ActiveSupport::BroadcastLogger.new(Logger.new(STDOUT)) if Rails.env.development?
+Delayed::Worker.logger = ActiveSupport::BroadcastLogger.new(Logger.new($stdout)) if Rails.env.development?
 
 Delayed::Worker.queue_attributes = {
   default: {priority: 0},
