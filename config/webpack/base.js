2646b2d5c9cf074a2fc36ff97f51ee1a02f7a381
Melody <meltheadorable@gmail.com>
2024-07-19 14:30:50 -0400

    kinda-sorta-working
     Conflicts:
    	.nvmrc
    	app/controllers/responses_controller.rb
    	package.json
    	yarn.lock

---
+++ config/webpack/base.js
@@ -0,0 +1,30 @@
+const { webpackConfig, merge } = require('@rails/webpacker')
+
+// webpackConfig.loaders.append('ignore', {
+//   test: /\.test\.js$/,
+//   loader: 'ignore-loader',
+// });
+
+module.exports = merge(
+  webpackConfig,
+  {
+    rules: [
+      {
+        test: /\.test\.js$/,
+        use: ['ignore-loader']
+      },
+      {
+        test: /node_modules\/(enketo-core|openrosa-xpath-evaluator)\//,
+        use: ['babel-loader']
+      }
+    ]
+  }
+)
+
+// Transpile Enketo files.
+// webpackConfig.loaders.append('enketo', {
+//   test: /node_modules\/(enketo-core|openrosa-xpath-evaluator)\//,
+//   loader: 'babel-loader',
+// });
+
+module.exports = webpackConfig
