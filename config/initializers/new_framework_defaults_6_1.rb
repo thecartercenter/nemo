ce12cb3c995ed9e080acdf7cf8233bea593b06f5
Alex <alex@sassafras.coop>
2025-08-07 14:20:36 -0400

    revert - uncheck everything

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -7,17 +7,17 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Support for inversing belongs_to -> has_many Active Record associations.
-Rails.application.config.active_record.has_many_inversing = true
+# Rails.application.config.active_record.has_many_inversing = true
 
 # Track Active Storage variants in the database.
-Rails.application.config.active_storage.track_variants = true
+# Rails.application.config.active_storage.track_variants = true
 
 # Apply random variation to the delay when retrying failed jobs.
-Rails.application.config.active_job.retry_jitter = 0.15
+# Rails.application.config.active_job.retry_jitter = 0.15
 
 # Stop executing `after_enqueue`/`after_perform` callbacks if
 # `before_enqueue`/`before_perform` respectively halts with `throw :abort`.
-Rails.application.config.active_job.skip_after_callbacks_if_terminated = true
+# Rails.application.config.active_job.skip_after_callbacks_if_terminated = true
 
 # Specify cookies SameSite protection level: either :none, :lax, or :strict.
 #
