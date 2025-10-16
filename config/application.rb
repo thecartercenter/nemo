6073c9354756c96ae9c90532b086be6af6dec98f
Kevin Cooper <cooperka92@gmail.com>
2025-10-16 15:58:53 -0400

    fully accept 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module ELMO
   # Application-wide settings and setup.
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults(7.1)
+    config.load_defaults(7.2)
 
     config.secret_key_base = Cnfg.secret_key_base
 
