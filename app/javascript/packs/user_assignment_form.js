30f3f7ce19bced061ef857a118a81abd7cb25768
Justin Gordon <justin@shakacode.com>
2026-04-14 22:10:24 -1000

    Fix Nemo migration lint issues

--- app/javascript/packs/user_assignment_form.js
+++ app/javascript/packs/user_assignment_form.js
@@ -1,3 +1,5 @@
+// The package exports this client entrypoint, but the repo's older ESLint resolver cannot follow it.
+// eslint-disable-next-line import/no-unresolved
 import ReactOnRails from 'react-on-rails/client';
 
 import UserAssignmentForm from '../ror_components/UserAssignmentForm';
